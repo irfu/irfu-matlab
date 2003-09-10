688e3043df8e67af5a438e93c60ad6feb0587100
andris <andris>
2003-09-10 15:10:28 +0000

    added 1 space

--- av_interp.m
+++ av_interp.m
@@ -1,6 +1,6 @@
 function [z]=av_interp(x,y,method)
 % AV_INTERP Interpolate time series
-%   [z]=av_interp(x,y,method) interpolates x to the time line of y
+%    [z]=av_interp(x,y,method) interpolates x to the time line of y
 %    x,y,z - row vectors where first column is time
 %    z has y-time and x-column number
 %    method - method of interpolation 'spline', 'linear' etc. (default 'linear')
