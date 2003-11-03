e754754139a9f8a77989333c355a64d37ebf995c
andris <andris>
2003-11-03 17:35:37 +0000

    no message

--- c_ri/c_get_bfgm.m
+++ c_ri/c_get_bfgm.m
@@ -1,12 +1,13 @@
-function [B1,B2,B3,B4]=c_get_bfgm(tint,sc_list)
+function [varargout]=c_get_bfgm(tint,sc_list)
 %function [B1,B2,B3,B4]=c_get_bfgm(tint)
 %
 % tint given in isdat_epoch
 % sc_list .. vector of satellite unmbers for which to dowload data, default sc_list=1:4
 if nargin ==1, sc_list=1:4;end
-for ic=sc_list,eval(av_ssub('B?=[];',ic)),end
 
-for ic=sc_list,
+for ic=sc_list,eval(av_ssub('B?=[];',ic)),end
+for j=1:length(sc_list),
+  ic=sc_list(j),
   [b_table, n_table] = create_timetable(tint(1),tint(2),ic);
 
   if b_table ~= 0
@@ -22,10 +23,11 @@ for ic=sc_list,
   if n_table ~= 0
     [n_s, col] = size(n_table);
     for i = 1:2:n_s
-      B=c_ri_get_B( n_table(i,1), n_table(i,2), cl_nr, 'n');
+      B=c_ri_get_B( n_table(i,1), n_table(i,2), ic, 'n');
       if B(1,1)>0,
         eval(av_ssub('B?=[B?;B];',ic));
       end
     end
   end
+  eval(av_ssub('varargout(j) = {B?};',ic));
 end
