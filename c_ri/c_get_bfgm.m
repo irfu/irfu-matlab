1031b9482ef81098ae4e91b991b4a811eb302f7b
andris <andris>
2003-11-03 17:12:13 +0000

    simplify things

--- c_ri/c_get_bfgm.m
+++ c_ri/c_get_bfgm.m
@@ -3,7 +3,7 @@ function [B1,B2,B3,B4]=c_get_bfgm(tint,sc_list)
 %
 % tint given in isdat_epoch
 % sc_list .. vector of satellite unmbers for which to dowload data, default sc_list=1:4
-if nargin ==1, sc_list=1:4;end
+if nargin ==1, sc_list=1:4;
 for ic=sc_list,eval(av_ssub('B?=[];',ic)),end
 
 for ic=sc_list,
@@ -12,7 +12,7 @@ for ic=sc_list,
   if b_table ~= 0
     [b_s, col] = size(b_table);
     for k = 1:b_s
-      B=c_ri_get_B( b_table(k,1), b_table(k,2), ic, 'b');
+      B=c_ri_get_B( b_table(k,1), b_table(k,2), ic, 'b',path_output);
       if B(1,1)>0,
         eval(av_ssub('B?=[B?;B];',ic));
       end
@@ -22,7 +22,7 @@ for ic=sc_list,
   if n_table ~= 0
     [n_s, col] = size(n_table);
     for i = 1:2:n_s
-      B=c_ri_get_B( n_table(i,1), n_table(i,2), ic, 'n');
+      B=c_ri_get_B( n_table(i,1), n_table(i,2), cl_nr, 'n',path_output);
       if B(1,1)>0,
         eval(av_ssub('B?=[B?;B];',ic));
       end
