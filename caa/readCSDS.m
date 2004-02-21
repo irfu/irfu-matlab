4e58d27eef0d57ceceb85386666a6e4494c5a6cf
yuri <yuri>
2004-02-21 23:44:52 +0000

    add support for multiple data sources

--- caa/readCSDS.m
+++ caa/readCSDS.m
@@ -4,8 +4,9 @@ function data=readCSDS(data_path,start_time,dt,cl_id,quantity)
 % data = readCSDS(data_path,start_time,dt,cl_id,quantity)
 %
 % Input:
-%	data_path - eather directory containing CSDS subdirectory with data, or 
-%	ISDAT database string host:NN
+%	data_path - ISDAT database strings and directories containing CSDS 
+%   subdirectory with data separated by '|'. These data sources are	being tried
+%	in the same order. A typical example would be: 'disco:10|/data/cluster'
 %	start_epoch - start time (isdat epoch)
 %	dt - time interval in sec
 %	cl_id - SC#
@@ -41,42 +42,61 @@ end
 % get data
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 data = [];
-
-if ~isempty(regexp(data_path,':\d{1,2}\>'))
-	% use ISDAT
-	useISDAT = 1;
-	lasterr('')
-	try
-		dbase = Mat_DbOpen(data_path);
-	catch
-		error('ISDAT:dbOpen','Cannot open ISDAT database')
-	end
-else
-	if exist(data_path,'dir')
-		% read from directory
-		useISDAT = 0;
-	else
-		error('caa:noSuchDir','Directory %s does not exist',data_path)
-	end
-end
-
 start_date_s = strrep(datestr(fromepoch(start_time),29),'-','');
 
-if useISDAT
-	warning('caa:dataSource','Using ISDAT')
-	[t, dat] = isGetDataLite( dbase, start_time, dt, ...
-	r.pr, r.mem, r.inst, r.sen);
-	if ~isempty(dat)
-		data = [double(t) double(dat)'];
-		clear t dat
-	else, warning('caa:noData')
+p = tokenize(data_path,'|');
+
+for i=1:length(p)
+
+	if ~isempty(regexp(p{i},':\d{1,2}\>'))
+		% use ISDAT
+		useISDAT = 1;
+	else
+		if exist(p{i},'dir')
+			% read from directory
+			useISDAT = 0;
+		else
+			warning('caa:noSuchDir','Directory %s does not exist',p{i})
+			continue
+		end
 	end
 
-else
-	warning('caa:dataSource','Using FILE')
-	data = av_read_cdf([data_path '/CSDS/' r.file start_date_s '*'], r.var);
-	if ~isempty(data)
-		data = av_t_lim(data,start_time + [0 dt]);
-	else, warning('caa:noData')
+
+	if useISDAT
+		warning('caa:dataSource','Using ISDAT')
+		lasterr('')
+		try
+			dbase = Mat_DbOpen(p{i});
+		catch
+			warning('ISDAT:dbOpen','Cannot open ISDAT database')
+			continue
+		end
+		if exist('dbase','var')
+			[t, dat] = isGetDataLite( dbase, start_time, dt, ...
+			r.pr, r.mem, r.inst, r.sen);
+			Mat_DbClose(dbase)
+			clear dbase
+
+			if ~isempty(dat)
+				data = [double(t) double(dat)'];
+				return
+			else
+				% warning('caa:noData','No data')
+			end
+		end
+
+	else
+		warning('caa:dataSource','Using FILE')
+		disp([p{i} '/CSDS/' r.file start_date_s '*']);
+		data = av_read_cdf([p{i} '/CSDS/' r.file start_date_s '*'], r.var);
+		if ~isempty(data)
+			data = av_t_lim(data,start_time + [0 dt]);
+			return
+		else 
+			% warning('caa:noData','No data')
+		end
 	end
 end
+
+% if we are here means there was no data
+warning('caa:noData','No data')
