015eb372365b223618a9d4c5f343b5b77178b2b5
yuri <yuri>
2004-02-21 15:46:24 +0000

    initial working version with quantity 'b'

---
+++ caa/readCSDS.m
@@ -0,0 +1,72 @@
+function data=readCSDS(data_path,start_time,dt,cl_id,quantity)
+%readCSDS read CSDS data from ISDAT or disk
+%
+% $Revision$  $Date$
+%
+% see also C_GET, TOEPOCH
+
+% Copyright 2004 Yuri Khotyaintsev
+% Parts of the code are (c) Andris Vaivads
+
+error(nargchk(5,5,nargin))
+
+cl_id_s = num2str(cl_id);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% define request parameters
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+switch (quantity)
+case 'b'
+	r.file	= ['PP/FGM/C' cl_id_s '/C' cl_id_s '_PP_FGM_'];
+	r.var	= ['B_xyz_gse__C' cl_id_s '_PP_FGM'];
+	r.pr	= 'CSDS_PP';
+	r.mem	= ['C' cl_id_s];
+	r.inst  = 'FGM';
+	r.sen	= ['B_xyz_gse__C' cl_id_s '_PP_FGM'];
+otherwise
+	error('caa:noSuchQuantity','Quantity ''%s'' is not recongized',quantity)
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% get data
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+data = [];
+
+if ~isempty(regexp(data_path,':\d{1,2}\>'))
+	% use ISDAT
+	useISDAT = 1;
+	lasterr('')
+	try
+		dbase = Mat_DbOpen(data_path);
+	catch
+		error('ISDAT:dbOpen','Cannot open ISDAT database')
+	end
+else
+	if exist(data_path,'dir')
+		% read from directory
+		useISDAT = 0;
+	else
+		error('caa:noSuchDir','Directory %s does not exist',data_path)
+	end
+end
+
+start_date_s = strrep(datestr(fromepoch(start_time),29),'-','');
+
+if useISDAT
+	warning('caa:dataSource','Using ISDAT')
+	[t, dat] = isGetDataLite( dbase, start_time, dt, ...
+	r.pr, r.mem, r.inst, r.sen);
+	if ~isempty(dat)
+		data = [double(t) double(dat)'];
+		clear t dat
+	else, warning('caa:noData')
+	end
+
+else
+	warning('caa:dataSource','Using FILE')
+	data = av_read_cdf([data_path '/CSDS/' r.file start_date_s '*'], r.var);
+	if ~isempty(data)
+		data = av_t_lim(data,start_time + [0 dt]);
+	else, warning('caa:noData')
+	end
+end
