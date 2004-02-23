e86cbdce737540c146d4b96be2d2d443c563229e
yuri <yuri>
2004-02-23 23:41:31 +0000

    add a possibility to specify averaging interval. It is not clear if it works better...

--- caa/@ClusterProc/corrADCOffset.m
+++ caa/@ClusterProc/corrADCOffset.m
@@ -1,4 +1,4 @@
-function [new_data,offset] = corrADCOffset(cp,data)
+function [new_data,offset] = corrADCOffset(cp,data,start_time,dt)
 %corrADCOffset correct the ADC offset
 %   [new_data,offset] = corrADCOffset(data) corrects the ADC offset 
 %	in p12 and p34 fignals by removing the average
@@ -15,5 +15,16 @@ if data(end,1)-data(1,1)<60 %interval is shorter then 1 min
 end
 
 new_data = data;
-offset = mean(data(:,2));
+if nargin==4
+	test_data = av_t_lim(data,start_time + [0 dt]);
+	if isempty(test_data)
+		test_data = data(:,2);
+	else
+		test_data = test_data(:,2);
+	end
+else
+	test_data = data(:,2);
+end
+
+offset = mean(test_data); clear test_data
 new_data(:,2) = data(:,2) - offset;
