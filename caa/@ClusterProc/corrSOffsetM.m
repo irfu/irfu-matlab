d06c7feed24f9305d5c933fc6a2a6ea528d9f4ef
andris <andris>
2004-03-01 13:18:04 +0000

    added offset also for Y

--- caa/@ClusterProc/corrSOffsetM.m
+++ caa/@ClusterProc/corrSOffsetM.m
@@ -1,18 +1,25 @@
 function corrSOffsetM(cp,cl_id)
 %corrSOffset correct the Sunward offset and amplitude factor
-% do a manual correction of the offsets by comparison with 
+% do a manual correction of the offsets by comparison with
 % CIS and EDI data (must be loaded)
 %
 % corrSOffset(cp,cl_id)
 % cp - ClusterProc object
 % cl_id - SC#
 %
+% Offset variable names
+%  Ddsi#     - sunward offset real(Ddsi#) is X offset and imag(Ddsi#) is Y offset
+%  Damp#     - factor with which to multiply amplitude
+%  D#p12p34  - difference in sunward offset between different probe pairs
+%              first value gives offset difference in X- and second in Y-direction
 % Example:
 % corrSOffset(ClusterProc('/home/yuri/caa-data/20020304'),1)
 %
 % $Id$
 
 % Copyright 2004 Yuri Khotyaintsev (yuri@irfu.se)
+%
+
 
 old_pwd = pwd;
 cd(cp.sp) %enter the storage directory
@@ -29,10 +36,10 @@ if exist('./mEDSI.mat','file')
 		if exist(av_ssub('D?p12p34',cl_id),'var')
 			eval(av_ssub('Del=D?p12p34;',cl_id))
 		else
-			disp('Cannot load D#p12p34. Probably we have only one probe pair')	
+			disp('Cannot load D#p12p34. Probably we have only one probe pair')
 			Del=0;
 		end
-		if isreal(Del) 
+		if isreal(Del)
 			eval(av_ssub('diEs=diEs?p34;',cl_id))
 		else
 			disp('correcting p34')
@@ -59,14 +66,14 @@ if exist('./mCIS.mat','file')
 	end
 	clear CIS
 end
-if ~exist('diVCEp','var') | ~exist('diVCEh','var') 
+if ~exist('diVCEp','var') | ~exist('diVCEh','var')
 	warning('caa:noData','no CIS data loaded')
 else
 	for i=1:length(var)
-		if exist(var{i},'var') 
+		if exist(var{i},'var')
 			var_list = [var_list ',' var{i}];
 			var_list1 = [var_list1 ',' var{i}];
-		end	
+		end
 	end
 end
 clear var
@@ -78,20 +85,22 @@ if exist('./mEDI.mat','file')
 	eval(av_ssub(['if isfield(EDI,''' var '?''); ' var '=EDI.' var '?; end; clear ' var '?'], cl_id));
 	clear EDI
 end
-if ~exist('diEDI','var') 
+if ~exist('diEDI','var')
 	warning('caa:noData','no EDI data loaded')
 else
 	var_list = [var_list ',diEDI'];
 	var_list1 = [var_list1 ',diEDI'];
 end
 
-offset = [0 1];
+offset = [0+0i 1];
 diE_tmp = diE;
-diE_tmp(:,2) = diE_tmp(:,2) - offset(1);
-diE_tmp(:,2:3) = diE_tmp(:,2:3)*offset(2);
+diE_tmp(:,2) = diE_tmp(:,2) - real(offset(1));
+diE_tmp(:,3) = diE_tmp(:,3) - imag(offset(1));
+diE_tmp(:,2:3) = diE_tmp(:,2:3)*real(offset(2));
 diEs_tmp = diEs;
-diEs_tmp(:,2) = diEs_tmp(:,2) - offset(1);
-diEs_tmp(:,2:3) = diEs_tmp(:,2:3)*offset(2);
+diEs_tmp(:,2) = diEs_tmp(:,2) - real(offset(1));
+diEs_tmp(:,3) = diEs_tmp(:,3) - imag(offset(1));
+diEs_tmp(:,2:3) = diEs_tmp(:,2:3)*real(offset(2));
 
 figure(17)
 clf
@@ -99,12 +108,13 @@ t = tokenize(var_list1,',');
 leg = ['''' t{1} ''''];
 for i=2:length(t), leg = [leg ',''' t{i} '''']; end
 eval(['plotExy(' var_list ');zoom on'])
-title(sprintf('Cluster %d : offset %.2f [mV/m], amplitude factor %.2f',cl_id,offset(1),offset(2)))
+title(sprintf('Cluster %d : offset X %.2f [mV/m], offset Y %.2f [mV/m], amplitude factor %.2f',cl_id,
+real(offset(1)),imag(offset(1)),offset(2)))
 eval(['legend(' leg ')'])
 
 q='0';
 while(q ~= 'q')
-	q=av_q('Give Ex offset [mV/m] and amplitude factor (s-save,q-quit)[%]>','',num2str(offset,'%.2f '));
+	q=av_q('Give Ex and Ey offset [mV/m] and amplitude factor (s-save,q-quit)[%]>','',num2str([real(offset(1)) imag(offset(1)) real(offset(2))],'%.2f '));
 	switch(q)
 	case 's'
 		disp(sprintf('Ddsi%d, Damp%d -> ./mEDSI.mat',cl_id,cl_id))
@@ -115,11 +125,14 @@ while(q ~= 'q')
 	otherwise
 		[o_tmp,ok] = str2num(q);
 		if ok
-			if length(o_tmp) > 1
-				offset = o_tmp(1:2);
+			if length(o_tmp) > 2
+				offset(1) = o_tmp(1)+1i*o_tmp(2);
+        offset(2) = o_tmp(3);
+			elseif length(o_tmp) > 1
+				offset(1) = o_tmp(1)+1i*o_tmp(2);
 			else
-				offset(1) = o_tmp(1);
-			end	
+				offset(1) = o_tmp(1)+1i*imag(offset(1));
+			end
 			diE_tmp = diE;
 			diE_tmp(:,2) = diE_tmp(:,2) - offset(1);
 			diE_tmp(:,2:3) = diE_tmp(:,2:3)*offset(2);
