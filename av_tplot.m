8e31f65906adbaf8a36c1c099011e58ed8c5a602
andris <andris>
2004-03-05 17:59:25 +0000

    added option 'comp'

--- av_tplot.m
+++ av_tplot.m
@@ -1,34 +1,59 @@
 function c=av_tplot(x,t_unit_in_original_units,t_origo_in_original_units,varargin);
 % function c=av_tplot(x,t_unit_in_original_units,t_origo_in_original_units,varargin);
 % function c=av_tplot(x,'subplot') to plot in separate subplots all x values
-% function c=av_tplot(x,'yy',factor_to_multiply) to plot in separate subplots all x values
-% function c=av_tplot({x y z}) to plot subplots with x y z in them
-% function c=av_tplot({p1 p2 p3 p4 ...},[dt1 dt2 dt3 dt4 ...]) to plot subplots with x y z in them with given time shifts
+% function c=av_tplot(x,'yy',factor_to_multiply) add second axis on right
+% function c=av_tplot({x, y, z},[dt1 dt2 ..]) 1st subplot with x, 2nd with y etc. Subtract time shifts if given
+% function c=av_tplot({x, y, z},[dt1 dt2 ...],'comp') 1.subplot is x(:,2),y(:,2)... 2nd subplot is x(:,3) y(:,3 .. etc.
+% function c=av_tplot({p1, p2, p3, p4 ...},[dt1 dt2 dt3 dt4 ...]) to plot subplots with x y z in them with given time shifts
 % function c=av_tplot(x,1,0,varargin) to pass different options to plot routines within av_tplot
 
 % flag_subplot 0 - one plot
 %              1 - separate subplots for every component
 %              2 - separate subplots for all variables in the cell array
-flag_subplot=0;flag_yy=0;
+flag_subplot=0;
+flag_yy=0;
+if nargin >=2, inp2=t_unit_in_original_units;end
+if nargin >=3, inp3=t_origo_in_original_units;end
+
 if ((nargin >= 2) & isstr(t_unit_in_original_units)),
- q=t_unit_in_original_units;t_unit_in_original_units=1;
+ q=t_unit_in_original_units;
+ t_unit_in_original_units=1;
  if strcmp(q,'subplot'),
   if isnumeric(x),flag_subplot=1;end    % plot separate subplots for all x components
  elseif strcmp(q,'yy'),
   if isnumeric(x),flag_yy=1;end    % add second yy axis
   if nargin > 2, scaleyy=t_origo_in_original_units; else scaleyy=1;end
- else,
-  return;
  end
 end
 
-if iscell(x), % plot separate subplots for all x cell arrays
- flag_subplot=2;
- if nargin == 2,
-  dt=t_unit_in_original_units;
-  t_unit_in_original_units=1;t_origo_in_original_units=0;
- else,
-  dt(1:size(x,2))=0;
+if iscell(x), % plot several variables 
+ if nargin == 1,
+    flag='subplot';
+    dt(1:size(x,2))=0;
+    t_unit_in_original_units=1;t_origo_in_original_units=0;
+ elseif nargin == 2,
+   if ischar(inp2),
+    flag=inp2;
+    dt(1:size(x,2))=0;
+    t_unit_in_original_units=1;t_origo_in_original_units=0;
+   else
+    dt=t_unit_in_original_units;
+    t_unit_in_original_units=1;t_origo_in_original_units=0;
+    flag='subplot';
+   end
+ elseif nargin>=3,
+   dt=t_unit_in_original_units;
+   t_unit_in_original_units=1;
+   flag=t_origo_in_original_units;
+   t_origo_in_original_units=0;
+ end
+ switch flag
+   case 'comp'
+     flag_subplot=3;
+   case 'subplot'
+     flag_subplot=2;
+   otherwise
+     error('input not allowed');
  end
 end
 
@@ -66,6 +91,17 @@ elseif flag_subplot==2,
     plot((y(:,1)-ts-dt(ipl))/tu,y(:,i),varargin{:});grid on;
   end
   tt=y(1,1);
+elseif flag_subplot==3,  % components of vectors in separate panels
+  npl=size(x{1},2)-1;
+  for ipl=1:npl,
+    c(ipl)=av_subplot(npl,1,-ipl);
+    line_colors={'b','g','r','c','m','y','k'}; 
+    for j=1:size(x,2),
+      y=x{j};
+      plot((y(:,1)-ts-dt(j))/tu,y(:,ipl+1),varargin{:},line_colors{j});grid on;hold on;
+    end
+  end
+  tt=y(1,1);
 end
 
   % in case time is in isdat_epoch add time_axis
@@ -86,4 +122,3 @@ else
   av_figmenu;user_data.av_figmenu=1;
 end
 set(gcf,'userdata',user_data);
-
