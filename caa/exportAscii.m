57dd8b5bcdd01b775bd4de6941fb6d7cba190aee
yuri <yuri>
2004-03-15 11:54:39 +0000

    add VxB, Es

--- caa/exportAscii.m
+++ caa/exportAscii.m
@@ -14,14 +14,15 @@ function exportAscii(var,vs,comment)
 %	exportAscii(Etemp,'diE3p1234')
 %	exportAscii(Etemp,'diE3p1234','electric field is high pass filtered at 3Hz')
 %
-% $Revision$  $Date$
-%
+% $Id$
 
 % Copyright 2004 Yuri Khotyaintsev
 
 if nargin<3, comment='';end
 if nargin==1, vs=inputname(1); end
 if nargin<1, help exportAscii;return; end
+
+com_Ez = 'Ez is not reliable when magnetic field is close to the spin plane\n%% The last column shows the angle of B with respect to the spin plane (B,spin)';
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % P 
@@ -40,7 +41,7 @@ if regexp(vs,'^P[1-4]$')==1
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % raw E p12 and p34
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-elseif regexp(vs,'wE[1-4]p(12|34)')==1
+elseif regexp(vs,'^wE[1-4]p(12|34)')==1
 
 	cl_id = vs(3);
 	inst = 'EFW';
@@ -57,7 +58,7 @@ elseif regexp(vs,'wE[1-4]p(12|34)')==1
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % spin fits E p12 and p34
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-elseif regexp(vs,'diEs[1-4]p(12|34)')==1
+elseif regexp(vs,'^diEs[1-4]p(12|34)')==1
 
 	cl_id = vs(5);
 	inst = 'EFW';
@@ -70,7 +71,7 @@ elseif regexp(vs,'diEs[1-4]p(12|34)')==1
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % despun full resolution E
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-elseif regexp(vs,'diE[1-4]p1234')==1
+elseif regexp(vs,'^diE[1-4]p1234')==1
 
 	cl_id = vs(4);
 	inst = 'EFW';
@@ -83,35 +84,55 @@ elseif regexp(vs,'diE[1-4]p1234')==1
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % despun full resolution E with assumption E.B = 0
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-elseif regexp(vs,'diE[1-4]')==1
+elseif regexp(vs,'^(diE[1-4]|diEs[1-4])$')==1
 
-	cl_id = vs(4);
+	cl_id = vs(end);
 	inst = 'EFW';
 	sig = 'E';
-	sen = 'p1234';
+	if vs(4)=='s', sen = 'spin fit';
+	else, sen = 'p1234';
+	end
 	frame = 'DSI,  approximately the same as GSE';
 	var_labels = {'Ex','Ey','Ez','(B,spin)'};
-  var_units =  {'mV/m','mV/m','mV/m','deg'};
-  com = 'Ez is not measured but calculated from E.B=0\nEz is not reliable when magnetic field B is close to the spin plane\n%% The last column shows the angle of B with respect to the spin plane (B,spin)';
+	var_units =  {'mV/m','mV/m','mV/m','deg'};
+	com = com_Ez;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % full resolution E in GSE coordinates
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-elseif regexp(vs,'E[1-4]')==1
+elseif regexp(vs,'^(E[1-4]|Es[1-4])')==1
 
 	cl_id = vs(2);
 	inst = 'EFW';
 	sig = 'E';
-	sen = 'p1234';
-	frame = 'GSE,  E along spin axis is not measured, but calculated from E.B=0';
+	if vs(2)=='s', sen = 'spin fit';
+	else sen = 'p1234';
+	end
+	frame = 'GSE';
 	var_labels = {'Ex','Ey','Ez','(B,spin)'};
-  var_units =  {'mV/m','mV/m','mV/m','deg'};
-  com = 'Ez is not reliable when magnetic field B is close to the spin plane\n%% The last column shows the angle of B with respect to the spin plane (B,spin)';
+	var_units =  {'mV/m','mV/m','mV/m','deg'};
+	com = com_Ez;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% ExB
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+elseif regexp(vs,'^(diVExBs[1-4]|VExBs[1-4])')==1
+	
+	cl_id = vs(end);
+	inst = 'EFW';
+	sig = 'V=ExB';
+	sen = 'spin fit';
+	if strcmp(vs(1:2),'di'), frame = 'DSI,  approximately the same as GSE';
+	else, frame = 'GSE';
+	end
+	var_labels = {'Vx','Vy','Vz','(B,spin)'};
+	var_units =  {'km/s','km/s','km/s','deg'};
+	com = com_Ez;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % full resolution satellite potential and derived density
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-elseif regexp(vs,'NVps[1-4]')==1
+elseif regexp(vs,'^NVps[1-4]')==1
 
 	cl_id = vs(5);
 	inst = 'EFW';
@@ -125,7 +146,7 @@ elseif regexp(vs,'NVps[1-4]')==1
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % phase
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-elseif regexp(vs,'A[1-4]')
+elseif regexp(vs,'^A[1-4]')
 
 	cl_id = vs(2);
 	inst = 'ephemeris';
@@ -169,11 +190,6 @@ var_s =    'time       ';
 var_unit = '[s]        ';
 mask = '%10.4f ';
 
-if		strcmp(sig,'E'),		units = 'mV/m';
-elseif	strcmp(sig,'phase'),	units = 'deg';
-else,	units = 'undef';
-end
-
 for i=1:n_data
 	var_length_nine=strvcat(var_labels{i},'         ');
 	var_unit_length_nine=strvcat(['[' var_units{i} ']'],'         ');
@@ -196,7 +212,7 @@ if comment,
     fprintf(fid,['%%   ' comm{1,j} ' \n']);
   end
 end
-fprintf(fid,['%% Time from: ' t0_s ' \n%%\n']);
+fprintf(fid,['%%\n%% Time from: ' t0_s ' \n%%\n']);
 fprintf(fid,['%% ' var_s ' \n']);
 fprintf(fid,['%% ' var_unit ' \n']);
 fprintf(fid,[mask '\n'],var');
