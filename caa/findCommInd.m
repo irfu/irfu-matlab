b3f233ccee00a9eef6fd015ce12225a4cf9cc013
yuri <yuri>
2004-03-23 14:16:54 +0000

    try to fix "early exits". does not fix the problem completely.

--- caa/findCommInd.m
+++ caa/findCommInd.m
@@ -5,10 +5,12 @@ function [ii1,ii2]=findCommInd(d1,d2)
 % This function can be used when combining two datasets with
 % gaps, like p12 and p34.
 %
-% $Revision$  $Date$
+% $Id$
 %
 
 % Copyright 2004 Yuri Khotyaintsev
+
+DEBUG = 1;
 
 ii1 = [];
 ii2 = [];
@@ -25,16 +27,18 @@ while 1
 	d = t1(s1:s1+la)-t2(s2:s2+la);
 	ii = find(abs(d) > 0);
 
-	if isempty(ii)
+	if isempty(ii) % time lines are identical
 		% save the interval and break
 		ii1 = [ii1 s1:s1+la];
 		ii2 = [ii2 s2:s2+la];
+		if DEBUG, disp('break at 1'), end
 		break 
 	end
 
-	ii = ii(1); % first point where times differ
+	ii = ii(1) % first point where times differ
 	if ii>1
 		% save the interval and shift
+		if DEBUG, disp('save interval and shift'), end
 		ii1 = [ii1 s1:s1+ii-2];
 		ii2 = [ii2 s2:s2+ii-2];
 		s1 = s1 + ii - 1;
@@ -42,13 +46,29 @@ while 1
 	end
 	if d(ii) > 0
 		% shift t2
+		if DEBUG, disp('shift t2'), end
+
 		i_t0 = find(t2(s2:end)==t1(s1));
-		if isempty(i_t0), break, end
-		s2 = s2 + i_t0 - 1;
+		if isempty(i_t0)
+			if t1(s1) > t2(end)
+				if DEBUG, disp('break at 2'), end
+				break
+			end
+			s1 = s1 + 1;
+		else
+			s2 = s2 + i_t0 - 1;
+		end
 	else
 		%shift t1
+		if DEBUG, disp('shift t1'), end
 		i_t0 = find(t1(s1:end)==t2(s2));
-		if isempty(i_t0), break, end
+		if isempty(i_t0)
+			if t2(s2) > t1(end)
+				if DEBUG, disp('break at 3'), end
+				break
+			end
+			s1 = s2 + 1;
+		end
 		s1 = s1 + i_t0 - 1;
 	end
 	n_loop = n_loop + 1;
