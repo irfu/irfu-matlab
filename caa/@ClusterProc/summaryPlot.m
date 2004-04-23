d1acf8c9a52722886a11b127d8b4a34ee76d60d2
yuri <yuri>
2004-04-23 15:50:05 +0000

    add B

--- caa/@ClusterProc/summaryPlot.m
+++ caa/@ClusterProc/summaryPlot.m
@@ -28,13 +28,13 @@ end
 
 % load data
 if strcmp(cs,'dsi') 
-	q_list = {'P?','diE?','diEs?','diVExBs?'};
-	l_list = {'SC pot [-V]','E DSI [mV/m]','E DSI [mV/m]','V=ExB DSI [km/s]'};
+	q_list = {'P?','diBPP?','diE?','diEs?','diVExBs?'};
+	l_list = {'SC pot [-V]','B PP DSI [nT]','E DSI [mV/m]','E DSI [mV/m]','V=ExB DSI [km/s]'};
 else
-	q_list = {'P?','E?','Es?','VExBs?'};
-	l_list = {'SC pot [-V]','E GSE [mV/m]','E GSE [mV/m]','V=ExB GSE [km/s]'};
+	q_list = {'P?','BPP?','E?','Es?','VExBs?'};
+	l_list = {'SC pot [-V]','B PP GSE [nT]','E GSE [mV/m]','E GSE [mV/m]','V=ExB GSE [km/s]'};
 end
-f_list = {'mP','mEdB','mEdB','mEdB'};
+f_list = {'mP','mBPP','mEdB','mEdB','mEdB'};
 
 old_pwd = pwd;
 cd(cp.sp) %enter the storage directory
@@ -47,7 +47,10 @@ for k=1:length(q_list)
 		eval(av_ssub(['load ' f_list{k} ' ' q_list{k}],cl_id))
 		if exist(av_ssub(q_list{k},cl_id))
 			n_plots = n_plots + 1;
-			if k==2 % E-field
+			if k==2 % B-field
+				c_eval(['data{n_plots}=av_abs(' q_list{k} '(:,1:4));'],cl_id)
+				labels{n_plots} = l_list{k};
+			elseif k==3 % E-field
 				eval(av_ssub(['data{n_plots}=' q_list{k} '(:,1:4);'],cl_id)) 
 				labels{n_plots} = l_list{k};
 				n_plots = n_plots + 1;
@@ -88,19 +91,26 @@ end
 clf
 orient tall
 
+dummy = 'data{1}';
+for k=2:n_plots, dummy = [dummy ',data{1}'];, end
+eval(['h = av_tplot({' dummy '});']) 
+clear dummy
+
 for k=1:n_plots
-	h{k} = subplot(n_plots,1,k);
+	axes(h(k))
 	av_tplot(data{k});
-	av_zoom([t_st t_end],'x',h{k})
+	av_zoom([t_st t_end],'x',h(k))
+	set(gca,'YLim',get(gca,'YLim')*.99)
 	ylabel(labels{k})
 	if k==1, title(['EFW, Cluster ' num2str(cl_id,'%1d')]), end
-	if k<n_plots, xlabel(''), end		
+	if k<n_plots, xlabel(''),set(gca,'XTickLabel',[]), end		
 end
 
 addPlotInfo
 
 for k=n_plots:-1:1
-	axes(h{k})
+	%axes(h{k})
+	axes(h(k))
 	if min(size(data{k}))>2, legend('X','Y','Z'), end
 end
 
