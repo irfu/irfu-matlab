f394aa1ead2e271449d8b45be4f0dbda7d32c90e
andris <andris>
2004-05-03 19:17:37 +0000

    improving, debugging

--- c_ri/c_ri_find.m
+++ c_ri/c_ri_find.m
@@ -3,7 +3,7 @@ function [events_tint]=c_ri_find(run_steps,st_m, et_m, min_angle, min_ampl, peri
 % c_ri_find(run_steps,st_m, et_m, min_angle, min_ampl, period)
 %
 %Example
-% c_ri_find([1 1 1 1 1 1 1], [2002 02 03 0 0 0], [2002 07 08 0 0 0], 150, 5, 3, 3,2);
+% c_ri_find([1 1 1 1], [2002 02 03 0 0 0], [2002 07 08 0 0 0], 150, 5, 3, 3,2);
 % does all steps from [2002 02 03 0 0 0] to [2002 07 08 0 0 0], with
 % minum angle at 150 degrees, ,minumum amplitud 5 nT, the period to class to events as one
 % is 3 seconds and the maximum distance to MP is 3 Re and the solarwind preassure i
@@ -16,10 +16,10 @@ function [events_tint]=c_ri_find(run_steps,st_m, et_m, min_angle, min_ampl, peri
 %            "st_m"
 % min_ampl -[5|10], must have same number of rows as
 %            "st_m"
-% period -time distane, in seconds
+% period - download data and plot time interval event+-period 
 % d2MP -distance to MP, in Re
 % psw -solarwind preassure, in nPa
-% run_steps - [ 0 | 1 | 1 | 1 | 0 | 0 | 0]
+% run_steps - [ 0 | 1 | 1 | 1 ]
 %       if one element is zero then the step will be jumped
 % 		the steps are:
 %		1) calculating the MP-crossings
@@ -44,7 +44,7 @@ function [events_tint]=c_ri_find(run_steps,st_m, et_m, min_angle, min_ampl, peri
 % for this period. Then the angles are calculated and if the angles and the
 % amplitude are larger than a certian threshold. This time is classed as
 % an event. The events are reduced by classing two events as the same events
-% if the timedifferance is less than period.
+% if the timedifferance is less than half of period.
 %
 % Adopted from c_ri_run_all
 
@@ -103,24 +103,24 @@ end
 
 path_ok='disp([])';
 while ~strcmp(path_ok,'c'),
- eval(path_ok);
- disp('=========== Path information =========');
- disp(['found events          > p_E  = ''' p_E ''';']);
- disp(['events ASCII, figures > p_R  = ''' p_R ''';']);
- disp('======================================');
- disp('To change enter new value, e.g. >p_A=[pwd ''/''];  or >p_R=''/share/tmp'';');
- disp('To continue >c');
- path_ok=input('>','s');
- if exist('.c_ri_parameters.mat'),
-  try save -append .c_ri_parameters.mat p_E p_R;
-  catch disp('Paths changes valid only for this run!');
+  eval(path_ok);
+  disp('=========== Path information =========');
+  disp(['found events          > p_E  = ''' p_E ''';']);
+  disp(['events ASCII, figures > p_R  = ''' p_R ''';']);
+  disp('======================================');
+  disp('To change enter new value, e.g. >p_A=[pwd ''/''];  or >p_R=''/share/tmp'';');
+  disp('To continue >c');
+  path_ok=input('>','s');
+  if exist('.c_ri_parameters.mat'),
+    try save -append .c_ri_parameters.mat p_E p_R;
+    catch disp('Paths changes valid only for this run!');
+    end
+  else
+    try save .c_ri_parameters.mat st_m et_m min_angle min_ampl period d2MP psw run_steps;
+    catch disp('Paths changes valid only for this run!');
+    end
   end
- else
-  try save .c_ri_parameters.mat st_m et_m min_angle min_ampl period d2MP psw run_steps;
-  catch disp('Paths changes valid only for this run!');
-  end
- end
-
+  
 end
 try save -append .c_ri_parameters.mat p_E p_R;
 catch disp('Input parameters not saved');
@@ -132,43 +132,38 @@ end
 for i = 1:i_end
   st = st_m(i,:);
   et = et_m(i,:);
-
-  %step one
+  
+  %step 1
   if run_steps(1) == 1
-  [passing_MP,dist_t]=c_ri_auto_event_search(st,et,d2MP,psw);
+    [passing_MP,dist_t]=c_ri_auto_event_search(st,et,d2MP,psw);
   end
-
-  %step two
+  
+  %step 2
   if run_steps(2) == 1
-  c_ri_run_get_B(st,et,p_MP, p_Bt);
+    for j=1:size(passing_MP,l)
+      [B1,B2,B3,B4]=c_get_bfgm(passing_MP(j,:),1:4);
+      c_eval('Binterp?=av_interp(B?,B1);',2:4);
+      [angles, ampl] = c_ri_angles_and_ampl(B1,Binterp2,Binterp3,Binterp4);
+    end
   end
-
-  %step three
+  
+  %step 3
   if run_steps(3) == 1
-  c_ri_four_B_files_2_one(p_Bt,p_Bd,st,et);
+    time_of_events = class_angle_as_event(angles,ampl, min_angle, min_ampl,-1) ; % -1 is mode (no idea which)
+    sort_events=1;
+    while sort_events
+      dt_events=diff(time_of_events(:,1)); % find distance between events
+      ind=find(dt_events<period/2); % find which events are closer than period/2 
+      if isempty(ind), sort_events=0;end
+      time_of_events(ind(1),:)=[]; 
+    end
   end
-
-  %step four
+  
+  %step 4
   if run_steps(4) == 1
-  c_ri_load_B_for_preprocess(st,et,p_Bd, p_Bp);
+    c_ri_get_event_data(time_interval,time_of_events(:,1),path_E, {'EFW_E','EFW_P'}, period);
+    per = period(i);
+    c_ri_run_events_into_pictures(st,et,p_MP,p_Bp,p_E,p_R, per);
   end
-
-  %step five
-  if run_steps(5) == 1
-  c_ri_run_calc_angles_w_pre(st,et, p_Bp, p_A);
-  end
-
-  %step  sex
-  if run_steps(6) == 1
-  m_ang = min_angle(i);
-  m_ampl = min_ampl(i);
-  c_ri_run_class_angle_as_event(st,et,m_ang,m_ampl,p_A,p_E);
-  end
-
-  %step seven
-  if run_steps(7) == 1
-  per = period(i);
-  c_ri_run_events_into_pictures(st,et,p_MP,p_Bp,p_E,p_R, per);
-  end
-
+  
 end
