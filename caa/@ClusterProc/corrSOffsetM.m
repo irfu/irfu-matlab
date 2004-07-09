a4bff7d016d71996fb149130fbbbd2abf728ad51
yuri <yuri>
2004-07-09 15:07:54 +0000

    fix zoom which was brokeb by previous commit.

--- caa/@ClusterProc/corrSOffsetM.m
+++ caa/@ClusterProc/corrSOffsetM.m
@@ -182,9 +182,10 @@ for j=1:length(t0)
 end
 
 addPlotInfo
-zoom on
 av_figmenu
 eval(['legend(h(1),' leg ')'])
+for j=1:length(t0)+2, axes(h(j)), end
+zoom on
 
 q='0';
 while(q ~= 'q')
@@ -267,6 +268,7 @@ while(q ~= 'q')
     	title(sprintf('Cluster %d : offset X %.2f [mV/m], offset Y %.2f [mV/m], amplitude factor %.2f',cl_id,real(offset(1)),imag(offset(1)),offset(2)))
 		addPlotInfo
 		eval(['legend(h(1),' leg ')'])
+		for j=1:length(t0)+2, axes(h(j)), end
 		zoom on
 		flag_replot=0;
 	end
