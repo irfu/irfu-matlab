2384ef78017f93d350a91752d6d2b0be1cd8ecf8
yuri <yuri>
2004-07-12 09:38:16 +0000

    add another format

--- caa/makeFName.m
+++ caa/makeFName.m
@@ -1,23 +1,32 @@
-function out=makeFName(st)
+function out=makeFName(st,fmt)
 %makeFName construc a filename string from time
-% fname = makeFName(st)
+% fname = makeFName(st, [FORMAT])
 % Input:
 % st - ISDAT epoch
 %
 % Output:
-% fname - string YYYYMMDD_HHMM
+% fname - string formatted accordint to FORMAT:
+%	0: YYYYMMDD_hhmm (default)
+%	1: YYMMDDhhmmss
 %
 % $Id$
 
 % Copyright 2004 Yuri Khotyaintsev (yuri@irfu.se)
 
-d = fromepoch(st);
+if nargin < 2, fmt = 0; end
 
+d = fromepoch(st);
 s{1} = num2str(d(1));
 
-for k=2:5
+for k=2:6
 	s{k} = num2str(d(k));
-	if length(s{k})<2, s{k} = ['0' s{k}]; end
+	if d(k)<10, s{k} = ['0' s{k}]; end
 end
 
-out = [s{1} s{2} s{3} '_' s{4} s{5}];
+if fmt==0
+	out = [s{1} s{2} s{3} '_' s{4} s{5}];
+elseif fmt==1
+	out = [s{1}(3:4) s{2} s{3} s{4} s{5} s{6}(1:2)];
+else
+	error('unknown format')
+end
