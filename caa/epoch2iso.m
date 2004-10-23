1c7cde7fa7d07311d649f893cc2d9b32cd3dcd16
yuri <yuri>
2004-10-23 23:08:52 +0000

    Completely rewrite this function. Matlabs DATESTR rounds seconds and it is a VERY bad for us. Do not use it.

--- caa/epoch2iso.m
+++ caa/epoch2iso.m
@@ -5,15 +5,40 @@ function out = epoch2iso(t)
 % as described in the CEF data file syntax
 % http://www.space-plasma.qmul.ac.uk/csds/welcome.html
 %
-%   See also epoch2date, datenum, datestr, toepoch, fromepoch
+% t_iso_s = iso2epoch(t_epoch)
+%
+%   See also EPOCH2DATE, ISO2EPOCH, TOEPOCH, FROMEPOCH
 %
 % $Id$
 
-s1=datestr(epoch2date(t),'yyyy-mm-ddTHH:MM:SS');
-s2=zeros(length(t),8);
-% take care about fraction of second
-for j=1:length(t)
-	ss = sprintf('%6f',t(j)-fix(t(j)));
-	s2(j,:) = [ss(1,2:8) 'Z'];
+% Copyright 2004 Yuri Khotyaintsev
+
+% We need to do all this because DATESTR rounds seconds
+d = fromepoch(t);
+
+for j=2:5, s1(j-1) = {add_zero(d(:,j),num2str(d(:,j)))}; end
+
+% Take care about seconds separately
+s2 = add_zero(d(:,6),num2str(d(:,6),'%6f'));
+
+sZ = s2(:,1); sZ(:) = 'Z';
+sT = sZ; sT(:) = 'T';
+sdash = sZ; sdash(:) = '-';
+scol = sZ; scol(:) = ':';
+
+out = [num2str(d(:,1)) sdash s1{1} sdash s1{2} sT s1{3} scol s1{4} scol s2 sZ];
+
+% Help function to insert zeros for numbers containing only one digit 
+function out = add_zero(d,s)
+out = s;
+ii = find(d<10);
+if ~isempty(ii)
+	ss = s(ii,1);
+	ss(:) = '0';
+	if length(ii)==length(d)
+		% Add to all lines
+		out = [ss s];
+	else
+		out(ii,:) = [ss s(ii,1:end-1)];
+	end
 end
-out = [s1 s2];
