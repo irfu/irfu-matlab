90602c73fc34d79b82a924ff1bd7f3ef68f819eb
yuri <yuri>
2004-10-28 09:54:49 +0000

    correct bad formatting

--- av_ssub.m
+++ av_ssub.m
@@ -1,12 +1,15 @@
 function [output_string]=av_ssub(input_string,varargin)
-%AV_SSUB change all appearences of '?' and/or '!' and/or '$' in string to some number
-% [output_string]=av_ssub(input_string,num) change all appearence of '?' in input_string to num
-%  num is converted to string using NUM2STR function
-% 
-% [output_string]=av_ssub2(input_string,num1,[num2],[num3])
-%  change all appearences of '?' to num1, '!' to num2, and '$' to num3
+%AV_SSUB change all appearences of '?' and/or '!' and/or '$' in string 
+%to some number
 %
-% Ex: for ic=1:4,eval(av_ssub('R?=r?;C?=R?.^2;',ic)),end
+% [OUTPUT_STRING]=AV_SSUB(INPUT_STRING,NUM) change all appearence of '?' 
+% in INPUT_STRING to NUM. NUM is converted to string using NUM2STR function
+%
+% [OUTPUT_STRING]=AV_SSUB2(INPUT_STRING,NUM1,[NUM2],[NUM3])
+%  change all appearences of '?' to NUM1, '!' to NUM2, and '$' to NUM3
+%
+% Example: 
+%	for ic=1:4,eval(av_ssub('R?=r?;C?=R?.^2;',ic)),end
 %     is the same as R1=r1;C1=R1.^2;R2=r2;C2=R2.^2;...
 %
 % $Id$
@@ -14,7 +17,6 @@ function [output_string]=av_ssub(input_string,varargin)
 error(nargchk(2,4,nargin))
 
 output_string = input_string;
-
 symb = '?!$';
 
 for j=nargin-1:-1:1
