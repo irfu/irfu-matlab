19256a50d7d71736d7e9e84918261c1d95dd7453
yuri <yuri>
2004-12-01 18:28:37 +0000

    start update. Not yet complete!

--- caa/c_desc.m
+++ caa/c_desc.m
@@ -1,4 +1,4 @@
-function varargout = c_desc(vs)
+function varargout = c_desc(vs,v_info)
 %C_DESC provide a description of a Cluster variable
 % C_DESC(V_S) prints out a descriptoon of variable VS
 %
@@ -34,9 +34,9 @@ function varargout = c_desc(vs)
 
 % Copyright 2004 Yuri Khotyaintsev (yuri@irfu.se)
 
-error(nargchk(1,1,nargin))
-
+error(nargchk(1,2,nargin))
 if ~isstr(vs), error('VS must be string'), end
+if nargin<2, v_info = []; end
 
 com_Ez = 'Ez is not reliable when magnetic field is close to the spin plane';
 
@@ -47,32 +47,47 @@ vvs(1:length(vs)) = vs;
 % P 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 if regexp(vs,'^P[1-4]$')==1
+	v.data = 1;
 	v.cl_id = vs(2);
 	v.inst = 'EFW';
 	v.frame = 'sc';
 	v.sig = 'P';
-	v.sen = 'all';
-	v.cs = {'scalar>na'};
+	if ~isempty(v_info) & isfield(v_info,'probe'), v.sen = num2str(v_info.probe);
+	else, v.sen = '1234';
+	end 
+	v.cs = {'na'};
+	v.rep = {'scalar'};
  	v.units =  {'V'};
 	v.si_conv = {''};
 	v.size = 1;
 	v.name = {'Spacecraft_potential'};
-	v.labels = {['P' v.sen]};
-	v.field_name = {'Averaged probe potential from all probes'};
-	v.com = 'this signal is averaged from all probes available at the time';
+	v.labels = {'P'};
+	v.field_name = {'Spacecraft potential'};
+	v.ent = {'Spacecraft'};
+	v.prop = {'Potential'};
+	v.fluc = {'Waveform'};
+	v.com = ['this signal is averaged from probes ' v.sen];
 	v.file = 'mP';
 	v.quant = 'p';
-
+elseif regexp(vs,'^P[1-4]_info$')==1
+	v.data = 0;
+	v.cl_id = vs(2);
+	v.inst = 'EFW';
+	v.com = 'Spacecraft potential INFO';
+	v.file = 'mP';
+	v.quant = 'p';
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % P - individual probes 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 elseif regexp(vs,'^P10Hz[1-4]p[1-4]$')==1
+	v.data = 1;
 	v.cl_id = vs(6);
 	v.inst = 'EFW';
 	v.frame = 'sc';
 	v.sig = 'P';
 	v.sen = vs(8);
-	v.cs = {'scalar>na'};
+	v.cs = {'na'};
+	v.rep = {'scalar'};
  	v.units =  {'V'};
 	v.si_conv = {''};
 	v.size = 1;
@@ -86,13 +101,15 @@ elseif regexp(vs,'^P10Hz[1-4]p[1-4]$')==1
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % raw E p12 and p34
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-elseif regexp(vs,'^wE[1-4]p(12|32|34)$') 
+elseif regexp(vs,'^wE[1-4]p(12|32|34)$')
+	v.data = 1;
 	v.cl_id = vs(3);
 	v.inst = 'EFW';
 	v.frame = 'sc';
 	v.sig = 'E';
 	v.sen = vs(4:6);
-	v.cs = {'scalar>na'};
+	v.cs = {'na'};
+	v.rep = {'scalar'};
  	v.units =  {'mV/m'};
 	v.si_conv = {'1.0e-3>V/m'};
 	v.size = [1];
@@ -108,6 +125,7 @@ elseif regexp(vs,'^wE[1-4]p(12|32|34)$')
 % spin fits E p12 and p34
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 elseif regexp(vs,'^(i)?diEs[1-4]p(12|32|34)')==1
+	v.data = 1;
 	if vvs(1)=='i', 
 		vvs = vvs(2:end);
 		v.frame = 'inertial'; 
@@ -122,7 +140,8 @@ elseif regexp(vs,'^(i)?diEs[1-4]p(12|32|34)')==1
 	v.inst = 'EFW';
 	v.sig = 'E';
 	v.sen = vvs(6:8);
-	v.cs = {'vector>dsi_xy'};
+	v.cs = {'DSI'};
+	v.rep = {'xy'};
  	v.units =  {'mV/m'};
 	v.si_conv = {'1.0e-3>V/m'};
 	v.size = [2];
@@ -130,12 +149,13 @@ elseif regexp(vs,'^(i)?diEs[1-4]p(12|32|34)')==1
 	v.labels = v.name;
 	v.label_1 = {'"x", "y"'};
 	v.field_name = {'Electric field'};
-	v.com = '';
+	v.com = 'Ez=0 by definition (not measured).';
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % despun full resolution E
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 elseif regexp(vs,'^(i)?di(b)?E[1-4]p1234$')==1
+	v.data = 1;
     switch vvs(1:findstr(vvs,'E')-1) % characters before 'E'
         case 'di'
             v.frame = 'sc';
@@ -157,8 +177,9 @@ elseif regexp(vs,'^(i)?di(b)?E[1-4]p1234$')==1
 	v.cl_id = vvs(findstr(vvs,'E')+1); % next character after 'E'
 	v.inst = 'EFW';
 	v.sig = 'E';
-	v.sen = 'all';
-	v.cs = {'vector>dsi_xy'};
+	v.sen = '1234';
+	v.cs = {'DSI'};
+	v.rep = {'xy'};
  	v.units =  {'mV/m'};
 	v.si_conv = {'1.0e-3>V/m'};
 	v.size = [2];
@@ -172,6 +193,7 @@ elseif regexp(vs,'^(i)?di(b)?E[1-4]p1234$')==1
 % despun full/spin resolution E with assumption E.B = 0
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 elseif regexp(vs,'^(i)?diE(s)?[1-4]$')
+	v.data = 1;
 	if vvs(1)=='i', 
 		vvs = vvs(2:end);
 		v.frame = 'inertial'; 
@@ -186,7 +208,7 @@ elseif regexp(vs,'^(i)?diE(s)?[1-4]$')
 	v.inst = 'EFW';
 	v.sig = 'E';
 	if vvs(4)=='s', v.sen = 's'; else, v.sen = ''; end
-	v.cs = {'vector>dsi_xyz','scalar>na'};
+	v.cs = {'vector>DSI_xyz','scalar>na'};
  	v.units =  {'mV/m','deg'};
 	v.si_conv = {'1.0e-3>V/m','1>degree'};
 	v.size = [3 1];
@@ -200,6 +222,7 @@ elseif regexp(vs,'^(i)?diE(s)?[1-4]$')
 % full/spin resolution E in GSE coordinates
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 elseif regexp(vs,'^(i)?E(s)?[1-4]$')
+	v.data = 1;
 	if vvs(1)=='i', 
 		vvs = vvs(2:end);
 		v.frame = 'inertial'; 
@@ -228,6 +251,7 @@ elseif regexp(vs,'^(i)?E(s)?[1-4]$')
 % ExB
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 elseif regexp(vs,'^(di)?VExB(s)?[1-4]$')
+	v.data = 1;
 	v.cl_id = vs(end);
 	v.inst = 'EFW';
 	v.frame = 'sc';
@@ -236,7 +260,7 @@ elseif regexp(vs,'^(di)?VExB(s)?[1-4]$')
 	else, v.sen = ''; 
 	end
 	if strcmp(vvs(1:2),'di')
-		v.cs = {'vector>dsi_xyz','scalar>na'};
+		v.cs = {'vector>DSI_xyz','scalar>na'};
 	else
 		v.cs = {'vector>gse_xyz','scalar>na'};
 	end
@@ -257,6 +281,7 @@ elseif regexp(vs,'^(di)?VExB(s)?[1-4]$')
 % full resolution satellite potential and derived density
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 elseif regexp(vs,'^NVps[1-4]$')==1
+	v.data = 1;
 	v.cl_id = vs(end);
 	v.inst = 'EFW';
 	v.frame = 'sc';
@@ -278,6 +303,7 @@ elseif regexp(vs,'^NVps[1-4]$')==1
 % phase
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 elseif regexp(vs,'^A[1-4]$')
+	v.data = 1;
 	v.cl_id = vs(end);
 	v.inst = 'Ephemeris';
 	v.frame = 'sc';
@@ -299,6 +325,7 @@ elseif regexp(vs,'^A[1-4]$')
 % spin axis orientation
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 elseif regexp(vs,'^SAX[1-4]$')
+	v.data = 1;
 	v.cl_id = vs(end);
 	v.inst = 'Ephemeris';
 	v.frame = 'sc';
@@ -320,12 +347,13 @@ elseif regexp(vs,'^SAX[1-4]$')
 % spacecraft velocity
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 elseif regexp(vs,'^(di)?V[1-4]$')
+	v.data = 1;
 	v.cl_id = vs(end);
 	v.inst = 'Ephemeris';
 	v.frame = 'sc';
 	v.sig = 'Velocity';
 	v.sen = '';
-	if strcmp(vvs(1:2),'di'), v.cs = {'vector>dsi_xyz'};
+	if strcmp(vvs(1:2),'di'), v.cs = {'vector>DSI_xyz'};
 	else, v.cs = {'vector>gse_xyz'};
 	end
 	v.units =  {'km/s'};
@@ -343,12 +371,13 @@ elseif regexp(vs,'^(di)?V[1-4]$')
 % spacecraft position
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 elseif regexp(vs,'^(di)?R[1-4]$')
+	v.data = 1;
 	v.cl_id = vs(end);
 	v.inst = 'Ephemeris';
 	v.frame = 'sc';
 	v.sig = 'Position';
 	v.sen = '';
-	if strcmp(vvs(1:2),'di'), v.cs = {'vector>dsi_xyz'};
+	if strcmp(vvs(1:2),'di'), v.cs = {'vector>DSI_xyz'};
 	else, v.cs = {'vector>gse_xyz'};
 	end
 	v.units =  {'km'};
@@ -366,7 +395,7 @@ elseif regexp(vs,'^(di)?R[1-4]$')
 % CIS N PP
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 elseif regexp(vs,'^NC(h|p)[1-4]$')
-
+	v.data = 1;
 	v.cl_id = vs(end);
 	v.inst = 'CIS';
 	v.frame = 'sc';
@@ -394,12 +423,13 @@ elseif regexp(vs,'^NC(h|p)[1-4]$')
 % CIS V PP
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 elseif regexp(vs,'^(di)?VC(h|p)[1-4]$')
+	v.data = 1;
 	v.cl_id = vs(end);
 	v.inst = 'CIS';
 	v.frame = 'sc';
 	if strcmp(vvs(1:2),'di')
 		vvs = vvs(3:end);
-		v.cs = {'vector>dsi_xyz','scalar>na'};
+		v.cs = {'vector>DSI_xyz','scalar>na'};
 	else
 		v.cs = {'vector>gse_xyz','scalar>na'};
 	end
@@ -426,6 +456,7 @@ elseif regexp(vs,'^(di)?VC(h|p)[1-4]$')
 % EDI E PP
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 elseif regexp(vs,'^(i)?(di)?EDI[1-4]$')
+	v.data = 1;
 	if vvs(1)=='i', 
 		vvs = vvs(2:end);
 		v.frame = 'inertial'; 
@@ -438,7 +469,7 @@ elseif regexp(vs,'^(i)?(di)?EDI[1-4]$')
 	v.sen = '';
 	if strcmp(vvs(1:2),'di')
 		vvs = vvs(3:end);
-		v.cs = {'vector>dsi_xyz','scalar>na'};
+		v.cs = {'vector>DSI_xyz','scalar>na'};
 	else
 		v.cs = {'vector>gse_xyz','scalar>na'};
 	end
@@ -457,6 +488,7 @@ elseif regexp(vs,'^(i)?(di)?EDI[1-4]$')
 % FGM B PP
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 elseif regexp(vs,'^(di)?BPP[1-4]$')
+	v.data = 1;
 	v.cl_id = vs(end);
 	v.inst = 'FGM';
 	v.frame = 'sc';
@@ -464,7 +496,7 @@ elseif regexp(vs,'^(di)?BPP[1-4]$')
 	v.sen = '';
 	if strcmp(vvs(1:2),'di')
 		vvs = vvs(3:end);
-		v.cs = {'vector>dsi_xyz','scalar>na'};
+		v.cs = {'vector>DSI_xyz','scalar>na'};
 	else
 		v.cs = {'vector>gse_xyz','scalar>na'};
 	end
@@ -483,6 +515,7 @@ elseif regexp(vs,'^(di)?BPP[1-4]$')
 % FGM B full resolution and resampled
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 elseif regexp(vs,'^(di)?B(r|rs)?[1-4]$')
+	v.data = 1;
 	v.cl_id = vs(end);
 	v.inst = 'FGM';
 	v.frame = 'sc';
@@ -490,7 +523,7 @@ elseif regexp(vs,'^(di)?B(r|rs)?[1-4]$')
 	v.sen = '';
 	if strcmp(vvs(1:2),'di')
 		vvs = vvs(3:end);
-		v.cs = {'vector>dsi_xyz','scalar>na'};
+		v.cs = {'vector>DSI_xyz','scalar>na'};
 	else
 		v.cs = {'vector>gse_xyz','scalar>na'};
 	end
@@ -529,20 +562,25 @@ else
 		%just print out everthing
 		disp(['SC#         : ' v.cl_id ]);
 		disp(['Instrument  : ' v.inst ]);
-		disp(['Ref Frame   : ' v.frame ]);
-		disp(['Signal      : ' v.sig ]);
-		disp(['Sensor      : ' v.sen ]);
+		if v.data
+			disp(['Ref Frame   : ' v.frame ]);
+			disp(['Signal      : ' v.sig ]);
+			disp(['Sensor      : ' v.sen ]);
+		end
 		disp(['Comment     : ' v.com ]);
 		disp(['File        : ' v.file ]);
 		disp(['getData q   : ' v.quant ]);
-		disp(['Size        : ' num2str(v.size) ]);
-		for j=1:length(v.size)
-			disp(['Var Name    : ' v.name{j} ]);
-			disp(['  Labels    : ' v.labels{j} ]);
-			disp(['  Field     : ' v.field_name{j} ]);
-			disp(['  Coord Sys : ' v.cs{j} ]);
-			disp(['  Units     : ' v.units{j} ]);
-			disp(['  SI conv   : ' v.si_conv{j} ]);
+		if v.data
+			disp(['Size        : ' num2str(v.size) ]);
+			for j=1:length(v.size)
+				disp(['Var Name    : ' v.name{j} ]);
+				disp(['  Labels    : ' v.labels{j} ]);
+				disp(['  Field     : ' v.field_name{j} ]);
+				disp(['  Coord Sys : ' v.cs{j} ]);
+				if isfield(v,'rep'), disp(['  Represent : ' v.rep{j} ]); end
+				disp(['  Units     : ' v.units{j} ]);
+				disp(['  SI conv   : ' v.si_conv{j} ]);
+			end
 		end
 	end
 end
