d1f84709aa6b889afac9332a3af66d4b3d835d4c
andris <andris>
2004-12-05 11:27:49 +0000

    possibility to add comment to th ewhole figure

--- av_pl_info.m
+++ av_pl_info.m
@@ -4,13 +4,21 @@ function outhandle=av_pl_info(string,inhandle,position);
 % add information in string to the plot given by handle inhandle
 % output is the handle of the text
 % position - [x y], where [0,1] is the left upper corner
-% varargin - pairs of property and proeprty values for the text
 % Example:
 %   ht=av_pl_info(['c_pl_sc_orientation() ' datestr(now)],gca,[0,1 ]); set(ht,'interpreter','none','FontSize', 10);
 
 if nargin == 1, inhandle=gca;position=[.02 1];end
 if nargin == 2, position=[.02 1];end
-outhandle=text(0,0,string);
-axes(inhandle);
-set(outhandle,'Units','normalized','Position',position,'HorizontalAlignment', 'left','VerticalAlignment', 'bottom','FontSize', 5);
-
+if inhandle ==0, % add to the whole figure if inhandle = 0
+    h0 = gca;
+    h1 = axes('Units','normalized', 'Position',[0 0 1 1], 'Visible','off', ...
+        'Tag','BackgroundAxes', 'HitTest','off');
+    outhandle = text('Units','normalized','FontSize',6,'HorizontalAlignment','left', ...
+        'Position',[0.01 0.97], 'String', string, 'Tag','CreatedText');
+    axes(h0)
+else% add to the given or current axis
+    outhandle=text(0,0,string);
+    axes(inhandle);
+    set(outhandle,'Units','normalized','Position',position, ...
+        'HorizontalAlignment', 'left','VerticalAlignment', 'bottom','FontSize', 5);
+end
