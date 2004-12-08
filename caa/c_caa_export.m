5615b799fed0ef046019981b3f2914284374723d
yuri <yuri>
2004-12-08 08:21:29 +0000

    new script

---
+++ caa/c_caa_export.m
@@ -0,0 +1,12 @@
+function c_caa_export(sc_list)
+
+l1 = {'P1','P2','P3','P4','P12','P32','P34'};
+l2 = {'P','E'};
+
+for j=sc_list
+	for k=1:length(l1), disp(['Level 1 : ' l1{k}]), export2caa(1,l1{k},j), end
+	for lev=2:3, for k=1:length(l2)
+		disp(['Level ' num2str(lev) ' : ' l2{k}])
+		export2caa(lev,l2{k},j)
+	end, end
+end
