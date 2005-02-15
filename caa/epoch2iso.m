698cd57c6ecc4ee10937f47eb478c293bdb61776
yuri <yuri>
2005-02-15 12:18:49 +0000

    add short format

--- caa/epoch2iso.m
+++ caa/epoch2iso.m
@@ -1,17 +1,20 @@
-function out = epoch2iso(t)
-%EPOCH2ISO converts ISDAT epoch to ISO time string
+function out = epoch2iso(t,fmt)
+%EPOCH2ISO   Convert ISDAT epoch to ISO time string
+%
 % ISDAT epoch is the number of seconds since 1-Jan-1970 and
 % ISO time string has a format yyyy-mm-ddTHH:MM:ss.wwwwwwZ
 % as described in the CEF data file syntax
 % http://www.space-plasma.qmul.ac.uk/csds/welcome.html
 %
-% t_iso_s = iso2epoch(t_epoch)
+% t_iso_s = iso2epoch(t_epoch,[fmt])
+%   fmt - 0: long (default), 1: short
 %
 %   See also EPOCH2DATE, ISO2EPOCH, TOEPOCH, FROMEPOCH
 %
 % $Id$
 
 % Copyright 2004 Yuri Khotyaintsev
+if nargin<2, fmt = 0; end
 
 % We need to do all this because DATESTR rounds seconds
 d = fromepoch(t);
@@ -20,6 +23,7 @@ for j=2:5, s1(j-1) = {add_zero(d(:,j),num2str(d(:,j),'%d'))}; end
 
 % Take care about seconds separately
 s2 = add_zero(d(:,6),num2str(d(:,6),'%6f'));
+if fmt, s2 = s2(:,1:6); end
 
 sZ = s2(:,1); sZ(:) = 'Z';
 sT = sZ; sT(:) = 'T';
