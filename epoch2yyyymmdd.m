b490a510436ffc229de254b12a7d8deb4f4d53e4
yuri <yuri>
2005-02-16 14:06:58 +0000

    move from av

---
+++ epoch2yyyymmdd.m
@@ -0,0 +1,11 @@
+function date_string=epoch2yyyymmdd(isdat_epoch)
+%EPOCH2YYYYMMDD   Convert ISDAT epoch to YYYYMMDD
+%
+% date_string=epoch2yyyymmdd(isdat_epoch)
+% epoch2yyyymmdd convert isdat epoch to string YYYYMMDD
+%
+% $Id$
+
+t=fromepoch(isdat_epoch);
+date_string=sprintf('%04d%02d%02d',t(1),t(2),t(3));
+
