95fe045c80e8abb1c526f05d020f4550ce0288e4
yuri <yuri>
2005-02-16 11:01:19 +0000

    Major cleanup

---
+++ irf_cross.m
@@ -0,0 +1,63 @@
+function [z]=irf_cross(x,y,flag)
+%IRF_CROSS   Cross product
+%
+% [z]=irf_cross(x,y)
+% [z]=irf_cross(x,y,flag)
+%
+% returns cross product of x and y
+% x,y, are vectors - [time],x,y,z
+% if necessary linearly interpolates (and/or extrapolates) y to x
+% if x == [x1 x2 x3] (x is single vector with 3 components) then use y time axis
+%
+% if given flag=1 then return only values without time column
+%
+% $Id$
+
+global AV_DEBUG; if isempty(AV_DEBUG), debug=0; else debug=AV_DEBUG;end
+time_axis=[];
+if nargin<3, flag=0;end
+
+if size(x,2) > 3, % if more than 3 columns assume that the first one is time
+  xx=x(:,[2  3 4]); % columns 2 3 4 are x, y, z
+  time_axis=x(:,1);
+elseif size(x,2) == 3,  
+  xx=x;       % if number of columns is 3 assume there is no time
+else,  
+  error('not enough components for x vector');
+end
+
+if size(y,2) > 3, % if more than 3 columns assume that the first one is time
+  yy=y(:,[2  3 4]); 
+  if isempty(time_axis), % if x does not have time axis use the time axis of y (if exist)
+    time_axis=y(:,1);
+  end  
+elseif size(y,2) == 3,  
+  yy=y;
+else,  
+  error('not enough components for y vector'); 
+end
+
+if size(xx,1) == 1 & size(x,2)==3, % if x is single vector WITHOUT TIME use it for all y vectors
+  xx=ones(size(yy,1),1)*xx;
+end
+if size(yy,1) == 1, % if y is single vector CAN BE WITH TIME use it for all x vectors
+  yy=ones(size(xx,1),1)*yy;
+end
+
+if size(xx,1) ~= size(yy,1),  % input vectors not of the same length (and none of them is single vector)
+  if size(x,2)>3 & size(y,2)>3, % then both vectors should have time axis
+    if debug ==1, disp('interpolating y to x in irf_cross(x,y)');end
+    yy=interp1(y(:,1),yy,x(:,1),'linear','extrap');
+  else
+    error('do not know how to interpret input');
+  end   
+end
+
+
+zout=[	xx(:,2).*yy(:,3)-xx(:,3).*yy(:,2), - (xx(:,1).*yy(:,3)-xx(:,3).*yy(:,1)), xx(:,1).*yy(:,2)-xx(:,2).*yy(:,1)];
+
+if flag==1,
+  z=zout;
+else
+  z=[time_axis zout];
+end
---
+++ irf_e_vxb.m
@@ -0,0 +1,42 @@
+function [E]=irf_e_vxb(v,b,flag)
+%IRF_E_VXB   Compute VxB and ExB/B^2
+%
+% [E]=irf_e_vxb(v,b)
+% [V]=irf_e_vxb(e,b,-1)
+%
+% calculates electric field or velocity E=-vxB or v=ExB/B^2
+% v[km/s], B[nT], E[mV/m]
+% v=[t vx vy vz] (t..vz column vectors)
+% b=[t bx by bz]
+% v and b can be at different sampling
+% E is at b sampling
+% V is at e sampling
+%
+% $Id$
+
+global AV_DEBUG;if isempty(AV_DEBUG), debug=0; else debug=AV_DEBUG;end
+
+if (nargin ==3) & (flag == -1),
+ e=v;
+ if size(b,1) ~= size(e,1), 
+  if debug == 1, disp('interpolating b to e');end
+  bb=c_resamp(b,e);b=bb;clear bb; 
+ end
+ v=av_mult_vector_scalar(irf_cross(e,b),[b(:,1) irf_abs(b,1)],-2);
+ v=irf_tappl(v,'*1e3');
+ E=v;
+else,
+  if size(v,1) == 1,    
+    if size(v,2)==3
+      v=[b(1,1) v];
+    elseif size(v,2)<3,
+      error('v has to few components');
+    end
+    v=irf_resamp(v,b); 
+  else,                 
+    b=irf_resamp(b,v);
+  end
+  % estimating E =(v x B) 
+  E=irf_tappl(irf_cross(v,b),'*1e3*1e-9*1e3*(-1)');
+end
+
