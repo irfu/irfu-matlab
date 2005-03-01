7508f54a6df172ad702ff28ff2fbc481b2629053
andris <andris>
2005-03-01 17:09:35 +0000

    en rutin som genererar slumpmässiga tetraedrar med givna L, E, P.

---
+++ c_umu_rotmatr.m
@@ -0,0 +1,20 @@
+function [R] = Rotmatr(alpha, beta, gamma)
+
+% function [R] = Rotmatr(alpha, beta, gamma)
+% 
+%   (alpha, beta, gamma) are angles in radians.
+%
+%  Computes the matrix for rotation through
+%  1. the angle alpha about the z axis to give (x', y', z'),
+%  2. the angle beta  about the y' axis to give (x'', y'', z''),
+%  3. and the angle gamma about the z'' axis to give (x_, y_, z_)
+%  which are the coordinates in the new system.
+
+R = ...
+[ cos(alpha)*cos(beta)*cos(gamma)-sin(alpha)*sin(gamma)  sin(alpha)*cos(beta)*cos(gamma)+cos(alpha)*sin(gamma)  -sin(beta)*cos(gamma); 
+ -cos(alpha)*cos(beta)*sin(gamma)-sin(alpha)*cos(gamma) -sin(alpha)*cos(beta)*sin(gamma)+cos(alpha)*cos(gamma)   sin(beta)*sin(gamma);
+  cos(alpha)*sin(beta)                                   sin(alpha)*sin(beta)                                    cos(beta);           ];
+ 
+
+
+
