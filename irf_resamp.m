b233d0946f7b64747df46a42ea04aa44f493d668
andris <andris>
2005-03-12 08:31:53 +0000

    small improvement in clarity and behaviour

--- irf_resamp.m
+++ irf_resamp.m
@@ -1,11 +1,12 @@
 function out = irf_resamp(x,y,method)
 %IRF_RESAMP   Resample X to the time line of Y
 %
-% if sampling of X is more than two times higher than Y, we average X, 
+% if sampling of X is more than two times higher than Y, we average X,
 % otherwise we interpolate X.
 %
 % out = irf_resamp(x,y,[method])
 % method - method of interpolation 'spline', 'linear' etc. (default 'linear')
+%          if method is given the interpolate independant of sampling
 %
 % See also INTERP1
 %
@@ -15,42 +16,53 @@ function out = irf_resamp(x,y,method)
 %
 
 error(nargchk(2,3,nargin))
+if nargin==3,
+  flag_do='interpolation'; % if method is given do interpolation
+else
+  flag_do='check'; % if no method check if interpolate or average
+end
 
-% guess the sampling frequency
-if min(size(y))==1, t = y(:); % y is only time 
+% construct output time axis
+if min(size(y))==1, t = y(:); % y is only time
 else, t = y(:,1); t = t(:);   % first column of y is time
 end
+ndata = length(t);
 
-if size(x,1) == 1,            % ??????? what this loop does? 
-	out = [t (t*0+1)*x(:,2:end)];
-	return
-end
-		
-ndata = length(t); 
 
-if ndata>1, 
-  sfy = ndata/(t(end) - t(1)); % guess samplings frequency y
-else
-  sfy=Inf;                     % if one output time then do interpolation
+if size(x,1) == 1,            % ??????? what this loop does?
+  out = [t (t*0+1)*x(:,2:end)];
+  return
 end
 
-if length(x(:,1))/(x(end,1) - x(1,1)) > 2*sfy
-	% we average
-	out = zeros(ndata,size(x,2));
-	out(:,1) = t;
-	dt2 = .5/sfy; % half interval
-	for j=1:ndata
-		ii = find(x(:,1) <  t(j) + dt2 & x(:,1) >  t(j) - dt2);
-		if isempty(ii), out(j,2:end) = NaN;
-		else, out(j,2:end) = mean(x(ii,2:end));
-		end
-	end
-else
-	% we interpolate
-	if nargin < 3, method = 'linear'; end
-	
-	% If time series agree, no interpolation is necessary. 
-	if size(x,1)==size(y,1), if x(:,1)==y(:,1), out = x; return, end, end  
-	
-	out = [y(:,1) interp1(x(:,1),x(:,2:end),y(:,1),method,'extrap')];
+if strcmp(flag_do,'check'), % check if interpolation or average
+  if ndata>1, % if more than one output time check sampling frequencies to decide interpolation/average
+    sfy = ndata/(t(end) - t(1)); % guess samplings frequency y
+    if length(x(:,1))/(x(end,1) - x(1,1)) > 2*sfy
+      flag_do='average';
+    else
+      flag_do='interpolation';
+    end
+  else
+    flag_do='interpolation';  % if one output time then do interpolation
+  end
+
+end
+
+if strcmp(flag_do,'average'),
+  out = zeros(ndata,size(x,2));
+  out(:,1) = t;
+  dt2 = .5/sfy; % half interval
+  for j=1:ndata
+    ii = find(x(:,1) <  t(j) + dt2 & x(:,1) >  t(j) - dt2);
+    if isempty(ii), out(j,2:end) = NaN;
+    else, out(j,2:end) = mean(x(ii,2:end));
+    end
+  end
+elseif strcmp(flag_do,'interpolation'),
+  if nargin < 3, method = 'linear'; end
+
+  % If time series agree, no interpolation is necessary.
+  if size(x,1)==size(y,1), if x(:,1)==y(:,1), out = x; return, end, end
+
+  out = [y(:,1) interp1(x(:,1),x(:,2:end),y(:,1),method,'extrap')];
 end
