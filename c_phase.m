6c6f1c68750bf35e07f82f4d3d116fada07b0e5a
yuri <yuri>
2005-03-18 16:10:22 +0000

    remove references to ISDAT check for points outside the time interval which result in NaNs

--- c_phase.m
+++ c_phase.m
@@ -2,27 +2,31 @@ function phase_out = c_phase(t,phase_2)
 %C_PHASE spacecraft phase for give time vector
 %
 % phase_out = c_phase(t,phase_2)
-% phase_out = c_phase(t,ic)
 %
 % Find spacecraft phase for give time vector t
 %
 % t - column vector with time in isdat epoch
 % phase_2 - column vector [time phase_2]
-%   if phase_2 is one number assume that it is sc_number and 
-%   download phase from isdat 'disco:10'
-% phase - column vector [t phase]
+% phase_out - column vector [t phase]
 %
 % $Id$
 
-if nargin < 2, help c_phase;return;end
+error(nargchk(2,2,nargin))
+
+if size(t,1)>1 & size(t,2)>1, error('t must be a vector'), end
 
 t=t(:); % t should be column vector
 
-if prod(size(phase_2))==1, % phase_2 is sc number, download phase_2 from isdat 
-  ic=phase_2;tint=[min(t) max(t)];
-  [t_isdat,ph2]=caa_is_get('disco:10',fromepoch(tint(1)),tint(2)-tint(1),ic,'ephemeris', 'phase_2');
-  phase_2=[t_isdat ph2];
-end  % 
-
-phase_out=[t interp1q(phase_2(:,1),phase_2(:,2),t)];
+% interp1q sets points which are outside the time interval to NaN.
+% We must check for this.
+ii = find(t>=phase_2(1,1) & t<=phase_2(end,1));
+if ~isempty(ii)
+	phase_out = [t(ii) interp1q(phase_2(:,1),phase_2(:,2),t(ii))];
+	n_out = length(t) - ii(end) + ii(1) - 1;
+	if n_out > 0
+		irf_log('proc',['throwing away ' num2str(n_out) ' points'])
+	end
+else 
+	phase_out = [];
+end
 
