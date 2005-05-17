519616d57035bd3f817dad562351f6e098da9497
yuri <yuri>
2005-05-17 14:27:14 +0000

    *** empty log message ***

---
+++ caa/caa_read_version.m
@@ -0,0 +1,38 @@
+function [v,vdate] = caa_read_version(sp)
+%CAA_READ_VERSION  read caa file version
+%
+% [v,vdate] = caa_read_version([sp])
+%
+% $Id$
+
+% Copyright 2005 Yuri Khotyaintsev
+
+if nargin<1, sp=pwd; end
+
+old_pwd = pwd;
+cd(sp);
+if exist('./.version','file')
+	[vers, d] = textread('./.version','%s %s',-1);
+	if nargout==0
+		for j=1:length(vers), disp([vers{j} ' : ' d{j}]), end
+	elseif nargout==1
+		v = str2num(vers{end});
+	elseif nargout==2
+		v = str2num(vers{end});
+		vdate = d{end};
+	else
+		error('wrong number of input arguments')
+	end
+else
+	if nargout==0
+		disp('cannot find .version')
+	elseif nargout==1
+		v = [];
+	elseif nargout==2
+		v = [];
+		vdate = '';
+	else
+		error('wrong number of input arguments')
+	end
+end
+cd(old_pwd)
