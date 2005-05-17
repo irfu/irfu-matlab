1100140e9aa402543025755c55972c92613e03c7
yuri <yuri>
2005-05-17 15:12:21 +0000

    add version

--- caa/caa_deliver.m
+++ caa/caa_deliver.m
@@ -23,9 +23,13 @@ if s~=0, irf_log('save',['problem compressing ' fname]),cd(old_pwd),return, end
 
 irf_log('proc',['uploading ' fname ' to the CAA'])
 [s,w] = unix(...
-	['echo "put ' fname '"| sftp -b - efw@caa.estec.esa.int:/c/data-20/EFW/'],...
+	['echo "put ' fname '"| sftp -b - efw@caa1.estec.esa.int:/c/data-20/EFW/'],...
 	'-echo');
 if s~=0, irf_log('save',['problem uploading ' fname]),cd(old_pwd),return, end
+[s,w] = unix(...
+	['echo "' fname ' ' epoch2iso(date2epoch(now),1) '">>/data/caa/log/delivered.log'],...
+	'-echo');
+if s~=0, irf_log('save','problem updating log '),cd(old_pwd),return, end
 
 irf_log('proc',['moving ' fname ' to storage'])
 [s,w] = unix(['mv ' fname ' /data/caa/delivered'],'-echo');
@@ -34,5 +38,9 @@ if s~=0, irf_log('save',['problem moving ' fname]),cd(old_pwd),return, end
 
 irf_log('proc','cleaning up')
 [s,w] = unix(['rm ' fmask],'-echo');
+
+irf_log('proc','moving summary plots to storage')
+[s,w] = unix('mv *.pdf *.png /data/caa/splots/','-echo');
+if s~=0, irf_log('save','problem moving summary plots'),cd(old_pwd),return, end
 
 cd(old_pwd)
--- caa/caa_export_batch.m
+++ caa/caa_export_batch.m
@@ -21,6 +21,16 @@ end
 l1 = {'P1','P2','P3','P4','P12','P32','P34'};
 l2 = {'P','E'};
 
+v = caa_read_version;
+if isempty(v), v = 0; end
+v = v + 1;
+v_s = num2str(v);
+if v<10, v_s = ['0'  v_s]; end
+[s,w] = unix(...
+	['echo "' v_s ' ' epoch2iso(date2epoch(now),1) '">>.version'],'-echo');
+if s~=0, irf_log('save','problem updating version'),cd(old_pwd),return, end
+disp(['new version is : ' v_s])
+
 for j=sc_list
 	disp(['Cluster ' num2str(j)])
 	for k=1:length(l1)
@@ -28,13 +38,13 @@ for j=sc_list
 		if k<=4, q = caa_q.p(j,1);
 		else, q = caa_q.e(j,1);
 		end
-		caa_export(1,l1{k},j,q) 
+		caa_export(1,l1{k},j,q,v_s) 
 	end
 	for lev=2:3, for k=1:length(l2)
 		disp(['Level ' num2str(lev) ' : ' l2{k}])
 		if k==1, q = caa_q.p(j,lev);
 		else, q = caa_q.e(j,lev);
 		end
-		caa_export(lev,l2{k},j,q)
+		caa_export(lev,l2{k},j,q,v_s)
 	end, end
 end
