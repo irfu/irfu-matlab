f25bdebe161cf2ab45bc70461b86a65433970af8
yuri <yuri>
2005-05-19 10:17:29 +0000

    add more flexibility

--- caa/caa_pl_summary.m
+++ caa/caa_pl_summary.m
@@ -1,23 +1,45 @@
-function caa_pl_summary(sp)
+function caa_pl_summary(sp,op)
 %CAA_PL_SUMMARY  make summary plots of the CAA data
 %
-% caa_pl_summary([sp])
+% caa_pl_summary([sp,options])
 %   sp - storage path
+%   options : 
+%     'noexport' do not export plot imaged (PS,PDF,PNG)
 %
 % $Id$
 
-if nargin<1, sp=pwd; end
+do_export = 1;
+
+if nargin<1, sp = pwd; 
+elseif nargin==1
+	if strcmp(sp,'noexport'), sp = pwd; do_export = 0; end
+elseif nargin==2
+	if strcmp(op,'noexport'), do_export = 0; 
+	else, irf_log('fcal',['unknown option ' op])
+	end
+end
 
 old_pwd = pwd;
 cd(sp);
 
+% Save the screen size
+	sc_s = get(0,'ScreenSize');
+	if sc_s(3)==1600 & sc_s(4)==1200, scrn_size = 2;
+	else, scrn_size = 1;
+	end
+	
 for j=1:4
 	figure(j)
-	set(gcf,'position',[601   167   999   960])
+	if scrn_size==1 ,set(gcf,'position',[91  40 909 640])
+	else, set(gcf,'position',[691   159   909   916])
+	end
 end
 
 c_pl_summary
-c_export_ps
-c_export_png
+
+if do_export
+	c_export_ps
+	c_export_png
+end
 
 cd(old_pwd)
