cb4e0f1dc1682a84041975494673f7f3c16160e1
yuri <yuri>
2005-05-20 16:55:34 +0000

    use scp instead of mv. this way it works better for BM data

--- caa/caa_deliver.m
+++ caa/caa_deliver.m
@@ -14,11 +14,11 @@ cd(sp);
 dname = pwd;
 ii = find(dname=='/');
 
-fname = [dname(ii(end)+1:end) '.tgz'];
+fname = [dname(ii(end)+1:end) '.tgz']; 
 fmask = 'C[1-4]_CP_EFW_L[1-3]_*_V*.cef';
+
 irf_log('proc',['compressing ' fname])
 [s,w] = unix(['tar zcvf ' fname ' ' fmask],'-echo');
-
 if s~=0, irf_log('save',['problem compressing ' fname]),cd(old_pwd),return, end
 
 irf_log('proc',['uploading ' fname ' to the CAA'])
@@ -26,13 +26,14 @@ irf_log('proc',['uploading ' fname ' to the CAA'])
 	['echo "put ' fname '"| sftp -b - efw@caa1.estec.esa.int:/c/data-20/EFW/'],...
 	'-echo');
 if s~=0, irf_log('save',['problem uploading ' fname]),cd(old_pwd),return, end
+
 [s,w] = unix(...
 	['echo "' fname ' ' epoch2iso(date2epoch(now),1) '">>/data/caa/log/delivered.log'],...
 	'-echo');
 if s~=0, irf_log('save','problem updating log '),cd(old_pwd),return, end
 
 irf_log('proc',['moving ' fname ' to storage'])
-[s,w] = unix(['mv ' fname ' /data/caa/delivered'],'-echo');
+[s,w] = unix(['scp ' fname ' yuri@amanda:/data/caa/delivered && rm ' fname],'-echo');
 
 if s~=0, irf_log('save',['problem moving ' fname]),cd(old_pwd),return, end
 
