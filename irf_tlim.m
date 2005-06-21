9663347e60722693448204072095f78cddcd243b
yuri <yuri>
2005-06-21 08:43:12 +0000

    include t1 into the interval

--- irf_tlim.m
+++ irf_tlim.m
@@ -1,15 +1,15 @@
 function y=irf_tlim(x,t1,t2)
-%IRF_TLIM   iFind a subinterval defined by time limits
+%IRF_TLIM   Find a subinterval defined by time limits
 %
-% y=irf_tlim(x,t1,t2)
-% y=irf_tlim(x,[t1 t2])
-% y is part of the x that is within interval t1 < x(:,1) < t2
+% y = irf_tlim(x,t1,t2)
+% y = irf_tlim(x,[t1 t2])
+% y is part of the x that is within interval t1 <= x(:,1) < t2
 %
 % $Id$
 
-if nargin == 2
- t2=t1(2)+1e-7;t1=t1(1)-1e-7;
-end
+error(nargchk(2,3,nargin))
 
-in=find((x(:,1) > t1) & (x(:,1) < t2));
-y=x(in,:);
+if nargin==2, t2 = t1(2) + 1e-7; t1 = t1(1) - 1e-7; end
+
+in = find((x(:,1) >= t1) & (x(:,1) < t2));
+y = x(in,:);
