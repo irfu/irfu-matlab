b15b704846a6590b7d429b736a9a68880187af68
yuri <yuri>
2005-06-23 16:34:51 +0000

    there was more 180Hz data in sep 2001

--- caa/@ClusterDB/getData.m
+++ caa/@ClusterDB/getData.m
@@ -349,7 +349,7 @@ elseif strcmp(quantity,'e') | strcmp(quantity,'eburst')
 		if start_time>toepoch([2001 07 31 00 00 00])&start_time<toepoch([2001 09 02 23 50 00])
 			% all sc run on 180Hz filter in august 2001
 			param='180Hz';
-		elseif start_time>toepoch([2001 09 15 04 30 00])& start_time<toepoch([2001 09 15 06 30 00])
+		elseif start_time>toepoch([2001 09 10 04 21 57.6])& start_time<toepoch([2001 09 15 06 30 00])
 			% this needs to be investigated.... 
 			param='180Hz';
 		elseif start_time>toepoch([2001 07 23 00 00 00])&cl_id==2
