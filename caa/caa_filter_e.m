5ec39de5c12c7b710e3d26711c18fa5addab8cd0
yuri <yuri>
2005-06-30 11:36:18 +0000

    use subintervals to avoid "out of memory" when running in burst mode.

--- caa/caa_filter_e.m
+++ caa/caa_filter_e.m
@@ -7,6 +7,8 @@ function res = caa_filter_e(data,wind)
 %
 % $Id$
 
+MAXDATA = 100000;
+
 if nargin<2, wind=.5; end
 
 ii = find( ~isnan(data(:,2)) );
@@ -16,7 +18,6 @@ nw2 = ceil(sf*wind/2);
 
 % Order data
 ndata = round((data(end,1)-data(1,1))*sf+1);
-t2 = data(1,1) + (ndata-1)/sf;
 
 nkomp = size(data,2)-1;
 value = 1e10;
@@ -28,14 +29,26 @@ E(ind,:) = data(:,2:end);
 E(find(E(:,1)==value),:) = NaN;
 ttt = ones(nw2,nkomp)*NaN;
 E_tmp = [ttt; E; ttt];
+clear ttt
 
-EE = zeros(ndata,2*nw2+1);
-
-for komp = 1:nkomp
-	for k=1:ndata, EE(k,:) = E_tmp(k:k+nw2*2,komp); end
-	m = mean(EE,2);
-	E(:,komp) = E(:,komp) - m;
+n_start = 1;
+if ndata>MAXDATA; n_end = MAXDATA;
+else, n_end = ndata;
 end
+
+EE = zeros(n_end,2*nw2+1);
+while n_start<=ndata
+	for komp = 1:nkomp
+		for k=n_start:n_end, EE(k-n_start+1,:) = E_tmp(k:k+nw2*2,komp); end
+		m = mean(EE(1:n_end-n_start+1,:),2);
+		E(n_start:n_end,komp) = E(n_start:n_end,komp) - m;
+	end
+	if n_end==ndata, break, end
+	n_start = n_end + 1;
+	n_end = n_end + MAXDATA;
+	if n_end>ndata, n_end = ndata; end
+end
+clear EE
 
 res = data;
 res(:,2:end) = E(ind,:);
