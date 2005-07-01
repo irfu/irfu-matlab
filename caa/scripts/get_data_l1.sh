bcd76199b56e58f0e91724b6583dfa39c998be45
yuri <yuri>
2005-07-01 10:19:15 +0000

    many good features

--- caa/scripts/get_data_l1.sh
+++ caa/scripts/get_data_l1.sh
@@ -15,7 +15,7 @@ error()
 
 usage()
 {
-	echo "Usage: $PROGRAM YYYY MM DD [NDAYS]"
+	echo "Usage: $PROGRAM [-h] [-v] [-d|--data] [-nd|--no-data] [-sp|--splot] YYYY MM DD [NDAYS]"
 }
 
 usage_and_exit()
@@ -32,15 +32,50 @@ version()
 get_one_int()
 {
 	echo Processing $start_time dt=$dt sec ...
-	echo "irf_log('log_out','$log_dir/$start_time.log'); caa_get_batch_l1('$start_time',$dt,'$out_dir'); exit" | $MATLAB > $log_dir/$start_time-get_data.log 2>&1	
+	echo "irf_log('log_out','$log_dir/$start_time.log'); caa_get_batch_l1('$start_time',$dt,'$out_dir'); exit" | $MATLAB " -nodisplay" >> $log_dir/$start_time-get_data.log 2>&1	
+}
+do_one_splot()
+{
+	echo Summary plot $start_time dt=$dt sec ...
+	echo "irf_log('log_out','$log_dir/$start_time-splot.log'); caa_pl_summary_l1('$start_time',$dt,'$out_dir/$@','save'); exit" | $MATLAB ' -nosplash' >> $log_dir/$start_time-get_data.log 2>&1	
 }
 
 PROGRAM=`basename $0`
 VERSION=1.0
 MATLABSETUP='TMP=/tmp LD_LIBRARY_PATH=$IS_MAT_LIB:$LD_LIBRARY_PATH'
-MATLAB='/usr/local/matlab/bin/matlab -c 1712@flexlmtmw1.uu.se:1712@flexlmtmw2.uu.se:1712@flexlmtmw3.uu.se -nojvm -nodisplay'
+MATLAB='/usr/local/matlab/bin/matlab -c 1712@flexlmtmw1.uu.se:1712@flexlmtmw2.uu.se:1712@flexlmtmw3.uu.se -nojvm'
 #MATLAB=/bin/cat
 INT_HOURS=3
+data=yes
+splot=no
+while test $# -gt 0
+do
+	case $1 in
+		-d | --data )
+		data=yes
+		;;
+		-nd | --no-data )
+		data=no
+		;;
+		-h | --help | -help | '-?' )
+		usage_and_exit 0
+		;;
+		-sp | --splot )
+		splot=yes
+		;;
+		-v | --version )
+		version
+		exit 0
+		;;
+		-*)
+		error "Unrecognized option $1"
+		;;
+		*)
+		break
+		;;
+	esac
+	shift
+done
 
 if test $# -lt 3
 then
@@ -88,8 +123,21 @@ do
 		fi
 		start_time=$YYYY-$MM-$DD'T'$HOURS':00:00.000Z'
 		dt="$INT_HOURS*60*60"
-
-		get_one_int
+		
+		if test "$data" = "yes"
+		then
+			get_one_int
+		fi
+		if test "$splot" = "yes"
+		then
+			cdir=$YYYY$MM$DD'_'$HOURS'00'
+			if test -d $out_dir/$cdir
+			then
+				do_one_splot $cdir
+			else
+				echo Skipping $start_time
+			fi
+		fi
 
 		DAYS=$(($DAYS+1))
 		HOURS=$(($HOURS+$INT_HOURS))
