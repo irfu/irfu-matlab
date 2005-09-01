9540f1a34bfd359d7bc698a2d52591261b4e7fef
yuri <yuri>
2005-09-01 12:10:33 +0000

    typo

--- caa/caa_export_batch_l1.m
+++ caa/caa_export_batch_l1.m
@@ -15,7 +15,7 @@ l1p = {'P1','P2','P3','P4'};
 l1e = {'P12','P32','P34'};
 
 if ~(exist('./mPR.mat','file') | exist('./mER.mat','file'))
-	c_log('load','no data'), return
+	irf_log('load','no data'), return
 end
 
 v = caa_read_version;
