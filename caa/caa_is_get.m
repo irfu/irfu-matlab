9584acea38da092fce0c4a21aeb0e6df9644c0dd
yuri <yuri>
2005-11-14 16:54:39 +0000

    implement error handling code

--- caa/caa_is_get.m
+++ caa/caa_is_get.m
@@ -1,57 +1,132 @@
 function [t,d] = caa_is_get(db_s,st,dt,cli,ins,sig,sen,cha,par)
-% caa_is_get help wrapper around isGetDataLite
-% [t,d] = caa_is_get(db_s,st,dt,cli,ins,sig,sen,cha,par)
+% CAA_IS_GET  get Cluster data from ISDAT database
 %
+% data = caa_is_get(db_s,st,dt,cl_id,ins,sig,[sen,cha,par])
+%  get data from ISDAT database
+%
+% OUTPUT
+%  data  - data matrix in AV format,first column is time (epoch)
 % INPUT
-%  db_s - string defining dbh data base, if several separate by '|', e.g. 'disco:10|sanna:1'
-%       - if db_s is not string assume that it is handler of dbh connection, see Mat_DbOpen
-%  st   - start time vector, e.g. [2002 2 6 0 0 0]
-%  dt   - time interval in seconds
-%  cli,.. - client, instrument, signal, ...
+%  db_s  - string defining dbh data base
+%  st    - start time vector, e.g. [2002 2 6 0 0 0]
+%  dt    - time interval in seconds
+%  cl_id - Cluster ID 1..4
+%  ins,sig,sen,cha,par - data request parameters
 %
-% Last 3 input arguments can be skipped
+% In case ISDAT server returned DbBAD_INTERNAL (may be caused by the server 
+% crash) program will sleep for some time and retry the request.
 %
-% Eample: [t,mlt]=caa_is_get('disco:10',[2002 2 6 0 0 0],100,4,'ephemeris', 'mlt');
+% Example: 
+%  [t,d] = caa_is_get('disco:10',[2002 2 6 0 0 0],100,4,'ephemeris','mlt');
+%
+% See also isGetDataLite
 %
 % $Id$
-% see also isGetDataLite
 
+% TODO: we may try to open persistent connection when ISDAT will get stable
+% TODO: can be use Mat_DbErrorString
 
+MAXNRET = 2;  % Number of times to retry in case of internal server error
+SLEEPINT = 5; % Number of seconds to sleep when waiting for server restart
+
+error(nargchk(6,9,nargin))
 if nargin < 9, par = ' '; end
 if nargin < 8, cha = ' '; end
 if nargin < 7, sen = ' '; end
 
-if ischar(db_s) % db_s is dbh database list
-  p = tokenize(db_s,'|');
-  db_s_is_database_list=1;
-else            % assume db_s is dbh handler
-  p= 'null';
-  db_s_is_database_list=0;
+% Check imput
+if ~ischar(db_s), error('DB_S must be a string'), end
+if ~isnumeric(st) | length(st) ~= 6
+	error('ST must be a vector [YY MM DD hh mm ss.ss]')
 end
+if ~isnumeric(st), error('DT must be a number'), end
+if ~isnumeric(cli)| cli<0 | cli>4, error('CL_ID must be a number 1..4'), end
+if ~ischar(sig), error('SIG must be a string'), end
+if ~ischar(sen), error('SEN must be a string'), end
+if ~ischar(cha), error('CHA must be a string'), end
+if ~ischar(par), error('PAR must be a string'), end
 
-for i=1:length(p)
-	% reset errors, otherwise try/catch fails
+nret = 0;
+while nret<MAXNRET
+	nret = nret + 1;
+	% Reset errors, otherwise try/catch fails
 	lasterr('')
 	try
-    if db_s_is_database_list,
-  		dbase = Mat_DbOpen(p{i});
-    else % assume db_s is databse handler
-      dbase=db_s;
-    end
-	if ~isempty(lasterr), irf_log('dsrc',lasterr), end
-
-		[t, d] = isGetDataLite(dbase, st, dt, ...
-		'Cluster',num2str(cli),ins,sig,sen,cha,par);
-
-		if db_s_is_database_list,
-      if exist('dbase'), Mat_DbClose(dbase), clear dbase, end
-    end
-
-		if ~isempty(d), return, end
-
+		dbase = Mat_DbOpen(db_s);
 	catch
-		irf_log('dsrc',...
-		'Error getting data from ISDAT server %s: %s', p{i},lasterr)
-		t = []; d = [];
+		error('ISDAT:Mat_DbOpen',['ISDAT : error opening database ' db_s])
 	end
+	
+	[t, d, iserr] = isGetDataLite(dbase, st, dt, ...
+		'Cluster',num2str(cli),ins,sig,sen,cha,par);
+	Mat_DbClose(dbase);
+	
+	% If we got DbBAD_INTERNAL we suppose it was cause by the server crash,
+	% so we sleep for SLEEPINT sec and try again
+	if iserr ~= 15, break, end
+	pause(SLEEPINT)
+end
+
+if iserr>0, error('ISDAT:Mat_DbGetDataLite',['ISDAT : ' is_err2str(iserr)]), end
+if isempty(d), error('ISDAT:Mat_DbGetDataLite','empty return'), end
+
+function res = is_err2str(err_id)
+% Help function to get error message from ISDAT error code
+% Isdat_root/lib/Db/StringFunc.c : DbErrorString
+
+switch err_id
+	case 0  % DbSUCCESS
+		res = 'no error';
+	case 1  % DbBAD_DROP
+		res = 'data drop';
+	case 2  % DbBAD_TIME
+		res = 'no data';
+	case 3  % DbBAD_PROJECT
+		res = 'bad project';
+	case 4  % DbBAD_MEMBER
+		res = 'bad member';
+	case 5  % DbBAD_EOF
+		res = 'end of file';
+	case 6  % DbBAD_INSTRUMENT
+		res = 'bad instrument';
+	case 7  % DbBAD_SENSOR
+		res = 'bad sensor';
+	case 8  % DbBAD_SIGNAL
+		res = 'bad signal';
+	case 9  % DbBAD_CHANNEL
+		res = 'bad channel';
+	case 10 % DbBAD_PARAMETER
+		res = 'bad parameter';
+	case 11 % DbBAD_UNITS
+		res = 'bad units';
+	case 12 % DbBAD_REDUCTION
+		res = 'bad reduction';
+	case 13 % DbBAD_GAPFILL
+		res = 'bad gapfill';
+	case 14 % DbBAD_ALLOC
+		res = 'out of memory';
+	case 15 % DbBAD_INTERNAL
+		res = 'internal error, possible server crash.';
+	case 16 % DbBAD_GAP
+		res = 'data gap';
+	case 17 % DbBAD_ZONE
+		res = 'bad zone (between two samples)';
+	case 21 % DbBAD_REQUEST
+		res = 'bad protocol request';
+	case 22 % DbBAD_VALUE
+		res = 'bad parameter value';
+	case 23 % DbBAD_ACCESS
+		res = 'data access denied';
+	case 24 % DbBAD_LENGTH
+		res = 'protocol length error';
+	case 25 % DbNOT_IMPLEMENTED
+		res = 'not implemented';
+	case 26 % DbBAD_INTERVAL
+		res = 'bad interval';
+	case 28 % DbBAD_MEMLIMIT
+		res = 'memory limit exceeded';
+	case 29 % DbBAD_FILE
+		res = 'file system error, (missing file or wrong permissions).';
+	otherwise
+		res = 'unknown error'
 end
