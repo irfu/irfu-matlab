2ff9e9e7fe8fb8f3f4750cde467fb23778ddde5c
andris <andris>
2005-12-02 20:38:40 +0000

    returns also indexes of points within time interval

--- irf_tlim.m
+++ irf_tlim.m
@@ -1,9 +1,13 @@
-function y=irf_tlim(x,t1,t2)
+function [y,in]=irf_tlim(x,t1,t2)
 %IRF_TLIM   Find a subinterval defined by time limits
+% function [y,in]=irf_tlim(x,t1,t2)
 %
 % y = irf_tlim(x,t1,t2)
 % y = irf_tlim(x,[t1 t2])
 % y is part of the x that is within interval t1 <= x(:,1) < t2
+% 
+% [y,ind]=irf_tlim(x,t1,t2)
+% ind - returns inndexes of rows that were within interval
 %
 % $Id$
 
