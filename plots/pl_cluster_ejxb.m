59055f9100cc9b0fa20ff13e058b7fa5cb3283f5
yuri <yuri>
2005-12-07 13:49:24 +0000

    make it working

--- plots/pl_cluster_ejxb.m
+++ plots/pl_cluster_ejxb.m
@@ -1,5 +1,5 @@
 function [Blnm1,h]=p1052_ar(tint,Vmp,L,Nsign,flagEdotB);
-% function [Blnm]=p1052_ar(tint,Vmp,L,Nsign,flagEdotB);
+% function [Blnm]=pl_cluster_ejxb(tint,Vmp,L,Nsign,flagEdotB);
 %
 % Magnetic field in LMN
 % electric field in comparisoin to jxB
@@ -9,19 +9,26 @@ function [Blnm1,h]=p1052_ar(tint,Vmp,L,Nsign,flagEdotB);
 %
 % Vmp - magnetopause velocity, N=norm(Vmp)
 % L - L vector
-% Nsign - 1 if N in the direction of Vmp, -1 if N in the direction opposite to Vmp (e.g., N shall always point from MSph to Msh)
+% Nsign - 1 if N in the direction of Vmp, -1 if N in the direction 
+%         opposite to Vmp (e.g., N shall always point from MSph to Msh)
 % flagEdotB - 1 if use E.B=o assumption for the 3rd component
 %
-% Usage: [blnm]=p1052(toepoch([2002 03 30 13 11 42])+[0 8],31.4* [ -0.94 -0.21 -0.25],[0 0 1],-1,0);
+% Usage: 
+%   [blnm] = pl_cluster_ejxb(toepoch([2002 03 30 13 11 42])+[0 8],...
+%          31.4* [ -0.94 -0.21 -0.25],[0 0 1],-1,0);
+%
+% $Id$
 
 persistent Vmp_str L_str tint_str sc_list icb  Te 
 
 c_load('B?');
 c_load('diB?');
 c_load('diE?p1234');
+c_load Damp?
+c_load Ddsi?
+c_eval('diE?p1234 = caa_corof_dsi(diE?p1234,Ddsi?,Damp?);')
 %load mE dvE1 dvE2 dvE3 dvE4;
 c_load('P?');
-c_load('NVps?');
 %load mP P1 P2 P3 P4 NVps1 NVps2 NVps3 NVps4;
 
 if nargin <1,
@@ -53,7 +60,6 @@ tint_data=tint+[min(dt) max(dt)];
 c_eval('B?=irf_tlim(irf_abs(B?),tint_data);');
 c_eval('diB?=irf_tlim(diB?,tint_data);');
 c_eval('dvE?=irf_tlim(diE?p1234,tint_data);');
-c_eval('NVps?=irf_tlim(NVps?,tint_data);');
 
 if flag_EdotB==1,
   c_eval('[dvE?,d?]=irf_edb(dvE?,diB?,10);');
@@ -86,24 +92,28 @@ c_eval('Blnm?=irf_newxyz(B?,L,N,M);');
 
 Te=irf_ask('Electron energy Te?[%]>','Te',1);
 
-c_eval('dtn?=NVps?;dtn?(1,2)=0;dtn?(2:end,1)=0.5*(dtn?(2:end,1)+dtn?(1:end-1,1));');
-c_eval('dtn?(2:end,2)=(NVps?(2:end,2)-NVps?(1:end-1,2))./(NVps?(2:end,1)-NVps?(1:end-1,1));');
+c_eval('dtn?=n?;dtn?(1,2)=0;dtn?(2:end,1)=0.5*(dtn?(2:end,1)+dtn?(1:end-1,1));');
+c_eval('dtn?(2:end,2)=(n?(2:end,2)-n?(1:end-1,2))./(n?(2:end,1)-n?(1:end-1,1));');
 c_eval('vn?=irf_resamp([0 v],dtn?);');
-c_eval('egradn?=[dtn?(:,1) Te./irf_abs(vn?,1)./NVps?(:,2).*dtn?(:,2)];');
-c_eval('egradn3d?=irf_vec_x_scal(irf_tappl(irf_norm(vn?),''*(-1)''),egradn?);');
+% Approved formula for -grad(P)/ne = T/(ne)*dtn*Nsign/abs(Vmp)
+c_eval('egradn?=[dtn?(:,1) Nsign*Te./irf_abs(vn?,1)./n?(:,2).*dtn?(:,2)];');
+%c_eval('egradn3d?=irf_vec_x_scal(irf_tappl(irf_norm(vn?),''*(-1)''),egradn?);');
 
 sc_list=irf_ask('For which s/c plot single s/c plots ?[%]>','sc_list',[]);
 
-icb=irf_ask('For which s/c to include internal burst ?[%]>','ic',[]);
+icb=irf_ask('For which s/c to include internal burst ?[%]>','icb',[]);
+
 for j=1:length(icb),
-  sc_id=ic(j);
+  sc_id=icb(j);
   c_load('P4kHz?p4',sc_id);
-  c_eval('nburst?=c_efw_scp2ne(P4kHz?p4);nburst?(1:2,:)=[];nburst?=irf_filt(nburst?,0,180);',sc_id);
-  c_eval('dtn?=nburst?;dtn?(1,2)=0;dtn?(2:end,1)=0.5*(dtn?(2:end,1)+dtn?(1:end-1,1));',sc_id);
-  c_eval('dtn?(2:end,2)=(nburst?(2:end,2)-nburst?(1:end-1,2))./(nburst?(2:end,1)-nburst?(1:end-1,1));',sc_id);
-  c_eval('vn?=irf_resamp([0 v],dtn?);',sc_id);
-  c_eval('egradn_burst?=[dtn?(:,1) Te./irf_abs(vn?,1)./nburst?(:,2).*dtn?(:,2)];',sc_id);
-  c_eval('egradn3d_burst?=irf_vec_x_scal(irf_tappl(irf_norm(vn?),''*(-1)''),egradn_burst?);',sc_id);
+  c_eval('nburst?=c_efw_scp2ne(P4kHz?p4);',sc_id);
+  %c_eval('nburst?(1:2,:)=[];nburst?(:,2)=lowpass(nburst?(:,2),30,9000);',sc_id);
+  %c_eval('dtn?=nburst?;dtn?(1,2)=0;dtn?(2:end,1)=0.5*(dtn?(2:end,1)+dtn?(1:end-1,1));',sc_id);
+  %c_eval('dtn?(2:end,2)=(nburst?(2:end,2)-nburst?(1:end-1,2))./(nburst?(2:end,1)-nburst?(1:end-1,1));',sc_id);
+  %c_eval('vn?=irf_resamp([0 v],dtn?);',sc_id);
+  % Approved formula for -grad(P)/ne
+  %c_eval('egradn_burst?=[dtn?(:,1) Nsign*Te./irf_abs(vn?,1)./nburst?(:,2).*dtn?(:,2)];',sc_id);
+  %c_eval('egradn3d_burst?=irf_vec_x_scal(irf_tappl(irf_norm(vn?),''*(-1)''),egradn_burst?);',sc_id);
 end
 
 
@@ -114,7 +124,7 @@ end
 figure;h=irf_plot({Blnm1,Blnm1,Blnm2,Blnm3,Blnm3,Blnm3,Blnm3});
 
 axes(h(1));
-c_pl_tx(Blnm1,Blnm2,Blnm3,Blnm4,2,1,dt.*0);ylabel('B_L [nT]');
+c_pl_tx('Blnm?',2,dt.*0);ylabel('B_L [nT]');
 ht=irf_pl_info([mfilename '  ' datestr(now) ...
   ' Vmp=' num2str(irf_abs(Vmp,1)) ' [' num2str(Vmp(1:3),' %6.2f') ']km/s,' ...
   ' dt=[' num2str(dt,' %6.2f') ']s,' ...
@@ -126,13 +136,13 @@ for ic=1:4,
 end
 
 axes(h(2));
-c_pl_tx(Blnm1,Blnm2,Blnm3,Blnm4,2,1,dt);ylabel('B_L [nT]');
+c_pl_tx('Blnm?',2,dt);ylabel('B_L [nT]');
 
 axes(h(3));
-c_pl_tx(Blnm1,Blnm2,Blnm3,Blnm4,4,1,dt);ylabel('B_M [nT]');
+c_pl_tx('Blnm?',4,dt);ylabel('B_M [nT]');
 
 axes(h(4));
-c_pl_tx(Blnm1,Blnm2,Blnm3,Blnm4,3,1,dt);ylabel('B_N [nT]');
+c_pl_tx('Blnm?',3,dt);ylabel('B_N [nT]');
 
 axes(h(5));
 %  c_eval('dEnf?=irf_filt(dEn?,0,20,[],5);dEnfw?=irf_add(1,dEn?,-1,dEnf?);');
@@ -147,14 +157,15 @@ hold on;
 c_pl_tx('dejbn?',2,dt,{'.','.','.','.'});
 
 axes(h(6));
-c_pl_tx(dEm2(1,:),dEm2,dEm3,dEm4,2,1,dt);ylabel('E_{tang} [mV/m]');
+c_pl_tx(dEm2(1,:),dEm2,dEm3,dEm4,2,dt);ylabel('E_{tang} [mV/m]');
 %  c_pl_tx(dEm2(1,:),dEnfw2,irf_tappl(dEnfw3,'+50'),irf_tappl(dEnfw4,'+100'),2,1,dt);
 ylabel('E_{M} [mV/m]');
-ht=irf_pl_info(['f=20-180Hz'],gca,[0.05,.8]); set(ht,'interpreter','none','FontSize', 10);
+%sht=irf_pl_info(['f=20-180Hz'],gca,[0.05,.8]); set(ht,'interpreter','none','FontSize', 10);
 
 axes(h(7));
-c_pl_tx(NVps1,NVps2,NVps3,NVps4,2,1,dt);
+c_pl_tx('n?',2,dt);
 ylabel('N_{Vps} [cc]');
+set(h(7),'YScale','log')
 
 lw=1;k=-3:0;
 for j=1:7,
@@ -165,7 +176,7 @@ axis(h,'tight');
 irf_zoom(tint,'x',h);
 add_timeaxis(h);
 
-ht=irf_pl_number_subplots(h);
+%ht=irf_pl_number_subplots(h);
 
 %%%%%%%%%%%%%%  Figure single s/c %%%%%%%%%%%%%%
 %%%%%%%%%%%%%% Separate s/c %%%%%%%%%%%%%%%%%
@@ -175,11 +186,11 @@ for ic=sc_list;
   h=irf_plot({Blnm1,Blnm1,Blnm2,Blnm3});
 
   axes(h(1));
-  if find(ic==icb), % plot also burst data
-    c_eval('irf_plot({dEn?,dejbn?,egradn?,egradn_burst},''comp''); ylabel(''E,jxB/ne,\nabla p/ne\newline [mV/m] sc?'');',ic);
-  else
+  %if find(ic==icb), % plot also burst data
+  %  c_eval('irf_plot({dEn?,dejbn?,egradn?,egradn_burst?},''comp''); ylabel(''E,jxB/ne,\nabla p/ne\newline [mV/m] sc?'');',ic);
+  %else
     c_eval('irf_plot({dEn?,dejbn?,egradn?},''comp''); ylabel(''E,jxB/ne,\nabla p/ne \newline[mV/m] sc?'');',ic);
-  end
+  %end
   %c_eval('irf_plot(Blnm?(:,[1 5])); ylabel(''B [nT] sc?''); ',ic);
   ht=irf_pl_info([mfilename '  ' datestr(now) ...
     ' Vmp=' num2str(irf_abs(Vmp,1)) ' [' num2str(Vmp(1:3),' %6.2f') ']km/s,' ...
@@ -188,23 +199,24 @@ for ic=sc_list;
   leg_coord=[.2,.6];font_size=13;cluster_labels={'C1 ','C2 ','C3 ','C4 '};cluster_colors='krgb';
 
   axes(h(2));
-  c_eval('irf_plot(NVps?(:,[1 2]));',ic);
+  c_eval('irf_plot(n?(:,[1 2]));',ic);
   if find(ic==icb), % plot also burst data
     c_eval('hold on; irf_plot(nburst?);',ic);
   end
   c_eval('ylabel(''N_{Vps} [cc] sc?'');',ic);
+  set(h(2),'YScale','log')
 
   axes(h(3));
   c_eval('irf_plot(Blnm?(:,[1 5])); ylabel(''B [nT] sc?''); ',ic);
 
   axes(h(4));
-  c_eval('irf_plot(irf_tappl([jz? jperp?(:,2)],''*1e6''));ylabel(''j_\|,jperp [\mu A/m2] sc?'');',ic);
+  c_eval('irf_plot(irf_tappl([jz? jperp?(:,2)],''*1e6''));ylabel(''j_{||},jperp [\mu A/m^2] sc?'');',ic);
 
   axis(h,'tight');
   irf_zoom(tint,'x',h);
   add_timeaxis(h);
 
-  ht=irf_pl_number_subplots(h)
+  %ht=irf_pl_number_subplots(h)
 
 end
 %%%%%%%%%%%%%%  Figure 3 %%%%%%%%%%%%%%
@@ -216,8 +228,9 @@ figure;
 h=irf_plot({Blnm1,Blnm1,Blnm2,Blnm3,Blnm3});
 
 axes(h(1));
-c_pl_tx(NVps1,NVps2,NVps3,NVps4,2,1,dt);
+c_pl_tx('n?',2,dt);
 ylabel('N_{Vps} [cc]');
+set(h(1),'YScale','log')
 ht=irf_pl_info([mfilename '  ' datestr(now) ...
   ' Vmp=' num2str(irf_abs(Vmp,1)) ' [' num2str(Vmp(1:3),' %6.2f') ']km/s,' ...
   ' dt=[' num2str(dt,' %6.2f') ']s,' ...
@@ -233,4 +246,4 @@ axis(h,'tight');
 irf_zoom(tint,'x',h);
 add_timeaxis(h);
 
-ht=irf_pl_number_subplots(h)
+%ht=irf_pl_number_subplots(h)
