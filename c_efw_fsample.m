e8d675a0f83e8db90cfa00b38cf81b721595c901
yuri <yuri>
2006-03-24 09:47:33 +0000

    add restrictions

--- c_efw_fsample.m
+++ c_efw_fsample.m
@@ -1,12 +1,17 @@
-function sf = c_efw_fsample(data)
+function sf = c_efw_fsample(data,mode)
 %C_EFW_FSAMPLE  guess sampling frequency
 %
-% fs = c_efw_fsample(data)
+% fs = c_efw_fsample(data,[mode])
 % returns sampling frequency in Hz
+% mode - restrict to particular type of data :
+%        'any' - default 0.25/5/25/450/4500/9000 Hz
+%        'lx'  - LX data 5 Hz
+%        'hx'  - HX data 25/450 Hz
+%        'ib'  - internal burst data 450/4500/9000 Hz
 %
 % $Id$
 
-% Copyright 2005 Yuri Khotyaintsev
+% Copyright 2005,2006 Yuri Khotyaintsev
 
 data = data(:,1);
 
@@ -14,16 +19,21 @@ if length(data)<=1
 	error('cannot compute sampling frequency from less than two points')
 end
 
-sf = guess_fsample((length(data) - 1)/(data(end) - data(1)));
+if nargin <2, mode='any'; end
+if ~(strcmp(mode,'any') | strcmp(mode,'lx') | strcmp(mode,'hx') | strcmp(mode,'ib'))
+	error('bad value for MODE')
+end
+
+sf = guess_fsample((length(data) - 1)/(data(end) - data(1)),mode);
 if sf, return
 else
-	sf = guess_fsample(1/(data(2) - data(1)));
+	sf = guess_fsample(1/(data(2) - data(1)),mode);
 	if sf, return
 	elseif length(data)>2
-		sf = guess_fsample(1/(data(3) - data(2)));
+		sf = guess_fsample(1/(data(3) - data(2)),mode);
 		if sf, return
 		else
-			sf = guess_fsample(1/(data(end) - data(end-1)));
+			sf = guess_fsample(1/(data(end) - data(end-1)),mode);
 			if sf, return, end
 		end
 	end
@@ -32,14 +42,20 @@ end
 if ~sf, irf_log('proc','cannot guess sampling frequency'), end
 return
 
-function sf = guess_fsample(f)
+function sf = guess_fsample(f,mode)
 K_PLUS = 1.1;
 K_MINUS = .9;
 
-if f<K_PLUS*5 & f>K_MINUS*5, sf = 5;          % LX
-elseif f<K_PLUS*25 & f>K_MINUS*25, sf = 25;      % NM
-elseif f<K_PLUS*450 & f>K_MINUS*450, sf = 450;   % BM1
-elseif f<K_PLUS*9000 & f>K_MINUS*9000, sf = 9000;% IB
-elseif f<K_PLUS*.25 & f>K_MINUS*.25, sf = .25;   % SPIN
+if f<K_PLUS*5 & f>K_MINUS*5 & (strcmp(mode,'any')|strcmp(mode,'lx'))
+	sf = 5;     % LX
+elseif f<K_PLUS*25 & f>K_MINUS*25 & (strcmp(mode,'any')|strcmp(mode,'hx'))
+	sf = 25;    % NM
+elseif f<K_PLUS*450 & f>K_MINUS*450 & (strcmp(mode,'any')|strcmp(mode,'hx')|strcmp(mode,'ib'))
+	sf = 450;   % BM1/IB
+elseif f<K_PLUS*4500 & f>K_MINUS*4500 & (strcmp(mode,'any')|strcmp(mode,'ib'))
+	sf = 4500;  % IB
+elseif f<K_PLUS*9000 & f>K_MINUS*9000 & (strcmp(mode,'any')|strcmp(mode,'ib'))
+	sf = 9000;  % IB
+elseif f<K_PLUS*.25 & f>K_MINUS*.25 & strcmp(mode,'any'), sf = .25;   % SPIN
 else, sf = 0;
 end
