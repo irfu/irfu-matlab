d42961aa45f827b7893b9809be392a1c69771dec
andris <andris>
2006-08-29 10:43:53 +0000

    required by lp_ routines and can be usefull in general

---
+++ NaturalConstants.m
@@ -0,0 +1,24 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% NaturalConstants
+%
+%   Some constants of nature is practical to have as globals.
+%
+%   Created by Jan-Erik Wahlund, Cornell University, October-94.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% First some true constants.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  global qe kb mH me eps0 mu0 c_light R_Earth
+
+  qe    = 1.60219e-19;
+  kb    = 1.3807e-23;
+  mH    = 1.67265e-27;
+  me    = 9.10953e-31;
+  eps0  = 8.85418782e-12;
+  mu0   = 4*pi*1e-7;
+
+  c_light = 3e8;
+  R_Earth = 6370e3; 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
