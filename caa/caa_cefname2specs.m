a4b1e516f4df17cfd5c7b20aa23634516520083f
yuri <yuri>
2006-08-31 15:10:43 +0000

    new functions to help file reexport

---
+++ caa/caa_cefname2specs.m
@@ -0,0 +1,41 @@
+function [data_level,caa_vs,cl_id,DATA_VERSION,sp,st,dt]=caa_cefname2specs(s)
+%CAA_CEFNAME2SPECS  extract details from a CEF file name
+%
+% [data_level,caa_vs,cl_id,DATA_VERSION,sp,st,dt]=caa_cefname2specs(cef_fname)
+%
+% See also CAA_EXPORT
+%
+% $Id$
+
+cl_id = str2num(s(2));
+data_level = str2num(s(12));
+
+s = s(14:end);
+if s(2)=='_', caa_vs = s(1); s = s(4:end);
+elseif s(3)=='_', caa_vs = s(1:2); s = s(5:end);
+else caa_vs = s(1:3); s = s(6:end);
+end
+
+YY = s(1:4);
+MM = s(5:6);
+DD = s(7:8);
+st_hh = s(10:11);
+st_mm = s(12:13);
+st_ss = s(14:15);
+et_hh = s(17:18);
+et_mm = s(19:20);
+et_ss = s(21:22);
+DATA_VERSION = s(25:26);
+
+h3 = num2str( fix( str2num(st_hh)/3 )*3 );
+if length(h3) == 1, h3 = [ '0' h3 ]; end
+
+sp = [ '/data/caa/l1/' YY '/' YY MM DD '_' h3 '00/C' num2str(cl_id) '/' ...
+	YY MM DD '_' st_hh st_mm ];
+
+st = iso2epoch([ YY '-' MM '-' DD 'T' st_hh ':' st_mm ':' st_ss 'Z' ]);
+et = iso2epoch([ YY '-' MM '-' DD 'T' et_hh ':' et_mm ':' et_ss 'Z' ]);
+if str2num(et_hh)==0 && str2num(et_mm)==0 && str2num(et_ss)==0
+	et = et + 86400;
+end
+dt = et - st;
---
+++ caa/caa_reexport.m
@@ -0,0 +1,17 @@
+function caa_reexport(s,QUALITY)
+%CAA_REEXPORT  ReExport CEF file
+%
+% caa_reexport(caa_fname,[QUALITY])
+%
+% See also CAA_EXPORT, CAA_CEFNAME2SPECS
+%
+% $Id$
+
+if nargin<2, QUALITY = 3; end
+[data_level,caa_vs,cl_id,DATA_VERSION,sp,st,dt] = caa_cefname2specs(s);
+
+disp(['ReExport ' caa_vs ' L' num2str(data_level) ' C' num2str(cl_id) ...
+	' V' DATA_VERSION ' Q=' num2str(QUALITY) ' : '...
+	epoch2iso(st,1) '--' epoch2iso(st+dt,1) ])
+
+caa_export(data_level,caa_vs,cl_id,QUALITY,DATA_VERSION,sp,st,dt)
