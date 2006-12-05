2eadfa0d03b9e70f15fce0594436f4995fcdcc95
yuri <yuri>
2006-12-05 14:54:47 +0000

    fix a severe bug causing phase error

--- c_efw_sfit.m
+++ c_efw_sfit.m
@@ -132,7 +132,7 @@ for i=1:n
 		if method==1
 			% Use Fortran version of spin fit
 			[bad,x(i-n_gap,:),spinfit(i-n_gap,6),spinfit(i-n_gap,7),lim] = ...
-				c_efw_spinfit_mx(fnterms,maxit,2*pi/4.0,te(eind)-te(eind(1)),data(eind));
+				c_efw_spinfit_mx(fnterms,maxit,2*pi/4.0,te(eind)-t0,data(eind));
 			tsfit = t0 + 2;	
 			pol = polyfit(tpha(pind),pha(pind),1);
 			phi(i-n_gap) = polyval(pol,tsfit);
