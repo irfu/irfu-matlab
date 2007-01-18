cb20c680a9db036ec114cc0ab8bc463e3811b64e
yuri <yuri>
2007-01-18 13:06:34 +0000

    M-lint reformat help

--- caa/caa_rm_blankt.m
+++ caa/caa_rm_blankt.m
@@ -1,17 +1,18 @@
 function res = caa_rm_blankt(data,time_int)
-%caa_rm_blankt remove time intervals from data (sets to NaN)
+%CAA_RM_BLANKT  remove time intervals from data (sets to NaN)
+%
 % res = caa_rm_blankt(data,time_int)
+%
 % Input:
-% data - Cluster AV format
-% time_int - time intervals (ISDAT epoch)
+%   data     - Cluster AV format
+%   time_int - time intervals (ISDAT epoch)
 % 
 % Example:
-% P_NO_WHI=caa_rm_blankt(P1,WHIP1);
+%   P_NO_WHI = caa_rm_blankt(P1,WHIP1);
 %
 % $Id$
-%
 
-% Copyright 2004 Yuri Khotyaintsev
+% Copyright 2004-2007 Yuri Khotyaintsev
 
 error(nargchk(2,2,nargin))
 
@@ -19,5 +20,5 @@ res = data;
 if isempty(data), return, end
 
 for j=1:size(time_int,1)
-	res(find(data(:,1)>=time_int(j,1) & data(:,1)<=time_int(j,2)),2:end) = NaN;
+	res( data(:,1)>=time_int(j,1) & data(:,1)<=time_int(j,2) ,2:end) = NaN;
 end
