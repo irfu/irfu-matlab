97c164880cd135f6f817babec60993c30b1ab776
andris <andris>
2007-03-12 20:51:54 +0000

    improved help

--- NaturalConstants.m
+++ NaturalConstants.m
@@ -6,6 +6,15 @@
 %
 %   Created by Jan-Erik Wahlund, Cornell University, October-94.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%  qe    = 1.60219e-19;
+%  kb    = 1.3807e-23;
+%  mH    = 1.67265e-27;
+%  me    = 9.10953e-31;
+%  eps0  = 8.85418782e-12;
+%  mu0   = 4*pi*1e-7;
+%
+%  c_light = 3e8;
+%  R_Earth = 6370e3; 
 
 % First some true constants.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
