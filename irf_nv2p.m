bfe655aa50fa2a712c931083972fbf9f9a5ee852
yuri <yuri>
2007-06-12 12:44:19 +0000

    *** empty log message ***

---
+++ irf_nv2p.m
@@ -0,0 +1,15 @@
+function res = nv2press(n,v2)
+%NV2PRESS  Calculate plasma dynamic pressure
+%
+% res = nv2press(n,v2)
+%
+% Calculate plasma dynamic pressure in nPa
+% n in 1/cc
+% v^2 in [km/s]^2
+%
+% $Id$
+
+n = n(:);
+v2 = v2(:);
+% p=nmv^2 ;-)
+res = 1.6726*1e-6*v2.*n;
