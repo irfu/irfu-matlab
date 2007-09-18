1e03f2efe883976e5625c6dfed1ebdc58b869a2b
andris <andris>
2007-09-18 19:40:10 +0000

    *** empty log message ***

--- c_rapid_sector_pitch_angle.m
+++ c_rapid_sector_pitch_angle.m
@@ -16,9 +16,8 @@ phase=mod(phase(:,2)*180/pi,360); % take away time column
 
 phase_rapid=phase/180*pi + 60.167/180*pi; % rapid phase   
 
-polar_angles=[10 30 50 70 90 110 130 150 170]*pi/180;
+polar_angles=[170:-20:10]*pi/180;
 sector_numbers=1:9;
-
 
 c_eval('sector_?_dsi=[t sin(polar_angles(?))*cos(phase_rapid) sin(polar_angles(?))*sin(phase_rapid) cos(polar_angles(?)*ones(size(t)))];',1:9);
 c_eval('sector_?_dsi=irf_resamp(sector_?_dsi,diB);',1:9);
