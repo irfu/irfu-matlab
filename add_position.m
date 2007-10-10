0ae84bdd60a20163ab4c90ca0f3738f650b48d72
yuri <yuri>
2007-10-10 08:08:44 +0000

    new func to add position labels

---
+++ add_position.m
@@ -0,0 +1,18 @@
+function add_position(h,r)
+%ADD_POSITION  add SC position to a plot
+%
+% ADD_POSITION(H,R)  Add position labels to axis with handle H
+%
+%   See also ADD_TIMEAXIS
+%
+% $Id$
+
+error(nargchk(2,2,nargin))
+
+if ~ishandle(h), error('H is not an axis handle'), end
+if isempty(r), irf_log('func','empty position'), return, end 
+if size(r,2)~=4, error('R has bad size'), end
+
+r = irf_abs(r);
+add_timeaxis(h,'usefig',[r(:,1) r(:,2:end)/6371.2],...
+	{'X [Re]','Y [Re]','Z [Re]','R [Re]'})
