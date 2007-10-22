984875af7a19623e3eab1d3f93f58fae52f2daf4
andris <andris>
2007-10-22 07:03:02 +0000

    make work like irf_ssub

--- c_eval.m
+++ c_eval.m
@@ -1,17 +1,21 @@
-function c_eval(ev_str,sc_list)
+function c_eval(ev_str,sc_list_1,sc_list_2)
 %C_EVAL evaluate expression for list of spacecraft
 %
-% c_eval(ev_str,[sc_list])
+% c_eval(ev_str,[sc_list_1],[sc_list_2])
 %
 % Input:
 % ev_str - string to evaluate.
-% '?' sign in ev_str is replaced by SC number from sc_list
+% '?' sign in ev_str is replaced by SC number from sc_list_1
+% '!' sign in ev_str is replaced by SC number from sc_list_2
 % sc_list - list of SC [optional], 1:4 is assumed when not given
 % 
 % Example:
 % c_eval('R?=r?;C?=R?.^2;',2:4)
-%
 % is the same as R2=r2;C2=R2.^2;R3=r3;C3=R3.^2;...
+% 
+% c_eval('r!r?=irf_abs(r!r?);');
+% is the same as
+% r1r1=irf_abs(r1r1);r1r2=irf_abs(r1r2);...r4r4=irf_abs(r4r4);
 %
 % See also IRF_SSUB, EVALIN
 %
@@ -19,8 +23,26 @@ function c_eval(ev_str,sc_list)
 
 % Copyright 2004 Yuri Khotyaintsev
 
-error(nargchk(1,2,nargin))
+error(nargchk(1,3,nargin))
 
-if nargin<2, sc_list=1:4; end
+if nargin==0,
+    help c_eval;
+elseif nargin==1,
+    sc_list_1=1:4;
+    sc_list_2=1:4;
+elseif nargin==2,
+    sc_list_2=1:4;
+end
 
-for cl_id=sc_list, evalin('caller', irf_ssub(ev_str, cl_id)), end
+if findstr(ev_str,'?'),
+    if findstr(ev_str,'!'),
+        for num1=sc_list_1, 
+            for num2=sc_list_2,
+                evalin('caller', irf_ssub(ev_str, num1,num2)), 
+            end
+        end
+    else,
+        for cl_id=sc_list_1, evalin('caller', irf_ssub(ev_str, cl_id)), end
+    end
+end
+
