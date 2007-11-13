428304e5871079cf8938473abf8f31b17372748f
yuri <yuri>
2007-11-13 15:25:10 +0000

    Added better algoritm for sampling frequency. Update license

--- irf_resamp.m
+++ irf_resamp.m
@@ -12,14 +12,18 @@ function out = irf_resamp(x,y,method)
 %
 % $Id$
 
-% Copyright 2004, 2007 Yuri Khotyaintsev (yuri@irfu.se)
-%
+% ----------------------------------------------------------------------------
+% "THE BEER-WARE LICENSE" (Revision 42):
+% <yuri@irfu.se> wrote this file.  As long as you retain this notice you
+% can do whatever you want with this stuff. If we meet some day, and you think
+% this stuff is worth it, you can buy me a beer in return.   Yuri Khotyaintsev
+% ----------------------------------------------------------------------------
 
 error(nargchk(2,3,nargin))
 if nargin==3,
-  flag_do='interpolation'; % if method is given do interpolation
+	flag_do='interpolation'; % if method is given do interpolation
 else
-  flag_do='check'; % if no method check if interpolate or average
+	flag_do='check'; % if no method check if interpolate or average
 end
 
 % return in case inputs are empty
@@ -39,24 +43,46 @@ if size(x,1) == 1,            % if X has only one point, this is a trivial
   return
 end
 
-if strcmp(flag_do,'check'), % check if interpolation or average
-  if ndata>1, % if more than one output time check sampling frequencies 
-              % to decide interpolation/average
-    sfy = ndata/(t(end) - t(1)); % guess samplings frequency y
-    if length(x(:,1))/(x(end,1) - x(1,1)) > 2*sfy
-      flag_do='average';
-    else
-      flag_do='interpolation';
-    end
-  else
-    flag_do='interpolation';  % if one output time then do interpolation
-  end
+if strcmp(flag_do,'check'), % Check if interpolation or average
+	if ndata>1 
+		% If more than one output time check sampling frequencies
+		% to decide interpolation/average
+		
+		% Guess samplings frequency y
+		sfy1 = 1/(t(2) - t(1));
+		if ndata==2, sfy = fsy1;
+		else
+			not_found = 1; cur = 3; MAXTRY = 10;
+			while (not_found && cur<=ndata && cur-3<MAXTRY)
+				sfy = 1/(t(cur) - t(cur-1));
+				if abs(sfy-sfy1)<sfy*0.001
+					not_found = 0;
+					sfy = (sfy+sfy1)/2;
+					break
+				end
+				cur = cur + 1;
+			end
+			if not_found
+				sfy = fsy1;
+				irf_log('proc',	sprintf(...
+					'Cannot guess sampling frequency. Tried %d times',MAXTRY));
+			end
+		end
+		clear sfy1
+		if length(x(:,1))/(x(end,1) - x(1,1)) > 2*sfy
+			flag_do='average';
+		else
+			flag_do='interpolation';
+		end
+	else
+		flag_do='interpolation';  % If one output time then do interpolation
+	end
 end
 
 if strcmp(flag_do,'average'),
   out = zeros(ndata,size(x,2));
   out(:,1) = t;
-  dt2 = .5/sfy; % half interval
+  dt2 = .5/sfy; % Half interval
   for j=1:ndata
     ii = find(x(:,1) <  t(j) + dt2 & x(:,1) >  t(j) - dt2);
     if isempty(ii), out(j,2:end) = NaN;
