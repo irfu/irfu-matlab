61af4e28a70e419297e8a91f58a99561e432f593
yuri <yuri>
2007-11-14 09:34:42 +0000

    remove unnecesasry prints

--- irf_average_mx.c
+++ irf_average_mx.c
@@ -1,20 +1,23 @@
 /*
- * Mex function to do averages
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <yuri@irfu.se> wrote this file.  As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return.   Yuri Khotyaintsev
+ * ----------------------------------------------------------------------------
+ *
+ * irf_average_mx.c  MEX function to do averages
  *
  * RES = IRF_AVERAGE_MX(X, Y, DT2);
  *
  * Resample X to timeline of Y, using half-window of DT2
  *
+ * Compile with:
+ *   mex -v irf_average_mx.c CFLAGS='$CFLAGS -O2 -mtune=opteron -funroll-loops'
+ *
  * $Id$
- 
-----------------------------------------------------------------------------
-"THE BEER-WARE LICENSE" (Revision 42):
-<yuri@irfu.se> wrote this file.  As long as you retain this notice you
-can do whatever you want with this stuff. If we meet some day, and you think
-this stuff is worth it, you can buy me a beer in return.   Yuri Khotyaintsev
-----------------------------------------------------------------------------
-
  */
+
 #include <limits.h>
 #include "mex.h"
 
@@ -30,15 +33,12 @@ void mexFunction(
     
     /* Check for proper number of input and output arguments */    
     if ( nrhs != 3 )
-	{
 		mexErrMsgTxt("Three input arguments required.");
-    } 
+
     if ( nlhs > 1 )
-	{
 		mexErrMsgTxt("Too many output arguments.");
-    }
-    
-    /* Check data type of input argument  */
+
+	/* Check data type of input argument  */
     if ( !(mxIsDouble(prhs[0])) || !(mxIsDouble(prhs[1])) || 
 			!(mxIsDouble(prhs[2])) )
 	{
@@ -46,27 +46,19 @@ void mexFunction(
     }	
 
     if( mxIsEmpty(prhs[0]) ) 
-	{
 		mexErrMsgTxt("First input argument is empty\n");
-    }
-	
+
 	if( mxGetNumberOfDimensions(prhs[0]) != 2 )
-	{
 		mexErrMsgTxt("First input arguments must be a 2D matrix.");
-	}
-	
-	if(mxIsEmpty(prhs[1])) {
+
+	if(mxIsEmpty(prhs[1]))
 		mexErrMsgTxt("Second input argument is empty");
-    }
-	
+
 	if( mxGetNumberOfDimensions(prhs[1]) != 2 )
-	{
 		mexErrMsgTxt("Second input arguments must be a 2D matrix.");
-	}
 	
-	if(mxIsEmpty(prhs[2])) {
+	if(mxIsEmpty(prhs[2]))
 		mexErrMsgTxt("Third input argument is empty");
-    }
 
     data = mxGetPr(prhs[0]);
     ndata = mxGetM(prhs[0]);
@@ -78,19 +70,15 @@ void mexFunction(
     /* Create output array */
     plhs[0] = mxCreateDoubleMatrix(ntref,ncomp,0);
     res = mxGetPr(plhs[0]);
-    for (i=0; i < ntref; i++) res[i] = tref[i];
-	
-	/*
-	for (i=0;i<3;i++)
-		printf("[%d] data: %g  tref : %g  res : %g\n",i,data[i],tref[i],res[i]);
-	return;
-	 */
+    for (i=0; i < ntref; i++)
+		res[i] = tref[i];
 	
 	/* check is there is a total interval mismatch */
 	if ( (data[0] > res[ntref-1] + dt2) || (data[ndata-1] <= res[0] - dt2) )
 	{
 		mexWarnMsgTxt("interval mismatch\n");
-		for (i=0; i<ndata*ncomp; i++) res[i] = NaN;
+		for (i=0; i<ndata*ncomp; i++) 
+			res[i] = NaN;
 		return;
 	}
 	
@@ -98,35 +86,29 @@ void mexFunction(
 	{
 		/* check if we have been through all the data 
 		 * or that the data starts after the current interval */
+		while ( (start<ndata) && (data[start] < res[i] - dt2) ) 
+			start++;
+		
 		/*
-		printf("interval(%d) -> res[i] + dt2: %g\n",i,res[i] + dt2-res[0]);
-		printf("interval(%d) -> start: %d\n",i,start);
-		 */
-		while ( (start<ndata) && (data[start] < res[i] - dt2) ) start++;
 		if ( start > stop )
 			printf("interval(%d) : skipping %d points\n",i,start-stop);
-		/*
-		printf("interval(%d) -> start_up: %d\n",i,start);
-		 */
-		if ( (start==ndata) || (data[start] > res[i] + dt2) )
-		{
-			for (comp=1; comp<ncomp; comp++) res[i+comp*ntref] = NaN;
-			printf("interval(%d) : no data\n",i);
-			continue;
-		}
-		/*
-		printf("interval(%d) -> data[start]: %g\n",i,data[start]-res[0]);
 		 */
 		
+		if ( (start==ndata) || (data[start] > res[i] + dt2) )
+		{
+			for (comp=1; comp<ncomp; comp++) 
+				res[i+comp*ntref] = NaN;
+			/*
+			printf("interval(%d) : no data\n",i);
+			 */
+			continue;
+		}
 		
 		for (comp=1; comp<ncomp; comp++)
 		{
 			mwSize cur = start, nav = 0;
 			while ( (data[cur] <= res[i] + dt2) && (cur < ndata) )
 			{
-				/*
-				printf("[%d] -> data[cur]: %g\n",cur-start,data[cur]-res[0]);
-				 */
 				if ( mxIsNaN(data[cur+comp*ndata]) )
 				{
 					res[i+comp*ntref] = NaN;
@@ -135,23 +117,18 @@ void mexFunction(
 				}
 				else
 				{
-					/*
-					printf("   [%d]  %g + %g -> ",nav,
-						res[i+comp*ntref],data[cur+comp*ndata]);
-					 */
 					res[i+comp*ntref] += data[cur+comp*ndata];
 					nav++;
-					/*
-					printf("%g\n",res[i+comp*ntref]);
-					 */
 				}
 				cur++;
 			}
-			if ( nav ) res[i+comp*ntref] = res[i+comp*ntref]/(double)nav;
+			if ( nav )
+				res[i+comp*ntref] = res[i+comp*ntref]/(double)nav;
 			
-			if ( cur>stop ) stop = cur;
+			if ( cur>stop )
+				stop = cur;
 		}
-		
 		start = stop;
+		
 	}
 }
