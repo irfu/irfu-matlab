54e15d3a239d81bbb9d83e32ca263b427fd65863
yuri <yuri>
2007-12-19 14:50:39 +0000

    hide plotting

--- c_efw_lobewake.m
+++ c_efw_lobewake.m
@@ -1,7 +1,7 @@
-function c_efw_lobewake(cl_id)
+function res = c_efw_lobewake(cl_id,diE,diB,Ps)
 %C_EFW_LOBEWAKE  detect lobe wakes
 %
-% c_efw_lobewake(cl_id)
+% wake = c_efw_lobewake(cl_id,[diEs,diBrs,Ps])
 %
 % $Id$
 
@@ -24,17 +24,21 @@ EPERP_RATIO_LIM = 1.8; % Ratio between full Eperp (E*B=0) and
                        % E along the projecttion of B to spin plane
 SCPOT_LIM = -5; % Limit for spacecraft potential
 
+DOPLOT = 0;
+
 % Load data
-pp = caa_sfit_probe(cl_id);
-[ok,diE] = c_load(sprintf('diEs%dp%d',cl_id,pp));
-if ~ok, error('cannot load E'), end
-[Ddsi,Damp] = c_efw_dsi_off(diE(1,1),cl_id);
-diE = caa_corof_dsi(diE,Ddsi,Damp);
- 
-[ok,diB] = c_load('diBPP?',cl_id);
-if ~ok, error('cannot load B'), end
-[ok,Ps] = c_load('Ps?',cl_id);
-[ok,diEDI] = c_load('diEDI?',cl_id);
+probe_p = caa_sfit_probe(cl_id);
+if nargin==1
+	[ok,diE] = c_load(sprintf('diEs%dp%d',cl_id,probe_p));
+	if ~ok, error('cannot load E'), end
+
+	[ok,diB] = c_load('diBrs?',cl_id);
+	if ~ok, error('cannot load B'), end
+	[ok,Ps] = c_load('Ps?',cl_id);
+	if ~ok, error('cannot load P'), end
+	[ok,diEDI] = c_load('diEDI?',cl_id);
+	if ~ok, disp('cannot load EDI'), end
+end
 
 ndata = ceil((diE(end,1) - diE(1,1))/TAV);
 t = diE(1,1) + (1:ndata)*TAV - TAV/2; t = t';
@@ -44,39 +48,40 @@ Psr = irf_resamp(Ps,t,'fsample',1/TAV);
 diBr = irf_resamp(diB,t,'fsample',1/TAV);
 bele = (180/pi)*asin(...
 	diBr(:,4)./sqrt(diBr(:,2).^2 + diBr(:,3).^2 + diBr(:,4).^2) );
-
-nplots = 6;
-h = 1:nplots;
-h(1) = irf_subplot(nplots,1,-1);
-irf_plot(Ps);
-ylabel('Sc pot [-V]')
-st_iso = epoch2iso(diE(end,1));
-title(['Cluster ' num2str(cl_id) ', ' st_iso(1:10)])
-h(2) = irf_subplot(nplots,1,-2);
-irf_plot(diE(:,[1 2]));
-set(gca,'YLim',[-9 9])
-ylabel('Ex [mV/m]')
-h(3) = irf_subplot(nplots,1,-3);
-irf_plot(diE(:,[1 3]));
-set(gca,'YLim',[-9 9])
-ylabel('Ey [mV/m]')
-h(4) = irf_subplot(nplots,1,-4);
-if ok
-	diEDI = diEDI(~isnan(diEDI(:,2)),:);
-	diEDIr = irf_resamp(diEDI,t,'fsample',1/TAV,'thresh',1.3);
-	axes(h(2)), hold on
-	irf_plot(diEDIr(:,[1 2]),'k.');
-	axes(h(3)), hold on
-	irf_plot(diEDIr(:,[1 3]),'k.');
-	diEDIr = diEDIr(:,1:3);
-	diEDIr(:,2:3) = diEr(:,2:3) - diEDIr(:,2:3);
-	axes(h(4))
-	irf_plot(diEDIr)
-	ylabel('E diff [mV/m]')
+if DOPLOT
+	nplots = 6;
+	h = 1:nplots;
+	h(1) = irf_subplot(nplots,1,-1);
+	irf_plot(Ps);
+	ylabel('Sc pot [-V]')
+	st_iso = epoch2iso(diE(end,1));
+	title(['Cluster ' num2str(cl_id) ', ' st_iso(1:10)])
+	h(2) = irf_subplot(nplots,1,-2);
+	irf_plot(diE(:,[1 2]));
+	set(gca,'YLim',[-9 9])
+	ylabel('Ex [mV/m]')
+	h(3) = irf_subplot(nplots,1,-3);
+	irf_plot(diE(:,[1 3]));
+	set(gca,'YLim',[-9 9])
+	ylabel('Ey [mV/m]')
+	h(4) = irf_subplot(nplots,1,-4);
+	if ok
+		diEDI = diEDI(~isnan(diEDI(:,2)),:);
+		diEDIr = irf_resamp(diEDI,t,'fsample',1/TAV,'thresh',1.3);
+		axes(h(2)), hold on
+		irf_plot(diEDIr(:,[1 2]),'k.');
+		axes(h(3)), hold on
+		irf_plot(diEDIr(:,[1 3]),'k.');
+		diEDIr = diEDIr(:,1:3);
+		diEDIr(:,2:3) = diEr(:,2:3) - diEDIr(:,2:3);
+		axes(h(4))
+		irf_plot(diEDIr)
+		ylabel('E diff [mV/m]')
+	end
+	h(5) = irf_subplot(nplots,1,-5);
+	irf_plot([diEr(:,1) bele]);
+	ylabel('\theta [deg]')
 end
-h(5) = irf_subplot(nplots,1,-5);
-irf_plot([diEr(:,1) bele]);
-ylabel('\theta [deg]')
 
 % Look for strong apparent Epar if B field is within 15 deg of spin plane:
 ind = find( abs(bele) < ANG_LIM & Psr(:,2) < SCPOT_LIM);
@@ -87,7 +92,7 @@ Eperp = abs( diEr(:,2).*diBr(:,3) - diEr(:,3).*diBr(:,2) )...
 wind = ind(abs(Epar(ind)) > EPAR_LIM & abs(Eperp(ind)) > EPERP_LIM &...
 	abs(Epar(ind)./Eperp(ind)) > EPAR_EPERP_RATIO_LIM);
 
-if ~isempty(wind)
+if DOPLOT && ~isempty(wind)
 	axes(h(2)), hold on
 	irf_plot(diEr(wind,[1 2]),'g.');
 	axes(h(3)), hold on
@@ -101,7 +106,7 @@ diEr = irf_abs(diEr); % diEr(:,6) now contains abs(E)
 ind = ind(abs(diEr(ind,4)) > EZ_LIM & diEr(ind,6) > EPERP_LIM &...
 	diEr(ind,6)./Eperp(ind) > EPERP_RATIO_LIM & diEr(ind,5) < EDEV_LIM);
 
-if ~isempty(ind)
+if DOPLOT && ~isempty(ind)
 	axes(h(2)), hold on
 	irf_plot(diEr(ind,[1 2]),'r.');
 	axes(h(3)), hold on
@@ -109,16 +114,34 @@ if ~isempty(ind)
 end
 
 wind = sort([wind; ind]);
-for j=wind'
-	%diE( diE(:,1)>=t(j)-TAV/2 & diE(:,1)<=t(j)+TAV/2, 2:end) = NaN;
-	diE( diE(:,1)>=t(j)-TAV & diE(:,1)<=t(j)+TAV, 2:end) = NaN;
+clear ind
+
+if DOPLOT
+	for j=wind'
+		%diE( diE(:,1)>=t(j)-TAV/2 & diE(:,1)<=t(j)+TAV/2, 2:end) = NaN;
+		diE( diE(:,1)>=t(j)-TAV & diE(:,1)<=t(j)+TAV, 2:end) = NaN;
+	end
+
+	h(6) = irf_subplot(nplots,1,-6);
+	irf_plot(diE(:,1:3))
+	ylabel('E cor [mV/m]')
+
+	irf_zoom([t(1)-TAV/2 t(end)+TAV/2],'x',h)
+
+	[ok,r] = c_load('R?',cl_id);
+	if ok, add_position(h(6),r), end
+elseif ~isempty(wind)
+	DT2 = TAV;
+	res = t(wind(1)) + [-DT2 DT2];
+	wind(1) = [];
+	if ~isempty(wind)
+		for j=wind'
+			if res(end,2)>=t(j)-DT2*2, res(end,2) = t(j) + DT2; % throw away one good point in between bad (DT2*2)
+			else res = [res; t(j)-DT2 t(j)+DT2];
+			end
+		end
+	end
+else
+	irf_log('proc','no lobe wakes')
+	res = [];
 end
-
-h(6) = irf_subplot(nplots,1,-6);
-irf_plot(diE(:,1:3))
-ylabel('E cor [mV/m]')
-
-irf_zoom([t(1)-TAV/2 t(end)+TAV/2],'x',h)
-
-[ok,r] = c_load('R?',cl_id);
-if ok, add_position(h(6),r), end
