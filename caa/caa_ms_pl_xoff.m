e6298e9425afd0efa959e494183de7a1221e0883
yuri <yuri>
2008-01-16 15:45:08 +0000

    *** empty log message ***

---
+++ caa/caa_ms_pl_xoff.m
@@ -0,0 +1,38 @@
+function caa_ms_pl_xoff(st,et)
+%CAA_MS_PL_XOFF  visualize offset in the magnetosphere
+%
+% caa_ms_pl_xoff(st,et)
+%
+% See also CAA_SH_PL_XOFF
+%
+% $Id$
+
+% ----------------------------------------------------------------------------
+% "THE BEER-WARE LICENSE" (Revision 42):
+% <yuri@irfu.se> wrote this file.  As long as you retain this notice you
+% can do whatever you want with this stuff. If we meet some day, and you think
+% this stuff is worth it, you can buy me a beer in return.   Yuri Khotyaintsev
+% ----------------------------------------------------------------------------
+
+[stepoch,dt] = irf_stdt(st,et);
+
+c_eval('diEs?p34=caa_get(st,et,?,''diEs?p34'');')
+c_eval('Ps?=caa_get(st,et,?,''Ps?'');')
+c_eval('diEDI?=caa_get(st,et,?,''diEDI?'');')
+R1 = caa_get(st,et,1,'R?');
+
+t = stepoch:600:(stepoch+dt); t = t'; %#ok<NASGU>
+c_eval('diEs?r=irf_resamp(diEs?p34(~isnan(diEs?p34(:,2)),1:2),t);')
+c_eval('if isempty(diEDI?), diEDI?r=[]; else diEDI?r=irf_resamp(diEDI?(:,1:2),t); end')
+
+clf
+h(1) = irf_subplot(2,1,-1);
+c_pl_tx('diEs?r',2,'.')
+hold on
+c_pl_tx('diEDI?r',2,'x')
+
+h(2) = irf_subplot(2,1,-2);
+c_pl_tx('Ps?')
+
+irf_zoom(stepoch + [0 dt],'x',h)
+add_position(h(2),R1)
