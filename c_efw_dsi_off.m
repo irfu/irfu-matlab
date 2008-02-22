7659c72db2553a389c4f35ed01e136543fae0528
yuri <yuri>
2008-02-22 17:24:02 +0000

    add new offsets for C4 in 2002, > may

--- c_efw_dsi_off.m
+++ c_efw_dsi_off.m
@@ -29,6 +29,7 @@ elseif t>=toepoch([2004 11 01 00 0 0]), Ddsi = [ .35      .78 .51  .62 ];
 elseif t>=toepoch([2004 05 01 00 0 0]), Ddsi = [ .35      .78 .41  .72 ];
 elseif t>=toepoch([2004 01 01 00 0 0]), Ddsi = [ .23      .72 .50  .92 ];
 elseif t>=toepoch([2003 01 01 00 0 0]), Ddsi = [ .15      .53 .47  .71 ];
+elseif t>=toepoch([2002 05 02 00 0 0]), Ddsi = [ .33      .69 .73  .33 ];
 elseif t>=toepoch([2002 01 01 00 0 0]), Ddsi = [ .33      .69 .73  .92 ];
 elseif t>=toepoch([2001 07 01 00 0 0]), Ddsi = [ .47      .82 .89 1.04 ];
 elseif t>=toepoch([2001 06 01 00 0 0]), Ddsi = [ .31      .60 .52  .64 ];
@@ -60,6 +61,7 @@ if isempty(ii), return, end
 if     t>=toepoch([2005 01 01 00 0 0]), Ddsi = [ 1.26  2.34 1.81  1.37 ];
 elseif t>=toepoch([2004 01 01 00 0 0]), Ddsi = [ 1.35  2.06 1.45  1.15 ];
 elseif t>=toepoch([2003 01 01 00 0 0]), Ddsi = [ 1.42  2.18 1.64  1.43 ];
+elseif t>=toepoch([2002 05 02 00 0 0]), Ddsi = [ 1.33  1.98 1.66  1.30 ];
 elseif t>=toepoch([2002 01 01 00 0 0]), Ddsi = [ 1.33  1.98 1.66  2.00 ];
 else
 	Ddsi = DdsiSW;
