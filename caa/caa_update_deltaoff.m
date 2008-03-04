33cbd6b48401a00fa478e948f3827a61a36e6e34
yuri <yuri>
2008-03-04 10:20:35 +0000

    add plotting

--- caa/caa_update_deltaoff.m
+++ caa/caa_update_deltaoff.m
@@ -15,11 +15,32 @@ function caa_update_deltaoff(d,cl_id)
 % ----------------------------------------------------------------------------
 
 DT = 86400*6;
+SY = 2001;
+EY = 2006;
 
 d = real(d) - imag(d); % Old style delta offsets (imag is applied to p34)
 ndata = fix((d(end,1)-d(1,1))/DT);
 t = d(1,1):DT:( d(1,1) + DT*ndata ) + DT/2;
 da = irf_resamp(d,t','thresh',.5); %#ok<NASGU>
-c_eval('DeltaOff?=da; save deltaoff DeltaOff? -append; disp(''DeltaOff? -> deltaoff.mat'')',cl_id)
+
 figure
-irf_plot({d,da},'comp')
+h = irf_plot({d,da},'comp','linestyle',{'-','*'});
+ylabel(h(1),'\Delta Ex [mV/m]')
+ylabel(h(2),'\Delta Ey [mV/m]')
+title(h(1),sprintf('Cluster %d',cl_id))
+
+ud = get(gcf,'userdata');
+t_st_e = double(ud.t_start_epoch);
+tt = zeros(1,EY-SY+1);
+ts = cell(size(tt));
+for i=1:length(tt)
+	tt(i) = toepoch([SY+i-1 1 1 0 0 0]) - t_st_e;
+	ts{i} = num2str(SY+i-1);
+end
+set(h(1),'XTick',tt,'XLim',[tt(1) tt(end)],'Ylim',[-2 2])
+set(h(2),'XTick',tt,'XLim',[tt(1) tt(end)],'Ylim',[-2 2],'XTickLabel',ts)
+
+y = irf_ask('Save? yes/no [%]>','y','no');
+if strcmpi(y,'y') || strcmpi(y,'yes')
+	c_eval('DeltaOff?=da; save deltaoff DeltaOff? -append; disp(''DeltaOff? -> deltaoff.mat'')',cl_id)
+end
