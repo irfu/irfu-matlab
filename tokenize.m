4368e88baddb923e75bb7fca850061e2fd72f700
yuri <yuri>
2008-04-25 10:31:48 +0000

    check nargin

--- tokenize.m
+++ tokenize.m
@@ -1,19 +1,24 @@
 function tokens = tokenize(str, delimiter)
-%TOKENIZE tokenize the string
+%TOKENIZE  extract tokens from string
+%
 %   TOKENIZE(S) returns a cellarray of tokens in the string
 %   S delimited by "white space" characters.
 %
 %   TOKENIZE(S,D) returns a cellarray of tokens in the string
 %   S delimited by string D.
 %
-%   $Revision$  $Date$
-%
 %   See also STRTOK,STRFIND
 %
 % $Id$
-%
 
-% Copyright 2003 Yuri Khotyaintsev (yuri@irfu.se)
+% ----------------------------------------------------------------------------
+% "THE BEER-WARE LICENSE" (Revision 42):
+% <yuri@irfu.se> wrote this file.  As long as you retain this notice you
+% can do whatever you want with this stuff. If we meet some day, and you think
+% this stuff is worth it, you can buy me a beer in return.   Yuri Khotyaintsev
+% ----------------------------------------------------------------------------
+
+error(nargchk(1,2,nargin))
 
 if nargin < 2
 	delimiter = ' ';
