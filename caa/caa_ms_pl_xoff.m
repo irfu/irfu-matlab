5dd19622e4cdc1b9967b915c922e390c2b704851
yuri <yuri>
2008-05-12 11:28:07 +0000

    add hia

--- caa/caa_ms_pl_xoff.m
+++ caa/caa_ms_pl_xoff.m
@@ -19,17 +19,20 @@ function caa_ms_pl_xoff(st,et)
 c_eval('diEs?p34=caa_get(st,et,?,''diEs?p34'');')
 c_eval('Ps?=caa_get(st,et,?,''Ps?'');')
 c_eval('diEDI?=caa_get(st,et,?,''diEDI?'');')
+c_eval('diVCEh?=caa_get(st,et,?,''diVCEh?'');')
 R1 = caa_get(st,et,1,'R?');
 
 t = stepoch:600:(stepoch+dt); t = t'; %#ok<NASGU>
 c_eval('diEs?r=irf_resamp(diEs?p34(~isnan(diEs?p34(:,2)),1:2),t);')
 c_eval('if isempty(diEDI?), diEDI?r=[]; else diEDI?r=irf_resamp(diEDI?(:,1:2),t); end')
+c_eval('if isempty(diVCEh?), diVCEh?r=[]; else diVCEh?r=irf_resamp(diVCEh?(:,1:2),t); end')
 
 clf
 h(1) = irf_subplot(2,1,-1);
 c_pl_tx('diEs?r',2,'.')
 hold on
 c_pl_tx('diEDI?r',2,'x')
+c_pl_tx('diVCEh?r',2,'+')
 
 h(2) = irf_subplot(2,1,-2);
 c_pl_tx('Ps?')
