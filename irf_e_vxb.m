b4d7b18a21433385e4f2f12643b05316573ca794
yuri <yuri>
2008-05-12 11:42:18 +0000

    use irf_resamp update help reformat

--- irf_e_vxb.m
+++ irf_e_vxb.m
@@ -1,42 +1,38 @@
-function [E]=irf_e_vxb(v,b,flag)
+function res = irf_e_vxb(v,b,flag)
 %IRF_E_VXB   Compute VxB and ExB/B^2
 %
-% [E]=irf_e_vxb(v,b)
-% [V]=irf_e_vxb(e,b,-1)
+% [E] = IRF_E_VXB(V,B)
+%        Calculate electric field E = -VxB
 %
-% calculates electric field or velocity E=-vxB or v=ExB/B^2
-% v[km/s], B[nT], E[mV/m]
-% v=[t vx vy vz] (t..vz column vectors)
-% b=[t bx by bz]
-% v and b can be at different sampling
-% E is at b sampling
-% V is at e sampling
+% [VExB] = IRF_E_VXB(E,B,-1)
+%     Calculate convection velocity VExB = ExB/B^2
+%
+%     Units: v[km/s], B[nT], E[mV/m]
+%     V = [T VX VY VZ] (T..VZ column vectors)
+%     B = [T BX BY BZ]
+%     V and B can be at different sampling
+%     Resulting E is at B sampling, VExB is at E sampling
 %
 % $Id$
 
-global AV_DEBUG;if isempty(AV_DEBUG), debug=0; else debug=AV_DEBUG;end
-
-if (nargin ==3) && (flag == -1),
- e=v;
- if size(b,1) ~= size(e,1), 
-  if debug == 1, disp('interpolating b to e');end
-  bb=c_resamp(b,e);b=bb;clear bb; 
- end
- v=irf_vec_x_scal(irf_cross(e,b),[b(:,1) irf_abs(b,1)],-2);
- v=irf_tappl(v,'*1e3');
- E=v;
+if (nargin ==3) && (flag == -1)
+	% Estimate VExB = ExB/B^2
+	e = v;
+	if size(b,1) ~= size(e,1), b = irf_resamp(b,e); end
+	res = irf_vec_x_scal( irf_cross(e,b), [b(:,1) irf_abs(b,1)], -2 );
+	res = irf_tappl(res,'*1e3');
 else
-  if size(v,1) == 1,    
-    if size(v,2)==3
-      v=[b(1,1) v];
-    elseif size(v,2)<3,
-      error('v has to few components');
-    end
-    v=irf_resamp(v,b); 
-  else                 
-    b=irf_resamp(b,v);
-  end
-  % estimating E =(v x B) 
-  E=irf_tappl(irf_cross(v,b),'*1e3*1e-9*1e3*(-1)');
+	% Estimate E =(v x B)
+	if size(v,1) == 1
+		if size(v,2) == 3
+			v = [b(1,1) v];
+		elseif size(v,2) < 3
+			error('v has to few components');
+		end
+		v = irf_resamp(v,b);
+	else
+		b = irf_resamp(b,v);
+	end
+	res = irf_tappl( irf_cross(v,b), '*1e3*1e-9*1e3*(-1)' );
 end
 
