723d2cd1aa71c39ac406a66bc3b65326f3d25feb
yuri <yuri>
2008-05-30 07:39:34 +0000

    add missing Clusterproc/EDI

--- caa/caa_ms_reproc.m
+++ caa/caa_ms_reproc.m
@@ -42,6 +42,7 @@ for d=1:length(dirs)
 		getData(ClusterDB,st,dt,cl_id,'b');
 		getData(ClusterDB,st,dt,cl_id,'edi');
 		getData(ClusterProc(pwd),cl_id,'brs');
+		getData(ClusterProc(pwd),cl_id,'edi');
 		getData(ClusterProc(pwd),cl_id,'wake');
 
 		% Create .caa_ms_interval
