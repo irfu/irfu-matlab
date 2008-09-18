ecae86beeec939b90d933bb4a190043b1bcdb496
yuri <yuri>
2008-09-18 11:28:04 +0000

    add support for format string
    Obtained from: Chris Cully

--- caa/cefprint_mx.c
+++ caa/cefprint_mx.c
@@ -13,7 +13,11 @@
  * CEF header is written fromMatlab by caa_export.m
  * 
  * Usage:
- *   STATUS = cefprint_mx(FILENAME,DATA)
+ *   STATUS = cefprint_mx(FILENAME,DATA,FORMAT_STRING)
+ *
+ *   FORMAT_STRING is optional, and should be an n-column array, where n is the number of data columns in DATA
+ *   FORMAT_STRING should be transposed from a n-row string array to account for the different row-column ordering in C
+ *   e.g.: FORMAT_STRING=['%8.3f'; '%8.3f'; '%8.3f'; '%2.0f'; '%7.0f']';
  *
  *   STATUS = 0 means everything went OK
  *   STATUS = 1 means error on the data writing stange
@@ -25,6 +29,7 @@
 #include <string.h>
 #include <time.h>
 #include <math.h>
+#include <unistd.h>
 
 /* converts ISDAT epoch to ISO time string */
 void epoch2iso(double *epoch, char *str)
@@ -44,37 +49,31 @@ void epoch2iso(double *epoch, char *str)
 }
 
 void mexFunction( int nlhs, mxArray *plhs[],
-                  int nrhs, const mxArray *prhs[])
+		int nrhs, const mxArray *prhs[])
 {
-	char *f_name;
+	char *f_name, *format, *formats=NULL;
 	char tmp_s[BUFSIZ], tmp_s1[64], buf[BUFSIZ*64], unix_c[BUFSIZ];
 	double *data, *res;
-	int   buflen,status,t_mrows,t_ncols,d_mrows,d_ncols,i,j;
+	int   buflen,status,t_mrows,t_ncols,d_mrows,d_ncols,i,j,formatlen;
 	FILE *fp;
-	
+    
 	/* check for proper number of arguments */
-	if(nrhs!=2) 
-		mexErrMsgTxt("Two inputs required.");
-	else if(nlhs > 2) 
-		mexErrMsgTxt("Too many output arguments.");
 	if(nlhs!=1)
 		mexErrMsgTxt("One output required.");
-	
+	if(nrhs<2)
+		mexErrMsgTxt("At least two inputs required.");
+	else if(nrhs > 3)
+		mexErrMsgTxt("Too many input arguments.");
+    
 	/* check input*/
 	if ( mxIsChar(prhs[0]) != 1)
 		mexErrMsgTxt("First input must be a string.");
 	if (mxGetM(prhs[0])!=1)
-		mexErrMsgTxt("Input must be a row vector.");
+		mexErrMsgTxt("First input must be a row vector.");
 	if ( mxIsDouble(prhs[1])!= 1)
 		mexErrMsgTxt("Second input must be a double.");
-	
-	/* filename */
-	buflen = (mxGetM(prhs[0]) * mxGetN(prhs[0])) + 1;
-	f_name = mxCalloc(buflen, sizeof(char));
-	status = mxGetString(prhs[0], f_name, buflen);
-	if(status != 0) 
-		mexWarnMsgTxt("Not enough space. String is truncated.");
-	/* printf("Filename : %s\n",f_name); */
+	if ( (nrhs == 3) && ( mxIsChar(prhs[2])!= 1) )
+		mexErrMsgTxt("Third input must be a string.");
     
 	/* data */
 	d_mrows = mxGetM(prhs[1]);
@@ -83,7 +82,29 @@ void mexFunction( int nlhs, mxArray *plhs[],
 	if ( d_ncols < 2 )
 		mexErrMsgTxt("Input must have at least two columns.");
 	data = mxGetPr(prhs[1]);
-	
+    
+	/* set up formatting string(s) */
+	if ( nrhs == 3 ) {
+		if (mxGetN(prhs[2])!=d_ncols-1)
+			mexErrMsgTxt("Third input must have the same number of rows as the number of data columns.");
+		formatlen=mxGetM(prhs[2]);
+		format=mxCalloc(formatlen+8, sizeof(char));
+		formats = mxArrayToString(prhs[2]);
+		if(formats == NULL)
+			mexErrMsgTxt("Could not convert third input to string.");
+	} else {
+		format = mxCalloc(16, sizeof(char));
+		strcpy(format,"%s, %8.3f");
+	}
+    
+	/* filename */
+	buflen = (mxGetM(prhs[0]) * mxGetN(prhs[0])) + 1;
+	f_name = mxCalloc(buflen, sizeof(mxChar));
+	status = mxGetString(prhs[0], f_name, buflen);
+	if(status != 0)
+		mexWarnMsgTxt("Not enough space. String is truncated.");
+	printf("Filename : %s\n",f_name);
+    
 	/*  set the output pointer to the output matrix */
 	plhs[0] = mxCreateDoubleMatrix(1, 1, mxREAL);
 
@@ -106,11 +127,20 @@ void mexFunction( int nlhs, mxArray *plhs[],
 
 		for (i=0; i<d_mrows; i++){
 			epoch2iso(data+i, tmp_s);
-			sprintf(tmp_s,"%s, %8.3f",tmp_s,*(data +d_mrows +i));
+			if ( nrhs == 3 ) {
+				strcpy(format,"%s, ");
+				strncat(format,formats,formatlen);                
+			}
+			sprintf(tmp_s,format,tmp_s,*(data +d_mrows +i));
 			
 			if ( d_ncols > 2)
 				for ( j=2; j<d_ncols; j++ ){
-					sprintf(tmp_s1,", %8.3f",*(data +d_mrows*j +i));
+					if ( nrhs == 3) {
+						strcpy(format,", ");
+						strncat(format,formats+formatlen*(j-1),formatlen);
+						sprintf(tmp_s1,format,*(data +d_mrows*j +i));
+					} else
+						sprintf(tmp_s1,", %8.3f",*(data +d_mrows*j +i));
 					strcat(tmp_s,tmp_s1);
 				}
 
@@ -123,11 +153,28 @@ void mexFunction( int nlhs, mxArray *plhs[],
 		}
 		fclose(fp);
 	}
-	if (*res)
+
+	/* Free allocated dynamic memory*/
+	mxFree(format);
+	mxFree(formats);
+    
+	if (*res) {
+		mxFree(f_name);
 		return;
+	}
 	
 	/* gzip the output */
-	strcpy(unix_c,"/usr/bin/gzip ");
+	if ( access("/usr/bin/gzip",X_OK) == 0 )
+		strcpy(unix_c,"/usr/bin/gzip "); /* FreeBSD */
+	else if ( access("/bin/gzip",X_OK) == 0 )
+		strcpy(unix_c,"/bin/gzip "); /* Linux */
+	else {
+		mexWarnMsgTxt("gzip not found");
+		mxFree(f_name);
+		*res = 2;
+		return;
+	}
+
 	strcat(unix_c,f_name);
 	if ( system(unix_c) < 0)
 	{
@@ -140,6 +187,8 @@ void mexFunction( int nlhs, mxArray *plhs[],
 		strcat(unix_c,".gz");
 		system(unix_c);
 	}
+   
+	mxFree(f_name);
 	return;
 }
 
