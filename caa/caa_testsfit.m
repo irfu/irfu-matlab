7c22a21a3e74daed21a3fffde88521f7731bb3da
yuri <yuri>
2008-09-30 14:54:25 +0000

    lic

--- caa/caa_testsfit.m
+++ caa/caa_testsfit.m
@@ -2,6 +2,13 @@
 %
 % $Id$
 
+% ----------------------------------------------------------------------------
+% "THE BEER-WARE LICENSE" (Revision 42):
+% <yuri@irfu.se> wrote this file.  As long as you retain this notice you
+% can do whatever you want with this stuff. If we meet some day, and you think
+% this stuff is worth it, you can buy me a beer in return.   Yuri Khotyaintsev
+% ----------------------------------------------------------------------------
+
 fomega = pi/2;
 fnterms = 3;
 fitmax = 10;
