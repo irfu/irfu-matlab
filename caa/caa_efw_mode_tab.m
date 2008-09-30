03584bbebaa421118fbf634106b1cd17903f3eb7
yuri <yuri>
2008-09-30 14:24:13 +0000

    M-lint

--- caa/caa_efw_mode_tab.m
+++ caa/caa_efw_mode_tab.m
@@ -1,7 +1,7 @@
 function [t_start_save,t_stop_save,fdm_save]=caa_efw_mode_tab(fdm,var_list_s)
-%caa_efw_mode_tab reads EFW FDM and extracts diffs
-% [t_start_save,t_stop_save,fdm_save]=
-% 	caa_efw_mode_tab(fdm,var_list_s)
+%CAA_EFW_MODE_TAB  read EFW FDM and extract time intervals of constant FDM
+%
+% [t_start_save,t_stop_save,fdm_save] = caa_efw_mode_tab(fdm,var_list_s)
 %
 % fdm - FRM{cl_id} produced by ClusterDB/getData
 % var_list_s is in form 'px|r|ss'
@@ -16,15 +16,19 @@ function [t_start_save,t_stop_save,fdm_save]=caa_efw_mode_tab(fdm,var_list_s)
 %
 % $Id$
 
+% ----------------------------------------------------------------------------
+% "THE BEER-WARE LICENSE" (Revision 42):
+% <yuri@irfu.se> wrote this file.  As long as you retain this notice you
+% can do whatever you want with this stuff. If we meet some day, and you think
+% this stuff is worth it, you can buy me a beer in return.   Yuri Khotyaintsev
+% ----------------------------------------------------------------------------
+
 t_fdm = fdm(:,1);
 fdm_is = fdm(:,2:end)';
 var_list = tokenize(var_list_s,'|');
 t_start_save = [];
 t_stop_save = [];
 fdm_save = {};
-n_good = 0;
-n_jumpy = 0;
-l = 0;
 
 if ~isempty(t_fdm);
 	% this info is needed if next interval has no data
@@ -39,38 +43,38 @@ if ~isempty(t_fdm);
 	
 	clear fdm_is
 	
-	fdm_px = str2num( s_pbbbxrwc(:,1) ); 
-	fdm_bbb = str2num( s_pbbbxrwc(:,2) ); 
-	fdm_bbb(:,2) = str2num( s_pbbbxrwc(:,3) ); 
-	fdm_bbb(:,3) = str2num( s_pbbbxrwc(:,4) ); 
-	fdm_px(:,2) = str2num( s_pbbbxrwc(:,5) );
-	fdm_r = str2num( s_pbbbxrwc(:,6) );
-	fdm_w = str2num( s_pbbbxrwc(:,7) );
-	fdm_s = str2num( s_ssqiiiii(:,1) );
-	fdm_s(:,2) = str2num( s_ssqiiiii(:,2) );
-	fdm_e = str2num( s_mmmmeeee(:,5) );
-	fdm_e(:,2) = str2num( s_mmmmeeee(:,6) );
-	fdm_e(:,3) = str2num( s_mmmmeeee(:,7) );
-	fdm_e(:,4) = str2num( s_mmmmeeee(:,8) );
+	fdm_px = str2num( s_pbbbxrwc(:,1) );  %#ok<ST2NM>
+	fdm_bbb = str2num( s_pbbbxrwc(:,2) );  %#ok<ST2NM>
+	fdm_bbb(:,2) = str2num( s_pbbbxrwc(:,3) );  %#ok<ST2NM>
+	fdm_bbb(:,3) = str2num( s_pbbbxrwc(:,4) );  %#ok<ST2NM>
+	fdm_px(:,2) = str2num( s_pbbbxrwc(:,5) ); %#ok<ST2NM>
+	fdm_r = str2num( s_pbbbxrwc(:,6) ); %#ok<ST2NM>
+	fdm_w = str2num( s_pbbbxrwc(:,7) ); %#ok<ST2NM>
+	fdm_s = str2num( s_ssqiiiii(:,1) ); %#ok<ST2NM>
+	fdm_s(:,2) = str2num( s_ssqiiiii(:,2) ); %#ok<ST2NM>
+	fdm_e = str2num( s_mmmmeeee(:,5) ); %#ok<ST2NM>
+	fdm_e(:,2) = str2num( s_mmmmeeee(:,6) ); %#ok<ST2NM>
+	fdm_e(:,3) = str2num( s_mmmmeeee(:,7) ); %#ok<ST2NM>
+	fdm_e(:,4) = str2num( s_mmmmeeee(:,8) ); %#ok<ST2NM>
 	
 	clear s_pbbbxrwc s_ssqiiiii s_mmmmeeee
 	
 	t_fdm_last = t_fdm(1);
 	for j=1:length(var_list)
 		if strcmp(var_list(j),'px')
-			fdm_last_px = fdm_px(1,:);
+			fdm_last_px = fdm_px(1,:); %#ok<NASGU>
 		elseif strcmp(var_list(j),'bbb')
-			fdm_last_bbb = fdm_bbb(1,:);
+			fdm_last_bbb = fdm_bbb(1,:); %#ok<NASGU>
 		elseif strcmp(var_list(j),'r')
-			fdm_last_r = fdm_r(1,:);
+			fdm_last_r = fdm_r(1,:); %#ok<NASGU>
 		elseif strcmp(var_list(j),'w')
-			fdm_last_w = fdm_w(1,:);
+			fdm_last_w = fdm_w(1,:); %#ok<NASGU>
 		elseif strcmp(var_list(j),'s')
-			fdm_last_s = fdm_s(1,:);
+			fdm_last_s = fdm_s(1,:); %#ok<NASGU>
 		elseif strcmp(var_list(j),'e')
-			fdm_last_e = fdm_e(1,:);
+			fdm_last_e = fdm_e(1,:); %#ok<NASGU>
 		elseif strcmp(var_list(j),'tm')
-			fdm_last_tm = fdm_tm(1);
+			fdm_last_tm = fdm_tm(1); %#ok<NASGU>
 		else
 			error('unknown variable')
 		end
@@ -81,7 +85,7 @@ if ~isempty(t_fdm);
 		empty = 1;
 		for j=1:length(var_list)
 			eval(['ii{j} = irf_find_diff(fdm_' var_list{j} ',fdm_last_' var_list{j} ');']);
-			if empty & ~isempty(ii{j}), empty = 0; end 
+			if empty && ~isempty(ii{j}), empty = 0; end  %#ok<USENS>
 		end
 		
 		% no changes (should be a typical situation)
@@ -134,7 +138,6 @@ if (t_fdm_last <= t_fdm(end))
 	for j=1:length(var_list)
 		eval(['if length(fdm_last_' var_list{j} ')>1,fdm_save_' var_list{j} '(l+1,:)=fdm_last_' var_list{j} '; else, fdm_save_' var_list{j} '(l+1)=fdm_last_' var_list{j} '; end ']);
 	end
-	n_good = n_good + 1;
 end
 end
 
@@ -145,6 +148,6 @@ if length(var_list)>1
 else
 	if exist(['fdm_save_' var_list{1}],'var')
 		eval(['fdm_save=fdm_save_' var_list{1} ';']);
-	else, fdm_save = [];
+	else fdm_save = [];
 	end
 end
