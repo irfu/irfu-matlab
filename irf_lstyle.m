6807471ed39009a4c3d6e751bef0fb5cc9e497bd
yuri <yuri>
2008-10-06 20:59:00 +0000

    help

--- irf_lstyle.m
+++ irf_lstyle.m
@@ -1,10 +1,22 @@
 function res = irf_lstyle(n)
 %IRF_LSTYLE    Generate line style
 %
-% res = irf_lstyle(n)
-% n - integer starts from 0
+% RES = IRF_LSTYLE(N)
+%       Generate line style for curve number N
+%
+% Example:
+%       for i=1:5
+%         plot(t,x(:,i),irf_lstyle(i)); hold on
+%       end
 %
 % $Id$
+
+% ----------------------------------------------------------------------------
+% "THE BEER-WARE LICENSE" (Revision 42):
+% <yuri@irfu.se> wrote this file.  As long as you retain this notice you
+% can do whatever you want with this stuff. If we meet some day, and you think
+% this stuff is worth it, you can buy me a beer in return.   Yuri Khotyaintsev
+% ----------------------------------------------------------------------------
 
 colors = {'k' 'r' 'g' 'b' 'm' 'c' 'y'};
 markers = {'-' '--' '.-' '-.-'};
