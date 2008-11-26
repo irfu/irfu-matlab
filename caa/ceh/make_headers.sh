eb1c658f4e2d558bf6836e68c63f5e0bc44c6b33
yuri <yuri>
2008-11-26 10:49:21 +0000

    handle new caveat files

--- caa/ceh/make_headers.sh
+++ caa/ceh/make_headers.sh
@@ -2,8 +2,18 @@
 #
 # $Id$
 
-flist="CX_CH_EFW_INST.ceh \
-CX_CH_EFW_L2_E.ceh \
+f=CX_CH_EFW_INST.ceh
+CLI=1
+while test $CLI -le 4
+do
+   fout=`echo $f| sed -e "s=X=${CLI}="`
+   echo "Writing $fout"
+   cat $f|sed -e "s=XXX=${CLI}=" > $fout
+   cat C${CLI}_CH_EFW_INST_CAV.ceh >> $fout
+   CLI=$(($CLI+1))
+done
+
+flist="CX_CH_EFW_L2_E.ceh \
 CX_CH_EFW_L3_E.ceh \
 CX_CH_EFW_L3_DER.ceh"
 
