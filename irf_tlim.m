2123620df5d06f079aff7c04b59fdd0a03a09418
yuri <yuri>
2009-03-25 13:36:54 +0000

    fix parsing arguments

--- irf_tlim.m
+++ irf_tlim.m
@@ -2,7 +2,7 @@ function [x,in]=irf_tlim(x,t1,t2,mode)
 %IRF_TLIM   Find a subinterval defined by time limits
 %
 % [Y, IN] = IRF_TLIM(X,T1,T2,[MODE])
-% [Y, IN] = IRF_TLIM(X,[T1 T2])
+% [Y, IN] = IRF_TLIM(X,[T1 T2],[MODE])
 % Y is part of the X that is within interval T1 <= X(:,1) < T2
 % if MODE is nonzero, part of X outside the interval :
 % X(:,1) < T1 & X(:,1) > T2
@@ -22,8 +22,16 @@ function [x,in]=irf_tlim(x,t1,t2,mode)
 error(nargchk(2,4,nargin))
 
 if isempty(x), in = []; return, end
-if nargin==2, t2 = t1(2) + 1e-7; t1 = t1(1) - 1e-7; end
-if nargin<4, mode = 0; end
+
+if nargin==2
+	mode = 0;
+	t2 = t1(2) + 1e-7; t1 = t1(1) - 1e-7;
+elseif nargin<4
+	if length(t1)==2 && length(t2)==1
+		mode = t2;
+		t2 = t1(2) + 1e-7; t1 = t1(1) - 1e-7;
+	end
+end
 
 [t1,dt] = irf_stdt(t1,t2);
 t2 = t1 + dt;
