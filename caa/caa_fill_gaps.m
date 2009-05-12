84a1d0640c8e55ff5b854204bf2a9898b8257dd9
chris <chris>
2009-05-12 11:04:25 +0000

    New optional input: maximum number of gaps to fill.

--- caa/caa_fill_gaps.m
+++ caa/caa_fill_gaps.m
@@ -1,4 +1,4 @@
-function [data, filldata] = caa_fill_gaps(data,te)
+function [data, filldata] = caa_fill_gaps(data,te,max_gaps)
 %CAA_FILL_GAPS(data,te)  fill gaps in the of a dataset
 %
 % res = caa_fill_gaps(data,te)
@@ -13,7 +13,7 @@ function [data, filldata] = caa_fill_gaps(data,te)
 % this stuff is worth it, you can buy me a beer in return.   Yuri Khotyaintsev
 % ----------------------------------------------------------------------------
 
-error(nargchk(2,2,nargin))
+error(nargchk(2,3,nargin))
 
 if size(data,1)<2
 	irf_log('proc','cannot fill gaps (not enough samples)')
@@ -30,6 +30,14 @@ end
 
 ngap = fix( (te -data(end,1))*fs - 1); % NOTE: Possible error here. Not filling the last point up to TE,
                                        %       if TE-last_point < 2/fs !   (ML)
+
+if nargin > 2
+  if ngap > max_gaps
+    irf_log('proc',sprintf('Request to fill %d gaps at %s gaps exceeds max_gap.',ngap,epoch2iso(data(end,1),1)))
+    return
+  end
+end
+
 if ngap>0
 	tt = zeros(ngap+1,size(data,2));
 	tt(:,1) = linspace(data(end,1),data(end,1)+ngap/fs,ngap+1);
