b443f2b1c00f7a872f489ad0013c954487f223b9
chris <chris>
2010-01-11 12:06:06 +0000

    Put a try/catch on the summary plot creation so that processing does not stop if there's an error.

--- caa/caa_sp_batch.m
+++ caa/caa_sp_batch.m
@@ -18,13 +18,33 @@ function caa_sp_batch(fname)
 old_pwd = pwd;
 BASE_DIR = '/data/caa/l1';
 dirs = textread(fname,'%s');
+errors=0;
+errlog=[fname '.failed'];
 
 if isempty(dirs), disp('NO DIRS'), cd(old_pwd), return, end
 
 for d=1:length(dirs)
 	s = dirs{d};
 	if length(s)>12 && strcmp(BASE_DIR,s(1:12)), s = s(14:end); end
-	caa_pl_summary_l1(...
-		sprintf('%s-%s-%sT%s:00:00Z',s(1:4),s(10:11),s(12:13),s(15:16)),...
-		3*3600,	[BASE_DIR '/' s],'saveps')
+    try
+        caa_pl_summary_l1(...
+            sprintf('%s-%s-%sT%s:00:00Z',s(1:4),s(10:11),s(12:13),s(15:16)),...
+            3*3600,	[BASE_DIR '/' s],'saveps')
+    catch ME
+        cd(old_pwd);
+        errors=errors+1;
+        fprintf(1,'Error encountered for %s:\n',s);
+        disp(ME.identifier);
+        disp(ME.message);
+        fid=fopen(errlog,'a');
+        fprintf(fid,'Error encountered for %s:\n',s);
+        fprintf(fid,'%s\n',ME.identifier);
+        fprintf(fid,'%s\n',ME.message);
+        fprintf(fid,'\n');
+        fclose(fid);
+    end
 end
+
+cd(old_pwd);
+fprintf(1,'%i errors encountered. See %s.\n',errors,errlog);
+
