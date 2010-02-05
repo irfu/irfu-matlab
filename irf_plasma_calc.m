c409c852f2ee6e00891d57d0797402e62f4fc552
andris <andris>
2010-02-05 09:48:45 +0000

    *** empty log message ***

--- irf_plasma_calc.m
+++ irf_plasma_calc.m
@@ -224,6 +224,7 @@ disp(sprintf('\nPlasma dimensionless parameters\n'))
 
 beta  = Vtp^2/Va^2;
 disp(sprintf('beta  = %1.5f',Vtp^2/Va^2))
-disp(sprintf('beta*Mp_Me = %1.5f',Vtp^2/Va^2*Mp_Me))
+disp(sprintf('beta*sqrt(Mp/Me)  = %1.5f',Vtp^2/Va^2*sqrt(Mp_Me)))
+disp(sprintf('beta*(Mp/Me) = %1.5f',Vtp^2/Va^2*Mp_Me))
 
 if nargout>0, Fpe_out = Fpe; end
