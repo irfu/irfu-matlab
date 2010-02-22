35753823321fd5fd9f0e9375344b71a1ea01d41d
chris <chris>
2010-02-22 08:39:50 +0000

    Insert a blank plot if the interval contains no data.

--- caa/caa_pl_summary_l1_batch.m
+++ caa/caa_pl_summary_l1_batch.m
@@ -1,10 +1,10 @@
-function caa_pl_summary_l1_batch(iso_st,iso_et,dir)
+function caa_pl_summary_l1_batch(iso_st,iso_et,sp_dir)
 %CAA_PL_SUMMARY_L1_BATCH: generate CAA summary plots for a given interval.
 %
-% caa_pl_summary_l1_batch(iso_st,iso_et,[dir])
+% caa_pl_summary_l1_batch(iso_st,iso_et,[sp_dir])
 % Inputs:
 %   iso_st,iso_et: start and end times for the interval (ISO format)
-%   dir: directory in which to put the plots. Default: /data/caa/sp/YYYY
+%   sp_dir: directory in which to put the plots. Default: /data/caa/sp/YYYY
 %
 % Example:
 %  caa_pl_summary_l1_batch('2007-01-01T00:00:00Z', '2007-02-01T00:00:00Z')
@@ -16,8 +16,8 @@ old_pwd=pwd;
 if nargin<3
 	default_dir=1;
 else
-	cd(dir);
-	defualt_dir=0;
+	cd(sp_dir);
+	default_dir=0;
 end
 BASE_DIR='/data/caa/l1';
 
@@ -29,7 +29,27 @@ for t=t0:3*3600:t1
 	y=fromepoch(t);
 	l1dir = [BASE_DIR '/' num2str(y(1)) '/' irf_fname(t)];
 	if default_dir, cd(['/data/caa/sp/' num2str(y(1))]), end
-	caa_pl_summary_l1(-1,-1,l1dir,'savepdf')
+	if isempty(dir([l1dir '/C*']))
+		figure(78)
+		clf
+		text(0.2,0.5,['No data for '  epoch2iso(t)]);
+		fn = sprintf('EFW_SPLOT_L1ESPEC__%s',irf_fname(t));
+		fne = sprintf('EFW_SPLOT_L1ERSPEC__%s',irf_fname(t));
+		fnq = sprintf('EFW_SPLOT_L1QUAL__%s',irf_fname(t));
+		fone = sprintf('EFW_SPLOT_L1__%s',irf_fname(t));
+		irf_log('save',['saving  blank pdf plots.'])
+		print( 78, '-dpdf', fn), print( 78, '-dpdf', fne), print( 78, '-dpdf', fnq)
+		if exist('/usr/local/bin/pdfjoin','file')
+			irf_log('save',['joining to ' fone '.pdf'])
+			s = unix(['LD_LIBRARY_PATH="" /usr/local/bin/pdfjoin ' fn '.pdf ' fne '.pdf ' fnq '.pdf --outfile ' fone '.pdf']);
+			if s~=0, irf_log('save','problem with pdfjoin'), end
+		else
+			irf_log('proc',...
+				'cannot join PDFs: /usr/local/bin/pdfjoin does not exist')
+		end
+	else
+		caa_pl_summary_l1(-1,-1,l1dir,'savepdf')
+	end
 end
 
 cd(old_pwd);
