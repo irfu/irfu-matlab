3ee4138c6c6c80dea7c8a61eacb9de40a7e7137c
chris <chris>
2010-03-03 09:23:01 +0000

    Fix corner case,

--- caa/caa_append_data.m
+++ caa/caa_append_data.m
@@ -30,9 +30,14 @@ else
 end
 
 if tapp(1) <= t(end)
-   if tapp(1) == t(end), error('Last point in DATA has same timestamp as first point in APP'), end
-	app = irf_tlim(app,tapp(1),t(end),1);
-	if isempty(app), return, end
+   if tapp(1) == t(end)
+       %error('Last point in DATA has same timestamp as first point in APP')
+       if length(tapp) == 1, return, end
+       app=app(2:end,:);
+   else
+       app = irf_tlim(app,tapp(1),t(end),1);
+       if isempty(app), return, end
+   end
 end
 
 if isstruct(data)
