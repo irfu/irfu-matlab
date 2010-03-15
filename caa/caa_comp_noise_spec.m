8939105d1292152a8f99bda74647d782b49b8e64
yuri <yuri>
2010-03-15 09:28:25 +0000

    add averages

--- caa/caa_comp_noise_spec.m
+++ caa/caa_comp_noise_spec.m
@@ -1,4 +1,4 @@
-function res = caa_comp_noise_spec(data,whip)
+function [res,detailed_spec] = caa_comp_noise_spec(data,whip)
 
 if c_efw_fsample(data)~=450,
 	error('DATA must be 450 Hz')
@@ -7,21 +7,34 @@ end
 irf_log('proc','blanking Whisper pulses')
 data = caa_rm_blankt(data,whip);
 
-STEP=450;
+STEP=4*450;
 FREQS=50:100;
 pos = 0;
 count = 0;
 
-while length(data) >=pos+STEP
+if nargout > 1, ncol = 2;
+else ncol = 1;
+end
+detailed_spec = zeros(fix(size(data,1)/STEP),ncol);
+
+while size(data,1) >=pos+STEP
 	% check for nans and data gaps
 	if any(isnan(data(pos+1:pos+STEP,2))) || ...
 		data(pos+STEP,1) - data(pos+1) > STEP/450.0
 		pos = pos + STEP;
 		continue
 	end
-	Pxx = irf_psd(data(pos+1:pos+STEP,:));
+	Pxx = irf_psd(data(pos+1:pos+STEP,:),256,450);
 	count = count + 1;
-	res(count,1) = data(pos+fix(STEP/2),1);
-	res(count,2) = median(Pxx(FREQS));
+    if nargout > 1
+        detailed_spec(count,1) = data(pos+fix(STEP/2),1);
+    end
+	detailed_spec(count,ncol) = median(Pxx(FREQS));
+    
 	pos = pos + STEP;
 end
+if count < size(detailed_spec,1), detailed_spec(count+1:end,:) = []; end
+
+res = sort(detailed_spec(:,ncol));
+res = res(1:fix(length(res)/10));
+res = median(res);
