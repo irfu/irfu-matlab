81c965f910de31a68f2c196c3d1b8039644c1a12
andris <andris>
2010-03-25 15:10:06 +0000

    updated help

--- irf_legend.m
+++ irf_legend.m
@@ -4,7 +4,12 @@ function irf_legend(axis_handle,labels,position,varargin)
 % labels - cell array with strings
 % position - in normalized units 
 %
+% Examples:
 % irf_legend(gca,{'B_X','B_Y','B_Z','B'},[0.02, 0.1]);
+%
+% set(gca,'ColorOrder',[[0 0 1];[0 1 0];[0 0 0];[1 0 0]]);
+% irf_legend(gca,{'B_X','B_Y','B_Z','B'},[0.02, 0.1]);
+
 
 colord=get(axis_handle, 'ColorOrder');
 
@@ -14,5 +19,5 @@ for i=1:length(labels),
   for j=1:size(varargin,2)/2
       set(ht,varargin{2*j-1},varargin{2*j});
   end
-  ext=get(ht,'extent'); position(1)=position(1)+ext(3)*1.2;
+  ext=get(ht,'extent'); position(1)=position(1)+ext(3)*1.4;
 end
