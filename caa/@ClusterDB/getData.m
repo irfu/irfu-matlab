a6d36e7b0e7596635b5f8552a6483086c35bed50
chris <chris>
2010-04-09 12:33:40 +0000

    Allow the phase data to skip over a gap (as caused, for example, by a maneuver). Avoids the need to re-fetch and re-process intervals with maneuvers.

--- caa/@ClusterDB/getData.m
+++ caa/@ClusterDB/getData.m
@@ -701,8 +701,7 @@ elseif strcmp(quantity,'a')
 	
 	% We ask for 2 sec more from each side 
 	% to avoid problems with interpolation.
-	[t,data] = caa_is_get(cdb.db, start_time-2, dt+4, ...
-		cl_id, 'ephemeris', 'phase'); %#ok<ASGLU>
+	[t,data] = c_get_phase(cdb.db,start_time-2,dt+4,cl_id,'phase');%#ok<ASGLU>
 	if ~isempty(data)
 		c_eval('A?=[t data];save_list=[save_list '' A? ''];',cl_id);
 		n_ok = n_ok + 1;
@@ -710,8 +709,7 @@ elseif strcmp(quantity,'a')
 	end
 	clear t data
 	
-	[t,data] = caa_is_get(cdb.db, start_time-2, dt+4, ...
-		cl_id, 'ephemeris', 'phase_2'); %#ok<ASGLU>
+	[t,data] = c_get_phase(cdb.db,start_time-2,dt+4,cl_id,'phase_2');%#ok<ASGLU>
 	if ~isempty(data)
 		c_eval('Atwo?=[t data];save_list=[save_list '' Atwo? ''];',cl_id);
 		n_ok = n_ok + 1;
