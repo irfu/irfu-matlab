237223c2b5fbb6f18212003117a3d7335c20c297
chris <chris>
2010-04-09 12:30:52 +0000

    Initial check-in.

---
+++ c_get_phase.m
@@ -0,0 +1,45 @@
+function [t,data] = c_get_phase(db,start_time,dt,cl_id,phasetype)
+%C_GET_PHASE fetches phase, skipping over a potential gap
+%              (as caused by a maneuver, for example)
+%
+% [t,data] = c_get_phase(db,start_time,dt,cl_id,phasetype)
+%
+% Primarily intended as a subroutine for getData. Skips at most one gap.
+%  db: ISDAT database name
+%  start_time: epoch start time
+%  dt: length of data interval
+%  cl_id: Cluster satellite number
+%  phasetype: which phase variable you want ('phase' or 'phase_2')
+
+[t,data] = caa_is_get(db, start_time, dt,cl_id, 'ephemeris', phasetype); 
+
+if isempty(t)
+	low=start_time+60;
+else
+	if t(end)+4-start_time > dt, return, end
+	low=t(end)+60;
+end
+irf_log('dsrc',[phasetype ' for C' num2str(cl_id) ' does not cover the full interval.'])
+
+up=start_time+dt;
+while up>(low+60)
+	test_t=floor((up+low)/2);
+	[t1,data1] = caa_is_get(db, test_t, dt-(test_t-start_time),cl_id, 'ephemeris', phasetype); 
+	if isempty(t1)
+		low=test_t;
+	else
+		up=test_t;
+	end
+end
+
+if isempty(t1), return, end
+if isempty(t)
+	t=t1;
+	data=data1;
+	return;
+end
+
+irf_log('dsrc',['Skipped over ' num2str(t1(1)-t(end)) ' second phase gap in ' ...
+	phasetype ' for C' num2str(cl_id)])
+t=[t' t1']';
+data=[data' data1']';
