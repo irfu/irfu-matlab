d9bdfcf409631fe1e44e1f626d522cafb6a83de4
andris <andris>
2010-04-14 12:42:59 +0000

    *** empty log message ***

--- irf_gradient.m
+++ irf_gradient.m
@@ -16,20 +16,20 @@ if nargin < 2, % estimate time step
     time_steps=diff(x(:,1));
     [time_step,ind_min]=min(time_steps);
     time_steps(ind_min)=[]; % remove the smallest time step in case some problems
-    time_step=min(time_steps),
+    time_step=min(time_steps);
 end
 
+if size(x,2)==1, data_columns=1; else data_columns=2:size(x,2);end
+    
 data_gaps=find(dt>3*time_step);
 dt(data_gaps)=0;
 y=x;
 if data_gaps,
-    y(1:data_gaps(1),j)=gradient(x(1:data_gaps(1),j),time_step);
+    y(1:data_gaps(1),data_columns)=gradient(x(1:data_gaps(1),data_columns)',time_step)';
     for j=1:(length(data_gaps)-1),
-        y((data_gaps(j)+1):data_gaps(j+1),j)=gradient(x((data_gaps(j)+1):data_gaps(j+1),j),time_step);
+        y((data_gaps(j)+1):data_gaps(j+1),data_columns)=gradient(x((data_gaps(j)+1):data_gaps(j+1),data_columns)',time_step)';
     end
-    y(data_gaps(end):end,j)=gradient(x(data_gaps(end):end,j),time_step);
+    y(data_gaps(end):end,data_columns)=gradient(x(data_gaps(end):end,data_columns)',time_step)';
 else
-    for j=2:size(x,2),
-        y(:,j)=gradient(x(:,j),time_step);
-    end
+    y(:,data_columns)=gradient(x(:,data_columns)',time_step)';
 end
