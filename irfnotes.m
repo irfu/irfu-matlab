84951cebe42b8adb655492e6e18d9edb6095f732
andris <andris>
2010-04-22 11:25:37 +0000

    *** empty log message ***

--- irfnotes.m
+++ irfnotes.m
@@ -47,3 +47,28 @@ ax2 = axes('Position',get(ax1,'Position'),...
            'Color','none',...
            'XColor','k','YColor','k');
 end
+%% Reading files
+for ha=1, % formatted file reading
+%File contents are time intervals in format "T1 T2 Comments":
+%2008-03-03T22:50:00 2008-03-03T23:30:00
+%2008-03-10T22:10:00 2008-03-10T22:45:00 !
+%2008-03-13T07:40:00 2008-03-13T09:40:00 ? shock?
+
+%file reading 
+[t1,t2,tint_comments]=textread('Events_reconnection.txt','%s%s%[^\n]');
+for j=1:size(t1,1),
+  tint(j,1)=iso2epoch(t1{j});tint(j,2)=iso2epoch(t2{j});
+end
+clear t1 t2 j;
+end
+%% Cluster data reading
+for ha=1, % using c_get_batch
+    c_get_batch(toepoch([2002 03 04 10 00 00]),30*60,'sp','/home/yuri/caa-data/20020304')
+% if time intervals to download are in matrix tint 
+for j=1:size(tint,1),
+ c_get_batch(tint(j,1),tint(j,2)-tint(j,1),'sp',['./' epoch2iso(tint(j,1),1) '-' epoch2iso(tint(j,2),1)]);
+end
+clear j;
+end
+
+%% Other
