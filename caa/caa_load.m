d6066013b2319d356fb05e049f459de207b589b3
andris <andris>
2011-01-24 08:32:44 +0000

    caa load takes also parameters to load only a few variables, example caa_load C1 RAP

--- caa/caa_load.m
+++ caa/caa_load.m
@@ -1,7 +1,11 @@
+function c=caa_load(varargin)
 %Script to load data downloaded from the CAA in CDF format.
 %Downloaded zip file must be unpacked, and script must be run 
 %from a CAA_Download_YYYYMMDD_hhmm directory.
-%
+% Examples:
+% caa_load
+% caa_load CIS 
+% caa_load C1
 % $Id$
 
 % ----------------------------------------------------------------------------
@@ -11,6 +15,17 @@
 % this stuff is worth it, you can buy me a beer in return.   Yuri Khotyaintsev
 % ----------------------------------------------------------------------------
 
+flag_filter = 0;
+if nargin > 0, % filter which variables to load 
+    i=1;clear variable_filter;
+    for j=1:length(varargin),
+        if ischar(varargin{j}),
+            variable_filter{i}=varargin{j};
+            i=i+1;flag_filter=1;
+        end
+    end
+end
+
 nloaded = 0;
 dirs = dir;
 old_pwd = pwd;
@@ -19,14 +34,24 @@ for j = 1:numel(dirs)
         var_name = dirs(j).name;
         d3 = findstr(var_name,'-');
         if d3, var_name( d3 ) = '_'; end
-		disp(['loading ' var_name]);
-		try
-			cd(dirs(j).name)
-			eval([var_name '=dataobj(''*.cdf'');'])
-			nloaded = nloaded + 1;
-		catch
-			disp(['error loading ' var_name]);
-		end
+        flag_load_variable=1;
+        if flag_filter==1, % if there is name filtering required check if to load variable
+            for jj=1:length(variable_filter),
+                if isempty(findstr(variable_filter{jj},var_name)),
+                    flag_load_variable=0;
+                end
+            end
+        end
+        if flag_load_variable,
+            try
+                disp(['loading ' var_name]);
+                cd(dirs(j).name)
+                evalin('caller',[var_name '=dataobj(''*.cdf'');'])
+                nloaded = nloaded + 1;
+            catch
+                disp(['error loading ' var_name]);
+            end
+        end
 		cd(old_pwd)
 	end
 end
