1e97c47cb6b6d937a62a7db56361d93f290147e1
andris <andris>
2011-02-01 13:42:57 +0000

    returns variable attributes also

--- @dataobj/getv.m
+++ @dataobj/getv.m
@@ -26,7 +26,17 @@ if nvars>0
 		if strcmpi(dobj.vars{v,1},var_s) || strcmpi(dobj.vars{v,2},var_s)
 			res = dobj.data.(dobj.vars{v,1});
 			res.name = var_s;
-			return
+			% Add Variable attributes to the returned variable
+            variable_attribute_names=fieldnames(dobj.VariableAttributes);
+            for j=1:length(variable_attribute_names),
+                current_attribute=dobj.VariableAttributes.(variable_attribute_names{j});
+                for jj=1:size(current_attribute,1),
+                    if strcmpi(dobj.vars{v,2},current_attribute{jj,1})
+                        res.(variable_attribute_names{j})=current_attribute{jj,2};
+                    end
+                end
+            end
+            return
 		end
 	end
 end
