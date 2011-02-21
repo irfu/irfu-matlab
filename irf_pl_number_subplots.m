09a11b2aa114eb4468cac564657f76234de59b4d
andris <andris>
2011-02-21 20:36:31 +0000

    can use varargin in input

--- irf_pl_number_subplots.m
+++ irf_pl_number_subplots.m
@@ -1,14 +1,17 @@
-function ht=irf_pl_number_subplots(h)
-% ht=irf_pl_number_subplots(h)
+function ht=irf_pl_number_subplots(h,position,varargin)
+% ht=irf_pl_number_subplots(h,position,text_property,text_value,...)
 % 
 % number subplots by putting in each subplot 'a', 'b', 'c', etc.
-%
-% h is handles to subplots
+% INPUT 
+%   h           handles to subplots
+%   position    position of legend
+% any text property/value pairs can be passed 
 
-h=h(:);
+if nargin==1, position=[0.01, 0.97];end
+
+h=h(:);ht=h;
 abc='a':'z';
 for j=1:length(h)
-  axes(h(j));
-  ht(j)=irf_pl_info(abc(j),gca,[0.01,.8]);
-  set(ht(j),'fontsize',12);
+  hleg=irf_legend(h(j),abc(j),position,varargin{:});
+  ht(j)=hleg;
 end
