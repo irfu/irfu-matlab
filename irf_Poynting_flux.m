0e064e49518ee93dacd92d4fbabdceefc094ad5a
andris <andris>
2011-02-28 18:54:06 +0000

    include options of calcuating integrating poynting flux

--- irf_Poynting_flux.m
+++ irf_Poynting_flux.m
@@ -2,6 +2,7 @@ function [S,Sz,intSz]=irf_Poynting_flux(E,B,Bo)
 % function [S,Sz,intSz]=irf_Poynting_flux(E,B,Bo)
 % function [S,Sz]=irf_Poynting_flux(E,B,Bo)
 % function [Sz]=irf_Poynting_flux(E,B,Bo)
+% function [S,intS]=irf_Poynting_flux(E,B)
 % function [S]=irf_Poynting_flux(E,B)
 %   estimates Poynting flux S and Poynting flux along Bo
 %   from electric field E and magnetic field B
@@ -15,9 +16,12 @@ function [S,Sz,intSz]=irf_Poynting_flux(E,B,Bo)
 %intSz units [mW*s/m^2]
 
 % check which Poynting flux to calculate
-flag_Sz=1;flag_intSz=0;
-if nargin <3,flag_Sz=0;end
-if nargout >2, flag_intSz=1;end
+flag_Sz=0;flag_intSz=0;flag_intS=0;
+if nargin == 2 && nargout == 1, flag_Sz=0;flag_intSz=0;flag_intS=0; end
+if nargin == 2 && nargout == 2, flag_Sz=0;flag_intSz=0;flag_intS=1; end
+if nargin == 3 && nargout == 1, flag_Sz=1;flag_intSz=0;flag_intS=0; end
+if nargin == 3 && nargout == 2, flag_Sz=1;flag_intSz=0;flag_intS=0; end
+if nargin == 3 && nargout == 3, flag_Sz=1;flag_intSz=1;flag_intS=0; end
 
 % resample if necessary
 Fs_E=1/(E(2,1)-E(1,1));Fs_B=1/(B(2,1)-B(1,1));
@@ -41,10 +45,18 @@ if flag_Sz,
   if nargout ==1,S=Sz;end
 end
 
-% time integral of Poynting flux
+% time integral of Poynting flux along ambient magnetic field
 if flag_intSz,
  ssz=Sz;
  ssz(isnan(Sz))=0; % set to zero points where Sz=NaN
  intSz=ssz;
  intSz(:,2)=cumsum(ssz(:,2))/Fs;
+end
+
+if flag_intS,% time integral of all Poynting flux components
+ ss=S;
+ ss(isnan(S))=0; % set to zero points where Sz=NaN
+ intS=ss;
+ intS(:,2:end)=cumsum(ss(:,2:end))/Fs;
+ Sz=intS;
 end
