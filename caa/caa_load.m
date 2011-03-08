a77e5941fde2437260e5cde21765307fb13dab2a
andris <andris>
2011-03-08 09:43:13 +0000

    variable name can also be given as parameter

--- caa/caa_load.m
+++ caa/caa_load.m
@@ -1,10 +1,10 @@
 function caa_load(varargin)
 %Script to load data downloaded from the CAA in CDF format.
-%Downloaded zip file must be unpacked, and script must be run 
+%Downloaded zip file must be unpacked, and script must be run
 %from a CAA_Download_YYYYMMDD_hhmm directory.
 %  Examples:
 %   caa_load
-%   caa_load CIS 
+%   caa_load CIS
 %   caa_load C1 CIS
 % $Id$
 
@@ -16,51 +16,58 @@ function caa_load(varargin)
 % ----------------------------------------------------------------------------
 
 flag_filter = 0;
-if nargin > 0, % filter which variables to load 
-    i=1;
-    variable_filter=cell(nargin,1);
-    for j=1:length(varargin),
-        if ischar(varargin{j}),
-            variable_filter{i}=varargin{j};
-            i=i+1;
-            flag_filter=1;
-        end
+if nargin > 0, % filter which variables to load
+  i=1;
+  variable_filter=cell(nargin,1);
+  for j=1:length(varargin),
+    if ischar(varargin{j}),
+      if strfind(varargin{j},'__') % variable name specified as input
+        dd=regexp(varargin{j}, '__', 'split');
+        variable_filter{i}=dd{end};
+      else
+        variable_filter{i}=varargin{j};
+      end
+      i=i+1;
+      flag_filter=1;
     end
+  end
 end
 
 if isdir([pwd filesep 'CAA']), % check if is CAA folder, then assume data are there
-    dirs = dir('CAA');
+  dirs = dir('CAA');
+  caa_data_directory='CAA/';
 else % otherwise assume one is in the CAA data folder
-    dirs = dir;
+  dirs = dir;
+  caa_data_directory='';
 end
 
 nloaded = 0;
 for j = 1:numel(dirs)
-	if regexp(dirs(j).name,'^C[1-4]_(C|P)P_')
-        var_name = dirs(j).name;
-        var_name(strfind(var_name,'-'))='_'; % substitute '-' to '_'
-        flag_load_variable=1;
-        if flag_filter==1, % if there is name filtering required check if to load variable
-            for jj=1:length(variable_filter),
-                if isempty(strfind(var_name,variable_filter{jj})),
-                    flag_load_variable=0;
-                end
-            end
+  if regexp(dirs(j).name,'^C[1-4]_(C|P)P_')
+    var_name = dirs(j).name;
+    var_name(strfind(var_name,'-'))='_'; % substitute '-' to '_'
+    flag_load_variable=1;
+    if flag_filter==1, % if there is name filtering required check if to load variable
+      for jj=1:length(variable_filter),
+        if isempty(strfind(var_name,variable_filter{jj})),
+          flag_load_variable=0;
         end
-        if flag_load_variable,
-            try
-                disp(['caa_load ' var_name]);
-                if evalin('caller',['exist(''' var_name ''',''var'')']),
-                    disp('Variable exist in memory. NOT LOADING FROM FILE!')
-                else
-                    evalin('caller',[var_name '=dataobj(''' dirs(j).name filesep '*.cdf'');']);
-                end
-                nloaded = nloaded + 1;
-            catch
-                disp(['error loading ' var_name]);
-            end
+      end
+    end
+    if flag_load_variable,
+      try
+        disp(['caa_load ' var_name]);
+        if evalin('caller',['exist(''' var_name ''',''var'')']),
+          disp('Variable exist in memory. NOT LOADING FROM FILE!')
+        else
+          evalin('caller',[var_name '=dataobj(''' caa_data_directory dirs(j).name filesep '*.cdf'');']);
         end
-	end
+        nloaded = nloaded + 1;
+      catch
+        disp(['error loading ' var_name]);
+      end
+    end
+  end
 end
 if nloaded, fprintf('\nCAA_LOAD : loaded %d variables\n',nloaded);
 else disp('CAA_LOAD : nothing to load')
