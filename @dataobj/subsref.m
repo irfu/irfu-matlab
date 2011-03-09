983bcd824c6fb49eb8d6c5ffb7549f4e22d635ce
andris <andris>
2011-03-09 10:18:09 +0000

    improvements

--- @dataobj/subsref.m
+++ @dataobj/subsref.m
@@ -1,18 +1,33 @@
 function b = subsref(a,index)
 %SUBSREF Define field name indexing for dataobj objects
 
-switch index.type
-  case '()'
-    switch index.subs{:}
-      case 1,
-        b=a.GlobalAttributes;
-      case 2,
-        b=a.VariableAttributes;
-      otherwise
-        error('Index out of range')
-    end
-  case '.'
-    b=eval(['a.' index.subs]);
-  case '{}'
-    error('Cell array indexing not supported by dataobj objects')
+expr='';
+for j=1:length(index),
+  switch index(j).type
+    case '()'
+      if j==1,
+        switch index.subs{:}
+          case 1,
+            b=a.GlobalAttributes;
+          case 2,
+            b=a.VariableAttributes;
+          otherwise
+            error('Index out of range')
+        end
+        return
+      else
+        expr=[expr index(j).subs];
+      end
+    case '.'
+      expr=[expr '.' index(j).subs];
+    case '{}'
+      expr=[expr '{'];
+      expr=[expr num2str(index(j).subs{1})];
+      for jj=2:length(index(j).subs),
+        expr=[expr ',' num2str(index(j).subs{jj})];
+      end
+      expr=[expr '}'];
+  end
 end
+%disp(['a' expr]);
+b=eval(['a' expr]);
