8b97497164424738d93284dc66d928037597c272
andris <andris>
2011-03-15 08:25:51 +0000

    CODIF fixed. still problems with HIA.

--- c_caa_construct_subspin_res_data.m
+++ c_caa_construct_subspin_res_data.m
@@ -28,6 +28,7 @@ if strfind(variable_name,'PITCH_3DR'), % PEACE variable
   phi=peace.dep_x{1}.data(1,:);
   theta=peace.dep_x{2}.data(1,:);
   en=peace.dep_x{3}.data(1,:);nan_en=isnan(en);en(nan_en)=[];
+  peace.data(:,:,:,nan_en)=[]; % remove NaN energy data
   dataraw=peace.data;
   t=peace.t(:);
 elseif strfind(variable_name,'RAP_L3DD'), % RAPID variable
@@ -43,6 +44,7 @@ elseif strfind(variable_name,'RAP_L3DD'), % RAPID variable
   theta=10:20:180; % pitch angles
   en=rapid.dep_x{1}.data(1,:);nan_en=isnan(en);en(nan_en)=[];
   rapid.data=permute(rapid.data,[1 4 3 2]); % permute in the order time, polar angle, azimuth angle, energy
+  rapid.data(:,:,:,nan_en)=[]; % remove NaN energy data
   % read pitch angle information
   variable_pitch_name=['Electron_Pitch_' variable_name(regexp(variable_name,'_C?_')+(1:4)) 'CP_RAP_EPITCH'];
   [variable_pitch,dataobject_pitch]=c_caa_var_get(variable_pitch_name);
@@ -51,10 +53,46 @@ elseif strfind(variable_name,'RAP_L3DD'), % RAPID variable
   dataraw=ftheta(rapid.data,rapid_pitch,theta);
   dataraw=permute(dataraw,[1 3 2 4]); % permute in order [time, azimuth, pitch, energery]
   t=rapid.t(:);
+elseif any([strfind(variable_name,'CODIF_HS') strfind(variable_name,'HIA_HS_MAG')]) % CIS variable
+  % CODIF does not have pitch angle matrix data, therefore rebinning
+  % necessary  
+  theta=11.25:22.5:180; % pitch angles to which rebin
+  [variable,dataobject]=c_caa_var_get(variable_name); % check that it is loaded in memory
+  varmat=getmat(dataobject,variable_name);
+  dataunits=getunits(dataobject,variable_name);
+  phivar=c_caa_var_get(variable.DEPEND_2);phi=phivar.data(1,:);
+  polarvar=c_caa_var_get(variable.DEPEND_1);polar=polarvar.data(1,:);
+  envar=c_caa_var_get(variable.DEPEND_3);en=envar.data(1,:);
+  enunits=getfield(getv(dataobject,variable.DEPEND_3),'UNITS');
+  enlabel=enunits;
+  pitchangle=variable.data; % indices are 1) time 2) polar 3) azimuthal 4) energy
+  t=varmat.data(:,1);
+  [tt,dtsampling]=subspintime(dataobject,phi);
+  
+  dd=regexp(variable_name, '__', 'split');
+  ic=str2num(dd{2}(2));
+  c_eval('caa_load C?_CP_FGM_FULL;',ic);
+  c_eval('B=getmat(C?_CP_FGM_FULL,''B_vec_xyz_gse__C?_CP_FGM_FULL'');',ic);
+  B_ISR2=irf_resamp(c_gse2dsc(B,ic,2),tt,'nearest'); % sample to t 
+  b_ISR2=irf_norm(B_ISR2);
+  nsec=zeros(length(polar),length(phi),length(t),3);
+  cosphi=cos(phi/180*pi);sinphi=sin(phi/180*pi);
+  cospolar=cos(polar/180*pi);sinpolar=sin(polar/180*pi);
+  for jphi=1:length(phi),
+    for jpolar=1:length(polar),
+      % nn vector of given sector in ISR2 ref rframe
+      nsector=[cospolar(jpolar).*cosphi(jphi) cospolar(jpolar).*sinphi(jphi) sinpolar(jpolar)];
+      pitchsector=acos(dot(b_ISR2(jphi:length(phi):end,2:4),repmat(nsector,[length(t) 1]),2))*180/pi;
+      pitchangle(:,jpolar,jphi,:)=repmat(pitchsector,[1 1 1 length(en)]);      
+    end
+  end
+  variable.data(variable.data == variable.FILLVAL)=NaN; % FILLVALs put to NaN
+  dataraw=ftheta(variable.data,pitchangle,theta);
+  dataraw=permute(dataraw,[1 3 2 4]); % permute in order [time, azimuth, pitch, energery]  
 end
 
-dataraw(dataraw == variable.FILLVAL)=NaN;
-dataraw(:,:,:,nan_en)=[]; % remove NaN energy data
+% assume dataraw in order [time, azimuth, pitch, energery]
+dataraw(dataraw == variable.FILLVAL)=NaN; % FILLVALs put to NaN
 [en,ix]=sort(en); % sort energy in ascending order
 dataraw=dataraw(:,:,:,ix); % sort data accordingly
 dataraw=permute(dataraw,[2 1 3 4]); % permute the order azimuts, pitch angle, energy
@@ -65,28 +103,12 @@ phiphi=tt;
 
 % Fix permutations of matrix to get high time resolution
 
-timevar=getv(dataobject,dataobject.VariableAttributes.DEPEND_0{1,2});
-if isfield(timevar,'DELTA_PLUS') && isfield(timevar,'DELTA_MINUS')
-  if ischar(timevar.DELTA_PLUS)
-    deltaplus= getv(dataobject,timevar.DELTA_PLUS);
-    deltaminus= getv(dataobject,timevar.DELTA_MINUS);
-    dtplus=deltaplus.data(1,:);
-    dtminus=deltaminus.data(1,:);
-  elseif isnumeric(timevar.DELTA_PLUS)
-    dtplus=getv(dataobject,timevar.DELTA_PLUS);
-    dtminus=getv(dataobject,timevar.DELTA_MINUS);
-  end
-else
-  dtplus=2;
-  dtminus=2;
-end
-spin_period=dtplus+dtminus;
-dtsampling=spin_period/length(phi);
+[tt,dtsampling]=subspintime(dataobject,phi);
+
 for j=length(phi):-1:1,
-  tt(:,j)=tt(:,1)+double(-dtminus+(j-0.5)*dtsampling);
   phiphi(:,j)=phi(j);
 end
-tt=reshape(tt',numel(tt),1);
+
 ind_data = data; ind_data(~isnan(data)) = 1; ind_data(isnan(data)) = 0;
 data_with_nan=data;
 data(isnan(data)) = 0;
@@ -112,6 +134,7 @@ function ftheta=ftheta(fpol,thetapol,theta)
 % rebinning to theta vector
 % assuming fpol second dimension is polar angle
 % rebinnning fpol to ftheta
+% assuming thetapol exactly the same size as fpol
 ftheta_dim=size(fpol);
 ftheta_dim(2)=length(theta);
 ftheta=zeros(ftheta_dim);
@@ -128,4 +151,36 @@ for j=1:length(theta),
   end
 end
 
+end
+function [tt,dtsampling]=subspintime(dataobject,phi)
+% construct subspin time vector
+% phi are azimuthal angles (spin period is divided in the number of azimuth
+% angles)
+timevar=getv(dataobject,dataobject.VariableAttributes.DEPEND_0{1,2});
+tt=timevar.data(:);
+tt=repmat(tt,1,length(phi));
+
+if isfield(timevar,'DELTA_PLUS') && isfield(timevar,'DELTA_MINUS')
+  if ischar(timevar.DELTA_PLUS)
+    deltaplus= getv(dataobject,timevar.DELTA_PLUS);
+    dtplus=deltaplus.data(1,:);
+  elseif isnumeric(timevar.DELTA_PLUS)
+    dtplus=timevar.DELTA_PLUS;
+  end
+  if ischar(timevar.DELTA_MINUS)
+    deltaminus= getv(dataobject,timevar.DELTA_MINUS);
+    dtminus=deltaplus.data(1,:);
+  elseif isnumeric(timevar.DELTA_MINUS)
+    dtminus=timevar.DELTA_MINUS;
+  end
+else
+  dtplus=2;
+  dtminus=2;
+end
+spin_period=dtplus+dtminus;
+dtsampling=spin_period/length(phi);
+for j=length(phi):-1:1,
+  tt(:,j)=tt(:,1)+double(-dtminus+(j-0.5)*dtsampling);
+end
+tt=reshape(tt',numel(tt),1);
 end
