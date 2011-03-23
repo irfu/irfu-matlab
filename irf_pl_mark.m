255574e08a8819b3a7b6e3000e95c2b0d26797ff
andris <andris>
2011-03-23 17:22:33 +0000

    now one can mark also time instants as lines, not only time intervals

--- irf_pl_mark.m
+++ irf_pl_mark.m
@@ -1,4 +1,4 @@
-function [outhandle colr]=irf_pl_mark(tlim,inhandle,color);
+function [outhandle colr]=irf_pl_mark(tlim,inhandle,color)
 %IRF_PL_MARK   Mark that time interval(s) with color backround
 %
 % outhandle=irf_pl_mark(tlim,inhandle,color);
@@ -20,7 +20,10 @@ if nargin == 1, inhandle=gca;end
 if nargin>=2, if isempty(inhandle), inhandle=gca;end;end
 
 % transpose tlim if needed
-if size(tlim,1) == 2 && size(tlim,2) ~= 2, tlim=tlim';end; 
+%if size(tlim,1) == 2 && size(tlim,2) ~= 2, tlim=tlim';end; 
+
+% if mark time instants instead of time intervals (only one time given)
+if size(tlim,2) == 1, tlim(:,2)=tlim(:,1); end
 
 if nargin < 3
   if size(tlim,1) == 1
@@ -31,8 +34,8 @@ if nargin < 3
 end   
 
 % create 1 x n x 3 color matrix
-if ndims(color) == 2
-  color = reshape(color, 1, size(color,1), size(color,2));
+if ischar(color),
+    color = repmat(color, size(tlim,1), 1);
 end
 
 
@@ -44,18 +47,24 @@ tpoints = [tlim(:,1) tlim(:,2) tlim(:,2) tlim(:,1)];
 
 %tlim = reshape( tlim, 1, prod(size(tlim)) );
 
-h = reshape( inhandle, 1, prod(size(inhandle)) );
+h = reshape( inhandle, 1, numel(inhandle) );
+hp=zeros(length(h),size(tlim,1)); % predefine patch handles
 for j=1:length(h)
-  axes( h(j) );
   ylim=get(h(j),'ylim');
   ypoints=zeros(size(tpoints));
   ypoints(:,1:2) = ylim(1);
   ypoints(:,3:4) = ylim(2);
   zpoints = -1 * ones(size(ypoints,1),4); % to put patches under all plots
-  hp(j)=patch(tpoints', ypoints', zpoints', color,'edgecolor','none');
-  fc=get(hp(j),'facecolor');
-  fc=[1 1 1]-([1 1 1]-fc)/3; % make facecolor lighter
-  set(hp(j),'facecolor',fc);
+  for jj=1:size(tpoints,1),
+      if tlim(jj,1)==tlim(jj,2) % draw line instead of patch
+          hp(j,jj)=line(tpoints(jj,1:2), ypoints(jj,[1 3]), zpoints(jj,[1 3]),'color',color(jj,:),'parent',h(j));
+      else % make patch
+          hp(j,jj)=patch(tpoints(jj,:)', ypoints(jj,:)', zpoints(jj,:)', color(jj,:),'edgecolor','none','parent',h(j));
+          fc=get(hp(j,jj),'facecolor');
+          fc=[1 1 1]-([1 1 1]-fc)/3; % make facecolor lighter
+          set(hp(j,jj),'facecolor',fc);
+      end
+  end
   set(h(j),'layer','top');
 end
 
