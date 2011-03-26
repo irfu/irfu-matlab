ff89af381628c857dfd77d7e0c861238962ba4b4
andris <andris>
2011-03-26 13:53:05 +0000

    bug fix for case there is no subplots

--- irf_plot_get_subplot_handles.m
+++ irf_plot_get_subplot_handles.m
@@ -1,15 +1,26 @@
-function h=irf_plot_get_subplot_handles
+function h=irf_plot_get_subplot_handles(figurehandle)
 %IRF_PLOT_GET_SUBPLOT_HANDLES return subplot handles of current figure
 
-  htmp=findobj(gcf,'type','axes','-not','tag','Colorbar');
-  hmax=1;
-  for ih=1:length(htmp),
-    ax=get(htmp(ih),'position');
-    axy(ih)=ax(2);axx(ih)=ax(1);
-  end
-  ind_ax=find(axx<0.2); % find 
-  hax=htmp(ind_ax);
-  [xx,ind]=sort(axy(ind_ax));
-  ind=fliplr(ind);
-  h=hax(ind);
+if nargin==0, figurehandle=gcf;end
+
+htmp=findobj(figurehandle,'type','axes','-not','tag','Colorbar');
+number_of_subplots=numel(htmp);
+
+if number_of_subplots>=1, % if there are subplots
+    axy=zeros(1,number_of_subplots);
+    axx=axy;
+    for ih=1:length(htmp),
+        ax=get(htmp(ih),'position');
+        axy(ih)=ax(2);
+        axx(ih)=ax(1);
+    end
+    ind_ax=find(axx<0.2); % find
+    hax=htmp(ind_ax);
+    [~,ind]=sort(axy(ind_ax));
+    ind=fliplr(ind);
+    h=hax(ind);
+else
+    h=[];
+end
+
 
