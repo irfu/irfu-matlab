03a22b81fd86732fa896aea1266f3b3f4f1bcf7c
andris <andris>
2011-03-28 06:42:57 +0000

    minor improvement

--- irf_pl_number_subplots.m
+++ irf_pl_number_subplots.m
@@ -1,17 +1,20 @@
 function ht=irf_pl_number_subplots(h,position,varargin)
 % ht=irf_pl_number_subplots(h,position,text_property,text_value,...)
 % 
-% number subplots by putting in each subplot 'a', 'b', 'c', etc.
+% number subplots by putting in each subplot 'a)', 'b)', 'c)', etc.
 % INPUT 
 %   h           handles to subplots
 %   position    position of legend
 % any text property/value pairs can be passed 
 
-if nargin==1, position=[0.01, 0.97];end
+if nargin<=1, position=[0.01, 0.97];end
+if nargin==0, h=irf_plot_get_subplot_handles; end
 
 h=h(:);ht=h;
 abc='a':'z';
 for j=1:length(h)
-  hleg=irf_legend(h(j),abc(j),position,varargin{:});
+  hleg=irf_legend(h(j),[abc(j) ')'],position,varargin{:});
   ht(j)=hleg;
 end
+
+if nargout==0, clear ht;end
