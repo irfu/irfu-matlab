ad9ec83a5ed074d5263505d1d2527f99606a074b
andris <andris>
2011-03-30 07:49:46 +0000

    input parameters compliant to matlab (axis first)

--- irf_pl_mark.m
+++ irf_pl_mark.m
@@ -1,37 +1,61 @@
-function [outhandle colr]=irf_pl_mark(tlim,inhandle,color,varargin)
-%IRF_PL_MARK   Mark that time interval(s) with color backround
+function [outhandle colr]=irf_pl_mark(varargin)
+%IRF_PL_MARK   Mark time intervals or instants
+% Marks time intervals with transparent rectangle or time instants with line
 %
-% outhandle=irf_pl_mark(tlim,inhandle,color);
-% mark that time interval(s) with color backround
-% output is the handle of the patch
+%   IRF_PL_MARK(tlim) mark time interval
 %
-% [outhandle color]=irf_pl_mark(tlim,inhandle);
-% returns also the color table
+%   IRF_PL_MARK(AX,tlim) mark in the specified axis (default gca)
+% 
+%   IRF_PL_MARK(AX,tlim,color) mark with the specified color 
+% 
+%   IRF_PL_MARK(AX,tlim,color,'Property1',PropertyValue1,..) specify properties 
+% 
+%   [H,COLOR]=IRF_PL_MARK(...) returns handle to patch or line and the color table 
 %
-% tlim - time interval or array of intervals to mark
-% inhandle - is the handle of axes
+% tlim - time interval or array of intervals to mark, or column vector of time instants
 % color - string, rgb 1x3, nx3, or 1xnx3 specifying color(s); 
 %         if omitted colors are chosen randomly.
 %
+%
+% WARNING!!! IRF_PL_MARK has changed (2011-03-30) the order of input parameters
+%  now more compliant to MATLAB
+
 % $Id$
 
-if nargin<1, help irf_pl_mark;return;end
-if nargin == 1, inhandle=gca;end
-if nargin>=2, if isempty(inhandle), inhandle=gca;end;end
 
-% transpose tlim if needed
-%if size(tlim,1) == 2 && size(tlim,2) ~= 2, tlim=tlim';end; 
+[ax,args,nargs] = axescheck(varargin{:});
+if isempty(ax),
+    ax=gca;
+end
+if nargs == 0, 
+    help irf_pl_mark;
+    return
+else
+    tlim=args{1};
+    args=args(2:end);
+    % if mark time instants instead of time intervals (only one time given)
+    if size(tlim,2) == 1, tlim(:,2)=tlim(:,1); end
+end
 
-% if mark time instants instead of time intervals (only one time given)
-if size(tlim,2) == 1, tlim(:,2)=tlim(:,1); end
-
-if nargin < 3
+if nargs == 1, % if only time intervals given, specify color
   if size(tlim,1) == 1
     color='yellow';
   else % choose random colors
     color=rand(size(tlim,1),3);
   end
-end   
+end
+
+if nargs >= 2, % color i specified
+    color = args{2};
+end
+
+if nargs > 2 && (rem(nargs,2) ~= 0)
+  error('IRFU-MATLAB:irf_pl_mark:InvalidNumberOfInputs','Incorrect number of input arguments')
+end
+
+% properties specified
+pvpairs = args(3:end);
+
 
 % create 1 x n x 3 color matrix
 if ischar(color),
@@ -47,22 +71,23 @@ tpoints = [tlim(:,1) tlim(:,2) tlim(:,2) tlim(:,1)];
 
 %tlim = reshape( tlim, 1, prod(size(tlim)) );
 
-h = reshape( inhandle, 1, numel(inhandle) );
+h = reshape( ax, 1, numel(ax) );
 hp=zeros(length(h),size(tlim,1)); % predefine patch handles
 for j=1:length(h)
   ylim=get(h(j),'ylim');
   ypoints=zeros(size(tpoints));
   ypoints(:,1:2) = ylim(1);
   ypoints(:,3:4) = ylim(2);
-  zpoints = -1 * ones(size(ypoints,1),4); % to put patches under all plots
+  zpoints = zeros(size(ypoints,1),4); % to put patches under all plots
   for jj=1:size(tpoints,1),
       if tlim(jj,1)==tlim(jj,2) % draw line instead of patch (in this case draw line above everything, therefore "+2" in the next line)
-          hp(j,jj)=line(tpoints(jj,1:2), ypoints(jj,[1 3]), zpoints(jj,[1 3])+2,'color',color(jj,:),'parent',h(j),varargin{:});
+          hp(j,jj)=line(tpoints(jj,1:2), ypoints(jj,[1 3]), zpoints(jj,[1 3])+1,'color',color(jj,:),'parent',h(j),pvpairs{:});
       else % make patch
-          hp(j,jj)=patch(tpoints(jj,:)', ypoints(jj,:)', zpoints(jj,:)', color(jj,:),'edgecolor','none','parent',h(j),varargin{:});
-          fc=get(hp(j,jj),'facecolor');
-          fc=[1 1 1]-([1 1 1]-fc)/3; % make facecolor lighter
-          set(hp(j,jj),'facecolor',fc);
+%          hp(j,jj)=patch(tpoints(jj,:)', ypoints(jj,:)', zpoints(jj,:)', color(jj,:),'edgecolor','none','parent',h(j),varargin{:});
+          hp(j,jj)=patch(tpoints(jj,:)', ypoints(jj,:)', zpoints(jj,:)', color(jj,:),'edgecolor','none','parent',h(j),'facealpha',.3,pvpairs{:});
+%          fc=get(hp(j,jj),'facecolor');
+%          fc=[1 1 1]-([1 1 1]-fc)/3; % make facecolor lighter
+%          set(hp(j,jj),'facecolor',fc);
       end
   end
   set(h(j),'layer','top');
