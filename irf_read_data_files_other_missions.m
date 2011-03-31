4854c0aa04f75adc97ca899efd8d0738af060a70
andris <andris>
2011-03-31 16:34:42 +0000

    improved reading of IAGA2002 format

--- irf_read_data_files_other_missions.m
+++ irf_read_data_files_other_missions.m
@@ -20,13 +20,31 @@ switch lower(data_source)
         res.n=C{16};
     case 'iaga2002'
         fid = fopen(file_name);
-        C = textscan(fid,'%f-%f-%f %f:%f:%f %*f %f %f %f %f','headerlines',15);
-        fclose(fid);
-        res.t=toepoch([C{1} C{2} C{3} C{4} C{5} C{6}]); % time in isdat epoch
-        res.AE=C{7};
-        res.AU=C{8};
-        res.AL=C{9};
-        res.AO=C{10};
+        for j=1:3, fgetl(fid);end
+        iagacode=textscan(fgetl(fid),'%s');
+        for j=5:14, fgetl(fid);end
+        header=textscan(fgetl(fid),'%s');
+        %        C = textscan(fid,'%f-%f-%f %f:%f:%f %*f %f %f %f %f','headerlines',15);
+        switch lower(iagacode{1}{3})
+            case 'ae'
+                C = textscan(fid,'%f-%f-%f %f:%f:%f %*f %f %f %f %f');
+                fclose(fid);
+                res.t=toepoch([C{1} C{2} C{3} C{4} C{5} C{6}]); % time in isdat epoch
+                res.AE=C{7};
+                res.AU=C{8};
+                res.AL=C{9};
+                res.AO=C{10};
+            otherwise % assume magnetometer data
+                res.station=iagacode{1}{3}; % magnetometer station name
+                C = textscan(fid,'%f-%f-%f %f:%f:%f %*f %f %f %f %f');
+                fclose(fid);                
+                res.t=toepoch([C{1} C{2} C{3} C{4} C{5} C{6}]); % time in isdat epoch
+                res.D=C{7};
+                res.H=C{8};
+                res.Z=C{9};
+                res.F=C{10};
+        end
+        
     otherwise
         disp('Data source not recognized');
         disp('Reading assuming first row is variable, second units and then comes data');
