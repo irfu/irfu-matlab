2a53bd80f5104b6d7ae5054c8ad0e8bfda99e4ab
andris <andris>
2011-05-24 19:04:54 +0000

    *** empty log message ***

--- irf_isdat_get.m
+++ irf_isdat_get.m
@@ -1,27 +1,32 @@
-function [t,res]=irf_isdat_get(datasource,st,dt)
+function [t,res]=irf_isdat_get(datasource,start_time,dt_interval)
 % getting data from isdat (using ISDAT.jar)
 % FUNCTIONS STILL IN DEVELOPMENT, do not use
 % [t,res]=irf_isdat_get('Cluster/1/ephemeris/position','2002-03-04T10:00:00.000Z',60)
 % irf_stdt to get st,dt
 
 %% Fix path
-wd=which('irf_plot.m');
-ii=strfind(wd,filesep);
-wdir=wd(1:ii(end));
-javaclasspath([wdir 'ISDAT.jar']);
+if isempty(strfind(javaclasspath,'ISDAT.jar'))
+    wd=which('irf_plot.m');
+    ii=strfind(wd,filesep);
+    wdir=wd(1:ii(end));
+    javaclasspath([wdir 'ISDAT.jar']);
+end
 
 %% Get data from isdat
 %Open connection
 isdatConnection = ISDAT.NetConnection('db.irfu.se',0);
-dataSrcName=ISDAT.DataSourceName('Cluster/1/ephemeris/position');
+dataSrcName=ISDAT.DataSourceName(datasource);
+%dataSrcName=ISDAT.DataSourceName('Cluster/1/ephemeris/position');
 %dataSrcName=ISDAT.DataSourceName('Cluster/1/efw/E/p4/10Hz/lx');
 
 isdatDbSession=ISDAT.DbSession(isdatConnection);
 dataSrc=isdatDbSession.SourceName2ID(dataSrcName);
 %isdatDbSession.nameThisSourceFully(dataSrc)
 
-st = ISDAT.Epoch('2002-03-04T10:00:00.000Z');
-dt = ISDAT.Epoch(2.0);
+st = ISDAT.Epoch(start_time);
+%st = ISDAT.Epoch('2002-03-04T10:00:00.000Z');
+dt = ISDAT.Epoch(dt_interval);
+%dt = ISDAT.Epoch(2.0);
 
 dataReq = ISDAT.DbDataRequest(dataSrc,st,dt);
                         
