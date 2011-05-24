4e9e5424f6c226939aedea3c3c073863ee58bdb3
yuri <yuri>
2011-05-24 13:35:57 +0000

    *** empty log message ***

--- irf_isdat_get.m
+++ irf_isdat_get.m
@@ -1,14 +1,15 @@
 function [t,res]=irf_isdat_get(datasource,st,dt)
-% getting data from isdat (using ISDAT.rar)
+% getting data from isdat (using ISDAT.jar)
 % FUNCTIONS STILL IN DEVELOPMENT, do not use
 % [t,res]=irf_isdat_get('Cluster/1/ephemeris/position','2002-03-04T10:00:00.000Z',60)
 
-
+%% Fix path
 wd=which('irf_plot.m');
 ii=strfind(wd,filesep);
 wdir=wd(1:ii(end));
 javaclasspath([wdir 'ISDAT.jar']);
 
+%% Get data from isdat
 %Open connection
 isdatConnection = ISDAT.NetConnection('db.irfu.se',0);
 dataSrcName=ISDAT.DataSourceName('Cluster/1/ephemeris/position');
@@ -16,14 +17,25 @@ dataSrcName=ISDAT.DataSourceName('Cluster/1/ephemeris/position');
 
 isdatDbSession=ISDAT.DbSession(isdatConnection);
 dataSrc=isdatDbSession.SourceName2ID(dataSrcName);
-isdatDbSession.nameThisSourceFully(dataSrc)
+%isdatDbSession.nameThisSourceFully(dataSrc)
 
 st = ISDAT.Epoch('2002-03-04T10:00:00.000Z');
 dt = ISDAT.Epoch(2.0);
 
 dataReq = ISDAT.DbDataRequest(dataSrc,st,dt);
                         
-res = isdatDbSession.getSciData(dataReq);
-disp(sprintf('totNrSamples : %d',res.totNrSamples))
+sciData = isdatDbSession.getSciData(dataReq);
+%disp(sprintf('totNrSamples : %d',sciData.totNrSamples))
 
 isdatConnection.close()
+
+%% Prepare data
+res = []; t = [];
+for seg = 1:sciData.segments
+    data = reshape(sciData.data(seg,:),...
+        length(sciData.data(seg,:))/sciData.samples(seg),...
+        sciData.samples(seg))';
+    res = [res; data];
+    time = ISDAT.Epoch.toDoubleArray(sciData.time(seg));
+    t = [t; time];
+end
