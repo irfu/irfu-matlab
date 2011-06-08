12361726c085f83498f7ffde27910b850b27622e
andris <andris>
2011-06-08 17:13:27 +0000

    added DMSP magnetometer data and fixed bug in reading DMSP driftmeter data (date became wrong by one day)

--- irf_read_data_files_other_missions.m
+++ irf_read_data_files_other_missions.m
@@ -13,11 +13,24 @@ switch lower(data_source)
         year=1900+floor(C{1}/1000); %
         zero=zeros(size(year));
         one=zero+1;
-        res.t=toepoch([year one one zero zero zero])+mod(C{1},1000)*24*3600+C{2}; % time in isdat epoch
+        res.t=toepoch([year one one zero zero zero])+(mod(C{1},1000)-1)*24*3600+C{2}; % time in isdat epoch
         res.Vx=C{10};res.Vx(res.Vx==-9999.0)=NaN;
         res.Vy=C{11};res.Vx(res.Vy==-9999.0)=NaN;
         res.Vz=C{12};res.Vx(res.Vz==-9999.0)=NaN;
         res.n=C{16};
+    case 'dmsp_ssm'
+        fid = fopen(file_name);
+        C = textscan(fid,'%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f','headerlines',28);
+        fclose(fid);
+        res.t=irf_epoch([C{1} C{2} C{3} C{4} C{5} C{6}]); % time in isdat epoch
+        res.lat=C{7};
+        res.long=C{8};
+        res.alt=C{9};
+        res.mdiplat=C{10};
+        res.mlat=C{11};
+        res.mlt=C{12};
+        res.Bx=C{13};res.By=C{14};res.Bz=C{15};res.B=C{16};
+        res.dBx=C{17};res.dBy=C{18};res.dBz=C{19};res.dB=C{20};
     case 'iaga2002'
         fid = fopen(file_name);
         for j=1:3, fgetl(fid);end
