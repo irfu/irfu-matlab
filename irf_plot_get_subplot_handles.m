419e7ab614f3dcf031624707faafddba3913063c
andris <andris>
2011-07-01 12:21:54 +0000

    using figures 'userdata'.subplot_handles if such exists

--- irf_plot_get_subplot_handles.m
+++ irf_plot_get_subplot_handles.m
@@ -3,24 +3,30 @@ function h=irf_plot_get_subplot_handles(figurehandle)
 
 if nargin==0, figurehandle=gcf;end
 
-htmp=findobj(figurehandle,'type','axes','-not','tag','Colorbar');
-number_of_subplots=numel(htmp);
-
-if number_of_subplots>=1, % if there are subplots
-    axy=zeros(1,number_of_subplots);
-    axx=axy;
-    for ih=1:length(htmp),
-        ax=get(htmp(ih),'position');
-        axy(ih)=ax(2);
-        axx(ih)=ax(1);
-    end
-    ind_ax=find(axx<0.2); % find
-    hax=htmp(ind_ax);
-    [~,ind]=sort(axy(ind_ax));
-    ind=fliplr(ind);
-    h=hax(ind);
+ud=get(figurehandle,'userdata');
+if isfield(ud,'subplot_handles')
+    h=ud.subplot_handles;
 else
-    h=[];
+    
+    htmp=findobj(figurehandle,'type','axes','-not','tag','Colorbar');
+    number_of_subplots=numel(htmp);
+    
+    if number_of_subplots>=1, % if there are subplots
+        axy=zeros(1,number_of_subplots);
+        axx=axy;
+        for ih=1:length(htmp),
+            ax=get(htmp(ih),'position');
+            axy(ih)=ax(2);
+            axx(ih)=ax(1);
+        end
+        ind_ax=find(axx<0.2); % find
+        hax=htmp(ind_ax);
+        [~,ind]=sort(axy(ind_ax));
+        ind=fliplr(ind);
+        h=hax(ind);
+    else
+        h=[];
+    end
 end
 
 
