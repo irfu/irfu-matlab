fb1f971beffa68eed9c12bb18de8bb46bd3ff0d2
yuri <yuri>
2011-08-22 15:03:37 +0000

    fix a typo bug

--- c_gse2dsc.m
+++ c_gse2dsc.m
@@ -91,7 +91,7 @@ if nargin == 4, flag_db=1; else flag_db=0;                           end
      end
      if flag_read_isdat,  % try if there are auxilaruy files from CAA
        caa_load CL_SP_AUX
-       if isvarname('CL_SP_AUX'), % succeeded to load CAA data files
+       if exist('CL_SP_AUX','var'), % succeeded to load CAA data files
          c_eval('lat=getmat(CL_SP_AUX,''sc_at?_lat__CL_SP_AUX'');',ic);
          c_eval('long=getmat(CL_SP_AUX,''sc_at?_long__CL_SP_AUX'');',ic);
          if (t > lat(1,1)-60) && (t < lat(end,1)+60),
