cc8e1a124a5b12d324d4115e77803c75e7849934
andris <andris>
2011-09-22 20:20:45 +0000

    added additional minvar method, td (see help)

--- irf_minvar.m
+++ irf_minvar.m
@@ -1,4 +1,4 @@
-function [out,l,v]=irf_minvar(inp)
+function [out,l,v]=irf_minvar(inp,flag)
 %IRF_MINVAR minimum variance analysis
 %
 % [out,l,v]=irf_minvar(inp)
@@ -9,9 +9,16 @@ function [out,l,v]=irf_minvar(inp)
 % v - column vector with corresponding eigenvectors
 % v(1,:) - first vector, ...
 %
+% [out,l,v]=irf_minvar(inp,'td') minimum variance under tangential discontinuity
+% constraint (minimizing sum of squares of normal component), Eq. 8.17 ISSI book
+%
 % See also IRF_MINVAR_GUI, IRF_MINVAR_NESTED, IRF_MINVAR_NESTED_GUI
 %
 % $Id$
+
+if nargin==1,
+    flag='mvar'; % default is to do unconstrained minimum variance
+end
 
 ooo=inp;
 qx = size(inp(1,:)); lx=qx(2); % the number of vector components
@@ -24,8 +31,15 @@ end
 inp_m=mean(inp);
 Mm2=inp_m([1 2 3 1 1 2]).*inp_m([1 2 3 2 3 3]);
 Mm1=mean(inp(:,[1 2 3 1 1 2]).*inp(:,[1 2 3 2 3 3])); % all 6 elements of triagonal matrix
-Mm=Mm1-Mm2;
-M=[[Mm(1) Mm(4) Mm(5)]' [Mm(4) Mm(2) Mm(6)]' [Mm(5) Mm(6) Mm(3)]'];
+
+switch lower(flag)
+    case 'mvar'
+        Mm=Mm1-Mm2;
+        M=[[Mm(1) Mm(4) Mm(5)]' [Mm(4) Mm(2) Mm(6)]' [Mm(5) Mm(6) Mm(3)]'];
+    case 'td'
+        Mm=Mm1;
+        M=[[Mm(1) Mm(4) Mm(5)]' [Mm(4) Mm(2) Mm(6)]' [Mm(5) Mm(6) Mm(3)]'];
+end
 
 %    [V,D] = EIG(X) produces a diagonal matrix D of eigenvalues and a
 %    full matrix V whose columns are the corresponding eigenvectors so
@@ -45,9 +59,9 @@ l(3)=max(ll);
 out=(v*inp')';
 
 if nargout < 1,
-disp(strcat('l1=',num2str(l(1),3),' v1=[',num2str(v(1,1),3),', ',num2str(v(1,2),3),', ',num2str(v(1,3),3),']'))
-disp(strcat('l2=',num2str(l(2),3),' v2=[',num2str(v(2,1),3),', ',num2str(v(2,2),3),', ',num2str(v(2,3),3),']'))
-disp(strcat('l3=',num2str(l(3),3),' v3=[',num2str(v(3,1),3),', ',num2str(v(3,2),3),', ',num2str(v(3,3),3),']'))
+disp(strcat('   max l1=',num2str(l(1),3),'v1=[',num2str(v(1,1),3),', ',num2str(v(1,2),3),', ',num2str(v(1,3),3),']'))
+disp(strcat('interm l2=',num2str(l(2),3),'v2=[',num2str(v(2,1),3),', ',num2str(v(2,2),3),', ',num2str(v(2,3),3),']'))
+disp(strcat('   min l3=',num2str(l(3),3),'v3=[',num2str(v(3,1),3),', ',num2str(v(3,2),3),', ',num2str(v(3,3),3),']'))
 end
 
 if lx>3, ooo(:,[2 3 4])=out;out=ooo; end
