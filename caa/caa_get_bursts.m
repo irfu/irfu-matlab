8ed9c298adc5537ab652062116f79134bf4c7c88
yuri <yuri>
2011-10-13 20:00:04 +0000

    do not save any intermediate data move out of order detection into separate function

--- caa/caa_get_bursts.m
+++ caa/caa_get_bursts.m
@@ -22,16 +22,12 @@ end
 old_pwd = pwd;
 
 ret=1;
-
 plot_save=1;
 plotpath=[getenv('HOME') '/figures/'];
-
 flag_save = 1;
-save_list = '';
 
 DP = c_ctl(0,'data_path');
 DB = c_ctl(0,'isdat_db');
-
 B_DT = 300;
 B_DELTA = 60;
 
@@ -118,234 +114,93 @@ for out = 1:varsbsize;
     
     [t,data] = caa_is_get(DB,st-B_DELTA,B_DT,cl_id,instrument,field,...
         sen,filter,'burst','tm');
-    start_satt = c_efw_burst_chkt(DB,[DP '/burst/' filename]);
-    if isempty(start_satt)
-        irf_log('dsrc','burst start time was not corrected')
-    elseif isempty(t)
-        irf_log('proc','t is empty. no iburst data?!');
-        cd(old_pwd);
-        return;
-    else
-        err_t = t(1) - start_satt;
-        irf_log('dsrc',['burst start time was corrected by ' ...
-            num2str(err_t) ' sec'])
-        t = t - err_t;
-    end
-    d_phys=data*0.00212;
-    data_phys = [t d_phys];
     
     if (out==1) % Create data matrix for t and all 8 possible variables
         if size(t,1)<3 || size(data,1)<3 % sanity check
             irf_log('proc','No usable burst data');
             return;
         end
-        dlen=size(data,1);
-        data8=NaN(dlen,9);
+        data8 = NaN(size(data,1),9);
+        
+        start_satt = c_efw_burst_chkt(DB,[DP '/burst/' filename]);
+        if isempty(start_satt)
+            irf_log('dsrc','burst start time was not corrected')
+        elseif isempty(t)
+            irf_log('proc','t is empty. no iburst data?!');
+            cd(old_pwd);
+            return;
+        else
+            err_t = t(1) - start_satt;
+            irf_log('dsrc',['burst start time was corrected by ' ...
+                num2str(err_t) ' sec'])
+            t = t - err_t;
+        end
         data8(:,1)=t;   % corrected time
     end
     data8(:,out+1)=data;
-    
-    
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %%%%%                                                  %%%%%%%%%%%%%%%%%%%%%
-    %%%%%    Saving the data to mEFWburstTM/mEFWburstR1    %%%%%%%%%%%%%%%%%%%%%
-    %%%%%                                                  %%%%%%%%%%%%%%%%%%%%%
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    
-    save_file = './mEFWburstTM.mat';
-    save_list = '';
-    if ~isempty(data)
-        data = [t data]; %#ok<NASGU,AGROW>
-        if vt(1)=='S'
-            eval(irf_ssub(['tm?!b$=data;' 'save_list=[save_list ''tm?!b$ ''];'],filter,cl_id,probe));
-        else
-            eval(irf_ssub(['tm?!p$=data;' 'save_list=[save_list ''tm?!p$ ''];'],filter,cl_id,probe));
-        end
-    else
-    end
-    if flag_save==1 && ~isempty(save_list) && ~isempty(save_file)
-        irf_log('save',[save_list ' -> ' save_file])
-        if exist(save_file,'file')
-            eval(['save -append ' save_file ' ' save_list]);
-        else
-            eval(['save ' save_file ' ' save_list]);
-        end
-    end
-    
-    
-    if field=='E'
-        data = data_phys; %#ok<NASGU>
-    elseif field=='B'
-        continue;
-    elseif strcmp(field,'dB')
-        continue;
-    else
-        disp(['Info: Unknown field ' field]);
-    end
-    
-    save_file = './mEFWburstR1.mat';
-    save_list = '';
-    data = data_phys; %#ok<NASGU>
-    eval(irf_ssub(['PP?!p$=data;' 'save_list=[save_list ''PP?!p$ ''];'],filter,cl_id,probe));
-    
-    if flag_save==1 && ~isempty(save_list) && ~isempty(save_file)
-        irf_log('save',[save_list ' -> ' save_file])
-        if exist(save_file,'file')
-            eval(['save -append ' save_file ' ' save_list]);
-        else
-            eval(['save ' save_file ' ' save_list]);
-        end
-    end
 end
+clear data
         
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%                                      %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%    Checking the order of the data    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%                                      %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-                    
-[ok,pha] = c_load('Atwo?',cl_id);
-if ~ok || isempty(pha)
-    irf_log('load',...
-        irf_ssub('No/empty Atwo? in mA. Use getData(CDB,...,cl_id,''a'')',cl_id))
-end
-
-%test = load('mEFWburstTM.mat');
-%load('mEFWburstTM.mat')
-%load('mER.mat')
-%fn = fieldnames(test);
-%bla=char(fn);
-%cc=size(bla);
-test2 = load('mEFWburstR1.mat');
-%load('mEFWburstR1.mat')
-load('mER.mat')
-fn2 = fieldnames(test2);
-bla2=char(fn2);
-cc2=size(bla2);
-ff=0;
-
-if cc2(1)>=4
-    if exist(irf_ssub('wE?p!',cl_id,12),'var')
-        %        name11 = irf_ssub('PP?!p$',filter,cl_id,1);
-        %        name22 = irf_ssub('PP?!p$',filter,cl_id,2);
-        nmdata=eval(irf_ssub('wE?p!',cl_id,12));   %(t1(:,2)-t2(:,2))/88;
-        probev=1;
-    elseif exist(irf_ssub('wE?p!',cl_id,34),'var')
-        %        name11 = irf_ssub('PP?!p$',filter,cl_id,3);
-        %        name22 = irf_ssub('PP?!p$',filter,cl_id,4);
-        nmdata=eval(irf_ssub('wE?p!',cl_id,34));   %(t1(:,2)-t2(:,2))/88;
-        probev=3;
-    else
-        nmdata=eval(irf_ssub('wE?p!',cl_id,32));   %(t1(:,2)-t2(:,2))/88;
-        probev=2;
+if varsbsize>=2
+    can_check_order = 1;
+    [ok,pha] = c_load('Atwo?',cl_id);
+    if ~ok || isempty(pha)
+        irf_log('load',...
+            irf_ssub('No/empty Atwo? in mA. Use getData(CDB,...,cl_id,''a'')',cl_id))
+        can_check_order = 0;
     end
     
-    % ff gg di
-    bestguess=[-1 -1 -1];
-    for di=1:2:varsbsize
-        t1(:,1:2)=data8(:,[1 di+1]);
-        t1m=mean(t1(:,2));
-        t2(:,1:2)=data8(:,[1 di+2]);
-        t2m=mean(t2(:,2));
-        %di
-        divf=t2m/t1m;
-        %    if divf<0.67 || divf>1.5    % different data types skip
-        if divf<0.5 || divf>2    % different data types skip
-            %divf
-            continue;
-        end
-        aa1=c_phase(nmdata(:,1),pha);
-        if isempty(aa1)
-            continue;
-        end
-        sp1=c_efw_sfit(12,3,10,20,nmdata(:,1),nmdata(:,2),aa1(:,1),aa1(:,2),1,'hx'); % org sfit2
-        distance=88;
-        tt2=(t1(:,2)-t2(:,2))/distance;
-        aa2=c_phase(t2(:,1),pha);
-        if isempty(aa2)
-            continue;
-        end
-        sp2=c_efw_sfit(12,3,10,20,t2(:,1),tt2,aa2(:,1),aa2(:,2),1,'ib');       % org sfit2
-        [a,~] = size(sp2);
-        gg=0;
-        i=1;
-        
-        while i<a+1
-            [c,]=find(sp1==sp2(i,1));
-            ex1=sp1(c,2);
-            ex2=sp2(i,2);
-            ey1=sp1(c,3);
-            ey2=sp2(i,3);
-            
-            %timevec=fromepoch(sp2(i,1));
-            z1=atan2(ey1,ex1);
-            z2=atan2(ey2,ex2);
-            y=round(abs(((z1-z2)/pi)*180));
-            
-            if ~isempty(y)
-                if isnan(y)==1
-                    %                irf_log('proc','NaN');
-                elseif  25<y && y<155
-                    gg=gg+1;
-                elseif  205<y && y<335
-                    gg=gg+1;
-                else
-                    ff=ff+1;
-                    %                irf_log('proc','the data match');
-                end
+    if can_check_order
+        % See which single-ended probes we got in the burst, e.g. V1H, V2H...
+        probes = [];
+        for i=1:length(varsb)
+            if varsb{i}(1)=='V' && ischar(varsb{i}(3))
+                probes = [probes str2double(varsb{i}(2))]; %#ok<AGROW>
             end
-            
-            i=i+1;
-            
         end
-        
-        % Remember best guess so far
-        %    if (ff-gg>bestguess(1)-bestguess(2)) || (ff-gg==bestguess(1)-bestguess(2) && gg<bestguess(2))
-        if (ff>bestguess(1) && gg==0)
-            bestguess=[ff gg di];
+        if isempty(probes)
+            can_check_order = 0;
+            irf_log('proc','Cannot check burst order: no single-ended probes')
         end
     end
-    %bestguess
-    xy=1:varsbsize;
-    svar=irf_ssub('V?',probev);
-    svar1=irf_ssub('V?',probev+1);
-    pfound=false;
-    % find variable position
-    for pos=1:varsbsize-1
-        if strcmp(varsb{pos}(1:2),svar) && length(varsb{pos})==3 && strcmp(varsb{pos+1}(1:2),svar1) && length(varsb{pos+1})==3
-            pfound=true;
-            break;
+    
+    if can_check_order
+        % See which NM data we may use
+        probep = [];
+        if any(probes==3) && any(probes==4), probep = 34; end
+        if any(probes==1) && any(probes==2), probep = [probep 12]; end
+        if any(probes==3) && any(probes==2), probep = [probep 32]; end
+        if isempty(probep)
+            can_check_order = 0;
+            irf_log('proc','Cannot check burst order: no useful probe pairs')
         end
     end
-    data8ord=data8; % assume no order change
-    %bestguess(3)=3;
-    %pos
-    if ~pfound || bestguess(1)==-1
-        irf_log('proc','Can not find burst order. standard order 1-n used');
-    elseif pos~=bestguess(3)
-        % make order vector
-        pcnt=pos;
-        for j=bestguess(3):varsbsize
-            xy(j)=pcnt;
-            pcnt=pcnt+1;
-            if pcnt>varsbsize
-                pcnt=1;
+    
+    if can_check_order
+        % Load NM data
+        for i=1:length(probep)
+            [ok,nmdata] = c_load(irf_ssub('wE?p!',cl_id,probep(i)));
+            if ok && ~isempty(nmdata)
+                ref_probep = probep(i);
+                irf_log('proc',sprintf('Using p%d NM data as reference',probep(i)))
+                break
             end
         end
-        if bestguess(3)>1
-            pcnt=pos-1;
-            for j=bestguess(3)-1:-1:1
-                if pcnt<1
-                    pcnt=varsbsize;
-                end
-                xy(j)=pcnt;
-                pcnt=pcnt-1;
-            end
+        if isempty(nmdata)
+            can_check_order = 0;
+            irf_log('proc','Cannot check burst order: no reference NM data')
         end
-        % order data
-        for j=1:varsbsize
-            data8ord(:,xy(j)+1)=data8(:,j+1);
-        end
+    end
+    
+    if can_check_order
+        data8ord = c_efw_burst_order_signals(data8, varsb, nmdata, pha, ref_probep);
+    else
+        data8ord = data8;
     end
 else
     data8ord=data8; % assume no order change
@@ -449,10 +304,7 @@ getData(cp,cl_id,'pburst');
 getData(cp,cl_id,'dieburst');
 getData(cp,cl_id,'dibscburst');
 
-if plot_flag
-    
-    
-    
+if plot_flag  
     st=data8ordfc(1,1);
     sp=data8ordfc(end,1);
     if st==-Inf || isnan(st)
@@ -474,10 +326,7 @@ if plot_flag
 end
 
 ret=0;
-delete('mEFWburstR1.mat'); %Remove some files
-delete('mEFWburstTM.mat');
 cd(old_pwd);
-
 end
 
 function filt = get_filter(ibstr)
