41b8ed4303a534bbf86151d93e43d89d00f4f2d9
andris <andris>
2011-11-12 08:59:51 +0000

    findstr > strfind also had to include fixes for the THEMIS cdf files (MATLAB cdfread does not work properly on those )

--- irf_cdf_read.m
+++ irf_cdf_read.m
@@ -54,8 +54,8 @@ if nargin==0,
   end
 end
 
-if findstr(cdf_file,'*'), % use wilcard '*' expansion
-  ii=findstr(cdf_file,'/');
+if strfind(cdf_file,'*'), % use wilcard '*' expansion
+  ii=strfind(cdf_file,filesep);
   if ii, cdf_directory=cdf_file(1:max(ii)); else cdf_directory=''; end
   ff=dir(cdf_file);
   switch size(ff,1)
@@ -89,17 +89,20 @@ irf_log('load',['cdf file: ' cdf_file]);
 
 cdf_file_info = cdfinfo(cdf_file);
 variable_names = cdf_file_info.Variables(:,1);
-epoch_column=0; % defualt there is no epoch variable
+epoch_column=0; % default there is no epoch variable
 for j=1:size(variable_names,1),
-	if any(findstr(variable_names{j,1},'Epoch')) || ...
-			any(findstr(variable_names{j,1},'time_tags__')) || ...
-			any(findstr(variable_names{j,1},'Time__'))
+	if any(strfind(variable_names{j,1},'Epoch')) || ...
+			any(strfind(variable_names{j,1},'time_tags__')) || ...
+			any(strfind(variable_names{j,1},'_time')) || ...
+			any(strfind(variable_names{j,1},'Time__'))
 		epoch_variable=variable_names(j,1);
 		%disp(['epoch variable: ' epoch_variable{1}]);
 		epoch_column=j;
+        break;
     elseif strcmp(cdf_file_info.Variables(j,4),'epoch')
         epoch_variable=variable_names(j,1);
         epoch_column=j;
+        break;
 	end
 end
 
@@ -112,9 +115,10 @@ elseif ischar(var_name) % one specifies the name of variable
   if epoch_column ~= 0,
       for j=2:size(cdf_file_info.Variables,1)
           if cdf_file_info.Variables{j,3}==cdf_file_info.Variables{epoch_column,3}
-              if isempty(findstr(cdf_file_info.Variables{j,1},'Epoch')) && ...
-                      isempty(findstr(variable_names{j,1},'time_tags__')) && ...
-                      isempty(findstr(variable_names{j,1},'Time__'))
+              if isempty(strfind(cdf_file_info.Variables{j,1},'Epoch')) && ...
+                      isempty(strfind(variable_names{j,1},'time_tags__')) && ...
+                      any(strfind(variable_names{j,1},'_time')) && ...
+                      isempty(strfind(variable_names{j,1},'Time__'))
                   i_time_series_variable=i_time_series_variable+1;
                   time_series_variables{i_time_series_variable}=cdf_file_info.Variables{j,1}; %#ok<AGROW>
               end
@@ -218,6 +222,8 @@ end
 
 if ischar(var_name), var_name={var_name};end
 variables=[epoch_variable; var_name(:)];
+% if requesting only time variable, remove the duplicate
+if numel(variables)==2 && strcmp(variables{1},variables{2}), variables(2)=[];end
 
 if isempty(old_cdfread_call)
   try
@@ -240,11 +246,19 @@ if(old_cdfread_call)
       temp=struct([DATA{:,1}]);
       t = [temp.date];
       t = t(:);
+      t = (t-62167219200000)/1000; %#ok<NASGU>
 else
-    t = DATA{1};
-    t = (t-1).* (24 * 3600 * 1000);
+    if iscell(DATA), 
+        t = DATA{1};
+    else
+        t=DATA;
+    end
+    if t<1e6 % conversion succeeded (there is bug on THEMIS files)
+        t = (t-1).* (24 * 3600 * 1000);
+        t = (t-62167219200000)/1000; %#ok<NASGU>
+    else % THEMIS case, dont do anything, is already in epoch
+    end
 end
-t = (t-62167219200000)/1000; %#ok<NASGU>
 
 for k=2:numel(variables)
   clear var;
@@ -281,5 +295,9 @@ if nargout==0,
  end
 else
 	varargout = cell(1,nargout);
-	for i=1:nargout, varargout(i) = {eval(variables{i+1})}; end
+    if numel(variables)==1, % return only time variable
+        varargout{1}=t;
+    else
+        for i=1:nargout, varargout(i) = {eval(variables{i+1})}; end
+    end
 end
