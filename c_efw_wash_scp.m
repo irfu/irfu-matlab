55fd33b12d84568070b41a60b4ec5c6a2cb30e4b
andris <andris>
2011-11-27 14:36:52 +0000

    updated so that if output requested then do not plot anything (according to discussion with Yuri on email)

--- c_efw_wash_scp.m
+++ c_efw_wash_scp.m
@@ -1,11 +1,16 @@
-function [res,resnan] = c_efw_wash_scp(data,method)
+function [res] = c_efw_wash_scp(data,method)
 %C_EFW_WASH_SCP  Make SC Potential look prettier
 %
-% [RES,RESNAN] = C_EFW_WASH_SCP(DATA,[METHOD])
+% RES = C_EFW_WASH_SCP(DATA,[METHOD]) 
+%   returns RES that is washed satellite potential DATA  
 %
 % METHOD of filling the gaps can be 
 %           'PREVSPIN' - copy data from previous spin [default]
 %           'LINEAR'   - linear interpolation through the gap
+% 
+% C_EFW_WASH_SCP(DATA,[METHOD]) 
+%   when nargout==0 plot the data
+%
 % $Id$
 
 % ----------------------------------------------------------------------------
@@ -60,19 +65,21 @@ if ~isempty(ii)
 					prevs( 1:ends(idx)-begs(idx)-1 , 2);
 			end
 		otherwise
-			error('MTHOD can be : LINEAR, PREVSPIN')
+			error('METHOD can be : LINEAR, PREVSPIN')
 	end
 			
 end
 
-res = clean_spec(res,4,0.75);
-res = clean_spec(res,4,0.2);
-res = clean_spec(res,4,0.2);
+res = clean_spec(res,4,0.75,nargout);
+res = clean_spec(res,4,0.2,nargout);
+res = clean_spec(res,4,0.2,nargout);
 
-resnan = res;
-resnan(isnan(data(:,2)),2) = NaN;
+%resnan = res;
+%resnan(isnan(data(:,2)),2) = NaN;
+res(isnan(data(:,2)),2) = NaN;
 
-function res = clean_spec(res,k1,k2)
+function res = clean_spec(res,k1,k2,nn)
+% nn if nn=0, do not plot, if nn=1 plot (nn is nargout of calling c_efw_wash_scp)
 
 m = mean(res(:,2));
 if isnan(m), error('data still contains NaNs'), end
@@ -83,17 +90,23 @@ else kfft=(nff+1)/2;
 end
 freq = c_efw_fsample(res)*(1:(kfft-1))'/nff;
 a = abs(f(1:kfft-1));
-clf,plot(freq,a,'k.-'), hold on
+if nn==0 
+    clf,plot(freq,a,'k.-'), hold on 
+end
 cls = 'brgmkbrgm';
 for n = 1:8
 	fref = .25 * n;
 	ip = find(freq>fref*4/5 & freq<fref*3/2); lip8 = fix(length(ip)/8);
 	iiii = find( a > mean(a(ip))+(k1+(n-1)*k2)*std(a(ip)) );
-	iiii = iiii( iiii>ip(1)+lip8 & iiii<ip(end)-lip8);
-	plot(freq(iiii),a(iiii),[cls(n) '*'])
+    iiii = iiii( iiii>ip(1)+lip8 & iiii<ip(end)-lip8);
+    if nn==0
+        plot(freq(iiii),a(iiii),[cls(n) '*'])
+    end
 	f(iiii) = mean(f(ip));
 end
-hold off
+if nn==0
+    hold off
+end
 res(:,2) = ifft(f,'symmetric') + m;
 
 
