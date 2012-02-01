af33f2ac8b6ebb6c5623e836d1a76de3291bc37c
chris <chris>
2012-02-01 13:06:39 +0000

    Remove the plotting (interferes with caa_check_for_n02_error)

--- caa/caa_comp_sun_angle.m
+++ caa/caa_comp_sun_angle.m
@@ -6,7 +6,7 @@ efwa = efwa{2};
 atwo = c_load('Atwo?',cl_id,'var');
 if(length(atwo)<10), return, end
 atwo = c_phase(efwa(:,1),atwo);
-irf_plot({efwa,atwo},'comp')
+%irf_plot({efwa,atwo},'comp')
 if(length(atwo)<10), return, end
 [ii1,ii2]=irf_find_comm_idx(efwa,atwo);
 dphi=efwa(ii1,2)-atwo(ii2,2);
