c1efe83d7078fbf228fae179507866e33df69bbe
andris <andris>
2012-02-13 08:20:55 +0000

    ylabel positions should be in normalized units

--- irf_plot_ylabels_align.m
+++ irf_plot_ylabels_align.m
@@ -12,21 +12,16 @@ if nargin==0, % get subplot handles and align all of them
 end
 
 ylext=zeros(1,numel(h));
+ylh=zeros(1,numel(h));
 for jh=1:numel(h)
-    ylh=get(h(jh),'ylabel');
-    un=get(ylh,'units');
-    set(ylh,'units','normalized');
-    ext=get(ylh,'extent');
+    ylh(jh)=get(h(jh),'ylabel');
+    set(ylh(jh),'units','normalized'); % if other units, labels will move when zooming plot
+    ext=get(ylh(jh),'extent');
     ylext(jh)=ext(1);
-    set(ylh,'units',un);
 end
 min_ylext=min(ylext);
 for jh=1:numel(h)
-    ylh=get(h(jh),'ylabel');
-    un=get(ylh,'units');
-    set(ylh,'units','normalized');
-    pos=get(ylh,'position');
+    pos=get(ylh(jh),'position');
     pos(1)=pos(1)-(ylext(jh)-min_ylext);
-    set(ylh,'position',pos);
-    set(ylh,'units',un);
+    set(ylh(jh),'position',pos);
 end
