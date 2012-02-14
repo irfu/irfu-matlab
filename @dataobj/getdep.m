01b5e2087b4f1f595073c046e3245d1de1373b1c
andris <andris>
2012-02-14 15:32:13 +0000

    return empty if no dependencies

--- @dataobj/getdep.m
+++ @dataobj/getdep.m
@@ -16,59 +16,63 @@ if ~ischar(var_s), error('VAR_S must be a stirng'), end
 
 nvars = size(dobj.vars,1);
 if nvars>0
-	for v=1:nvars
-		if strcmpi(dobj.vars{v,1},var_s) || strcmpi(dobj.vars{v,2},var_s)
-			v1_s = dobj.vars{v,2};
-			maxdep = length(dobj.data.(dobj.vars{v,1}).variance(3:end));
-				
-			dep_x = cell(maxdep,2);
-			found_any = 0;
-			for d=1:maxdep
-				found = 0;
-				
-				field = sprintf('LABEL_%d',d);
-				tt = findva(dobj,field,v1_s);
-				if ~isempty(tt)
-					irf_log('dsrc',[field ' : ' tt])
-					dep_x(d,:) = {tt,field};
-					found = 1;
-					found_any = 1;
-				end
-				
-				field = sprintf('DEPEND_%d',d);
-				tt = findva(dobj,field,v1_s);
-				if ~isempty(tt)
-					if found, error('found both LABEL_X and DEPEND_X'), end
-					irf_log('dsrc',[field ' : ' tt])
-					dep_x(d,:) = {tt,field};
-					found_any = 1;
-				end
-			end
-			if dobj.data.(dobj.vars{v,1}).variance(1) == 'T'
-				if isfield(dobj.VariableAttributes,'DEPEND_0')
-					va = dobj.VariableAttributes.DEPEND_0;
-					tvar = [];
-					for vv=1:size(va,1);
-						if strcmp(va{vv,1},v1_s)
-							irf_log('dsrc',sprintf('DEPEND_%d : %s',0,va{vv,2}))
-							tvar = getv(dobj,va{vv,2});
-						end
-					end
-					if isempty(tvar), error('No DEPEND_O for a T variable'), end
-				end
-				if found_any
-					res = struct('DEPEND_O',tvar.data,'DEPEND_X',{dep_x});
-				else
-					res = struct('DEPEND_O',tvar.data,'DEPEND_X',[]);
-				end
-			else
-				if found_any, res = struct('DEPEND_X',{dep_x});
-				else res = struct('DEPEND_X',[]);
-				end
-			end
-			return
-		end
-	end
+    for v=1:nvars
+        if strcmpi(dobj.vars{v,1},var_s) || strcmpi(dobj.vars{v,2},var_s)
+            v1_s = dobj.vars{v,2};
+            maxdep = length(dobj.data.(dobj.vars{v,1}).variance(3:end));
+            
+            dep_x = cell(maxdep,2);
+            found_any = 0;
+            for d=1:maxdep
+                found = 0;
+                
+                field = sprintf('LABEL_%d',d);
+                tt = findva(dobj,field,v1_s);
+                if ~isempty(tt)
+                    irf_log('dsrc',[field ' : ' tt])
+                    dep_x(d,:) = {tt,field};
+                    found = 1;
+                    found_any = 1;
+                end
+                
+                field = sprintf('DEPEND_%d',d);
+                tt = findva(dobj,field,v1_s);
+                if ~isempty(tt)
+                    if found, error('found both LABEL_X and DEPEND_X'), end
+                    irf_log('dsrc',[field ' : ' tt])
+                    dep_x(d,:) = {tt,field};
+                    found_any = 1;
+                end
+            end
+            if dobj.data.(dobj.vars{v,1}).variance(1) == 'T'
+                if isfield(dobj.VariableAttributes,'DEPEND_0')
+                    va = dobj.VariableAttributes.DEPEND_0;
+                    tvar = [];
+                    for vv=1:size(va,1);
+                        if strcmp(va{vv,1},v1_s)
+                            irf_log('dsrc',sprintf('DEPEND_%d : %s',0,va{vv,2}))
+                            tvar = getv(dobj,va{vv,2});
+                        end
+                    end
+                    if isempty(tvar)  % can be time variable itself because there is no DEPEND_O for a T variable
+                        res=[];
+                        irf_log('dsrc','No DEPEND_O for a T variable');
+                        return;
+                    end
+                end
+                if found_any
+                    res = struct('DEPEND_O',tvar.data,'DEPEND_X',{dep_x});
+                else
+                    res = struct('DEPEND_O',tvar.data,'DEPEND_X',[]);
+                end
+            else
+                if found_any, res = struct('DEPEND_X',{dep_x});
+                else res = struct('DEPEND_X',[]);
+                end
+            end
+            return
+        end
+    end
 end
 
 error(['No such variable : ' var_s])
