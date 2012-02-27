4dab4e4daff7d2a4f93c80ae8fc8a516ea8fbed8
chris <chris>
2012-02-27 08:34:08 +0000

    Don't except files. Export empty records instead.

--- caa/caa_export_month.m
+++ caa/caa_export_month.m
@@ -31,39 +31,10 @@ function caa_export_month(year,month,startday,stopday,sats,varargin)
 	  datatypes={ 'P' 'P' };
 	  levels= [2 3];
   else
-	  datatypes={'P1' 'P2' 'P3' 'P4' 'P12' 'P34' 'P32' 'P' 'P' 'DER' 'E' 'E' 'HK'};
-	  levels= [1 1 1 1 1 1 1 2 3 3 2 3 2];
+	  datatypes={'P1' 'P2' 'P3' 'P4' 'P12' 'P34' 'P32' 'P' 'P' 'DER' 'E' 'E' 'HK' 'SFIT'};
+	  levels=    [1     1   1    1    1     1      1    2   3    3    2   3   2    3];
   end
-  
-  switch year
-	  case 2006
-		  exceptions={{1 'P1'} {3 'P1'}  {2 'P3'} {1 'P12'} {3 'P12'} {2 'P32'} {4 'P32'}};
-	  case 2007
-		  if month <= 5     % probe failure on C2
-			  exceptions={{1 'P1'} {2 'P3'} {3 'P1'} {1 'P12'} {2 'P32'} {3 'P12'} {4 'P32'}};
-		  elseif month < 11 % software patch not yet uploaded on C2
-			  exceptions={{1 'P1'} {2 'P1'} {2 'P3'} {3 'P1'} {1 'P12'} {2 'P32'} {2 'P12'} {3 'P12'} {4 'P32'}};
-		  else
-			  exceptions={{1 'P1'} {2 'P1'} {2 'P3'} {3 'P1'} {1 'P12'} {2 'P12'} {3 'P12'} {4 'P32'}};
-		  end
-	  case 2008
-		  exceptions={{1 'P1'} {2 'P1'} {2 'P3'} {3 'P1'} {1 'P12'} {2 'P12'} {3 'P12'} {4 'P32'}};
-	  case 2009
-		  if month <= 10     % probe failure on C1
-			  exceptions={{1 'P1'} {2 'P1'} {2 'P3'} {3 'P1'} {1 'P12'} {2 'P12'} {3 'P12'} {4 'P32'}};
-		  else
-			  exceptions={{1 'P1'} {1 'P4'} {2 'P1'} {2 'P3'} {3 'P1'} {1 'P12'} {2 'P12'} {3 'P12'} {4 'P32'}};
-		  end
-	  case 2010
-		    % No TCOR yet...
-		    levels= [2 3 3 2];
-			datatypes={'P' 'P' 'E' 'HK'};
-			exceptions={};
-	  otherwise
-		  disp('Not sure which files to except.')
-		  disp('To continue, enter return at the prompt.')
-		  keyboard
-  end
+
   logfile = sprintf('/data/caa/cef/%4.4i/export_log_%4.4i%2.2i.txt',year,year,month);
   irf_log('log_out',logfile);
   failfile = sprintf('/data/caa/cef/%4.4i/export_failed_%4.4i%2.2i.txt',year,year,month);
@@ -77,14 +48,6 @@ function caa_export_month(year,month,startday,stopday,sats,varargin)
           level=levels(i);
           datatype=datatypes{i};
           for sat=sats
-              excepted=0;
-              for j=1:length(exceptions)
-                  if (exceptions{j}{1} == sat) && (strcmp(exceptions{j}{2},datatype)==1)
-                      excepted=1;
-                      continue
-                  end
-              end
-              if excepted==1, continue, end
               ret=caa_export_new(level, datatype, sat, 3,'00','Null', start_time, 3600*24);
               if ret ~=0
                   fprintf(1,'caa_export_new failed for %s for lev=%1.1i. C%1.1i %s\n',datatype,level,sat,datetxt);
