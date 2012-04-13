d86f63355b9c4db743c5fbb2342d06d6b88103a0
chris <chris>
2012-04-13 16:09:28 +0000

    *** empty log message ***

--- caa/scripts/cvs_synch.sh
+++ caa/scripts/cvs_synch.sh
@@ -6,7 +6,9 @@
   cvs up
   make all
   make txt
+  cvs -f commit -m '' ns_ops.xml
   cvs -f commit -m '' ns_ops_c?.dat
+  cvs -f commit -m '' manual_problems_c?.dat
   ssh hq 'cd /home/www/cluster/efw/ops; cvs up;'
   ssh db 'cd /data/cluster/irfu-matlab; cvs up; cd /data/cluster/caa-control/; cvs up'
 
