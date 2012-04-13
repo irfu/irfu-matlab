8561f1081e8f0451373bf0566764e30dbb366891
chris <chris>
2012-04-13 09:37:55 +0000

    Once again excepting certain files from being exported by default (e.g. after a probe failure). These should be handled by a single very long gap file.

--- caa/caa_export_month.m
+++ caa/caa_export_month.m
@@ -35,6 +35,62 @@ function caa_export_month(year,month,startday,stopday,sats,varargin)
 	  levels=    [1     1   1    1    1     1      1    2   3    3    2   3   2    3];
   end
 
+  switch year
+	  case 2001
+		  if month <= 7   % probe 3 failure on C2
+			  exceptions={ {1 'P32'} {2 'P32'} {3 'P32'} {4 'P32'}};
+		  else
+			  exceptions={ {2 'P3'} {1 'P32'} {2 'P32'} {3 'P32'} {4 'P32'}};
+		  end
+	  case 2002
+		  if month <= 7   % probe 1 failure on C1
+			  exceptions={ {1 'P1'} {2 'P3'} {1 'P12'} {1 'P32'} {2 'P32'} {3 'P32'} {4 'P32'}};
+		  else            % probe 1 failure on C3
+			  exceptions={ {1 'P1'} {2 'P3'} {3 'P1'} {1 'P12'} {3 'P12'} {1 'P32'} {2 'P32'} {3 'P32'} {4 'P32'}};
+		  end
+	  case 2003 % started making P32 data
+		  exceptions={{1 'P1'} {3 'P1'}  {2 'P3'} {1 'P12'} {3 'P12'} {2 'P32'} {4 'P32'}};
+	  case 2004
+		  exceptions={{1 'P1'} {3 'P1'}  {2 'P3'} {1 'P12'} {3 'P12'} {2 'P32'} {4 'P32'}};
+	  case 2005
+		  exceptions={{1 'P1'} {3 'P1'}  {2 'P3'} {1 'P12'} {3 'P12'} {2 'P32'} {4 'P32'}};
+	  case 2006
+		  exceptions={{1 'P1'} {3 'P1'}  {2 'P3'} {1 'P12'} {3 'P12'} {2 'P32'} {4 'P32'}};
+	  case 2007
+		  if month <= 5     % probe failure on C2
+			  exceptions={{1 'P1'} {2 'P3'} {3 'P1'} {1 'P12'} {2 'P32'} {3 'P12'} {4 'P32'}};
+		  elseif month < 11 % software patch not yet uploaded on C2
+			  exceptions={{1 'P1'} {2 'P1'} {2 'P3'} {3 'P1'} {1 'P12'} {2 'P32'} {2 'P12'} {3 'P12'} {4 'P32'}};
+		  else
+			  exceptions={{1 'P1'} {2 'P1'} {2 'P3'} {3 'P1'} {1 'P12'} {2 'P12'} {3 'P12'} {4 'P32'}};
+		  end
+	  case 2008
+		  exceptions={{1 'P1'} {2 'P1'} {2 'P3'} {3 'P1'} {1 'P12'} {2 'P12'} {3 'P12'} {4 'P32'}};
+	  case 2009
+		  if month <= 10     % probe 4 failure on C1
+			  exceptions={{1 'P1'} {2 'P1'} {2 'P3'} {3 'P1'} {1 'P12'} {2 'P12'} {3 'P12'} {4 'P32'}};
+		  else
+			  exceptions={{1 'P1'} {1 'P4'} {2 'P1'} {2 'P3'} {3 'P1'} {1 'P12'} {2 'P12'} {3 'P12'} {4 'P32'}};
+		  end
+	  case 2010
+			  exceptions={{1 'P1'} {1 'P4'} {2 'P1'} {2 'P3'} {3 'P1'} {1 'P12'} {2 'P12'} {3 'P12'} {4 'P32'}};
+	  case 2011
+		  if month <= 5     % probe 3 failure on C3
+			  exceptions={{1 'P1'} {1 'P4'} {2 'P1'} {2 'P3'} {3 'P1'} {1 'P12'} {2 'P12'} {3 'P12'} {4 'P32'}};
+		  else
+			  exceptions={{1 'P1'} {1 'P4'} {2 'P1'} {2 'P3'} {3 'P1'} {3 'P3'} {1 'P12'} {2 'P12'} {3 'P12'} {3 'P32'} {3 'P34'} {4 'P32'}};
+		  end
+	  otherwise
+		  if year > 2011
+			  disp('WARNING: uncertain which files to except.')
+			  exceptions={{1 'P1'} {1 'P4'} {2 'P1'} {2 'P3'} {3 'P1'} {3 'P3'} {1 'P12'} {2 'P12'} {3 'P12'} {3 'P32'} {3 'P34'} {4 'P32'}};
+		  else
+			  error('Year out of range.')
+		  end
+  end
+  
+  
+  
   logfile = sprintf('/data/caa/cef/%4.4i/export_log_%4.4i%2.2i.txt',year,year,month);
   irf_log('log_out',logfile);
   failfile = sprintf('/data/caa/cef/%4.4i/export_failed_%4.4i%2.2i.txt',year,year,month);
@@ -48,6 +104,14 @@ function caa_export_month(year,month,startday,stopday,sats,varargin)
           level=levels(i);
           datatype=datatypes{i};
           for sat=sats
+              excepted=0;
+              for j=1:length(exceptions)
+                  if (exceptions{j}{1} == sat) && (strcmp(exceptions{j}{2},datatype)==1)
+                      excepted=1;
+                      continue
+                  end
+              end
+              if excepted==1, continue, end
               ret=caa_export_new(level, datatype, sat, 3,'00','Null', start_time, 3600*24);
               if ret ~=0
                   fprintf(1,'caa_export_new failed for %s for lev=%1.1i. C%1.1i %s\n',datatype,level,sat,datetxt);
