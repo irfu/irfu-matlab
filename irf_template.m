5837eb901375792d74c428699975204dae4a0a61
andris <andris>
2012-10-02 06:06:07 +0000

    template for new irf_ functions

---
+++ irf_template.m
@@ -0,0 +1,20 @@
+function out=IRF_TEMPLATE_FUNCTION(varargin)
+% IRF_TEMPLATE_FUNCTION does something good for space scientist
+%	[out]=IRF_TEMPLATE_FUNCTION(X) does something on X where X is vector
+% 	or matrix or something.
+%
+% 	IRF_TEMPLATE_FUNCTION(X) plots the results instead of returning
+%
+%	IRF_TEMPLATE_FUNCTION(AX,...) applies function on axis with handle AX
+%
+%	Example:
+%		IRF_TEMPLATE_FUNCTION([1 1 2])
+%		IRF_TEMPLATE_FUNCTION(gca,[1 2 3])
+%
+% 	See also IRF_FUNCTION_A, IRF_FUNCTION_B.
+
+% When writing the code please follow guidelines in http://www.datatool.com/prod:Nls2.htm
+
+% $Id$
+% $Revision$  $Date$
+
