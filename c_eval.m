b490cb00d8fde4da4528ae8438f0cad907b005c3
andris <andris>
2012-11-14 07:57:22 +0000

    updated help that cell vector also can be used

--- c_eval.m
+++ c_eval.m
@@ -8,19 +8,22 @@ function c_eval(ev_str,sc_list_1,sc_list_2)
 % '?' sign in ev_str is replaced by SC number from sc_list_1
 % '!' sign in ev_str is replaced by SC number from sc_list_2
 % sc_list - list of SC [optional], 1:4 is assumed when not given
+%           sc_list can be also cell vector
 % 
 % Example:
-% c_eval('R?=r?;C?=R?.^2;',2:4)
-% is the same as R2=r2;C2=R2.^2;R3=r3;C3=R3.^2;...
+%   c_eval('R?=r?;C?=R?.^2;',2:4)
+%          is the same as R2=r2;C2=R2.^2;R3=r3;C3=R3.^2;...
 % 
-% c_eval('r!r?=irf_abs(r!r?);');
-% is the same as
-% r1r1=irf_abs(r1r1);r1r2=irf_abs(r1r2);...r4r4=irf_abs(r4r4);
+%   c_eval('r!r?=irf_abs(r!r?);');
+%          is the same as
+%          r1r1=irf_abs(r1r1);r1r2=irf_abs(r1r2);...r4r4=irf_abs(r4r4);
+%
+%	c_eval('a?=2;',{'a','b','c'});
+%			is the same as aa=2;ab=2;ac=2;
 %
 % See also IRF_SSUB, EVALIN
-%
-% $Id$
 
+% $Id$
 % Copyright 2004 Yuri Khotyaintsev
 
 error(nargchk(1,3,nargin))
