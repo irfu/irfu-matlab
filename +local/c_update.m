d4c3ed6dc17a1f76620deb3d448a35987b3eb3ac
andris <andris>
2012-11-14 13:59:01 +0000

    indexes all data sets

--- +local/c_update.m
+++ +local/c_update.m
@@ -1,29 +1,37 @@
 function c_update
-% LOCAL.C_UPDATE update file information in CAA directory
+% LOCAL.C_UPDATE update index information in CAA directory
 % 
 % See also:
-%	LOCAL.C_READ_AUX
-%
-
-curDir=pwd;
-dirCaa='~/data/CAA';
-cd(dirCaa);
-clear aux
-tmp=dir([dirCaa '/CL_SP_AUX']);
-isub = [tmp(:).isdir]; %# returns logical vector
-tmp(isub)=[];
-f=vertcat(tmp.name);
-fn=f;fn(:,end+1)='=';fn=fn';
-%tt=textscan(fn(:),'%*11s%4d%2d%2d_%2d%2d%2d_%4d%2d%2d_%2d%2d%2d%*s','delimiter','=');
-tt=textscan(fn(:),'%*11s%4f%2f%2f_%2f%2f%2f_%4f%2f%2f_%2f%2f%2f%*s','delimiter','=');
-
-aux.filename=[repmat('CL_SP_AUX/',size(f,1),1) f];
-aux.tstart=irf_time([tt{1} tt{2} tt{3} tt{4} tt{5} tt{6}],'vector2epoch');
-aux.tend=irf_time([tt{7} tt{8} tt{9} tt{10} tt{11} tt{12}],'vector2epoch');
-
-save('caa','aux','-append'); 
-cd(curDir);
+%	LOCAL.C_READ
 
 % $Id$
 % $Revision$  $Date$
+
+
+dirCaa='/data/caa/CAA';
+cd(dirCaa);
+tmp=dir(dirCaa);
+iDir = [tmp(:).isdir]; % find directories
+dataSetArray = {d(iDir).name}';
+dataSetArray(ismember(dataSetArray,{'.','..'})) = []; % remove '.' and '..'
+
+for iDataSet=1:numel(dataSetArray)
+	%% list files in data set directory
+	dataSet=dataSetArray{iDataSet};
+	listFiles=dir(dataSet);
+	iDir = [listFiles(:).isdir]; %# returns logical vector
+	listFiles(iDir)=[];
+	%% read in file time intervals
+	tmp=vertcat(listFiles.name);
+	listFilesNames=[tmp tmp(:,end)]; % add one more column at the end
+	listFilesNames(:,end)='=';       % changfe end column to character = (used as separator)
+	listFilesNames=listFilesNames';
+	tt=textscan(listFilesNames(:),'%*11s%4f%2f%2f_%2f%2f%2f_%4f%2f%2f_%2f%2f%2f%*s','delimiter','=');
+	%% create index
+	index.filename=[repmat([dataSet filesep],size(f,1),1) f];
+	index.tstart=irf_time([tt{1} tt{2} tt{3} tt{4} tt{5} tt{6}],'vector2epoch');
+	index.tend=irf_time([tt{7} tt{8} tt{9} tt{10} tt{11} tt{12}],'vector2epoch');
+	eval(['index_' dataSet '=index;']);
+	save('caa',['index_' datSet],'-append');
+end
 
