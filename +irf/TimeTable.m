5b752617f7975b2e23023d497162160c0dec3824
andris <andris>
2012-11-26 14:50:49 +0000

    time table class instead of irf_tt

---
+++ +irf/TimeTable.m
@@ -0,0 +1,207 @@
+classdef TimeTable
+	% irf.TimeTable class to handle time tables
+	%
+	% TT=irf.TimeTable	- generate empty time table
+	% TT=irf.TimeTable('demo') - generate short example time table
+	% TT=irf.TimeTable('http://..') - load ascii time table from internet link 
+	% TT=irf.TimeTable(filename) - load ascii time table form file
+	% TT=irf.TimeTable(ascii_text) - load time table from cell string array ascii_text
+	%
+	% N = numel(TT)		- number of time intervals
+	% out = ascii(TT)	- time table in ascii format
+	% TT = add(TT,..)	- add time interval to table, see help irf.TimeTable.add
+	% export(TT,filename) - export time table to file
+	% 
+	% Examples:
+	%	TT = irf.TimeTable % initialize empty time table
+	%	tint=irf_time([2001 1 1 0 0 0]) + [0 3600];
+	%	TT=add(TT,tint,{'Burst events','all tail crossings 2001'},'this is best event');
+	%	TT=add(TT,tint+7200,{},'this is another best');
+	%	ascii(TT)
+	%   
+	%   %read Cluster Master Science Plan
+	%   msp=irf.TimeTable('http://jsoc1.bnsc.rl.ac.uk/msp/full_msp_ascii.lst');
+
+	properties (Hidden)
+		AccountStatus = 'open';
+	end
+	% The following properties can be set only by class methods
+	properties (SetAccess = public)
+		TimeInterval
+		Comment
+		Description
+	end
+	methods
+		function TT		= TimeTable(varargin) % construct time table
+			TT.TimeInterval=[];	%	matrix with 2 columns, start/end
+			TT.Comment={};		%	comment for each time interval
+			TT.Description={};	%	description in front of each interval
+			if numel(varargin)==1,
+				source = varargin{1};
+			elseif numel(varargin)==0,
+				return;
+			else
+				irf_log('fcal','max 1 argument supported');
+				return
+			end
+			if ischar(source) && size(source,1)==1
+				if strcmpi(source,'demo') % return demo time table
+					TT = irf.TimeTable; % initialize empty time table
+					tint=irf_time([2001 1 1 0 0 0]) + [0 3600];
+					TT=add(TT,tint,{'The best events','the best of the best from 2001'},'this is best event');
+					TT=add(TT,tint+7200,{},'this is another best');
+				elseif strfind(source,'http:')==1 % start with http://
+					tempTT = urlread(source);
+					TT	 = irf.TimeTable(tempTT);
+				elseif exist(source,'file') % check if file
+					fid = fopen(source);
+					fileContents=fscanf(fid,'%c',inf);
+					fclose(fid);
+					TT=irf.TimeTable(fileContents);
+				else % assume source is ascii text of time table
+					ttAscii=textscan(source,'%s','delimiter',sprintf('\n'));
+					TT=irf.TimeTable(ttAscii{1});
+					return;
+				end
+			elseif iscellstr(source) % source is ascii text to be converted
+				% TT   = from_ascii(ascii)
+				% convert ascii to time table
+				% function out=from_ascii(ttascii) % convert ascii tt to matlab format
+				TT=irf.TimeTable;
+				
+				nTimeInterval				= 0;
+				description			    	= cell(size(source,1));
+				iDescriptionLine			= 1;
+				for iTtAscii=1:numel(source)
+					str=source{iTtAscii};
+					if ~isempty(regexp(str,'^\s*[#,!]','match')) % commented line
+						linetext=regexp(str,'^\s*[#,!]\s?(.*)','tokens');
+						description{iDescriptionLine} = linetext{1}{1};
+						iDescriptionLine = iDescriptionLine + 1;
+					else % assume number in iso format
+						timeInterval=regexp(str,'^\s*(?<start>[\d-]*T[\d:\.]*Z?)\s*(?<end>[\d-]*T[\d:\.]*Z?)\s?(?<comment>.*)','names');
+						if ~isempty(timeInterval) && isfield(timeInterval,'start') && isfield(timeInterval,'end')
+							nTimeInterval=nTimeInterval+1;
+							TT.TimeInterval(nTimeInterval,:) = [irf_time(timeInterval.start,'iso2epoch') irf_time(timeInterval.end,'iso2epoch')];
+							if isfield(timeInterval,'comment'),
+								TT.Comment{nTimeInterval}	= timeInterval.comment;
+							else
+								TT.Comment{nTimeInterval}	= [];
+							end
+							TT.Description{nTimeInterval}	= description(1:iDescriptionLine-1);
+							iDescriptionLine				= 1;  % reset counter of description lines for the next time interval
+						else
+							description{iDescriptionLine} = str;
+							iDescriptionLine = iDescriptionLine + 1;
+						end
+					end
+				end
+				
+			else
+				irf_log('fcal','Unknown argument.');
+				return;
+			end
+		end
+		function N		= numel(TT)
+			N = size(TT.TimeInterval,1);
+		end
+		function TT		= add(TT,tint,description,comment)
+			% TT = add(TT,tint,description,comment)
+			% add element to time table
+			% tint - time interval, vector with start and end time in epoch
+			%			or time interval in iso format
+			% description - cell string array describing time interval
+			% comment - character vector commenting time interval (in ascii format
+			%				located on the same line)
+			if nargin<4, comment='';end
+			if nargin<3, description={};end
+			if nargin<2, irf_log('fcal','not enough arguments');return;end
+			nElement=size(TT.TimeInterval,1)+1;
+			if isnumeric(tint),
+				if numel(tint)==0,
+					irf_log('fcal','Time interval not specified. Returning.');
+					return;
+				elseif numel(tint)==1, % only time instant give, end the same as start time
+					tint(2)=tint(1);
+				end
+			elseif ischar(tint) % assume iso format
+				tint=irf_time(tint,'iso2tint');
+			end
+			TT.TimeInterval(nElement,:)=tint;
+			if ~iscellstr(description), % description should be cellstr
+				if ischar(description)
+					description={description};
+				else
+				end
+			end
+			TT.Description{nElement}=description;
+			if ischar(comment),
+				TT.Comment{nElement}=comment;
+			else
+				TT.Comment{nElement}=[];
+			end
+		end
+		function out	= ascii(tt) % tt in ascii format
+			% Return time table in ascii format (cell string array)
+			tint=tt.TimeInterval;
+			description=tt.Description;
+			comment=tt.Comment;
+			nElements=numel(tt);
+			nDescriptionLines=0;
+			for j=1:nElements,
+				nDescriptionLines=nDescriptionLines+numel(description{j});
+			end
+			out=cell(nElements+nDescriptionLines,1);
+			currentLine=1;
+			for iTT=1:numel(tt)
+				for j=1:numel(description{iTT}),
+					out{currentLine}=['# ' description{iTT}{j}];
+					currentLine=currentLine+1;
+				end
+				if ~isempty(comment{iTT}),
+					if numel(comment{iTT})>1 && any(strcmp(comment{iTT}(1),'#')),
+						fmt='%s %s %s';
+					else
+						fmt='%s %s #%s';
+					end
+					out{currentLine}=sprintf(fmt,irf_time(tint(iTT,1),'iso'),...
+						irf_time(tint(iTT,2),'iso'),comment{iTT});
+				else
+					out{currentLine}=sprintf('%s %s',irf_time(tint(iTT,1),'iso'),...
+						irf_time(tint(iTT,2),'iso'));
+				end
+				currentLine=currentLine+1;
+			end
+			if nargout == 0, % interactive calling from command line
+				out=char(out);
+				if size(out,1)>10,
+					disp(out(1:7,:));
+					disp(['.... ' num2str(size(out,1)-10) ' lines ommitted.....']);
+					disp(out(end-2:end,:));
+				else
+					disp(out);
+				end
+				clear out;
+			end
+		end
+		function ok		= export_ascii(TT,filename) % export to file
+			% ok = export_ascii(TT,filename)
+			%	export time table TT to file 
+			out=ascii(TT);
+			if ischar(filename) % read from file
+				fid = fopen(filename,'w');
+				if fid == -1,
+					irf_log('fcal',['Cannot open file:' filename]);
+					ok=0;
+					return;
+				end
+				for j=1:numel(out),
+					fprintf(fid,'%s\n',out{j});
+				end
+				fclose(fid);
+				ok=1;
+				if nargout==0, clear ok, end;
+			end
+		end
+	end % methods
+end % classdef
---
+++ +irf/tt.m
@@ -0,0 +1,116 @@
+function out=tt(varargin)
+% IRF.TT work with time tables
+%   time table is a class defined by irf.TimeTable. 
+%	See methods of irf.TimeTable class on how to generate and export time tables
+%
+%   IRF.TT(tt) display time table on screen
+%
+%   IRF.TT('list_IRF') list time tables in IRF repository
+%
+%   tt=IRF.TT('read_IRF',tt_id) read time table tt_id from IRF
+%
+%   IRF.TT(tt,'write_IRF',tt_id) write time table tt_id to IRF
+%
+%   [tt]=IRF.TT('read_AMDA',tt_id) read time table tt_id from AMDA
+%		you can see AMDA shared time tables http://cdpp-amda.cesr.fr/DDHTML/SHARED/ttrepository.html
+%
+%	See also:
+%		irf.TimeTable
+%
+%   Example:
+%       TT=irf.TimeTable('demo');
+%       irf.tt(TT); % display time table, same as ascii(TT)
+%		TT=irf.tt('read_IRF','Example');
+%		irf.tt(TT,'write_IRF','demo'); % write time table TT to brain.irfu.se with new id 'new_id'
+%
+
+
+% $Id$
+
+%  =========== BELOW NOT IMPLEMENTED =========
+%
+%   hashid=IRF_TT(tt,'save_AMDA',tt_id,'user',username,'passw',password)
+%     save time table tt_id. Return hashid, if saving did not succeed return NaN.
+%
+%   out=IRF_TT(tt,'save_AMDA',hashid) replace time table with hashid with a new one
+%    return NaN if operation did not succeed.
+%
+%   out=IRF_TT(tt,'add_AMDA',hashid) add time intervals to the table hashid
+%    return NaN if operation did not succeed.
+%
+
+
+%% Check inputs
+
+if nargin==0,
+	help irf_tt;
+	return;
+end
+
+if isa(varargin{1},'irf.TimeTable') % first argument is TT in matlab format (structure)
+	tt=varargin{1};
+	if nargin==1 && nargout == 0, % IRF_TT(tt) display time table on screen
+		action='display';
+	elseif nargin>1 && ischar(varargin{2}), % second argument is action
+		action=varargin{2};
+		if strcmp(action,'write_IRF'), % [out]=IRF_TT(tt,'write_IRF',tt_id)
+			if nargin==2,
+				disp('ERROR! dont understand irf_tt arguments. See help!');
+				return
+			else
+				tt_id=varargin{3};
+				if ~ischar(varargin{3}), % filename should be character array
+					disp('ERROR! dont understand irf_tt arguments. See help!');
+					return
+				end
+			end
+		end
+	end
+elseif ischar(varargin{1}) % first argument is action
+	action=varargin{1};
+	if nargin>1 && ischar(varargin{2}) && strcmpi(varargin{1},'import_ascii')
+		filename=varargin{2};
+	end
+	if nargin>1 && ischar(varargin{2}) && ...
+			(strcmpi(varargin{1},'read_irf') ...
+			|| strcmpi(varargin{1},'read_www') ...
+			|| strcmpi(varargin{1},'read_amda'))
+		tt_id=varargin{2};
+	end
+end
+
+%% Act
+switch lower(action)
+	case 'display'
+		ascii(tt);
+	case 'read_irf'
+        out=irf.TimeTable(['http://www.space.irfu.se/TT/' tt_id]);
+	case 'read_amda'
+		httpLink=['http://cdpp-amda.cesr.fr/DDHTML/SHARED/' tt_id '.txt'];
+		out=irf.TimeTable(httpLink);
+	case 'write_irf'
+		remoteFile=['hq.irfu.se:/usr/home/www/space/TT/' tt_id];
+		tempFile=tempname;
+		export_ascii(tt,tempFile);
+		eval(['!scp ' tempFile ' ' remoteFile]);
+		delete(tempFile);
+	case 'list_irf'
+		s=urlread('http://www.space.irfu.se/TT/');
+		A = strread(s, '%s', 'delimiter', sprintf('\n'));
+		A(1:10)=[];
+		A(end-2:end)=[];
+		for i=1:numel(A),
+			A{i}(1:49)=[];
+			ii=strfind(A{i},'"');
+			A{i}(ii:end)=[];
+		end
+		disp(A)
+%		eval('!ssh hq.irfu.se ls /usr/home/www/space/TT');
+end
+
+if nargout==0,
+	clear out;
+end
+
+end
+
