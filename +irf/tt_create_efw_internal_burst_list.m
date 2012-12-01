10808c3bafa451f27bf51d5e217c11c6fa3cb156
andris <andris>
2012-12-01 13:43:48 +0000

    updated

--- +irf/tt_create_efw_internal_burst_list.m
+++ +irf/tt_create_efw_internal_burst_list.m
@@ -1,4 +1,4 @@
-% IRF.CREATE_EFW_INTERNAL_BURST_LIST
+% IRF.TT_CREATE_EFW_INTERNAL_BURST_LIST
 %	script used to generate efw internal burst list from CAA inventory
 %   and save it at the end on IRF server. 
 %
@@ -9,7 +9,7 @@
 
 tint=[irf_time([2001,1,1,0,0,0]) irf_time(now,'date2epoch')];
 irf_log('dsrc',['Checking interval: ' irf_time(tint,'tint2iso')]);
-c_eval('s?=caa_download(tint,''list:C?_CP_EFW_L2_PB'');');
+c_eval('disp(''Downloading inventory for Cluster ?'');s?=caa_download(tint,''list:C?_CP_EFW_L2_PB'');');
 disp('You can choose to save on server the final lists.')
 disp('If you choose not to save then locally will be created')
 disp('time tables with names TT_C1_EFW_internal_bursts,TT_C2...');
@@ -23,25 +23,10 @@ end
 
 for ic=1:4,
 	disp(['Creating time table for Cluster ' num2str(ic) '..']); 
-	c_eval('s=s?;',ic);
-	tint=zeros(numel(s),2);
-	toRemove= false(numel(s),1);
-	textLine=regexp(s,'^C\d?_CP_EFW_L2_PB\s*(?<start>[\d-]*\s[\d:]*)\s*(?<end>[\d-]*\s[\d:]*)\s*(?<number>\d*)\s*(?<version>\d*)','names');
-%	textLine=textscan(s,'^C\d?_CP_EFW_L2_PB\s*(?<start>[\d-]*\s[\d:]*)\s*(?<end>[\d-]*\s[\d:]*)\s*(?<number>\d*)\s*(?<version>\d*)','names');
-	for j=1:numel(textLine),
-		if isempty(textLine{j})
-			toRemove(j)=true;
-		elseif strcmp(textLine{j}.number,'0')
-			toRemove(j)=true;
-		else
-			tint(j,:)=irf_time([textLine{j}.start '/' textLine{j}.end],'iso2tint');
-			tint(j,1)=tint(j,1)-1;
-			tint(j,2)=tint(j,2)+1;
-		end
-	end
-	tint(toRemove,:)=[];
-	TT=irf.TimeTable(tint);
-	c_eval('TT.Header={''Intervals of EFW internal bursts. Cluster ?''};',ic);
+	c_eval('TT=s?;',ic);
+	TT.Header=[sprintf('Intervals of EFW internal bursts. Cluster %d\n\n',ic) TT.Header];
+	ii=find(TT.UserData.number);
+	TT=select(TT,ii);
 	label=['C' num2str(ic) '_EFW_internal_bursts'];
 	if saveOnServer,
 		irf.tt(TT,'write_IRF',label)
