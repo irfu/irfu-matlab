c03fd4141db52dcf5e92209a9110aceb70a4a789
andris <andris>
2012-12-07 08:19:41 +0000

    minor

--- +maarble/tt_define.m
+++ +maarble/tt_define.m
@@ -5,9 +5,6 @@
 %
 
 Units=irf_units;
-workDir=tempname;
-mkdir(workDir);
-cd(workDir);
 tintIso='2001-01-01T00:00:00.000Z/2012-01-01T00:00:00.000Z';
 tint=irf_time(tintIso,'iso2tint');
 disp(['Using time interval: ' tintIso]);
@@ -29,15 +26,13 @@ disp('Finding when Cluster satisfies MAARBLE conditions');
 tStep=median(diff(RRE1(:,1))); % time step
 minR=3;  % minimum distance from Earth 
 maxR=10;  % minimum distance from Earth 
-maxMlt=60; % maximum magnetic latitude
+maxMlat=60; % maximum magnetic latitude
 
 % maarble definition
 ttLabel='MAARBLE';
 ttTitle='Cluster ? inside MAARBLE area, 3RE<R<10RE,mlat<60deg';
-c_eval('imaarble?=(abs(RRE?(:,2))>minR & abs(RRE?(:,2))<maxR & mlat?(:,2) < maxMlt);')
-% tailbox definition
-% c_eval('itailbox?=(abs(RRE?(:,3))<tailBoxDY & abs(RRE?(:,4))<tailBoxDZ & RRE?(:,2)<tailBoxX);',sclist)
-% define intervals for tailbox
+c_eval('imaarble?=(abs(RRE?(:,2))>minR & abs(RRE?(:,2))<maxR & mlat?(:,2) < maxMlat);')
+% define intervals
 c_eval('indstart?=find(diff([0 imaarble?(:)'']) == 1);');
 c_eval('indend?=find(diff([imaarble?(:)'' 0]) == -1);');
 c_eval(['tt_C?_in_' ttLabel '.start=RRE?(indstart?,1)-tStep/2;'])
