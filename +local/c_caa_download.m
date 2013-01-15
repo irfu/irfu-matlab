3c5f456592d69da7be7b0f4d4d9ff57963201f81
andris <andris>
2013-01-15 14:55:25 +0000

    bug fix

--- +local/c_caa_download.m
+++ +local/c_caa_download.m
@@ -10,10 +10,10 @@ function out=c_caa_download(varargin)
 %   LOCAL.C_CAA_DOWNLOAD(dataset) download all dataset
 %
 %   LOCAL.C_CAA_DOWNLOAD(TTrequest) process request time table TTRequest
-% 
+%
 % Example:
-%		local.c_caa_download('C1_CP_PEA_MOMENTS')			
-% 
+%		local.c_caa_download('C1_CP_PEA_MOMENTS')
+%
 % 	See also CAA_DOWNLOAD
 %
 
@@ -35,7 +35,7 @@ dataDirectory = '/data/caa';
 maxSubmittedJobs = 13;
 maxNumberOfAttempts = 20;
 %% change to data directory
-if exist(dataDirectory,'dir')		
+if exist(dataDirectory,'dir')
 	disp(['!!!! Changing directory to ' dataDirectory ' !!!']);
 	cd(dataDirectory);
 else
@@ -44,7 +44,7 @@ else
 	return;
 end
 %% check input: get inventory and construct time table if dataset
-if nargin==1 && ischar(varargin{1})   
+if nargin==1 && ischar(varargin{1})
 	dataSet=varargin{1};
 	irf_log('dsrc','Checking list of available times');
 	tt=caa_download(['list:' dataSet]);
@@ -55,7 +55,7 @@ if nargin==1 && ischar(varargin{1})
 		irf_log('dsrc',['Checking inventory: ' irf_time(tt.TimeInterval(1,:),'tint2iso')]);
 		ttInventory = caa_download(tt.TimeInterval(1,:),['list:' dataSet]);
 	end
-
+	
 	iData=find([ttInventory.UserData(:).number]);
 	TT=select(ttInventory,iData);
 	TTRequest=TT;
@@ -71,9 +71,9 @@ end
 requestListVariableName=['TT_' dataSet ];
 requestListDirectory='CAA/matCaaRequests';
 requestListVariableFile=[requestListDirectory filesep requestListVariableName '.mat'];
-if ~exist(requestListDirectory,'dir'), 
+if ~exist(requestListDirectory,'dir'),
 	mkdir(requestListDirectory);
-else % merge request lists 
+else % merge request lists
 	if exist(requestListVariableFile,'file'),
 		load(requestListVariableFile,requestListVariableName);
 		TTRequest_old=eval(requestListVariableName);
@@ -82,7 +82,7 @@ else % merge request lists
 		irf_log('fcal',['Obselete intervals in old requests list: ' num2str(iiobsolete)]);
 		remove_datafiles(TTRequest_old,iiobsolete);
 		TTRequest_old=remove(TTRequest_old,iiobsolete);
-		[~,iinew]=setdiff(TTRequest,TTRequest_old); % new time intervals 
+		[~,iinew]=setdiff(TTRequest,TTRequest_old); % new time intervals
 		irf_log('fcal',['New time intervals, not in old requests list: ' num2str(iinew)]);
 		[~,iiold,iinew]=common(TTRequest_old,TTRequest); % common time intervals
 		updateFields={'Status','TimeOfRequest','TimeOfDownload','NumberOfAttemptsToDownload'};
@@ -105,83 +105,85 @@ assignin('base','TTRequest',TTRequest); % TTRequest assign so that one can work
 iRequest=find_first_non_processed_time_interval(TTRequest);
 nRequest=numel(TTRequest)-iRequest+1;
 while 1
-    while 1 % submit next unsubmitted job
-        if iRequest >= numel(TTRequest), break;end % no more jobs
-        if n_submitted_jobs(TTRequest)>=maxSubmittedJobs, break; end % max allowed submitted jobs reached
-        if isempty(TTRequest.UserData(iRequest).Status) || TTRequest.UserData(iRequest).Status==-1 % request not yet submitted or processed or did not succeed before
-            tint=TTRequest.TimeInterval(iRequest,:);
-            irf_log('fcal',['Requesting interval ' num2str(iRequest) '(' num2str(nRequest-(numel(TTRequest)-iRequest)) '/' num2str(nRequest) '): ' irf_time(tint,'tint2iso')]);
-            dataSet = TTRequest.UserData(iRequest).dataset;
-            try
-                [download_status,downloadfile]=caa_download(tint,dataSet,'schedule','nolog','nowildcard');
-            catch
-                download_status = -1; % something wrong with internet
-                irf_log('dsrc','**** DID NOT SUCCEED! ****');
-            end
-            if download_status == 0, % scheduling succeeded
-                TTRequest.UserData(iRequest).Status=0;
-                TTRequest.UserData(iRequest).Downloadfile=downloadfile;
-                TTRequest.UserData(iRequest).TimeOfRequest=now;
-                TTRequest.UserData(iRequest).TimeOfDownload=now;
-                TTRequest.UserData(iRequest).NumberOfAttemptsToDownload=0;
-                iRequest=iRequest+1;
-                break
-            elseif download_status == -1,
-                TTRequest.UserData(iRequest).Status=-1;
-                TTRequest.UserData(iRequest).Downloadfile=[];
-                TTRequest.UserData(iRequest).TimeOfRequest=now;
-            end
-        end
-        iRequest=iRequest+1;
-    end
-    while 1   % check submitted jobs
-        irf_log('dsrc',['Checking downloads. ' num2str(n_submitted_jobs(TTRequest)) ' jobs submitted.']);
-        iSubmitted=find_first_submitted_time_interval(TTRequest);
-		if now-TTRequest.UserData(iSubmitted).TimeOfDownload>(1+TTRequest.UserData(iSubmitted).NumberOfAttemptsToDownload)*1/24/60 % more than 1min*"number of attempts" since last request 
-		  try
-			irf_log('dsrc',['File #' num2str(iSubmitted) ': ' TTRequest.UserData(iSubmitted).Downloadfile]);
-			download_status=caa_download(TTRequest.UserData(iSubmitted).Downloadfile,'nolog');
-			TTRequest.UserData(iSubmitted).TimeOfDownload=now;
-		  catch
-			download_status = -1; % something wrong with internet
-			irf_log('dsrc','**** DID NOT SUCCEED! ****');
-		  end
-		if download_status==1,
-		  TTRequest.UserData(iSubmitted).Status=1; % submitted > downloaded 
-		  irf_log('dsrc',['Jobs downloaded so far: ' num2str(n_downloaded_jobs(TTRequest))]);
-		  if mod(n_downloaded_jobs(TTRequest),10)==0 || ...%save after every 10th request
-			n_submitted_jobs(TTRequest)==0 % save after last job
-				varName=['TT_' TTRequest.UserData(iSubmitted).dataset ];
-				irf_log('drsc',['Saving ' varName ' to CAA/matCaaRequests/' varName]);
-				eval([varName '= TTRequest;']);
-				dirName='CAA/matCaaRequests';
-				if ~exist(dirName,'dir'), mkdir(dirName);end
-				  save([dirName '/' varName],'-v7',varName);
+	while 1 % submit next unsubmitted job
+		if iRequest >= numel(TTRequest), break;end % no more jobs
+		if n_submitted_jobs(TTRequest)>=maxSubmittedJobs, break; end % max allowed submitted jobs reached
+		if ~isfield(TTRequest.UserData(iRequest),'Status') || ...
+				isempty(TTRequest.UserData(iRequest).Status) || ...
+				TTRequest.UserData(iRequest).Status==-1 % request not yet submitted or processed or did not succeed before
+			tint=TTRequest.TimeInterval(iRequest,:);
+			irf_log('fcal',['Requesting interval ' num2str(iRequest) '(' num2str(nRequest-(numel(TTRequest)-iRequest)) '/' num2str(nRequest) '): ' irf_time(tint,'tint2iso')]);
+			dataSet = TTRequest.UserData(iRequest).dataset;
+			try
+				[download_status,downloadfile]=caa_download(tint,dataSet,'schedule','nolog','nowildcard');
+			catch
+				download_status = -1; % something wrong with internet
+				irf_log('dsrc','**** DID NOT SUCCEED! ****');
+			end
+			if download_status == 0, % scheduling succeeded
+				TTRequest.UserData(iRequest).Status=0;
+				TTRequest.UserData(iRequest).Downloadfile=downloadfile;
+				TTRequest.UserData(iRequest).TimeOfRequest=now;
+				TTRequest.UserData(iRequest).TimeOfDownload=now;
+				TTRequest.UserData(iRequest).NumberOfAttemptsToDownload=0;
+				iRequest=iRequest+1;
+				break
+			elseif download_status == -1,
+				TTRequest.UserData(iRequest).Status=-1;
+				TTRequest.UserData(iRequest).Downloadfile=[];
+				TTRequest.UserData(iRequest).TimeOfRequest=now;
+			end
+		end
+		iRequest=iRequest+1;
+	end
+	while 1   % check submitted jobs
+		irf_log('dsrc',['Checking downloads. ' num2str(n_submitted_jobs(TTRequest)) ' jobs submitted.']);
+		iSubmitted=find_first_submitted_time_interval(TTRequest);
+		if now-TTRequest.UserData(iSubmitted).TimeOfDownload>(1+TTRequest.UserData(iSubmitted).NumberOfAttemptsToDownload)*1/24/60 % more than 1min*"number of attempts" since last request
+			try
+				irf_log('dsrc',['File #' num2str(iSubmitted) ': ' TTRequest.UserData(iSubmitted).Downloadfile]);
+				download_status=caa_download(TTRequest.UserData(iSubmitted).Downloadfile,'nolog');
+				TTRequest.UserData(iSubmitted).TimeOfDownload=now;
+			catch
+				download_status = -1; % something wrong with internet
+				irf_log('dsrc','**** DID NOT SUCCEED! ****');
+			end
+			if download_status==1,
+				TTRequest.UserData(iSubmitted).Status=1; % submitted > downloaded
+				irf_log('dsrc',['Jobs downloaded so far: ' num2str(n_downloaded_jobs(TTRequest))]);
+				if mod(n_downloaded_jobs(TTRequest),10)==0 || ...%save after every 10th request
+						n_submitted_jobs(TTRequest)==0 % save after last job
+					varName=['TT_' TTRequest.UserData(iSubmitted).dataset ];
+					irf_log('drsc',['Saving ' varName ' to CAA/matCaaRequests/' varName]);
+					eval([varName '= TTRequest;']);
+					dirName='CAA/matCaaRequests';
+					if ~exist(dirName,'dir'), mkdir(dirName);end
+					save([dirName '/' varName],'-v7',varName);
+				end
+			else
+				TTRequest.UserData(iSubmitted).NumberOfAttemptsToDownload=TTRequest.UserData(iSubmitted).NumberOfAttemptsToDownload+1;
+				if TTRequest.UserData(iSubmitted).NumberOfAttemptsToDownload > maxNumberOfAttempts
+					irf_log('dsrc',['*** Request #' num2str(iSubmitted) ' failed, more than 10 attempts to download ***']);
+					TTRequest.UserData(iSubmitted).Status=-1;
+					TTRequest.UserData(iSubmitted).Downloadfile=[];
+					TTRequest.UserData(iSubmitted).TimeOfRequest=now;
+					continue;
+				end
+				if n_submitted_jobs(TTRequest) > maxSubmittedJobs
+					irf_log('proc',['More than ' num2str(maxSubmittedJobs) ' jobs sumitted, none ready, waiting 1min ....']);
+					pause(60)
+				end
 			end
 		else
-		  TTRequest.UserData(iSubmitted).NumberOfAttemptsToDownload=TTRequest.UserData(iSubmitted).NumberOfAttemptsToDownload+1;
-		  if TTRequest.UserData(iSubmitted).NumberOfAttemptsToDownload > maxNumberOfAttempts
-			irf_log('dsrc',['*** Request #' num2str(iSubmitted) ' failed, more than 10 attempts to download ***']);
-			TTRequest.UserData(iSubmitted).Status=-1;
-			TTRequest.UserData(iSubmitted).Downloadfile=[];
-			TTRequest.UserData(iSubmitted).TimeOfRequest=now;
-			continue;
-		  end
-		  if n_submitted_jobs(TTRequest) > maxSubmittedJobs 
-			irf_log('proc',['More than ' num2str(maxSubmittedJobs) ' jobs sumitted, none ready, waiting 1min ....']);
-			pause(60)
-		  end
+			pause(10)
 		end
-	  else
-		pause(10)
-	  end
-	  assignin('base','TTRequest',TTRequest);
+		assignin('base','TTRequest',TTRequest);
 		if n_submitted_jobs(TTRequest) < maxSubmittedJobs; % check next request only if less than max allowed submitted
-		  break
+			break
 		end
 	end % checking submitted jobs end
 	if n_submitted_jobs(TTRequest)==0   %no more jobs in queue
-	  break;
+		break;
 	end
 end % going through all requests
 %% assign output
@@ -234,7 +236,7 @@ if isa(TT,'irf.TimeTable') && isnumeric(ii)
 		return;
 	elseif isempty(ii)
 		ok=true;
-		return;		
+		return;
 	end
 	TTremove=select(TT,ii);
 	dataSet=TTremove.UserData(1).dataset;
