bff89d8b4b3514ad8d58185b78ca985a8d675b1e
yuri <yuri>
2013-03-04 10:42:53 +0000

    add option to average over 5 points

--- irf_waverage.m
+++ irf_waverage.m
@@ -1,7 +1,9 @@
-function out = irf_waverage(data,fsample)
+function out = irf_waverage(data,fsample,nAveragePoints)
 %IRF_WAVERAGE  Compute weighted average
 %
-% out=irf_waverage(var,fsample)
+% out=irf_waverage(var,fsample,[nAveragePoints])
+%
+% nAveragePoints = 7 or 5 - number of point to average over
 %
 % $Id$
 
@@ -12,6 +14,10 @@ function out = irf_waverage(data,fsample)
 % this stuff is worth it, you can buy me a beer in return.   Yuri Khotyaintsev
 % ----------------------------------------------------------------------------
 
+if nargin <3, nAveragePoints = 7; end
+if ~ismember(nAveragePoints,[5 7])
+    error('nAveragePoints must be 5 or 7')
+end
 if size(data,1)<=1
 	irf_log('proc',['Not enough points (' num2str(size(data,1)) ') to average'])
 	out = data;
@@ -34,17 +40,31 @@ ind = round((data(:,1)-data(1,1))/dt + 1);
 out(ind,2:end) = data(:,2:end);
 out(isnan(out)) = 0; % set NaNs to zeros
 for col=2:ncol
-	dtmp =  [0 0 0 out(:,col)' 0 0 0];
+    switch nAveragePoints
+    case 5
+        dtmp =  [0 0 out(:,col)' 0 0];
+    case 7
+        dtmp =  [0 0 0 out(:,col)' 0 0 0];
+    otherwise
+        error('nAveragePoints must be 5 or 7')
+    end
+	
 	for j=1:ndata+1
-		out(j,col) = w_ave(dtmp(j:j+6));
+		out(j,col) = w_ave(dtmp(j:j+nAveragePoints-1),nAveragePoints);
 	end
 end
 % Make sure we do return matrix of the same size
 out = out(ind,:);
 
-function av = w_ave(x)
-%m = [.1 .25 .3 .25 .1];
-m = [.07 .15 .18 .2 .18 .15 .07];
+function av = w_ave(x,nAveragePoints)
+switch nAveragePoints
+    case 5
+        m = [.1 .25 .3 .25 .1];
+    case 7
+        m = [.07 .15 .18 .2 .18 .15 .07];
+    otherwise
+        error('nAveragePoints must be 5 or 7')
+end
 cor = sum(m(x==0)); % find missing points==0
 if cor==1, av = 0;
 else av = sum(x.*m)/(1-cor);
