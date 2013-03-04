f987e1a0879005b7c4b0a9d563d14464d5930471
andris <andris>
2013-03-04 12:35:48 +0000

    improved help

--- +irf/check_if_using_nasa_cdf.m
+++ +irf/check_if_using_nasa_cdf.m
@@ -4,6 +4,13 @@ function ok=check_if_using_nasa_cdf
 %   OK=IRF.CHECK_IF_USING_NASA_CDF
 %		ok=true  - NASA cdfread used
 %		ok=false - Matlab original cdfread used
+%
+% It is encouraged to use NASA cdfread as it gives faster reading
+% times and it directly works with EPOCH16 type of variables.
+% 
+% To obtain NASA cdf patch see their web page:
+% http://cdf.gsfc.nasa.gov/html/matlab_cdf_patch.html
+
 
 % $Id$
 
@@ -16,9 +23,10 @@ while 1
 		fprintf('\n\n\n');
 		disp('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
 		disp(' You are using NASA cdf patch which within ')
-		disp(' irfu-matlab is still experimental. Please report')
+		disp(' irfu-matlab is still experimental. It should')
+		disp(' give you faster reading times but please report')
 		disp(' if you encounter some problems.');
-		disp(' If you want to use matlab cdf execute:');
+		disp(' If you want to use original matlab cdf, execute:');
 		a=which('cdfread');
 		ai=strfind(a,'/');
 		disp(['> rmpath ' a(1:ai(end))]);
