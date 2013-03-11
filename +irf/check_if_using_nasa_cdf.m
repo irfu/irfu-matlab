3a399a00cddf86e5f24307ca7b97cdcb74134aeb
Andris Vaivads <andris.vaivads@gmail.com>
2013-03-11 09:39:12 +0100

    add path to nasa cdf patch automatically

--- +irf/check_if_using_nasa_cdf.m
+++ +irf/check_if_using_nasa_cdf.m
@@ -1,5 +1,8 @@
 function ok=check_if_using_nasa_cdf
 % IRF.CHECK_IF_USING_NASA_CDF returns which cdfread routines are used
+%	If NASA cdf patch is not used then routine adds the path to
+%	irfu-matlab/matlab_cdf341_patch directory so that NASA cdf patch is used. If
+%	this does not succeed, routine returns false value.
 %
 %   OK=IRF.CHECK_IF_USING_NASA_CDF
 %		ok=true  - NASA cdfread used
@@ -8,37 +11,46 @@ function ok=check_if_using_nasa_cdf
 % It is encouraged to use NASA cdfread as it gives faster reading
 % times and it directly works with EPOCH16 type of variables.
 % 
-% To obtain NASA cdf patch see their web page:
+% NASA cdf patch is located under 'irfu-matlab/matlab_cdf341_patch' 
+% The NASA cdf patch web page:
 % http://cdf.gsfc.nasa.gov/html/matlab_cdf_patch.html
 
+cdfDir = fileparts(which('cdfread'));
+irfDir = fileparts(which('irf.m'));
+irfNasaCdfDir = [irfDir filesep 'matlab_cdf341_patch'];
+ok = false; % default
 
-% $Id$
+if strfind(cdfDir,'patch')
+	usingNasaCdfRead = true;
+	if ~strcmp(cdfDir,irfNasaCdfDir),
+		disp('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+		disp('You are using NASA cdf patch but it is not located');
+		disp('in irfu-matlab directory. Please, check that it is');
+		disp('latest version NASA cdf patch!');
+		disp('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+		return
+	end
+else
+	usingNasaCdfRead = false;
+end
 
-
-fid=fopen(which('cdfread'));
-while 1
-	tline = fgetl(fid);
-	if ~ischar(tline), break, end
-	if strfind(tline,'Mike Liu')
+if usingNasaCdfRead,
 		fprintf('\n\n\n');
 		disp('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
 		disp(' You are using NASA cdf patch which within ')
 		disp(' irfu-matlab is still experimental. It should')
 		disp(' give you faster reading times but please report')
 		disp(' if you encounter some problems.');
-		disp(' If you want to use original matlab cdf, execute:');
-		a=which('cdfread');
-		ai=strfind(a,'/');
-		disp(['> rmpath ' a(1:ai(end))]);
-		disp('> clear databoj');
 		disp('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
 		fprintf('\n\n\n');
 		irf_log('fcal','Using NASA cdf is still experimental!');
 		ok=true;
-		break;
-	else
-		ok=false;
+else
+	addpath(irfNasaCdfDir);
+	ok = irf.check_if_using_nasa_cdf;
+	if ~ok,
+		disp(['ERROR: could not add to path: ' irfNasaCdfDir]);
 	end
 end
-fclose(fid);
-end
+
+if nargout == 0, clear ok; end
