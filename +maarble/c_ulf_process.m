ba6c2eedf8b0b04f62600991210e63c4dd402420
Yuri Khotyaintsev <yuri@irfu.se>
2013-03-13 16:49:06 +0100

    add inertial frame and pc12

--- +maarble/c_ulf_process.m
+++ +maarble/c_ulf_process.m
@@ -1,6 +1,7 @@
-cl_id = 2;
-tint=iso2epoch('2010-10-13T12:00:00Z') + [0 3*3600];
-freqRange = 'all';
+cl_id = 1;
+%tint=iso2epoch('2010-10-13T12:00:00Z') + [0 3*3600]; PC3-5 example
+tint=iso2epoch('2007-01-03T16:00:00Z') + [0 0.5*3600];
+freqRange = 'pc12';
 outDir = '.';
 plotFlag = 1;
 
@@ -25,22 +26,41 @@ t_1min = (tint(1):60:tint(end))';
 
 cl_s = int2str(cl_id);
 
-%% Download data && Load
+%% Download data
 if 0
     caa_download(tint+[-30 30],['C' cl_s '_CP_FGM_5VPS_ISR2'],'nowildcard');
     caa_download(tint+[-30 30],['C' cl_s '_CP_EFW_L3_E'],'nowildcard');
+    caa_download(tint+[-30 30],['C' cl_s '_CP_AUX_POSGSE_1M'],'nowildcard');
+    caa_download(tint+[-30 30],'CL_SP_AUX','nowildcard');
+    caa_download(tint+[-30 30],['C' cl_s '_CP_FGM_FULL_ISR2'],'nowildcard');
+    caa_download(tint+[-30 30],['C' cl_s '_CP_EFW_L2_E'],'nowildcard');
 end
+
+%% Load
 % Construct 1 min-B0 (lowpassed at 1/600 Hz)
 B_5VPS = c_caa_var_get(['B_vec_xyz_isr2__C' cl_s '_CP_FGM_5VPS_ISR2'],...
     'mat','tint',tint+[-30 30]);
 R = c_caa_var_get(['sc_pos_xyz_isr2__C' cl_s '_CP_FGM_5VPS_ISR2'],...
     'mat','tint',tint+[-30 30]);
+gseV = c_caa_var_get(['sc_v_xyz_gse__C' cl_s '_CP_AUX_POSGSE_1M'],...
+    'mat','tint',tint+[-30 30]);
+SAXlat = c_caa_var_get(['sc_at' cl_s '_lat__CL_SP_AUX'],...
+    'mat','tint',tint+[-30 30]);
+SAXlong = c_caa_var_get(['sc_at' cl_s '_long__CL_SP_AUX'],...
+    'mat','tint',tint+[-30 30]);
+[xspin,yspin,zspin] = sph2cart(mean(SAXlong(:,2))*pi/180,...
+    mean(SAXlat(:,2))*pi/180,1); SAX = [xspin yspin zspin];
+V = c_coord_trans('gse','isr2',gseV,'SAX',SAX);
+                
 if wantPC35
     E_4SEC = c_caa_var_get(['E_Vec_xy_ISR2__C' cl_s '_CP_EFW_L3_E'],...
         'mat','tint',tint+[-30 30]);
 end
 if wantPC12
-    %xxx
+    B_FULL = c_caa_var_get(['B_vec_xyz_isr2__C' cl_s '_CP_FGM_FULL_ISR2'],...
+        'mat','tint',tint+[-1 1]);
+    E_L2 = c_caa_var_get(['E_Vec_xy_ISR2__C' cl_s '_CP_EFW_L2_E'],...
+        'mat','tint',tint+[-1 1]);
 end
 
 %% Construct FAC
@@ -50,14 +70,36 @@ B0_1MIN = irf_resamp(bf,t_1min); clear bf
 %% PC3-5
 if wantPC35
     t_4SEC = ((tint(1)+2):4:tint(end))';
-    E3D_4SEC = irf_edb(irf_resamp(E_4SEC,t_4SEC),B_5VPS,15,'Eperp+NaN'); % Ez
     B_4SEC = irf_resamp(B_5VPS,t_4SEC);
+    E3D_4SEC = irf_edb(irf_resamp(E_4SEC,t_4SEC),B_4SEC,15,'Eperp+NaN'); % Ez
     
-    %XXX: to construct inertial frame
+    % Construct the inertial frame
+    evxb = irf_tappl(irf_cross(B_4SEC,irf_resamp(V,t_4SEC)),'*1e-3*(-1)');
+    iE3D_4SEC = E3D_4SEC;
+    iE3D_4SEC(:,2:4) = iE3D_4SEC(:,2:4) - evxb(:,2:4);
     
     [timeVector,frequencyVector,BB_xxyyzz_fac,EESum_xxyyzz_ISR2,EE_xxyyzz_FAC,...
         Poynting_xyz_FAC,Poynting_rThetaPhi_FAC,k_thphSVD_fac,polSVD_fac,ellipticity] = ...
-        irf_ebsp(E3D_4SEC,B_4SEC,[],B0_1MIN,R,'pc35','noresamp','fullB=dB');
+        irf_ebsp(iE3D_4SEC,B_4SEC,[],B0_1MIN,R,'pc35','noresamp','fullB=dB');
+    BMAG = irf_abs(B0_1MIN); BMAG(:,2:4) = []; BMAG = irf_resamp(BMAG,timeVector);
+    h=irf_pl_ebsp(cl_id,R,timeVector,'pc35',BMAG,BB_xxyyzz_fac,...
+        EESum_xxyyzz_ISR2,EE_xxyyzz_FAC,Poynting_xyz_FAC,Poynting_rThetaPhi_FAC,...
+        k_thphSVD_fac,polSVD_fac,ellipticity);
+end
+if wantPC12
+    fFGM = 22.5;
+    t_BASE = (fix(min(B_FULL(1,1),E_L2(1,1))):2.0/fFGM:ceil(max(B_FULL(end,1),E_L2(end,1))))';
+    B_BASE = irf_resamp(B_FULL,t_BASE);
+    E3D_BASE = irf_edb(irf_resamp(E_L2,t_BASE),B_BASE,15,'Eperp+NaN'); % Ez
+    
+    % Construct the inertial frame
+    evxb = irf_tappl(irf_cross(B_BASE,irf_resamp(V,t_BASE)),'*1e-3*(-1)');
+    iE3D_BASE = E3D_BASE;
+    iE3D_BASE(:,2:4) = iE3D_BASE(:,2:4) - evxb(:,2:4);
+    
+    [timeVector,frequencyVector,BB_xxyyzz_fac,EESum_xxyyzz_ISR2,EE_xxyyzz_FAC,...
+        Poynting_xyz_FAC,Poynting_rThetaPhi_FAC,k_thphSVD_fac,polSVD_fac,ellipticity] = ...
+        irf_ebsp(iE3D_BASE,B_BASE,[],B0_1MIN,R,'pc12','noresamp','fullB=dB','dedotb=0');
     BMAG = irf_abs(B0_1MIN); BMAG(:,2:4) = []; BMAG = irf_resamp(BMAG,timeVector);
     h=irf_pl_ebsp(cl_id,R,timeVector,'pc35',BMAG,BB_xxyyzz_fac,...
         EESum_xxyyzz_ISR2,EE_xxyyzz_FAC,Poynting_xyz_FAC,Poynting_rThetaPhi_FAC,...
