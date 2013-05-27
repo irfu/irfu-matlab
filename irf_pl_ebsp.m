c6808f79b123aebef221c4051a23cf657afc15eb
Yuri Khotyaintsev <yuri@irfu.se>
2013-05-27 11:52:54 +0200

    add functiuonality to limit output

--- irf_pl_ebsp.m
+++ irf_pl_ebsp.m
@@ -1,63 +1,173 @@
-function h = irf_pl_ebsp(ebsp,params)
+function out = irf_pl_ebsp(ebsp,params)
+%IRF_PL_EBSP  visualize EBSP 
+%
+%  H = IRF_PL_EBSP(EBSP,PARAMS)
+%
+%  Input:
+%
+%  EBSP is the output of IRF_EBSP()
+%
+%  PARAMS is cell array with a list of panels to plot: {PRAM, COMP, LIM_ARRAY}
+%  where
+%    PARAM     - one of the fields of EBSP
+%    COMP      - component(s) of param, empty=plot all components
+%    LIM_ARRAY - array of LIM_STRUCT used to limit the output. 
+%
+%  LIM_ARRAY has the following fields:
+%    LIM_ARRAY.param - one of the fields of EBSP
+%    LIM_ARRAY.comp  - component of LIM_ARRAY.param
+%    LIM_ARRAY.val   - limiting value
+%    LIM_ARRAY.type  - 'low' (data < LIM_ARRAY.val disregarded) or 'high'
+%
+%  Output:
+%
+%  H - handles to plots
+%
+%  Examples:
+%
+%  For a "MAARBLE type" plot:
+%
+%   limByDopStruct = struct('type','low','val',0.7,'param','dop','comp',1);
+%   limByPlanarityStruct = struct('type','low','val',0.6,'param','planarity','comp',1);
+%   limBSsumStruct = struct('type','low','val',.05,'param','bb_xxyyzzss','comp',4);
+%   params = {{'bb_xxyyzzss',4,{limBSsumStruct}},...
+%     {'ee_ss'},...
+%     {'dop'},{'planarity'},...
+%     {'ellipticity',[],{limByDopStruct,limByPlanarityStruct}},...
+%     {'k_tp',[],{limByDopStruct,limByPlanarityStruct}},...
+%     {'pf_rtp',1},...
+%     {'pf_rtp',2,{limByDopStruct}}};
+%
+%  See also: IRF_EBSP
 
+% ----------------------------------------------------------------------------
+% "THE BEER-WARE LICENSE" (Revision 42):
+% <yuri@irfu.se> wrote this file.  As long as you retain this notice you
+% can do whatever you want with this stuff. If we meet some day, and you think
+% this stuff is worth it, you can buy me a beer in return.   Yuri Khotyaintsev
+% ----------------------------------------------------------------------------
 
-
+%% Main function
+% Default plot
 if nargin==1 || isempty(params)
+  limByDopStruct = struct('type','low','val',0.7,'param','dop','comp',1);
+  limByPlanarityStruct = struct('type','low','val',0.6,'param','planarity','comp',1);
+  %limBSsumStruct = struct('type','low','val',.05,'param','bb_xxyyzzss','comp',4);
+  
   params = {{'bb_xxyyzzss',4},...
     {'ee_ss'},...
-    {'dop'},{'planarity'},{'ellipticity'},{'k_tp'},...
-    {'pf_rtp',[1 2]}};
+    {'dop'},{'planarity'},...
+    {'ellipticity',[],{limByDopStruct,limByPlanarityStruct}},...
+    {'k_tp',[],{limByDopStruct,limByPlanarityStruct}},...
+    {'pf_rtp',1},...
+    {'pf_rtp',2,{limByDopStruct}}};
 end
 
 fieldsEBSP = fields(ebsp);
 IGNORE_FIELDS = {'t','f','fac'};
 fieldsPlottable = setxor(fieldsEBSP,IGNORE_FIELDS);
-
-plotFields = ''; plotComps = ''; nPanels = 0;
-for idx = 1:length(params)
-  p = params{idx};
-  if ~ischar(p{1})
-    error('invalid FIELD_NAME for parameter #%d, expecting string',idx)
-  end
-  
-  if isempty(intersect(p(1),fieldsPlottable)), continue, end
-  plotFields = [plotFields p(1)]; %#ok<AGROW>
-  if length(p)>1
-    if ~isnumeric(p{2})
-      error('invalid COMP for parameter %s, expecting numerical array',p{1})
-    end
-    plotComps = [plotComps p(2)]; %#ok<AGROW>
-    nComps = length(p{2});
-  else
-    nComps = size(ebsp.(p{1}),3);
-    plotComps = [plotComps {1:nComps}]; %#ok<AGROW>
-  end
-  nPanels = nPanels + nComps;
-end
+plotFields = ''; plotComps = ''; limFields = ''; nPanels = 0;
+GetPlotParams();
     
 h = irf_plot(nPanels);
-
 sr = struct('t',ebsp.t,'f',ebsp.f);
-
-
 for idxField = 1:length(plotFields)
   for idxComp = 1:length(plotComps{idxField})
     field = plotFields{idxField}; comp = plotComps{idxField}(idxComp);
+    lim = limFields{idxField};
     [paramStr,compStr] = GetCompStrings();
     if isempty(compStr), panelStr = paramStr;
     else panelStr = [paramStr '_' compStr]; 
     end
     hca = irf_panel(panelStr);
     sr.p = UpdateUnits(ebsp.(field)(:,:,comp));
+    sr.p = LimitValues(sr.p);
     [sr.plot_type,sr.p_label] = GetPlotTypeLabel();
     irf_spectrogram(hca,sr)
     set(hca,'YScale','log')
     SetCaxis()
   end
 end
-
 irf_zoom(h,'x',ebsp.t([1 end])')
+if nargout, out = h; end % Return here
 
+%% Help functions
+  function GetPlotParams
+    for idx = 1:length(params)
+      p = params{idx};
+      
+      param = p{1};
+      if ~ischar(param)
+        error('invalid FIELD_NAME for parameter #%d, expecting string',idx)
+      end
+      if isempty(intersect({param},fieldsPlottable)), continue, end
+      plotFields = [plotFields {param}]; %#ok<AGROW>
+      
+      if length(p)>1 && ~isempty(p{2})
+        comps = p{2};
+        if ~isnumeric(comps) || any(comps<0) || any( comps ~= uint8(comps))
+          error('invalid COMP for parameter %s, expecting positive integer array',p{1})
+        end
+        nComps = length(comps);
+      else
+        nComps = size(ebsp.(p{1}),3);
+        comps = 1:nComps;
+      end
+      plotComps = [plotComps {comps}]; %#ok<AGROW>
+      
+      if length(p)>2 && ~isempty(p{3})
+        for idxLim = 1:length(p{3})
+          s = ValidateLimStruct(p{3}{idxLim});
+          if s, error('invalid LIM_STRUCT for parameter %s, %s',param, s), end
+        end
+        limFields = [limFields p(3)]; %#ok<AGROW>
+      else
+        limFields = [limFields {''}]; %#ok<AGROW>
+      end
+      
+      nPanels = nPanels + nComps;
+    end
+    function s = ValidateLimStruct(limStruct)
+      s= '';
+      if ~isstruct(limStruct), s = 'expecting a structure'; return, end
+      if any(~isfield(limStruct,{'type','val','param','comp'}))
+        s = 'expecting a structure with fileds: type, val, param, comp';
+        return
+      end
+      if ~ischar(limStruct.type) || ...
+          isempty(intersect({lower(limStruct.type)},{'low','high'}))
+        s = 'field TYPE must be ''low'' or ''high''';
+        return
+      end
+      if ~isnumeric(limStruct.val)
+        s = 'value of field VAL must be numeric'; return
+      end
+      if ~ischar(limStruct.param)
+        s = 'field PARAM must be a string'; return
+      end
+      if isempty(intersect({limStruct.param},fieldsPlottable))
+        s = ['invalid field PARAM: ''' limStruct.param ''' is not a member of EBSP'];
+        return
+      end
+      if ~isnumeric(limStruct.comp) || length(limStruct.comp) > 1 || ...
+          limStruct.comp < 0 || limStruct.comp ~= uint8(limStruct.comp)
+        s = 'value of field COMP must be a positive integer'; return
+      end
+    end
+  end
+  function data = LimitValues(data)
+    if isempty(lim), return, end
+    for idx = 1:length(lim)
+      limStruct = lim{idx};
+      limData = ebsp.(limStruct.param)(:,:,limStruct.comp);
+      switch lower(limStruct.type)
+        case 'low'
+          data(limData < limStruct.val) = NaN;
+        case 'high'
+          data(limData > limStruct.val) = NaN;
+      end
+    end
+  end
   function [f,c] = GetCompStrings
     a = tokenize(field,'_');
     f = a{1}; c = '';
@@ -110,7 +220,6 @@ irf_zoom(h,'x',ebsp.t([1 end])')
         % do nothing
     end
   end
-
   function a=UpdateUnits(a)
     switch compStr
       case {'t','p'}
