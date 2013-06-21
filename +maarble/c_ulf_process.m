0f8b1eabaeb189177a12a56c9686cb55936cc3eb
Yuri Khotyaintsev <yuri@irfu.se>
2013-06-21 13:13:19 +0200

    add help

--- +maarble/c_ulf_process.m
+++ +maarble/c_ulf_process.m
@@ -1,4 +1,31 @@
-freqRange = 'pc35';cl_id = 1; tint=iso2epoch('2010-10-13T12:00:00Z') + [0 3*3600]; % PC3-5 example
+function c_ulf_process(tint,cl_id,freqRange)
+% C_ULF_PROCESS  process Cluster ULF data
+%
+%  c_ulf_process(tint,cl_id,freqRange)
+%
+%  freqRange - 'all' (default), 'pc35', 'pc12'
+%
+%  Reads CAA data from disk and computes wave spectra and polarization
+%  parameters.
+%
+%  See also IRF_EBSP
+
+% ----------------------------------------------------------------------------
+% "THE BEER-WARE LICENSE" (Revision 42):
+% <yuri@irfu.se> wrote this file.  As long as you retain this notice you
+% can do whatever you want with this stuff. If we meet some day, and you think
+% this stuff is worth it, you can buy me a beer in return.   Yuri Khotyaintsev
+% ----------------------------------------------------------------------------
+%
+% This software was developed as part of the MAARBLE (Monitoring,
+% Analyzing and Assessing Radiation Belt Energization and Loss)
+% collaborative research project which has received funding from the
+% European Community's Seventh Framework Programme (FP7-SPACE-2010-1)
+% under grant agreement n. 284520.
+
+if nargin < 1
+  % These are example test intervals
+freqRange = 'all';cl_id = 1; tint=iso2epoch('2010-10-13T12:00:00Z') + [0 3*3600]; % PC3-5 example
 %freqRange = 'pc35';cl_id = 1; tint=iso2epoch('2011-08-30T15:00:00Z') + [0 4*3600]; % PC3-5 example
 %freqRange = 'pc35';cl_id = 1; tint=iso2epoch('2011-08-28T09:30:00Z') + [0 3*3600]; % PC3-5 example
 %freqRange = 'pc35';cl_id = 4; tint=iso2epoch('2011-08-01T07:00:00Z') + [0 6*3600]; % PC3-5 example
@@ -6,9 +33,13 @@ freqRange = 'pc35';cl_id = 1; tint=iso2epoch('2010-10-13T12:00:00Z') + [0 3*3600
 %freqRange = 'pc12';cl_id = 1;tint=iso2epoch('2007-01-03T16:00:00Z') + [0 0.5*3600]; % PC1-2 example
 %freqRange = 'pc12';cl_id = 1;tint=iso2epoch('2011-11-01T20:13:00Z') + [0 25*60]; % PC1-2 example
 %freqRange = [10 180]; cl_id = 4;tint=iso2epoch('2001-02-26T05:18:00Z') + [0 60]; % VLF example
+elseif nargin < 3
+  freqRange = 'all';
+end
 
 outDir = '.';
 plotFlag = 1;
+exportFlag = 0;
 
 wantPC12 = 0;
 wantPC35 = 0; wantSCM = 0;
@@ -69,7 +100,8 @@ B_5VPS = c_coord_trans('gse','isr2',gseB_5VPS,'SAX',SAX);
 if wantPC35
     E_4SEC = c_caa_var_get(['E_Vec_xy_ISR2__C' cl_s '_CP_EFW_L3_E'],...
         'mat','tint',tint+[-30 30]);
-elseif wantPC12
+end
+if wantPC12
     B_FULL = c_caa_var_get(['B_vec_xyz_isr2__C' cl_s '_CP_FGM_FULL_ISR2'],...
         'mat','tint',tint+[-1 1]);
     % XXX
@@ -77,7 +109,8 @@ elseif wantPC12
     % quality.
     E_L2 = c_caa_var_get(['E_Vec_xy_ISR2__C' cl_s '_CP_EFW_L2_E'],...
         'mat','tint',tint+[-1 1]);
-else
+end
+if ~wantPC35 && ~wantPC12
     E_L2 = c_caa_var_get(['E_Vec_xy_ISR2__C' cl_s '_CP_EFW_L2_E'],...
         'mat','tint',tint+[-1 1]);
     B_FULL = c_caa_var_get(['B_vec_xyz_isr2__C' cl_s '_CP_FGM_FULL_ISR2'],...
@@ -92,35 +125,54 @@ bf = irf_filt(B_5VPS,0,1/600,1/5,5);
 B0_1MIN = irf_resamp(bf,t_1min); clear bf
 
 if wantPC35
-    t_4SEC = ((tint(1)+2):4:tint(end))';
-    B_4SEC = irf_resamp(B_5VPS,t_4SEC);
-    E3D_4SEC = irf_edb(irf_resamp(E_4SEC,t_4SEC),B_4SEC,15,'Eperp+NaN'); % Ez
-    
-    % Construct the inertial frame
-    evxb = irf_tappl(irf_cross(B_4SEC,irf_resamp(V,t_4SEC)),'*1e-3*(-1)');
-    iE3D_4SEC = E3D_4SEC;
-    iE3D_4SEC(:,2:4) = iE3D_4SEC(:,2:4) - evxb(:,2:4);
-    
-    tic; ebsp = ...
-        irf_ebsp(iE3D_4SEC,B_4SEC,[],B0_1MIN,R,'pc35',...
-        'fac','polarization','noresamp','fullB=dB');
-    toc
-elseif wantPC12
-    fFGM = 22.5;
-    t_BASE = (fix(min(B_FULL(1,1),E_L2(1,1))):2.0/fFGM:ceil(max(B_FULL(end,1),E_L2(end,1))))';
-    B_BASE = irf_resamp(B_FULL,t_BASE);
-    E3D_BASE = irf_edb(irf_resamp(E_L2,t_BASE),B_BASE,15,'Eperp+NaN'); % Ez
-    
-    % Construct the inertial frame
-    evxb = irf_tappl(irf_cross(B_BASE,irf_resamp(V,t_BASE)),'*1e-3*(-1)');
-    iE3D_BASE = E3D_BASE;
-    iE3D_BASE(:,2:4) = iE3D_BASE(:,2:4) - evxb(:,2:4);
-    
-    tic
-    ebsp = irf_ebsp(iE3D_BASE,B_BASE,[],B0_1MIN,R,'pc12',...
-        'fac','polarization','noresamp','fullB=dB','dedotb=0','nav',12);
-    toc
-else
+  t_4SEC = ((tint(1)+2):4:tint(end))';
+  B_4SEC = irf_resamp(B_5VPS,t_4SEC);
+  E3D_4SEC = irf_edb(irf_resamp(E_4SEC,t_4SEC),B_4SEC,15,'Eperp+NaN'); % Ez
+  
+  % Construct the inertial frame
+  evxb = irf_tappl(irf_cross(B_4SEC,irf_resamp(V,t_4SEC)),'*1e-3*(-1)');
+  iE3D_4SEC = E3D_4SEC;
+  iE3D_4SEC(:,2:4) = iE3D_4SEC(:,2:4) - evxb(:,2:4);
+  
+  ebsp = ...
+    irf_ebsp(iE3D_4SEC,B_4SEC,[],B0_1MIN,R,'pc35',...
+    'fac','polarization','noresamp','fullB=dB');
+  if plotFlag
+    h = irf_pl_ebsp(ebsp);
+    irf_zoom(h,'x',tint)
+    title(h(1),['Cluster ' cl_s ', ' irf_disp_iso_range(tint,1)])
+    set(gcf,'paperpositionmode','auto')
+    print('-dpng',['MAARBLE_ULF_PC35_' irf_fname(tint,5)])
+  end
+  if exportFlag
+  end
+end
+if wantPC12
+  fFGM = 22.5;
+  t_BASE = (fix(min(B_FULL(1,1),E_L2(1,1))):2.0/fFGM:ceil(max(B_FULL(end,1),E_L2(end,1))))';
+  B_BASE = irf_resamp(B_FULL,t_BASE);
+  E3D_BASE = irf_edb(irf_resamp(E_L2,t_BASE),B_BASE,15,'Eperp+NaN'); % Ez
+  
+  % Construct the inertial frame
+  evxb = irf_tappl(irf_cross(B_BASE,irf_resamp(V,t_BASE)),'*1e-3*(-1)');
+  iE3D_BASE = E3D_BASE;
+  iE3D_BASE(:,2:4) = iE3D_BASE(:,2:4) - evxb(:,2:4);
+  tic
+  ebsp = irf_ebsp(iE3D_BASE,B_BASE,[],B0_1MIN,R,'pc12',...
+    'fac','polarization','noresamp','fullB=dB','dedotb=0','nav',12);
+  toc
+  if plotFlag
+    h = irf_pl_ebsp(ebsp);
+    irf_zoom(h,'x',tint)
+    title(h(1),['Cluster ' cl_s ', ' irf_disp_iso_range(tint,1)])
+    set(gcf,'paperpositionmode','auto')
+    print('-dpng',['MAARBLE_ULF_PC12_' irf_fname(tint,5)])
+    close(gcf)
+  end
+  if exportFlag
+  end
+end
+if ~wantPC35 && ~wantPC12
   tic
   if wantSCM
     ebsp = irf_ebsp(E_L2,BSC,B_FULL,B_5VPS,R,freqRange,...
@@ -130,7 +182,8 @@ else
       'fac','polarization','dedotb=0','fullB=dB');
   end
   toc
+  h = irf_pl_ebsp(ebsp);
+    irf_zoom(h,'x',tint)
+    title(h(1),['Cluster ' cl_s ', ' irf_disp_iso_range(tint,1)])
 end
-h = irf_pl_ebsp(ebsp);
-irf_zoom(h,'x',tint)
-title(h(1),['Cluster ' cl_s ', ' irf_disp_iso_range(tint,1)])
+
