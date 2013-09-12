6254356a7aad356aadf5be446a174a672dadc374
Andris Vaivads <andris.vaivads@gmail.com>
2013-09-12 13:51:00 +0200

    new: cef_get_data() and using CESR cef reading libraries in sc position and timing

---
+++ cef/cef_get_data.m
@@ -0,0 +1,28 @@
+function out = cef_get_data(varName,fileName)
+%CEF_GET_DATA read variable form CEF file
+%
+% Example:
+%   R = CEF_GET_DATA('sc_r_xyz_gse','CL_SP_AUX__20101231_010001_20101231_010201.cef.gz')
+%
+% See also: CEF_INIT, CEF_READ
+
+% read file
+cef_init();
+cef_read(fileName);
+
+% get time 
+tt=cef_var('time_tags');
+tt=tt';
+tt=irf_time( cef_date(tt),'datenum2epoch');
+
+% get variable
+tempR = cef_var(varName);
+tempR = tempR';
+
+% define output
+out = [tt double(tempR)];
+
+% close file
+cef_close();
+end
+
