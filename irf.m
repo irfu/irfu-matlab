8d8756052ef5bb4de0665cc0b534973837781d30
Andris Vaivads <andris.vaivads@gmail.com>
2013-09-12 07:11:14 +0200

    irf: added check for necessary pathes, included cef

--- irf.m
+++ irf.m
@@ -1,24 +1,34 @@
 function out=irf(varargin)
 % IRF general info on irfu-matlab
 %
-% IRF runs basic tests
+% IRF checks version, installed libraries and sets up necessary pathes
 %
-% irf('help')
-% help irfu-matlab
-%		show general help on irfu-matlab.
+% IRF('help') or 'help irfu-matlab' shows general help on irfu-matlab.
 %
-% [out] = irf('check') check if using latest version of irfu-matlab
+% [out] = IRF('check') check if using latest version of irfu-matlab
 %	out is logical true if using latest and false if not. 
 %
-% [out] = irf('mice') check if spice/mice routines are installed properly
+% [out] = IRF('mice') check if spice/mice routines are installed properly
 % and if necessary add to the path. run irf('mice_help') if you want to see
 % more help on mice kernels. 
 % more SPICE info: http://naif.jpl.nasa.gov/pub/naif/toolkit_docs/MATLAB/
 % 
+% [out] = IRF('onera') check if onera library is installed
+%
+% version = IRF('version') return IRF version
+%
+% IRF('demo') demonstration how to use IRF
 
+%% Defaults
+% file to check version
+logFileUrl = 'http://www.space.irfu.se/~andris/irfu-matlab/log.txt';
+
+%% Input check
 if nargin == 0,
 	irf('check');
 	irf('mice');
+	irf('onera');
+	irf('check_path');
 	return;
 else
 	if ischar(varargin{1}),
@@ -28,54 +38,8 @@ else
 	end
 end
 
-logFileUrl = 'http://www.space.irfu.se/~andris/irfu-matlab/log.txt';
-
-switch action
-	case 'demo'
-		echodemo irfdemo
-	case 'help'
-		help irfu-matlab
-	case 'version'
-		fid=fopen(log_file);
-        tline = fgetl(fid);
-		fclose(fid);
-		versionTime = tline(1:10);
-		if nargout == 0,
-			disp(['You are using irfu-matlab version from ' versionTime]);
-		else
-			out = versionTime;
-		end
-	case 'mice'
-		if exist('cspice_j2000','file') % mice is installed
-			if (cspice_j2000 == 2451545),
-				disp('SPICE/MICE is installed and working properly');
-				if nargout, out=true; end
-				return;
-			else
-				disp('SPICE/MICE is installed but NOT WORKING PROPERLY!');
-				if nargout, out=false; end
-				return;
-			end
-		else
-			disp('adding MICE path to matlab');
-			addpath([irf('path') filesep  'mice']);
-			ok=irf('mice');
-			if ~ok, 
-				disp('There are mice problems. Please, contact irfu!');
-			end
-		end
-	case ('mice_help')
-		disp('Kernel files at IRFU are located at spis:/share/SPICE');
-		disp('Kernels at irfu: general, Cassini, Rosetta, Solar Orbiter, JUICE');
-		disp('');
-		disp('At other locations if you want to get kernel files, create directory and run:');
-		disp('> wget  --timestamping -r -nH --cut-dirs=2 -X *a_old_versions* ftp://naif.jpl.nasa.gov/pub/naif/generic_kernels');
-		disp('This will download all the latest versions of necessary general kernels.');
-		disp('If you want for example get all Rosetta kernels, execute:');
-		disp('> wget  --timestamping -r -nH --cut-dirs=2 -X *former_versions* ftp://naif.jpl.nasa.gov/pub/naif/ROSETTA');
-		disp('');
-	case('path')
-			out = fileparts(which('irf.m'));	
+%% Actions
+switch lower(action)
 	case 'check'
 		fprintf('Checking if you have latest irfu-matlab...');
 		try
@@ -106,10 +70,91 @@ switch action
 			disp('YES:)');
 			if nargout, out = true; end
 		end
+	case 'check_path'
+		irfPath = [irf('path') filesep];
+		notOnIrfPath = @(x) ~any(strfind(path, [irfPath x]));
+		strPath = {'plots','caa','isdat','cef',...
+			'matlab_central',['matlab_central' filesep 'cm_and_cb_utilities']};
+		for iPath = 1:numel(strPath)
+			if notOnIrfPath(strPath{iPath}),
+				pathToAdd = [irfPath strPath{iPath}];
+				addpath(pathToAdd);
+				disp(['Added to path: ' pathToAdd]);
+			end
+		end
+	case 'demo'
+		echodemo irfdemo
+	case 'help'
+		help irfu-matlab
+	case 'mice'
+		if exist('cspice_j2000','file') % mice is installed
+			if (cspice_j2000 == 2451545),
+				disp('SPICE/MICE is installed and seems working');
+				if nargout, out=true; end
+				return;
+			else
+				disp('SPICE/MICE is installed but NOT WORKING PROPERLY!');
+				if nargout, out=false; end
+				return;
+			end
+		else
+			micePath = [irf('path') filesep  'mice'];
+			disp(['adding MICE path to matlab: ' micePath]);
+			addpath(micePath);
+			ok=irf('mice');
+			if ~ok, 
+				disp('There are mice problems. Please, contact irfu!');
+			end
+		end
+	case 'mice_help'
+		disp('Kernel files at IRFU are located at spis:/share/SPICE');
+		disp('Kernels at irfu: general, Cassini, Rosetta, Solar Orbiter, JUICE');
+		disp('');
+		disp('At other locations if you want to get kernel files, create directory and run:');
+		disp('> wget  --timestamping -r -nH --cut-dirs=2 -X *a_old_versions* ftp://naif.jpl.nasa.gov/pub/naif/generic_kernels');
+		disp('This will download all the latest versions of necessary general kernels.');
+		disp('If you want for example get all Rosetta kernels, execute:');
+		disp('> wget  --timestamping -r -nH --cut-dirs=2 -X *former_versions* ftp://naif.jpl.nasa.gov/pub/naif/ROSETTA');
+		disp('');
+	case 'onera'
+		if exist('onera_desp_lib_coord_trans','file') % onera is installed
+			x=[0 0 1];
+			y=onera_desp_lib_coord_trans([0 0 1],'gse2geo', now);
+			yy=onera_desp_lib_coord_trans(y,'geo2gse',now);
+			if (max(abs(yy-x))<1e-3),
+				disp('onera is installed and seems working');
+				if nargout, out=true; end
+				return;
+			else
+				disp('onera is installed but NOT WORKING PROPERLY!');
+				disp('gse>geo>gse differs by more than 0.1% from original vector');
+				if nargout, out=false; end
+				return;
+			end
+		else
+			oneraPath = [irf('path') filesep  'onera'];
+			disp(['adding onera path to matlab: ' oneraPath]);
+			addpath(oneraPath);
+			ok=irf('onera');
+			if ~ok, 
+				disp('There are onera problems. Please, contact irfu!');
+			end
+		end
+	case 'path'
+			out = fileparts(which('irf.m'));	
+	case 'version'
+		logFile = [fileparts(which('irf_plot.m')) filesep 'log.txt'];
+		fid=fopen(logFile);
+        tline = fgetl(fid);
+		fclose(fid);
+		versionTime = tline(1:10);
+		if nargout == 0,
+			disp(['You are using irfu-matlab version from ' versionTime]);
+		else
+			out = versionTime;
+		end
 	otherwise
 		irf_log('fcal','unknown argument');
 end
 
-function logFile=log_file
-irfDir = fileparts(which('irf_plot.m'));
-logFile = [irfDir filesep 'log.txt'];
+
