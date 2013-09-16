679441ce986bc7425f22020bc88740b38fcf2db6
Yuri Khotyaintsev <yuri@irfu.se>
2013-09-17 00:48:17 +0200

    add more tests

--- irf/TimeTest.m
+++ irf/TimeTest.m
@@ -9,3 +9,10 @@ actSolution = t2-t1-int64((24*3600)*1e9);
 expSolution = int64(1e9);
 verifyEqual(testCase,actSolution,expSolution);
 end
+
+function testAddSeconds(testCase)
+t = irf.Time('2006-01-01T12:00:00Z')+5*3600+21*60+17;
+actSolution = toUTC(t);
+expSolution = '2006-01-01T17:21:17.000000000';
+verifyEqual(testCase,actSolution,expSolution);
+end
