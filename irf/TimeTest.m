6d3e212e08685064376fcb6355a0134c77b63297
Yuri Khotyaintsev <yuri@irfu.se>
2013-09-18 12:25:22 +0200

    rename functions to lowercase

--- irf/TimeTest.m
+++ irf/TimeTest.m
@@ -2,7 +2,7 @@ function tests = TimeTest
 tests = functiontests(localfunctions);
 end
 
-function testLeapSecond(testCase)
+function test_leap_second(testCase)
 t1=irf.Time('2005-12-31T12:00:00Z');
 t2=irf.Time('2006-01-01T12:00:00Z');
 actSolution = t2-t1-int64((24*3600)*1e9);
@@ -10,7 +10,7 @@ expSolution = int64(1e9);
 verifyEqual(testCase,actSolution,expSolution);
 end
 
-function testAddSeconds(testCase)
+function test_add_seconds(testCase)
 t = irf.Time('2006-01-01T12:00:00Z')+5*3600+21*60+17;
 actSolution = toUTC(t);
 expSolution = '2006-01-01T17:21:17.000000000';
