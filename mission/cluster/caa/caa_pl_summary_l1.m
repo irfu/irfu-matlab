6d71b9f5062eb4b62ee0b0e13d5edb47137cdce9
Jan Karlsson <jk@irf.se>
2013-11-20 16:00:54 +0100

    Fix for probe 320

--- mission/cluster/caa/caa_pl_summary_l1.m
+++ mission/cluster/caa/caa_pl_summary_l1.m
@@ -296,11 +296,11 @@ for cli=1:4
                 spinFits.diEs(:, end) = QUALITY;    % Default quality column to best quality, i.e. good data/no problems.
                 quality_column = size(spinFits.diEs, 2);
                 bitmask_column = quality_column - 1;
-                
+
                 % Identify and flag problem areas in data with bitmask and quality factor:
-		if probe_numeric == 420		% For C3 probe 420
-			probe_numeric = 42;
-		end
+                if probe_numeric == 320 || probe_numeric == 420		% For C3 probe 420
+                    probe_numeric = probe_numeric/10;
+                end
                 spinFits.diEs = caa_identify_problems(spinFits.diEs, data_level, sprintf('%d',probe_numeric), cli, bitmask_column, quality_column);
 %                spinFits.diEs = caa_identify_problems(spinFits.diEs, data_level, sprintf('%d',spinFits.probePair), cli, bitmask_column, quality_column);
                 
@@ -359,9 +359,9 @@ for cli=1:4
                 es = [es; spinFits.diEs]; %#ok<AGROW>
                 
                 % Load RSPEC
-		if spinFits.probePair == 420	% For C3 probe 420
-			spinFits.probePair = 42;
-		end
+                if spinFits.probePair == 320 || spinFits.probePair == 420	% For C3 probe 420
+                    spinFits.probePair = spinFits.probePair/10;
+                end
                 rspec_tmp = c_load(['RSPEC?p' num2str(spinFits.probePair)],cli,'var');
                 if ~isempty(rspec_tmp) && rspec_tmp(1,1)~=-157e8
                     rs = rspec_tmp;
