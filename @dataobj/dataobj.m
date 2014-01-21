a577afc014ff591588989e84d73743b608bddc9f
Thomas Nilsson <thomas-nilsson-irfu@users.noreply.github.com>
2014-01-21 17:57:10 +0100

    Fix similar typo

--- @dataobj/dataobj.m
+++ @dataobj/dataobj.m
@@ -122,8 +122,8 @@ switch action
                   info.Variables{iVar(i),3} = length(data{iVar(i)});
                 otherwise
                   errStr = sprintf('Function ''%s'' not implemented',virtFunc);
-                  irf.log('error',strErr)
-                  error('IRF:dataobj:dataobj:functionNotImplemented',strErr)
+                  irf.log('error',errStr)
+                  error('IRF:dataobj:dataobj:functionNotImplemented',errStr)
               end
               timeVariable = info.Variables{iVar(i),1};
               update_variable_attributes_time;
