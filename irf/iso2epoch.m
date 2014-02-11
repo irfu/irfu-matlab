d648b441e87aae6850e7c2fd536bf665489a7b55
Yuri Khotyaintsev <yuri@irfu.se>
2014-02-11 23:50:46 +0100

    add very long time format

--- irf/iso2epoch.m
+++ irf/iso2epoch.m
@@ -5,12 +5,11 @@ function out = iso2epoch(s)
 %
 % Input:
 %	s - string (may be a matrix) in format:
-%		yyyy-mm-ddTHH:MM:ss.wwwZ	// ISO Time
-%		yyyy-mm-ddTHH:MM:ss.wwwwwwZ	// Extended ISO Time
+%		yyyy-mm-ddTHH:MM:ss.mmmZ	// ISO Time
+%		yyyy-mm-ddTHH:MM:ss.mmmuuuZ	// Extended ISO Time
+%   yyyy-mm-ddTHH:MM:ss.mmmuuunnnZ // VERY Extended ISO Time
 %
 %    See also EPOCH2ISO, TOEPOCH, FROMEPOCH
-%
-% $Id$
 
 % Copyright 2004,2007 Yuri Khotyaintsev
 
@@ -18,7 +17,7 @@ mask = '%4d-%2d-%2dT%2d:%2d:%fZ';
 
 % If we have multiple rows, we need to turn the matrix
 if min(size(s))>1
-	if size(s,2)==27 || size(s,2)==24 || size(s,2)==20, s=s'; end
+	if any(size(s,2) == [30 27 24 20]), s=s'; end
 	n_column = size(s,2);
 else n_column = 1;
 end
@@ -26,7 +25,7 @@ end
 a = sscanf(s,mask);
 
 N = length(a)/6;
-if N~=fix(N) || N~=n_column, disp('something is wrong with input'), end
+if N~=fix(N) || N~=n_column, irf.log('warning','something is wrong with input'), end
 a = reshape(a,6,fix(N));
 a = a';
 out = toepoch([a(:,1) a(:,2) a(:,3) a(:,4) a(:,5) a(:,6)]);
