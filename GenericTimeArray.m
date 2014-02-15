8da9d80d53eb69baab0de0c2ae5c479fe5bcd9c8
Yuri Khotyaintsev <yuri@irfu.se>
2014-02-15 17:11:03 +0100

    add static method

--- GenericTimeArray.m
+++ GenericTimeArray.m
@@ -5,4 +5,13 @@ classdef (Abstract) GenericTimeArray
   methods 
     toUtc(obj)
   end
+  
+  methods (Static)
+    function [ output_args ] = validate_iso_time_str( input_args )
+      %verify_iso_time_str Summary of this function goes here
+      %   Detailed explanation goes here
+      
+      output_args = true;
+    end
+  end
 end
