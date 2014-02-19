b181903ca21f9aaac87980a2c49d192bd2734203
Yuri Khotyaintsev <yuri@irfu.se>
2014-02-19 18:13:05 +0100

    Revert "adjusted for ebsp angles in radians"
    This reverts commit c678ae17c0a1600affbe3a6e8d01ed3c5422e5bb.

--- irf/irf_pl_ebsp.m
+++ irf/irf_pl_ebsp.m
@@ -226,10 +226,10 @@ if nargout, out = h; end % Return here
         s = {['log(' paramStr '_{' upper(compStr) '})'],GetUnits()};
         t = 'log';
       case 't'
-        if isempty(unitsStr), unitsStr = 'rad'; end
+        if isempty(unitsStr), unitsStr = 'deg'; end
         s = ['\Theta_{' paramStr '} [' unitsStr ']'];
       case 'p'
-        if isempty(unitsStr), unitsStr = 'rad'; end
+        if isempty(unitsStr), unitsStr = 'deg'; end
         s = ['\Phi_{' paramStr '} [' unitsStr ']'];
       otherwise
         s = paramStr;
@@ -304,16 +304,16 @@ if nargout, out = h; end % Return here
     switch compStr
       case 't'
         if ~strcmpi(paramStr,'k')
-          caxis(hca,[0 pi]), set(hcb,'YTick',[0 pi/2 pi],'YTickLabel',{'0','p/2','p'},'fontname','symbol','TickDir','out')
+          caxis(hca,[0 180]), set(hcb,'YTick',[0 90 180],'TickDir','out')
         else
-          caxis(hca,[0 pi/2]), set(hcb,'YTick',[0 pi/4 pi/2],'YTickLabel',{'0','p/4','p/2'},'fontname','symbol','TickDir','out')
+          caxis(hca,[0 90]), set(hcb,'YTick',[0 45 90],'TickDir','out')
         end
         flagCmapPoy = 1; 
       case 'p'
         if ~strfind(paramStr,'k')
-          caxis(hca,[-pi pi]), set(hcb,'YTick',[-pi 0 pi],'YTickLabel',{'-p','0','p'},'fontname','symbol','TickDir','out')
+          caxis(hca,[-180 180]), set(hcb,'YTick',[-180 0 180],'TickDir','out')
         else
-          caxis(hca,[-pi pi]), set(hcb,'YTick',[-pi -pi/2 0 pi/2 pi],'YTickLabel',{'-p','-p/2','0','p/2','p'},'fontname','symbol','TickDir','out')
+          caxis(hca,[-180 180]), set(hcb,'YTick',[-180 -90 0 90 180],'TickDir','out')
         end
         flagCmapPoy = 1; 
       otherwise
@@ -338,10 +338,7 @@ if nargout, out = h; end % Return here
       set(hcbNew,'Position',[pos(1)-pos(3)*0.25 pos(2:4)])
       hYLabel = get(hcbNew,'ylabel');
       set(hYLabel,'string',yLabelStr,'fontsize',yLabelFontSize);
-      l = get(hcbNew,'YTickLabel'); 
-      if ischar(l);
-          l=[l(:,1) l]; l(:,1)=' ';
-      end
+      l = get(hcbNew,'YTickLabel'); l=[l(:,1) l]; l(:,1)=' ';
       set(hcbNew,'YTickLabel',l);
     end
   end
