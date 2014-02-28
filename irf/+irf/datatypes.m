43b1fe5a243b7393949370e149405796ff59653e
Andris Vaivads <andris.vaivads@gmail.com>
2014-02-28 20:16:34 +0100

    added irf.datatypes to explain typical datatypes

---
+++ irf/+irf/datatypes.m
@@ -0,0 +1,14 @@
+% IRF.DATATYPES show information on common data types
+%
+% Common data types
+% -----------------
+% DataObject     - the same as dataset in CAA, equal to cdf file representation in matlab
+% VariableStruct - Structure including all variable data in original format and metadata,
+%	usually extracted from DataObject.
+% VariableIrf    - Variable as a simple structure in a default format for irfu-matlab
+%	Variable.t        - TimeArray
+%	Variable.[data]   - data matrix of dimension [t x dim1 x dim2]... (typical example energy spectrograms)
+%	Variable.[unit,label,dimunit,dimlabel,dimvec]
+%	Variable.[vec]    - data matrix of dimension [t x ndim], (for example ndim=3 for field vector)
+%	Variable.[abs]    - absolute value of vector in case exists Variable.vec
+% VariableMat    - matrix where first column is time and the other columns are data [DEPRECATED]
