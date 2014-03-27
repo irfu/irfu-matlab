ca515cb21cd6d84161903eb642393110476b7ea9
Yuri Khotyaintsev <yuri@irfu.se>
2014-03-27 18:07:09 +0100

    implement set methods

--- mission/mms/mms_sdc_sdp_db.m
+++ mission/mms/mms_sdc_sdp_db.m
@@ -1,4 +1,4 @@
-classdef mms_sdc_sdp_db
+classdef mms_sdc_sdp_db < handle
   %MMS_DSC_SDP_DB Class containing raw data 
   %   Detailed explanation goes here
   
@@ -15,12 +15,12 @@ classdef mms_sdc_sdp_db
     bdce
     dcv   % Survey files fast/slow
     dce
+  end
+  properties (SetAccess = private)
     hk101 % sun pulse, required for SITL/QL
     hk105 % FLDHK4, e.g. sweep
     hk10d % FLDHK8
     hk10e % FLDHK64, e.g. bias, guard settings
-  end
-  properties (SetAccess = private)
     scId
     fastSurvey % fast=1, slow=0
     tint % time interval defined by input file(s)
@@ -38,86 +38,133 @@ classdef mms_sdc_sdp_db
       end
     end
     
-    function res = get.bdcv(obj)
-      fileId = 'bdcv';
-      if isempty(obj.(fileId)), load_files(obj,fileId);end
-      res = obj.(fileId);
+    function [varargout] = subsref(obj,idx)
+      %SUBSREF handle indexing
+        switch idx(1).type
+          % Use the built-in subsref for dot notation
+          case '.'
+            % For exmpty fileds try to load
+            if ~isempty(intersect(idx(1).subs,mms_sdc_sdp_db.files)) && ...
+                isempty(obj.(idx(1).subs))
+              load_files(obj,idx(1).subs);
+            end
+            [varargout{1:nargout}] = builtin('subsref',obj,idx);
+          case '()'
+            [varargout{1:nargout}] = builtin('subsref',obj,idx);
+          case '{}'
+            [varargout{1:nargout}] = builtin('subsref',obj,idx);
+        end
     end
     
     function load_files(obj,fileId)
+      % Load file Id
+      if ~isempty(obj.tint)
+        fprintf('loading %s',fileId)
+        obj.(fileId) = NaN;
+      end
     end
     
-    function load_file_ttt(obj)
-      modeTmp = '';
-      props = {'dcv','dce'}; files = {dcvFile,dceFile};
-      for i=1:length(props)
-        if ~isempty(files{i})
-          try
-            [~, fileName, ~] = fileparts(files{i});
-            [ok,mmsId,mode] = validate_file_name(props{i});
-            if ~ok
-              errMsg = sprintf('invalid %s filename: %s',props{i},fileName);
-              irf.log('critical',errMsg)
-              error(errMsg) %#ok<SPERR>
-            end
-            if isempty(obj.scId), dbOobjbj.scId = mmsId;
-            else
-              if mmsId~=dbOobjbj.scId
-                errMsg = sprintf('invalid %s filename: %s, expecting : mms%d',...
-                  props{i},fileName,obj.scId);
-                irf.log('critical',errMsg)
-                error(errMsg) %#ok<SPERR>
-              end
-            end
-            % XXX: implement modes for fast/slow + burst
-            if isempty(modeTmp), modeTmp = mode;
-            else
-              if ~strcmp(modeTmp,mode)
-                errMsg = sprintf('invalid %s filename: %s, expecting : %s',...
-                  props{i},fileName,modeTmp);
-                irf.log('critical',errMsg)
-                error(errMsg) %#ok<SPERR>
-              end
-            end
-            obj.(props{i}).data = dataobj(files{i});
-          catch err
-            errMsg = sprintf('failed to load %s file: %s',props{i},files{i});
-            irf.log('critical',errMsg)
-            rethrow(err)
-          end
-        end
-      end
-      
+    function set.bdcv(obj,fileName)
+      obj.bdcv = set_file_private(obj,'bdcv',fileName);
     end
-    function res = get.tint(obj)
-    % Return time interval spanning the files
-      res = [];
-      if ~isempty(obj.dcv)
-        res = obj.dcv.data.(sprintf('mms%s_sdp_epoch_dce',obj.scId)).data([1 end]);
-      end
-      if ~isempty(obj.dcv)
-        tmpInt = obj.dcv.data.(sprintf('mms%s_sdp_epoch_dce',obj.scId)).data([1 end]);
-        if isempty(res), res = tmpInt;
-        else
-          if tmpInt(1)<res(1), res(1) =  tmpInt(1); end
-          if tmpInt(2)>res(2), res(2) =  tmpInt(2); end
-        end
-      end
+    function set.bdce(obj,fileName)
+      obj.bdce = set_file_private(obj,'bdce',fileName);
+    end
+    function set.dcv(obj,fileName)
+      obj.dcv = set_file_private(obj,'dcv',fileName);
+    end
+    function set.dce(obj,fileName)
+      obj.dce = set_file_private(obj,'dce',fileName);
     end
   
-    function [ok,mmsId,mode] = validate_file_name(obj,prod)
-      ok = false; mmsId = 0; mode = '';
+    function ok = validate_file_name(obj,prod,fileName)
+      % validate L1b CDF file names
+      % HK: mms1_fields_hk_l1b_101_20130904_v0.0.0.cdf	
+      % B:  mms1_sdp_brst_l1b_dcv_20130904212500_v0.0.2.cdf
+      if isempty(intersect(prod,mms_sdc_sdp_db.files))
+        irf.log('critical','Invalid fileId')
+        error('irf:mms_sdc_sdp_db:validate_file_name','Invalid fileId')
+      end
+      ok = false;
       toks = tokenize(fileName,'_');
       if ~strcmp(toks{1}(1:3),'mms'), return, end
       mmsId =  str2double(toks{1}(4));
       if mmsId>4 || mmsId<1, return, end
-      if ~strcmp(toks{2},'sdp'), return, end
-      mode = toks{3};
-      if isempty(intersect(mode,{'slow','fast','burst'}))
+      if ~isempty(obj.scId) && obj.scId~=mmsId
+        irf.log('warning',['sc id mismatch, expecting ' num2str(obj.scId)])
         return
+      else obj.scId=mmsId;
       end
+      if strcmp(prod(1:2),'hk')
+        if ~strcmp(toks{2},'fields'), return, end
+        if ~strcmp(toks{3},'hk'), return, end
+        if ~strcmp(toks{4},'l1b'), return, end
+        if ~strcmp(toks{5},prod(3:5)), return, end
+        ok = true; return
+      end
+      % now DCE & DCV  
+      if ~strcmp(toks{2},'sdp'), return, end
       if ~strcmp(toks{4},'l1b'), return, end
-      if ~strcmp(toks{5},prod), return, end
+      
+      if strcmp(prod(1:3),'bdc')
+        if ~strcmp(toks{3},'brst'), return, end
+        if ~strcmp(toks{5},prod(2:4)), return, end
+      elseif strcmp(prod(1:2),'dc')
+        if isempty(intersect(toks{3},{'slow','fast'}))
+          return
+        end
+        if isempty(obj.fastSurvey) && strcmp(toks{3},'fast')
+          obj.fastSurvey = true;
+        else obj.fastSurvey = false;
+        end
+        if ~strcmp(toks{5},prod), return, end
+      else % not DCE | DCV
+        error('irf:mms_sdc_sdp_db:validate_file_name: should not be here')
+      end
+      ok = true;
+    end
+  end
+  
+  methods (Access=private)
+    function res = set_file_private(obj,fileId,fullFileName)
+      res = {};
+      if isempty(fullFileName), return, end
+      if ~isempty(obj.(fileId))
+        irf.log('critical',[fileId ' already set'])
+        error('irf:mms_sdc_sdp_db:set','already set')
+      end
+      % create dataobj from full file name 
+      if ~ischar(fullFileName)
+        errStr = 'Expecting string input (path+filename)';
+        irf.log('critical',errStr);
+        error('irf:mms_sdc_sdp_db:set_file_private',errStr)
+      end
+      [~, fileName, ~] = fileparts(fullFileName);
+      if ~validate_file_name(obj,fileId,fileName)
+        errStr = 'Invalid filename)';
+        irf.log('critical',errStr);
+        error('irf:mms_sdc_sdp_db:set_file_private',errStr)
+      end
+        
+      try 
+        % Create dataobj
+        res = {struct('fileName',fileName,'data',dataobj(fullFileName))};
+        % Update time
+        if fileId(1)=='b'
+          epochVar = sprintf('mms%d_sdp_epoch_dc%s',obj.scId,fileId(end));
+        else epochVar = 'Epoch';
+        end
+        tintTmp = res{1}.data.data.(epochVar).data([1 end]);
+        if isempty(obj.tint), obj.tint = tintTmp;
+        else
+          if obj.tint(1) > tintTmp(1), obj.tint(1) = tintTmp(1); end
+          if obj.tint(2) < tintTmp(2), obj.tint(2) = tintTmp(2); end
+        end
+      catch err
+        errMsg = sprintf('failed to load %s file: %s',fileId,fullFileName);
+        irf.log('critical',errMsg)
+        rethrow(err)
+      end
     end
   end
 end
