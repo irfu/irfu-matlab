7774d459c12d0f0eb2bff8fbb3bc75473abae86e
Yuri Khotyaintsev <yuri@irfu.se>
2014-09-18 13:33:41 +0200

    test on more files

--- mission/mms/mms_testFunctions/mms_sdc_sdp_mrt9_test.m
+++ mission/mms/mms_testFunctions/mms_sdc_sdp_mrt9_test.m
@@ -9,21 +9,34 @@ setenv('LOG_PATH_ROOT','')
 setenv('DROPBOX_ROOT',[data_root filesep 'out'])
 setenv('DATA_PATH_ROOT',[data_root filesep 'out'])
 
-modes={'fast','slow','brst'};
+modes    ={'slow', 'fast', 'brst' };
+versions ={'2.0.1','2.0.1','2.0.0'};
 procs={'usc','ql','sitl'};
+dates = {'20150410', '20160101'};
 
 %% Tests
 for scId=2:-1:1
   hk_101File = sprintf('%s/mms%d/mms%d_fields_hk_l1b_101_20150410_v0.1.0.cdf',...
     data_root,scId,scId);
   for iMode=1:length(modes)
-    dceFile = sprintf('%s/mms%d/mms%d_sdp_%s_l1b_dce_20150410_v1.0.%d.cdf',...
-      data_root,scId,scId,modes{iMode},scId-1);
-    dcvFile = sprintf('%s/mms%d/mms%d_sdp_%s_l1b_dcv_20150410_v1.0.%d.cdf',...
-      data_root,scId,scId,modes{iMode},scId-1);
-    for iProc=1:length(procs)
-      fprintf('TEST: MMS%d %s %s\n',scId,modes{iMode},procs{iProc})
-      mms_sdc_sdp_proc(procs{iProc},dceFile,dcvFile,hk_101File)
+    for iDate = 1:numel(dates)
+      dceFile = sprintf('%s/mms%d/mms%d_sdp_%s_l1b_dce_%s*_v%s.cdf',...
+        data_root,scId,scId,modes{iMode},dates{iDate},versions{iMode});
+      dcvFile = sprintf('%s/mms%d/mms%d_sdp_%s_l1b_dcv_%s*_v%s.cdf',...
+        data_root,scId,scId,modes{iMode},dates{iDate},versions{iMode});
+      d = dir(dceFile);
+      if isempty(d), dceFile = '';
+      else dceFile = sprintf('%s/mms%d/%s',data_root,scId,d(1).name);
+      end
+      d = dir(dcvFile);
+      if isempty(d), dcvFile = '';
+      else dcvFile = sprintf('%s/mms%d/%s',data_root,scId,d(1).name);
+      end
+      if isempty(dcvFile) && isempty(dceFile), continue, end
+      for iProc=1:length(procs)
+        fprintf('TEST: MMS%d %s %s\n',scId,modes{iMode},procs{iProc})
+        mms_sdc_sdp_proc(procs{iProc},dceFile,dcvFile,hk_101File)
+      end
     end
   end
 end
