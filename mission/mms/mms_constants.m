bcd6f9a2ffb07543463bd055291eecc1fcf45503
Yuri Khotyaintsev <yuri@irfu.se>
2014-10-16 11:23:44 +0200

    Add L2E process

--- mission/mms/mms_constants.m
+++ mission/mms/mms_constants.m
@@ -27,10 +27,11 @@ MMS_CONST.TmMode.fast = 2;
 MMS_CONST.TmMode.brst = 3;
 
 % SDC process names
-MMS_CONST.SDCProcs = {'sitl','ql','usc'};
+MMS_CONST.SDCProcs = {'sitl','ql','usc','l2e'};
 MMS_CONST.SDCProc.sitl = 1; % Number must corrspond to position in the list
 MMS_CONST.SDCProc.ql   = 2;
 MMS_CONST.SDCProc.usc  = 3;
+MMS_CONST.SDCProc.l2e  = 4;
 
 % Limits used in processing
 MMS_CONST.Limit.LOW_DENSITY_SATURATION = -100; % Probe stuck and below limit.
