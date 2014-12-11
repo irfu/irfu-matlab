46a2cfc182d0395bb844c2902f5c2ece41b44c97
Jan Karlsson <jk@irf.se>
2014-12-11 11:45:54 +0100

    Sanity check on phase_out.

--- mission/cluster/c_phase.m
+++ mission/cluster/c_phase.m
@@ -60,10 +60,12 @@ while size(phase_2,1)>2
 end
 
 % Sanity check
-phase_unwrapped = unwrap(phase_out(:,2)/180*pi);
-SpinRate = diff(phase_unwrapped)./diff(phase_out(:,1));
-ii = find( diff(phase_out(:,1))< 0.95*2*pi/median(SpinRate) &...
-    (SpinRate<2*pi/MAX_SPIN_PERIOD | SpinRate>2*pi/MIN_SPIN_PERIOD) );
+if ~isempty(phase_out())
+    phase_unwrapped = unwrap(phase_out(:,2)/180*pi);
+    SpinRate = diff(phase_unwrapped)./diff(phase_out(:,1));
+    ii = find( diff(phase_out(:,1))< 0.95*2*pi/median(SpinRate) &...
+        (SpinRate<2*pi/MAX_SPIN_PERIOD | SpinRate>2*pi/MIN_SPIN_PERIOD) );
+end
 
 if ~isempty(ii)
     ii_jump = find(diff(ii')>1);
