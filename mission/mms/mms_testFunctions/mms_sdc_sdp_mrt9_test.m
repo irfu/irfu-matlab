1165717512878cb3fcd13a85a5af946dc50e11f8
thomas-nilsson-irfu <thomas-nilsson-irfu@users.noreply.github.com>
2014-12-18 15:31:09 +0100

    MMS Do not use hk101 2015 file for 2016 processing

--- mission/mms/mms_testFunctions/mms_sdc_sdp_mrt9_test.m
+++ mission/mms/mms_testFunctions/mms_sdc_sdp_mrt9_test.m
@@ -1,7 +1,7 @@
 %% Init
-data_root='/data/mms/MRT9';
+%data_root='/data/mms/MRT9';
 %data_root='/Users/yuri/Dropbox (IRFU)/Projects/MMS/DataProcessing/Data/MRT9';
-%data_root='/home/thoni/MMS/MMS_cdf/MRT9';
+data_root='/home/thoni/MMS/MMS_cdf/MRT9';
 
 if ismac,
   setenv('CDF_BASE','/Applications/cdf35_0-dist/')
@@ -24,14 +24,14 @@ dates = {'20150410', '20160101'};
 
 %% Tests
 for scId=2:-1:1
-  hk_101File = sprintf('%s/mms%d/mms%d_fields_hk_l1b_101_20150410_v0.1.0.cdf',...
-    data_root,scId,scId);
   for iMode=1:length(modes)
     for iDate = 1:numel(dates)
       dceFile = sprintf('%s/mms%d/mms%d_sdp_%s_l1b_dce_%s*_v%s.cdf',...
         data_root,scId,scId,modes{iMode},dates{iDate},versions{iMode});
       dcvFile = sprintf('%s/mms%d/mms%d_sdp_%s_l1b_dcv_%s*_v%s.cdf',...
         data_root,scId,scId,modes{iMode},dates{iDate},versions{iMode});
+      hk_101File = sprintf('%s/mms%d/mms%d_fields_hk_l1b_101_%s*_v0.1.0.cdf',...
+        data_root,scId,scId,dates{iDate});
       d = dir(dceFile);
       if isempty(d), dceFile = '';
       else dceFile = sprintf('%s/mms%d/%s',data_root,scId,d(1).name);
@@ -39,6 +39,10 @@ for scId=2:-1:1
       d = dir(dcvFile);
       if isempty(d), dcvFile = '';
       else dcvFile = sprintf('%s/mms%d/%s',data_root,scId,d(1).name);
+      end
+      d = dir(hk_101File);
+      if isempty(d), hk_101File='';
+      else hk_101File = sprintf('%s/mms%d/%s',data_root,scId,d(1).name);
       end
       if isempty(dcvFile) && isempty(dceFile), continue, end
       for iProc=1:length(procs)
