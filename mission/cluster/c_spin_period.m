9a56af0893a5141d905c3b3f77438afcae0852af
Yuri Khotyaintsev <yuri@irfu.se>
2015-02-13 11:13:40 +0100

    additional return param

--- mission/cluster/c_spin_period.m
+++ mission/cluster/c_spin_period.m
@@ -1,11 +1,9 @@
-function [spin_period,err_angle,err_angle_mean] = c_spin_period(phase)
+function [spin_period,err_angle,err_angle_mean, phc_coef] = c_spin_period(phase)
 %C_SPIN_PERIOD  Compute cluster spin period
 %
-% [SPIN_PERIOD,ERR_ANGLE,ERR_ANGLE_MEAN] = C_SPIN_PERIOD(PHASE_2)
+% [SPIN_PERIOD,ERR_ANGLE,ERR_ANGLE_MEAN, POL_COEF] = C_SPIN_PERIOD(PHASE_2)
 %
 % Compute cluster spin period from PHASE_2
-%
-% $Id$
 
 % ----------------------------------------------------------------------------
 % "THE BEER-WARE LICENSE" (Revision 42):
@@ -13,13 +11,12 @@ function [spin_period,err_angle,err_angle_mean] = c_spin_period(phase)
 % can do whatever you want with this stuff. If we meet some day, and you think
 % this stuff is worth it, you can buy me a beer in return.   Yuri Khotyaintsev
 % ----------------------------------------------------------------------------
+spin_period = [];err_angle  = []; err_angle_mean = [];
 
 ph = phase; tref = phase(1,1); ph(:,1) = ph(:,1) - tref;
-phc = unwrap(ph(1:2,2)/180*pi);
-phc_coef = polyfit(ph(1:2,1),phc,1);
+phc = unwrap(ph(1:2,2)/180*pi); phc_coef = polyfit(ph(1:2,1),phc,1);
 if isnan(phc_coef(1))
     irf_log('proc','Cannot determine spin period!');
-    spin_period = [];
     return
 end
 for j=1:floor(log10(length(ph(:,1))))
@@ -30,10 +27,8 @@ for j=1:floor(log10(length(ph(:,1))))
     phc_coef(1) = phc_coef(1) + dph*pi/180/ph(ii,1);
 end
 diffangle = mod(ph(:,2) - polyval(phc_coef,ph(:,1))*180/pi,360);
-diffangle = abs(diffangle);
-diffangle = min([diffangle';360-diffangle']);
-err_angle_mean = mean(diffangle);
-err_angle = std(diffangle);
+diffangle = abs(diffangle); diffangle = min([diffangle';360-diffangle']);
+err_angle_mean = mean(diffangle); err_angle = std(diffangle);
 if err_angle>1 || err_angle_mean>1,
     irf_log('proc',['Spin period is changing! Phase errors>1deg. err=' num2str(err_angle) 'deg.']);
 end
