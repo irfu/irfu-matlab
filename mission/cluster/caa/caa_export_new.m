482f11e084d5074108d399b4bbd50d64ed18d3b2
Yuri Khotyaintsev <yuri@irfu.se>
2015-02-13 15:46:59 +0100

    -cvs id

--- mission/cluster/caa/caa_export_new.m
+++ mission/cluster/caa/caa_export_new.m
@@ -12,8 +12,6 @@ function status = caa_export_new(lev,caa_vs,cl_id,QUALITY,DATA_VERSION,sp,st,dt)
 % STATUS = 0 means everything went OK
 %
 % See also C_EXPORT_ASCII
-%
-% $Id$
 
 % ----------------------------------------------------------------------------
 % "THE BEER-WARE LICENSE" (Revision 42):
