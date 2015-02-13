191b7f14610ba5feb9176756a9da9df3cf473fe8
Yuri Khotyaintsev <yuri@irfu.se>
2015-02-13 17:57:15 +0100

    allow for more precice fitting using full phase

--- mission/cluster/c_spin_period.m
+++ mission/cluster/c_spin_period.m
@@ -1,9 +1,9 @@
-function [spin_period,err_angle,err_angle_mean, phc_coef] = c_spin_period(phase)
+function [spin_period,err_angle,err_angle_mean, phc_coef] = c_spin_period(phase, flag)
 %C_SPIN_PERIOD  Compute cluster spin period
 %
-% [SPIN_PERIOD,ERR_ANGLE,ERR_ANGLE_MEAN, POL_COEF] = C_SPIN_PERIOD(PHASE_2)
+% [SPIN_PERIOD,ERR_ANGLE,ERR_ANGLE_MEAN, POL_COEF] = C_SPIN_PERIOD(PHASE_2, flag)
 %
-% Compute cluster spin period from PHASE_2
+% Compute Cluster spin period from PHASE_2 or PHASE (FLAG=1)
 
 % ----------------------------------------------------------------------------
 % "THE BEER-WARE LICENSE" (Revision 42):
@@ -11,10 +11,15 @@ function [spin_period,err_angle,err_angle_mean, phc_coef] = c_spin_period(phase)
 % can do whatever you want with this stuff. If we meet some day, and you think
 % this stuff is worth it, you can buy me a beer in return.   Yuri Khotyaintsev
 % ----------------------------------------------------------------------------
+if nargin<2, flag = 0; end
 spin_period = [];err_angle  = []; err_angle_mean = [];
 
 ph = phase; tref = phase(1,1); ph(:,1) = ph(:,1) - tref;
-phc = unwrap(ph(1:2,2)/180*pi); phc_coef = polyfit(ph(1:2,1),phc,1);
+if flag
+  phc = unwrap(ph(:,2)/180*pi); phc_coef = polyfit(ph(:,1),phc,1);
+else
+  phc = unwrap(ph(:,2)/180*pi); phc_coef = polyfit(ph(:,1),phc,1);
+end
 if isnan(phc_coef(1))
     irf_log('proc','Cannot determine spin period!');
     return
