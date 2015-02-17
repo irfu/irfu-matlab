a11d8d22c4c361b9fe00808b977d36ac81bb30bd
Yuri Khotyaintsev <yuri@irfu.se>
2015-02-17 18:19:52 +0100

    add a threshold parameter

--- irf/irf_find_comm_idx.m
+++ irf/irf_find_comm_idx.m
@@ -1,12 +1,11 @@
-function [ii1,ii2]=irf_find_comm_idx(d1,d2)
+function [ii1,ii2]=irf_find_comm_idx(d1,d2,threshold)
 %IRF_FIND_COMM_IDX   Find common index of two time series
 %
-% [ii1,ii2]=irf_find_comm_idx(e1,e2)
+% [IDX1,IDX2] = IRF_FIND_COMM_IDX(E1,E2,[THRESHOLD])
 % 
 % This function can be used when combining two datasets with
-% gaps, like p12 and p34.
-%
-% $Id$
+% gaps, like p12 and p34. The timeslines are assumed to be identical, if
+% the difference is below THERESHOLD (default 1e-5)
 
 % ----------------------------------------------------------------------------
 % "THE BEER-WARE LICENSE" (Revision 42):
@@ -15,15 +14,14 @@ function [ii1,ii2]=irf_find_comm_idx(d1,d2)
 % this stuff is worth it, you can buy me a beer in return.   Yuri Khotyaintsev
 % ----------------------------------------------------------------------------
 
+if nargin==2, threshold = 1e-5; end
 
     DEBUG = 0;
 
-    ii1 = [];
-    ii2 = [];
+    ii1 = []; ii2 = [];
 
     % we use only time
-    t1 = d1(:,1);
-    t2 = d2(:,1);
+    t1 = d1(:,1); t2 = d2(:,1);
     s1 = 1; s2 = 1; % start index
     if min(t1(2:end)-t1(1:end-1))<0 % check for negative time jumps
         t1 = remnegtimejumps(t1);
@@ -31,17 +29,16 @@ function [ii1,ii2]=irf_find_comm_idx(d1,d2)
     if min(t2(2:end)-t2(1:end-1))<0 % check for negative time jumps
         t2 = remnegtimejumps(t2);
     end
+    
     n_loop = 0;
-
     while 1
         la = min(length(t1(s1:end)),length(t2(s2:end))) - 1;
         d = t1(s1:s1+la)-t2(s2:s2+la);
-        ii = find(abs(d) > 1e-5);
+        ii = find(abs(d) > threshold);
 
         if isempty(ii) % time lines are identical
             % save the interval and break
-            ii1 = [ii1 s1:s1+la];
-            ii2 = [ii2 s2:s2+la];
+            ii1 = [ii1 s1:s1+la]; ii2 = [ii2 s2:s2+la];
             if DEBUG, disp('break at 1'), end
             break 
         end
@@ -50,16 +47,14 @@ function [ii1,ii2]=irf_find_comm_idx(d1,d2)
         if ii>1
             % save the interval and shift
             if DEBUG, disp('save interval and shift'), end
-            ii1 = [ii1 s1:s1+ii-2];
-            ii2 = [ii2 s2:s2+ii-2];
-            s1 = s1 + ii - 1;
-            s2 = s2 + ii - 1;
+            ii1 = [ii1 s1:s1+ii-2]; ii2 = [ii2 s2:s2+ii-2];
+            s1 = s1 + ii - 1; s2 = s2 + ii - 1;
         end
         if d(ii) > 0
             % shift t2
             if DEBUG, disp('shift t2'), end
 
-            i_t0 = find(t2(s2:end)==t1(s1));
+            i_t0 = find(abs(t2(s2:end)-t1(s1))<threshold);
             if isempty(i_t0)
                 if t1(s1) > t2(end)
                     if DEBUG, disp('break at 2'), end
@@ -75,15 +70,14 @@ function [ii1,ii2]=irf_find_comm_idx(d1,d2)
                     if DEBUG, disp('break at 22'), end
                     break
                 end
-                s2 = it(1);
-                clear it
+                s2 = it(1); clear it
             else
                 s2 = s2 + i_t0(1) - 1;
             end
         else
             %shift t1
             if DEBUG, disp('shift t1'), end
-            i_t0 = find(t1(s1:end)==t2(s2));
+            i_t0 = find(abs(t1(s1:end)-t2(s2)) < threshold);
             if isempty(i_t0)
                 if t2(s2) > t1(end)
                     if DEBUG, disp('break at 3'), end
@@ -99,8 +93,7 @@ function [ii1,ii2]=irf_find_comm_idx(d1,d2)
                     if DEBUG, disp('break at 32'), end
                     break
                 end
-                s1 = it(1);
-                clear it
+                s1 = it(1); clear it
             else
                 s1 = s1 + i_t0(1) - 1;
             end
