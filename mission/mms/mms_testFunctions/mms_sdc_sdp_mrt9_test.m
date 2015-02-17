f166f69c24d28c2624475c05712edf619d612502
Yuri Khotyaintsev <yuri@irfu.se>
2015-02-17 09:56:34 +0100

    sdp->edp

--- mission/mms/mms_testFunctions/mms_sdc_sdp_mrt9_test.m
+++ mission/mms/mms_testFunctions/mms_sdc_sdp_mrt9_test.m
@@ -1,7 +1,7 @@
 %% Init
-%data_root='/data/mms/MRT9';
+data_root='/data/mms/MRT9';
 %data_root='/Users/yuri/Dropbox (IRFU)/Projects/MMS/DataProcessing/Data/MRT9';
-data_root='/home/thoni/MMS/MMS_cdf/MRT9';
+%data_root='/home/thoni/MMS/MMS_cdf/MRT9';
 
 if ismac,
   setenv('CDF_BASE','/Applications/cdf35_0-dist/')
@@ -17,18 +17,19 @@ setenv('DROPBOX_ROOT',[outDir filesep 'out'])
 setenv('DATA_PATH_ROOT',[outDir filesep 'out'])
 setenv('LOG_PATH_ROOT',[outDir filesep 'log'])
 
-modes    ={'slow', 'fast', 'brst' };
-versions ={'2.0.1','2.0.1','2.0.0'};
+modes    ={'slow', 'fast' };
+versions ={'3.5.0'};
 procs={'scpot','ql','sitl','l2pre'};
-dates = {'20150410', '20160101'};
+dates = {'20150410'};
+scList=[2];
 
 %% Tests
-for scId=2:-1:1
+for scId=scList
   for iMode=1:length(modes)
     for iDate = 1:numel(dates)
-      dceFile = sprintf('%s/mms%d/mms%d_sdp_%s_l1b_dce_%s*_v%s.cdf',...
+      dceFile = sprintf('%s/mms%d/mms%d_edp_%s_l1b_dce_%s*_v%s.cdf',...
         data_root,scId,scId,modes{iMode},dates{iDate},versions{iMode});
-      dcvFile = sprintf('%s/mms%d/mms%d_sdp_%s_l1b_dcv_%s*_v%s.cdf',...
+      dcvFile = sprintf('%s/mms%d/mms%d_edp_%s_l1b_dcv_%s*_v%s.cdf',...
         data_root,scId,scId,modes{iMode},dates{iDate},versions{iMode});
       hk_101File = sprintf('%s/mms%d/mms%d_fields_hk_l1b_101_%s*_v0.1.0.cdf',...
         data_root,scId,scId,dates{iDate});
