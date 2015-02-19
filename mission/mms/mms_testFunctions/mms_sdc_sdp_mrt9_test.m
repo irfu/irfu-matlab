0fac22c5c50b54f2866b3270442f25c0df47d9dc
thomas-nilsson-irfu <thomas-nilsson-irfu@users.noreply.github.com>
2015-02-19 12:15:19 +0100

    MMS Change to MMS 1, aslo include HK10e (bias settings). Use latest version found of HK files, also some minor changes to ease reading.

--- mission/mms/mms_testFunctions/mms_sdc_sdp_mrt9_test.m
+++ mission/mms/mms_testFunctions/mms_sdc_sdp_mrt9_test.m
@@ -13,45 +13,51 @@ end
 cd(outDir)
 if ~exist('log','dir'), mkdir('log'), end
 if ~exist('out','dir'), mkdir('out'), end
-setenv('DROPBOX_ROOT',[outDir filesep 'out'])
-setenv('DATA_PATH_ROOT',[outDir filesep 'out'])
-setenv('LOG_PATH_ROOT',[outDir filesep 'log'])
+setenv('DROPBOX_ROOT', [outDir filesep 'out'])
+setenv('DATA_PATH_ROOT', [outDir filesep 'out'])
+setenv('LOG_PATH_ROOT', [outDir filesep 'log'])
 
-modes    ={'slow'};
-%modes    ={'slow','fast'};
-versions ={'3.5.0'};
-procs={'scpot','ql','sitl','l2pre'};
-dates = {'20160101'};
-%dates = {'20150410','20160101'};
-scList=[2];
+modes    = {'slow'};
+%modes   = {'slow','fast'};
+versions = {'3.5.0'};
+procs    = {'scpot','ql','sitl','l2pre'};
+dates    = {'20160101'};
+%dates   = {'20150410','20160101'};
+scList   = [1];
 
 %% Tests
-for scId=scList
+for scId = scList
   for iMode=1:length(modes)
     for iDate = 1:numel(dates)
       dceFile = sprintf('%s/mms%d/mms%d_edp_%s_l1b_dce_%s*_v%s.cdf',...
-        data_root,scId,scId,modes{iMode},dates{iDate},versions{iMode});
+        data_root, scId, scId,modes{iMode}, dates{iDate}, versions{iMode});
       dcvFile = sprintf('%s/mms%d/mms%d_edp_%s_l1b_dcv_%s*_v%s.cdf',...
-        data_root,scId,scId,modes{iMode},dates{iDate},versions{iMode});
-      hk_101File = sprintf('%s/mms%d/mms%d_fields_hk_l1b_101_%s*_v0.1.0.cdf',...
-        data_root,scId,scId,dates{iDate});
+        data_root, scId, scId, modes{iMode}, dates{iDate}, versions{iMode});
+      hk_101File = sprintf('%s/mms%d/mms%d_fields_hk_l1b_101_%s*_v*.cdf',...
+        data_root, scId, scId, dates{iDate});
+      hk_10eFile = sprintf('%s/mms%d/mms%d_fields_hk_l1b_10e_%s*_v*.cdf',...
+        data_root, scId, scId, dates{iDate});
       d = dir(dceFile);
       if isempty(d), dceFile = '';
-      else dceFile = sprintf('%s/mms%d/%s',data_root,scId,d(1).name);
+      else dceFile = sprintf('%s/mms%d/%s', data_root, scId, d(1).name);
       end
       d = dir(dcvFile);
       if isempty(d), dcvFile = '';
-      else dcvFile = sprintf('%s/mms%d/%s',data_root,scId,d(1).name);
+      else dcvFile = sprintf('%s/mms%d/%s', data_root, scId, d(1).name);
       end
-      d = dir(hk_101File);
+      d = dir(hk_101File); % HK with sunpulse, use highest version found.
       if isempty(d), hk_101File='';
-      else hk_101File = sprintf('%s/mms%d/%s',data_root,scId,d(1).name);
+      else hk_101File = sprintf('%s/mms%d/%s', data_root, scId, d(end).name);
+      end
+      d = dir(hk_10eFile); % HK with bias settings, use highest version found.
+      if isempty(d), hk_10eFile='';
+      else hk_10eFile = sprintf('%s/mms%d/%s', data_root, scId, d(end).name);
       end
       if isempty(dcvFile) && isempty(dceFile), continue, end
       for iProc=1:length(procs)
         fprintf('TEST: %s MMS%d %s %s\n',...
-          dates{iDate},scId,modes{iMode},procs{iProc})
-        mms_sdc_sdp_proc(procs{iProc},dceFile,dcvFile,hk_101File)
+          dates{iDate}, scId, modes{iMode}, procs{iProc})
+        mms_sdc_sdp_proc(procs{iProc}, dceFile, dcvFile, hk_101File, hk_10eFile);
       end
     end
   end
