58a47fccc29387e3f251802b66e96fadbfca2ad2
Yuri Khotyaintsev <yuri@irfu.se>
2015-03-04 23:27:18 +0100

    add TensorBasis

--- TSeries.m
+++ TSeries.m
@@ -6,17 +6,20 @@ classdef TSeries
   %
   %   ARGS:
   %      'to','TensorOrder' - 0 (scalar-default), 1 (vector), 2 (tensor)
+  %      'tb','TensorBasis' - 
+  %                           xyz (Cartesian)
+  %                           rtp (Spherical,colatitude)
+  %                           rlp (Spherical,latitude)
+  %                           rpz (Cylindrical)
+  %                           xy (Cartesian 2D)
+  %                           tp (Polar 2D)
   %      'repres' - Representation for each of the dimensions of the DATA
   %      as a cell array containing description of tensor dimensionality
   %      or {} if the DATA dimension correspond to variables associated 
   %      with other independent variables of the data product, such and a 
   %      particle energy or a spectral frequency channel.
-  %      Possible representations are:
-  %                     Cartesian -> xyz
-  %           SphericalColatitude -> rtp
-  %             SphericalLatitude -> rlp
-  %                   Cylindrical -> rpz
-  %      See: Cluster Metadata Dictionary at
+  %
+  %      Fpr more infor on representation see Cluster Metadata Dictionary
   %           http://caa.estec.esa.int/caa/doc_format_meta.xml
   %
   %  Example:
@@ -26,14 +29,15 @@ classdef TSeries
   %  data4x3x3 = rand(4,3,3);
   %
   %  % TensorOrder=1, 2 components of imcomplete vector (2D)
-  %  tsVec = TSeries(epoch,data4x2,'TensorOrder',1,'repres',{'x','y'})
+  %  TsVec = TSeries(epoch,data4x2,'TensorOrder',1,'TensorBasis','xy',...
+  %          'repres',{'x','y'})
   %  
-  %  % TensorOrder=2, 3x3 components of tensor (3D)
-  %  tsTen = TSeries(epoch,data4x3x3,'TensorOrder',2,...
+  %  % TensorOrder=2, 3x3 components of tensor (3D, default basis=xyz)
+  %  TsTen = TSeries(epoch,data4x3x3,'TensorOrder',2,...
   %          'repres',{'x','y','z'},'repres',{'x','y','z'})
   %
   %  % TensorOrder=1, 3 energies x 3 components of vector (3D, spherical)
-  %  tsPflux = TSeries(epoch,data4x3x3,'TensorOrder',1,...
+  %  TsPflux = TSeries(epoch,data4x3x3,'TensorOrder',1,'TensorBasis','rtp',...
   %          'repres',{},'repres',{'r','t','p'})
   
   
@@ -48,7 +52,8 @@ classdef TSeries
     data_
     t_ % GenericTimeArray
     rep_
-    tensorOrder_ = [];
+    tensorOrder_ = 0;
+    tensorBasis_ = '';
   end
   
   properties (Dependent = true)
@@ -58,15 +63,15 @@ classdef TSeries
   
   properties (SetAccess = immutable,Dependent = true)
     tensorOrder
+    tensorBasis
   end
   
   properties (Constant = true, Hidden = true)
     MAX_TENSOR_ORDER = 2;
-    REPRESENTATIONS = struct(...
-      'Cartesian','xyz',...
-      'SphericalColatitude','rtp',...
-      'SphericalLatitude','rlp',...
-      'Cylindrical','rpz');
+    BASIS = {'xyz','rtp','rlp','rpz','xy','rp'};
+    BASIS_NAMES = {...
+      'Cartesian','Spherical,colatitude', 'Spherical,latitude','Cylindrical',...
+      'Cartesian 2D','Polar 2D'};
   end
   
   properties (SetAccess = protected)
@@ -98,25 +103,60 @@ classdef TSeries
       end
       
       args = varargin;
+      flagTensorOrderSet = false; flagTensorBasisSet = false;
       while ~isempty(args)
         x = args{1}; args(1) = [];
         switch lower(x)
           case {'to','tensororder'}
-            if ~isempty(obj.tensorOrder_),
+            if flagTensorOrderSet
               error('irf:GenericTimeArray:GenericTimeArray:badInputs',...
                 'tensorOrder already set')
             end
             if ~isempty(args), y = args{1}; args(1) = [];
             else
               error('irf:GenericTimeArray:GenericTimeArray:badInputs',...
-                'tensorOrder_ requires a second argument')
+                'tensorOrder requires a second argument')
             end
-            if ~isempty(intersect(y,(0:1:obj.MAX_TENSOR_ORDER)))
-              obj.tensorOrder_ = y;
+            if ~isempty(intersect(y,(0:1:obj.MAX_TENSOR_ORDER)))  
+              obj.tensorOrder_ = y; flagTensorOrderSet = true;
+              if y>0,
+                % Check if we have any data dimension with 1..3 elements
+                found = false;
+                for i=2:ndims(obj.data_)
+                  if size(obj.data_,i)<=3, found = true; break, end
+                end
+                if ~found,
+                  error('irf:GenericTimeArray:GenericTimeArray:badInputs',...
+                    'cannot construct tensor:all data dimensions have size<=3')
+                end
+                obj.tensorBasis_ = 1; % set default basis to XYZ
+              end
             else
               error('irf:GenericTimeArray:GenericTimeArray:badInputs',...
-                'tensorOrder_ must be 0<=tensorOrder_<=%d',...
+                'tensorOrder must be 0<=tensorOrder<=%d',...
                 obj.MAX_TENSOR_ORDER)
+            end
+            
+          case {'tb','basis','tensorbasis'}
+            if flagTensorBasisSet
+              error('irf:GenericTimeArray:GenericTimeArray:badInputs',...
+                'tensorBasis already set')
+            end
+            if obj.tensorOrder_==0
+              error('irf:GenericTimeArray:GenericTimeArray:badInputs',...
+                'cannot set tensorBasis for a scalar (tensorOrder=0)')
+            end
+            if ~isempty(args), y = args{1}; args(1) = [];
+            else
+              error('irf:GenericTimeArray:GenericTimeArray:badInputs',...
+                'tensorBasis requires a second argument')
+            end
+            [~,iB] = intersect(obj.BASIS,y);
+            if ~isempty(iB)
+              obj.tensorBasis_ = iB; flagTensorBasisSet = true;
+            else
+              error('irf:GenericTimeArray:GenericTimeArray:badInputs',...
+                'tensorBais value not recognized')
             end
           case {'rep','repres','representation'}
             if isempty(obj.tensorOrder_),
@@ -154,11 +194,11 @@ classdef TSeries
       
       function [ok,msg] = validate_representation(x)
         ok = ''; msg = '';
-        sDim = size(obj.data,iDim);
-        if sDim>3,
+        sDim = size(obj.data,iDim); tb = obj.BASIS{obj.tensorBasis_};
+        if sDim>length(tb),
           msg = sprintf(...
-            'Dimension %d has size %d>3 and thus cannot have Representation. Use Depend instead',...
-            iDim,sDim);
+            'Dimension %d size %d>%d (Basis=%s) cannot have Representation. Use Depend instead',...
+            iDim,sDim,length(tb),tb);
           return
         end
         if ~iscell(x), 
@@ -170,7 +210,7 @@ classdef TSeries
         end
         if ~all(cellfun(@(x) ischar(x) && length(x)==1,x))
           msg = sprintf(...
-            'Representation requires char(%d) cells eleements',...
+            'Representation requires char(%d) cells elements',...
             obj.tensorOrder_);
           return
         end
@@ -180,15 +220,17 @@ classdef TSeries
             'Representation (%s) contains repeating attributes',s);
           return
         end
-        s = unique(s);
-        f = fieldnames(obj.REPRESENTATIONS);
-        for ff=f'
-          rep = ff{:};
-          if length(s) == length((intersect(obj.REPRESENTATIONS.(rep),s)))
-            ok = rep; return
-          end
+        s = unique(s); 
+        c = length((intersect(tb,s)));
+        if c == 0, msg = sprintf('Unrecognized representation');
+        elseif length(s) == c, ok = 1; % complete dim
+        elseif length(s) < c, ok = -1; % incomplete dim
+        elseif length(s) > c
+          d = setdiff(s,tb); s1 = sprintf('%s',char(d)');
+          msg = sprintf(...
+            'Unrecognized entries (%s) for representation ''%s''',s1,tb);
+        else error('should not be here')
         end
-        msg = sprintf('Unrecognized representation');
       end
     end
     
@@ -199,6 +241,12 @@ classdef TSeries
     function value = get.t(obj)
       value = obj.t_;
     end
+    
+    function value = get.tensorBasis(obj)
+      value = [obj.BASIS{obj.tensorBasis_}...
+        ' (' obj.BASIS_NAMES{obj.tensorBasis_} ')'];
+    end
+    
     function value = get.tensorOrder(obj)
       switch obj.tensorOrder_
         case 0, value = '0 (Scalar)';
