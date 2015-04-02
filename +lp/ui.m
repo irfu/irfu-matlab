332ad3dc8932739080f87a815342d04508914b35
Andris Vaivads <andris.vaivads@gmail.com>
2015-04-02 12:42:15 +0200

    work on lp.ui

--- +lp/ui.m
+++ +lp/ui.m
@@ -2,6 +2,7 @@ function ui( varargin )
 %LP.UI User interface to +lp package
 %   LP.UI(spacecraft,probe,plasma,parameters)
 
+%% Message shown once during the session
 Units=irf_units;
 persistent message;
 if isempty(message), % run only the first time during the session
@@ -12,24 +13,27 @@ end
 %% Defaults
 SpacecraftList = lp.default_spacecraft;
 ProbeList      = lp.default_lprobe;
-PlasmaList     = lp.default_plasma_models;
+PlasmaList     = lp.default_plasma;
 
 %% Check input
+args=varargin;
 while ~isempty(args)
-	if isa(args{1},'spacecraft'),
+	if isa(args{1},'lp.spacecraft'),
 		SpacecraftList = args{1};
 		args(1) =[];
-	elseif isa(args{1},'lpProbe'),
+	elseif isa(args{1},'lp.lprobe'),
 		ProbeList = args{1};
 		args(1) =[];
-	elseif isa(args{1},'plasma')
+	elseif isa(args{1},'lp.plasma')
 		PlasmaList = args{1};
 		args(1) =[];
 	else
 		
 	end
 end
-if      nargin == 0, action='initialize';end
+
+%% Initialize IDE
+lp.gui_initialize(SpacecraftList, ProbeList, PlasmaList);
 
 end
 
