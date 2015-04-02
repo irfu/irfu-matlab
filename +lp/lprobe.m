ad8917c28a91935eed278f14ae90917120667caa
Andris Vaivads <andris.vaivads@gmail.com>
2015-04-02 08:01:47 +0200

    added Area to lp.lprobe properties

--- +lp/lprobe.m
+++ +lp/lprobe.m
@@ -6,10 +6,11 @@ classdef lprobe
 		type
 		surface
 		radiusSphere
-		radiusWire
+		radiusWire     % if radius wire is two numbers than use stazer formula
 		lengthWire
 	end
 	properties (Dependent)
+		Area           % structure with fields total, sunlit
 		capacitance
 	end
 	%     data.probe.surface='themis';
@@ -19,9 +20,30 @@ classdef lprobe
 %     set(data.inp.sc.surface,'Value',find(strcmp('solar cells',lp.photocurrent))+1); % solar cells
 
 	methods
+		function Area = get.Area(Lp)
+			Area.total = 0;
+			Area.sunlit = 0;
+			if isnumeric(Lp.radiusSphere),
+				Area.total  = Area.total  + 4*pi*Lp.radiusSphere^2;
+				Area.sunlit = Area.sunlit + pi*Lp.radiusSphere^2;
+			end
+			if isnumeric(Lp.radiusWire) && isnumeric(Lp.lengthWire),
+				Area.total  = Area.total  + pi*Lp.radiusWire*Lp.lengthWire;
+				Area.sunlit = Area.sunlit + Lp.radiusWire*Lp.lengthWire;
+			end
+		end
 		function capacitance = get.capacitance(Lp)
 			cSphere  = irf_estimate('capacitance_sphere',Lp.radiusSphere);
-			cWire    = irf_estimate('capacitance_wire',  Lp.radiusWire,Lp.lengthWire);
+			if isnumeric(Lp.radiusWire) && isnumeric(Lp.lengthWire),
+				if Lp.lengthWire > 10*Lp.radiusWire
+					cWire    = irf_estimate('capacitance_wire',  Lp.radiusWire,Lp.lengthWire);
+				elseif Lp.lengthWire > Lp.radiusWire
+					cWire    = irf_estimate('capacitance_cylinder',  Lp.radiusWire,Lp.lengthWire);
+				else
+					irf.log('error','estiamte of capacitance for cylinder requires length > radius');
+					cWire = [];
+				end
+			end
 			capacitance = sum([ cSphere cWire]);
 		end
 	end
