7bdadc604f037af285d76518e7d05ae7721ac45a
Yuri Khotyaintsev <yuri@irfu.se>
2015-04-06 22:14:12 -0600

    use new function

--- mission/mms/mms_sdp_comp_phase.m
+++ mission/mms/mms_sdp_comp_phase.m
@@ -40,35 +40,10 @@ switch procId
       errStr='Bad DEFATT input, cannot proceed.';
       irf.log('critical',errStr); error(errStr);
     end
-
-    % Every time differance is less than 180 degrees assume it wrap'ed once
-    % and add 360 degrees, cumulatively.
-    % Assumption, DEFATT datapoints at least every 10 second
-    % (nominal spinrate once every 20 seconds) and all s/c spinning in +Z
-    % direction.
-    phase_jumps = [0; diff(defatt.zphase)<-180]*360;
-    defatt.zphase = defatt.zphase + cumsum(phase_jumps);
-
-    % FIXME phase calculation primarly from polyfit() and polyval if fit was
-    % good, otherwise use interp1(). S gives normal residual (ie indicator of
-    %quality), mu is scaling and centering.
-    [p, S, mu] = polyfit(double(defatt.time), defatt.zphase, 1);
-    if (S.normr <= 5)
-      % "Good" fit, ie almost constant spin rate. Use polyval.
-      [dcephase, ~] = polyval(p, double(dce.time), S, mu);
-      % FIXME Set bitmask to indicate phase from DEFATT and polyval, possibly
-      % use delta for indicating quality of phase.
-    else
-      % "Bad" fit, somewhat changing spin rate. Use table lookup via
-      % interp1(), linear interpolation with extrapolation.
-      dcephase = interp1(double(defatt.time),defatt.zphase,double(dce.time),...
-        'linear','extrap');
-      % FIXME Set bitmask to indicate phase from DEFATT but with interp1.
-    end
-    % Then wrap to interval [0 - 360) degrees.
-    dcephase = mod(dcephase, 360); % interval [0 to 360)
-    dcephase_flag = zeros(size(dcephase)); % FIXME BETTER FLAG & BITMASKING!
-    phase = struct('data', dcephase, 'bitmask', dcephase_flag);
+    
+    phaseTS = mms_defatt_phase(defatt,dce.time);
+    dcephase_flag = zeros(size(phaseTS.data)); % FIXME BETTER FLAG & BITMASKING!
+    phase = struct('data', phaseTS.data, 'bitmask', dcephase_flag);
 
   case MMS_CONST.Error
     errStr = 'mms_sdp_datamanager not properly initialized';
