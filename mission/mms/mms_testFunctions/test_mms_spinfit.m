2cdf1f0883b861dc8ce431c1ad6c72226b4700bb
thomas-nilsson-irfu <thomas-nilsson-irfu@users.noreply.github.com>
2015-04-10 12:04:46 +0200

    MMS simples possible test (sin with A offset). Phase input in Rad, not deg. Also somewhat clearer input arguments in mms_sdp_comp_spinfits when calling spinfit_mx.

--- mission/mms/mms_testFunctions/test_mms_spinfit.m
+++ mission/mms/mms_testFunctions/test_mms_spinfit.m
@@ -37,7 +37,7 @@ classdef test_mms_spinfit < matlab.unittest.TestCase
       
       % Compute spinfit for E12
       [~,sfit,~,~,~] = ...
-                c_efw_spinfit_mx(3,10,5,...
+                c_efw_spinfit_mx(3,10,3,...
                 double(timeEpochTT200Req-t0)'*1e-9,e12',phaseRad12');
       sfit(sfit==-1.5900e+09)=NaN; sfit = sfit'; sfit(isnan(sfit(:,1)),:)=[];
       
@@ -48,7 +48,7 @@ classdef test_mms_spinfit < matlab.unittest.TestCase
       
       % Compute spinfit for E34
       [~,sfit,~,~,~] = ...
-                c_efw_spinfit_mx(3,10,5,...
+                c_efw_spinfit_mx(3,10,3,...
                 double(timeEpochTT200Req-t0)'*1e-9,e34',phaseRad34');
       sfit(sfit==-1.5900e+09)=NaN; sfit = sfit'; sfit(isnan(sfit(:,1)),:)=[];
       
@@ -56,6 +56,55 @@ classdef test_mms_spinfit < matlab.unittest.TestCase
       testCase.verifyEqual(median(sfit(:,1)),adcOff_34,'AbsTol',1e-6);
       testCase.verifyEqual(median(sfit(:,2)),Ex,'AbsTol',1e-6);
       testCase.verifyEqual(median(sfit(:,3)),Ey,'AbsTol',1e-6);
+    end
+    function test_mms_spinfit_mx_sinus_with_offset(testCase)
+      % Simplest of test cases. Input data is only a pure sinus wave with
+      % one static offset, a static spinrate is also applied. No
+      % phaseshift or anything complicated.
+      DEBUG = true; % Show some plots...
+      fitEvery = 5e9; fitInterv = 20e9; %ns
+      maxIt = 3; minPts = 10; nTerms = 3;
+      spinRate = 3.1; %rpm
+      % 0.5 sec sampling
+      timeSecPhase = (0:100)'*.5;
+      timeTT2000 = (timeSecPhase*1e9); %ns
+      t0 = 5e9; % first even fitEvery, accounting for leap seconds...
+      zphase = mod(360*timeSecPhase*spinRate/60, 360); % deg.
+      radPhase = unwrap(zphase*pi/180); % rad, unwrapped
+      % Pure sinus amplitude 1 with offset + 2
+      expected_A = 2;
+      expected_B = 0;
+      expected_C = 1;
+      % Data = A + B*cos(wt) + C*sin(wt)
+      dataInput = expected_A + expected_B*cos(radPhase) + expected_C*sin(radPhase);
+      [~, sfit, ~, ~, ~] = mms_spinfit_mx( maxIt, minPts, nTerms, ...
+        timeTT2000', dataInput', radPhase', fitEvery, fitInterv, t0 );
+      % Remove bad values
+      sfit(sfit==-1.5900e+09)=NaN; sfit = sfit'; sfit(isnan(sfit(:,1)),:)=[];
+      diffA = median(sfit(:,1)) - expected_A;
+      diffB = median(sfit(:,2)) - expected_B;
+      diffC = median(sfit(:,3)) - expected_C;
+      if(DEBUG)
+        timeEpoch = irf_time(int64(timeTT2000),'ttns>epoch');
+        extrapResult = median(sfit(:,1)) + median(sfit(:,2))*cosd(zphase) + median(sfit(:,3))*sind(zphase);
+        figure; h=irf_plot({[timeEpoch, zphase],[timeEpoch, dataInput],[timeEpoch, extrapResult]});
+        title(h(1),'Comparison of phase, input data and extrapolated data from median of spinfit coeff.');
+        ylabel(h(1),{'Zphase','[deg]'})
+        ylabel(h(2),{'Input data',...
+          'y=expected_A + expected_B*cosd(zphase) + expected_C*sind(zphase)',...
+          '[mV/m]'})
+        ylabel(h(2),{'Input data','y=A + B*cosd(zphase) + C*sind(zphase)',...
+          '[mV/m]'})
+        ylabel(h(2),{'Input data','y=A + ...','B*cosd(zphase) + ...',...
+          'C*sind(zphase)','[mV/m]'})
+        ylabel(h(3),{'Extrapol data from median spinfit',...
+          'y=sfit(1) + ...','sfit(2)*cosd(zphase) + ...',...
+          'sfit)*sind(zphase)','[mV/m]'}) % actually "median(sfit(1,:))" but write something shorter
+      end
+      % Verify all fits are as expected
+      testCase.verifyEqual(diffA, diffA*0,'AbsTol',1e-6);
+      testCase.verifyEqual(diffB, diffB*0,'AbsTol',1e-6);
+      testCase.verifyEqual(diffC, diffC*0,'AbsTol',1e-6);
     end
     function test_mms_spinfit_mx_simple(testCase)
       %% Test constant spin
