c1af590b3dcd533e877cde2e18e7aaa66edb0889
Yuri Khotyaintsev <yuri@irfu.se>
2015-04-12 19:56:08 -0600

    add help

--- mission/mms/mms_fields_file_info.m
+++ mission/mms/mms_fields_file_info.m
@@ -1,4 +1,14 @@
 function filenameData = mms_fields_file_info(fileName)
+%MMS_FIELDS_FILE_INFO  decode fields CDF file name
+%
+% filenameData = mms_fields_file_info(fileName)
+
+if length(fileName)<28
+  errStr = 'FILENAME too short'; irf.log('critical',errStr)
+  error('MATLAB:MMS_FIELDS_FILE_INFO:INPUTFILE',errStr);
+end
+
+if strcmpi(fileName(end-3:end),'.cdf'), fileName(end-3:end) = []; end
 
 pos = strfind(fileName,'_');
 if length(pos)<5
@@ -15,7 +25,7 @@ filenameData.dataType = '';
 if length(pos)==6, filenameData.dataType = fileName(pos(4)+1:pos(5)-1); end
 filenameData.startTime = fileName(pos(end-1)+1:pos(end)-1);
 filenameData.date = filenameData.startTime;
-filenameData.vXYZ = fileName(pos(end)+2:end-4);
+filenameData.vXYZ = fileName(pos(end)+2:end);
 % Also store the filename, used as Parents reference for output files.
 filenameData.filename = fileName;
 
