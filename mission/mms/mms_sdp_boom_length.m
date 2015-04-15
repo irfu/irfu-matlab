6098066ac3b88cc480b870ffe7ec4e66b1c1e41f
Yuri Khotyaintsev <yuri@irfu.se>
2015-04-14 22:26:03 -0600

    rework for better usability

--- mission/mms/mms_sdp_boom_length.m
+++ mission/mms/mms_sdp_boom_length.m
@@ -1,10 +1,54 @@
-function [Blen] = mms_sdp_boom_length(tInp,sc_id)
+function [Blen] = mms_sdp_boom_length(scId,tInp)
 %MMS_SDP_BOOM_LENGTH   get MMS SDP boom lengths
 %
-% [Blen] = mms_sdp_boom_length(t,sc_id)
+% [Blen] = mms_sdp_boom_length(scId)
 %
 
-narginchk(2,2)
+narginchk(1,2)
+
+% Table of boom lengths
+switch scId
+  case 1
+    Blen(1).time=get_tt([2015 03 13 00 00 00]); Blen(1).len=[ 00 00 00 00 ];
+    Blen(2).time=get_tt([2015 04 04 06 43 15]); Blen(2).len=[ 00 00 03 03 ];
+    Blen(3).time=get_tt([2015 04 04 06 54 51]); Blen(3).len=[ 03 03 03 03 ];
+    Blen(4).time=get_tt([2015 04 04 07 25 34]); Blen(4).len=[ 03 03 17 17 ];
+    Blen(5).time=get_tt([2015 04 04 07 58 51]); Blen(5).len=[ 17 17 17 17 ];
+    Blen(6).time=get_tt([2015 04 09 14 09 50]); Blen(6).len=[ 17 17 22 22 ];
+    Blen(7).time=get_tt([2015 04 13 01 11 13]); Blen(7).len=[ 22 22 22 22 ];
+    Blen(8).time=get_tt([2015 04 13 14 22 32]); Blen(8).len=[ 22 22 41 41 ];   
+  case 2
+    Blen(1).time=get_tt([2015 03 13 00 00 00]); Blen(1).len=[ 00 00 00 00 ];
+    Blen(2).time=get_tt([2015 04 06 10 49 15]); Blen(2).len=[ 00 00 03 03 ];
+    Blen(3).time=get_tt([2015 04 06 11 00 32]); Blen(3).len=[ 03 03 03 03 ];
+    Blen(4).time=get_tt([2015 04 06 11 31 53]); Blen(4).len=[ 03 03 17 17 ];
+    Blen(5).time=get_tt([2015 04 06 12 03 28]); Blen(5).len=[ 17 17 17 17 ];
+    Blen(6).time=get_tt([2015 04 09 16 23 38]); Blen(6).len=[ 17 17 22 22 ];
+    Blen(7).time=get_tt([2015 04 13 03 20 36]); Blen(7).len=[ 22 22 22 22 ];
+    Blen(8).time=get_tt([2015 04 13 04 07 12]); Blen(8).len=[ 22 22 41 41 ];
+  case 3
+    Blen(1).time=get_tt([2015 03 13 00 00 00]); Blen(1).len=[ 00 00 00 00 ];
+    Blen(2).time=get_tt([2015 04 07 06 24 26]); Blen(2).len=[ 00 00 03 03 ];
+    Blen(3).time=get_tt([2015 04 07 06 34 54]); Blen(3).len=[ 03 03 03 03 ];
+    Blen(4).time=get_tt([2015 04 07 06 49 46]); Blen(4).len=[ 03 03 7.3 7.3 ];
+    Blen(5).time=get_tt([2015 04 07 07 49 48]); Blen(5).len=[ 03 03 17 17 ];
+    Blen(6).time=get_tt([2015 04 07 08 21 36]); Blen(6).len=[ 17 17 17 17 ];
+    Blen(7).time=get_tt([2015 04 10 14 13 18]); Blen(7).len=[ 17 17 22 22 ];
+    Blen(8).time=get_tt([2015 04 12 14 04 22]); Blen(8).len=[ 22 22 22 22 ];
+    Blen(9).time=get_tt([2015 04 14 14 23 18]); Blen(9).len=[ 22 22 41 41 ];
+  case 4
+    Blen(1).time=get_tt([2015 03 13 00 00 00]); Blen(1).len=[ 00 00 00 00 ];
+    Blen(2).time=get_tt([2015 04 05 10 50 56]); Blen(2).len=[ 00 00 03 03 ];
+    Blen(3).time=get_tt([2015 04 05 11 01 46]); Blen(3).len=[ 03 03 03 03 ];
+    Blen(4).time=get_tt([2015 04 05 11 32 05]); Blen(4).len=[ 03 03 17 17 ];
+    Blen(5).time=get_tt([2015 04 05 12 03 35]); Blen(5).len=[ 17 17 17 17 ];
+    Blen(6).time=get_tt([2015 04 10 16 22 20]); Blen(6).len=[ 17 17 22 22 ];
+    Blen(7).time=get_tt([2015 04 12 16 16 49]); Blen(7).len=[ 22 22 22 22 ];
+    Blen(8).time=get_tt([2015 04 14 16 39 13]); Blen(8).len=[ 22 22 41 41 ];
+  otherwise
+    error('Invalid scId')
+end
+if nargin == 1, return, end
 
 if isa(tInp,'GenericTimeArray'), t = tInp.toEpochTT2000().epoch;
 elseif isa(tInp,'int64'), t = tInp;
@@ -12,62 +56,34 @@ else
   errStr = 'T must be tt2000_ns (int64) or GenericTimeArray';
   irf.log('critical',errStr), error(errStr)
 end
-t = t(1);
 
-% Table of boom lengths
-switch sc_id
-  case 1
-    if t>=get_ns([ 2015 04 13 14 22 32 ]), Blen = [ 22 22 41 41  ];
-    elseif t>=get_ns([ 2015 04 13 01 11 13 ]), Blen = [ 22 22 22 22  ];
-    elseif t>=get_ns([ 2015 04 09 14 09 50 ]), Blen = [ 17 17 22 22  ];
-    elseif t>=get_ns([ 2015 04 04 07 58 51 ]), Blen = [ 17 17 17 17  ];
-    elseif t>=get_ns([ 2015 04 04 07 25 34 ]), Blen = [ 03 03 17 17  ];
-    elseif t>=get_ns([ 2015 04 04 06 54 51 ]), Blen = [ 03 03 03 03  ];
-    elseif t>=get_ns([ 2015 04 04 06 43 15 ]), Blen = [ 00 00 03 03  ];
-    else
-      Blen = [ 0 0 0 0 ];
+if isscalar(t), Blen = Blen(find_idx(t));
+elseif isvector(t), Blen = Blen(find_idx_range(t));
+else
+  errStr = 'T(one point) or TINT (2 points) is expected';
+  irf.log('critical',errStr), error(errStr)
+end 
+  function idx = find_idx(t)
+    idx = arrayfun(@(x) x.time.epoch<=t,Blen);
+    if ~any(idx)
+      errS = 'Invalid time input (outside of range)';
+      irf.log('critical',errS), error(errS)
     end
-  case 2
-    if t>=get_ns([ 2015 04 13 04 07 12 ]), Blen = [ 22 22 41 41  ];
-    elseif t>=get_ns([ 2015 04 13 03 20 36 ]), Blen = [ 22 22 22 22  ];
-    elseif t>=get_ns([ 2015 04 09 16 23 38 ]), Blen = [ 17 17 22 22  ];
-    elseif t>=get_ns([ 2015 04 06 12 03 28 ]), Blen = [ 17 17 17 17  ];
-    elseif t>=get_ns([ 2015 04 06 11 31 53 ]), Blen = [ 03 03 17 17  ];
-    elseif t>=get_ns([ 2015 04 06 11 00 32 ]), Blen = [ 03 03 03 03  ];
-    elseif t>=get_ns([ 2015 04 06 10 49 15 ]), Blen = [ 00 00 03 03  ];
-    else
-      Blen = [ 0 0 0 0 ];
-    end
-  case 3
-    if t>=get_ns([ 2015 04 14 14 23 18 ]), Blen = [ 22 22 41 41  ];
-    elseif t>=get_ns([ 2015 04 12 14 04 22 ]), Blen = [ 22 22 22 22  ];
-    elseif t>=get_ns([ 2015 04 10 14 13 18 ]), Blen = [ 17 17 22 22  ];
-    elseif t>=get_ns([ 2015 04 07 08 21 36 ]), Blen = [ 17 17 17 17  ];
-    elseif t>=get_ns([ 2015 04 07 07 49 48 ]), Blen = [ 03 03 17 17  ];
-    elseif t>=get_ns([ 2015 04 07 06 49 46 ]), Blen = [ 03 03 7.3 7.3  ];
-    elseif t>=get_ns([ 2015 04 07 06 34 54 ]), Blen = [ 03 03 03 03  ];
-    elseif t>=get_ns([ 2015 04 07 06 24 26 ]), Blen = [ 00 00 03 03  ];
-    else
-      Blen = [ 0 0 0 0 ];
-    end
-  case 4
-    if t>=get_ns([ 2015 04 14 16 39 13 ]), Blen = [ 22 22 41 41  ];
-    elseif t>=get_ns([ 2015 04 12 16 16 49 ]), Blen = [ 22 22 22 22  ];
-    elseif t>=get_ns([ 2015 04 10 16 22 20 ]), Blen = [ 17 17 22 22  ];
-    elseif t>=get_ns([ 2015 04 05 12 03 35 ]), Blen = [ 17 17 17 17  ];
-    elseif t>=get_ns([ 2015 04 05 11 32 05 ]), Blen = [ 03 03 17 17  ];
-    elseif t>=get_ns([ 2015 04 05 11 01 46 ]), Blen = [ 03 03 03 03  ];
-    elseif t>=get_ns([ 2015 04 05 10 50 56 ]), Blen = [ 00 00 03 03  ];
-    else
-      Blen = [ 0 0 0 0 ];
-    end
-  otherwise
-    error('Invalid sc_id')
-end
-
-  function ns = get_ns(vec)
+    idx = find(idx); idx = idx(end);
+  end
+  function idx = find_idx_range(t)
+    errS = 'Invalid time input (outside of range)';
+    idx1 = arrayfun(@(x) x.time.epoch<=t(1),Blen);
+    if ~any(idx1), irf.log('critical',errS), error(errS), end
+    idx1(find(idx1,1,'last')) = false;
+    idx = arrayfun(@(x) x.time.epoch<=t(end),Blen);
+    if ~any(idx), irf.log('critical',errS), error(errS), end
+    idx(idx1) = false;
+  end
+  function ns = get_tt(vec)
     % Convert date to tt2000_ns
-    ns = irf_time(vec,'vector6>ttns');
+    ttns = irf_time(vec,'vector6>ttns');
+    ns = EpochTT2000(ttns);
   end
 end
 
