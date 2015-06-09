9431b2d2d86f523fed7c02cfee0fcfeff26d8d0e
thomas-nilsson-irfu <thomas-nilsson-irfu@users.noreply.github.com>
2015-06-09 11:05:10 +0200

    Add some TSeries support in irf_abs.

--- irf/irf_abs.m
+++ irf/irf_abs.m
@@ -8,26 +8,51 @@ function [y]=irf_abs(x,flag)
 % 
 % if flag==1 then y returns only abs(x)
 %
+% If input is of type TSeries and flag==1 then the returned value y is a
+% scalar time series corresponding to abs(x).
+%
 % $Id$
 
 if isempty(x), y=[];return;end % empty output for empty input
 
-xx= size(x); lx=xx(2); % the number of vector components
-
-y=[x x(:,1)*0];
-if lx == 2
-  y(:,lx+1)=sqrt(x(:,1).^2+x(:,2).^2);
-elseif lx == 3
-  y(:,lx+1)=sqrt(x(:,1).^2+x(:,2).^2+x(:,3).^2);
-elseif lx > 3
-  y(:,lx+1)=sqrt(x(:,2).^2+x(:,3).^2+x(:,4).^2);
+if isa(x,'TSeries')
+  % Time series
+  if nargin == 2 && x.tensorOrder==1 && flag == 1
+    if(regexp(x.tensorBasis,'xy')) % cartesian xy (2d) or xyz (3d)
+      data = sqrt( sum(abs(x.data).^2, 2) );
+      y = irf.ts_scalar(x.time, data);
+    elseif(regexp(x.tensorBasis,'r[tl]p')) % spherical, colatitude/latitude
+      data = abs(x.r.data);
+      y = irf.ts_scalar(x.time, data);
+    elseif(regexp(x.tensorBasis,'rpz')) % cylindrical
+      data = sqrt(abs(x.r.data).^2 + abs(x.z.data).^2);
+      y = irf.ts_scalar(x.time, data);
+    else
+      error('Not yet implemented!');
+    end
+  else
+    error('Not yet implemented!');
+  end
 else
-  disp('Not enough vector components in irf_abs()')
+  lx = size(x,2); % the number of vector components
+
+  y=[x x(:,1)*0];
+  if lx == 2
+    y(:,lx+1)=sqrt(x(:,1).^2+x(:,2).^2);
+  elseif lx == 3
+    y(:,lx+1)=sqrt(x(:,1).^2+x(:,2).^2+x(:,3).^2);
+  elseif lx > 3
+    y(:,lx+1)=sqrt(x(:,2).^2+x(:,3).^2+x(:,4).^2);
+  else
+    disp('Not enough vector components in irf_abs()')
+  end
+
+  % if flag=1 only abs(y) should be returned
+  if nargin == 2,
+    if flag == 1,
+      yy=y(:,lx+1);clear y;y=yy;
+    end
+  end
 end
 
-% if flag=1 only abs(y) should be returned 
-if nargin == 2,
-  if flag == 1,
-    yy=y(:,lx+1);clear y;y=yy;
-  end
 end
