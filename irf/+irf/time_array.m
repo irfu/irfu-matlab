ff2254924ec7c56006de709c15ba95d98c59a3ab
Andris Vaivads <andris.vaivads@gmail.com>
2015-09-26 20:06:13 +0200

    irf.time_array: added help

--- irf/+irf/time_array.m
+++ irf/+irf/time_array.m
@@ -1,45 +1,52 @@
-function TaTT = time_array(start,dt)
+function TaTT = time_array(tStart,dtArray)
 %TIME_ARRAY  Factory of time arrays (EpochTT)
 %
-% TaTT = irf.time_array(start,dt)
-% TaTT = irf.time_array(times)
-
-% ----------------------------------------------------------------------------
-% "THE BEER-WARE LICENSE" (Revision 42):
-% <yuri@irfu.se> wrote this file.  As long as you retain this notice you
-% can do whatever you want with this stuff. If we meet some day, and you think
-% this stuff is worth it, you can buy me a beer in return.   Yuri Khotyaintsev
-% ----------------------------------------------------------------------------
+% TaTT = irf.time_array(tStart,dtArray)
+% TaTT = irf.time_array(tArray)
+%
+% Input: 
+%   tStart is reference time given as either GenericTimeArray, UTC, or TT in
+%     seconds (double) or nanoseconds (int64)
+%   dtArray is an array of time values measured since tStart. dtArray can be
+%     specified in seconds (double) or nanoseconds (int64). 
+%   tArray is time array given either as GenericTimeArray, UTC or TT in
+%     seconds (doubel or nanoseconds (int64)
+% Output: 
+%  TaTT time array as EpochTT
+%
+% Example:
+%  TT = irf.time_array('2015-09-26T20:00:00',[0 10 20])
+% 
 
 narginchk(1,2)
 
-if isa(start,'GenericTimeArray')
+if isa(tStart,'GenericTimeArray')
   %
-elseif isa(start,'char')
-  if nargin==2 && ~isvector(start) || ~ismatrix(start)
+elseif isa(tStart,'char')
+  if nargin==2 && ~isvector(tStart) || ~ismatrix(tStart)
     errStr = 'START must be a string yyyy-mm-ddThh:mm:ss[.mmmuuunnnZ]';
     irf.log('critical', errStr), error(errStr)
   end
   %
-elseif (isa(start,'double') || isa(start,'int64')) && ...
-    ((nargin==2 && isscalar(start)) || isvector(start))
+elseif (isa(tStart,'double') || isa(tStart,'int64')) && ...
+    ((nargin==2 && isscalar(tStart)) || isvector(tStart))
   %
 else
   errStr = 'Unrecoglized input';
   irf.log('critical', errStr), error(errStr)
 end
 
-Epoch0 = EpochTT(start);
+Epoch0 = EpochTT(tStart);
 if nargin == 1, TaTT = Epoch0; return, end
 
-if ~isvector(dt) || ~isnumeric(dt)
+if ~isvector(dtArray) || ~isnumeric(dtArray)
   errStr = 'DT must be a numeric vector';
   irf.log('critical', errStr), error(errStr)
 end
 
-if isa(dt,'int64')
-  TaTT = EpochTT(Epoch0.epoch + dt);
-elseif isa(dt,'double')
-  TaTT = EpochTT(Epoch0.epoch + int64(dt*1e9));
+if isa(dtArray,'int64')
+  TaTT = EpochTT(Epoch0.epoch + dtArray);
+elseif isa(dtArray,'double')
+  TaTT = EpochTT(Epoch0.epoch + int64(dtArray*1e9));
 end
   
