64a0d8c3b6179f6425af57ef1a3f7a0f7d8f035c
Yuri Khotyaintsev <yuri@irfu.se>
2015-09-27 20:46:05 +0200

    move "new figure functionality" from irf_plot to this new function

---
+++ irf/irf_new_figure.m
@@ -0,0 +1,103 @@
+function c=irf_new_figure(varargin)
+%IRF_NEW_FIGURE  
+%  
+%  H = IRF_NEW_FIGURE(NPANELS)
+%  Initialize new figure with NPANELS new panels
+%  Will use current figure, if empty.
+%
+%  H = IRF_NEW_FIGURE(HF,NPANELS)
+%  Initialize existing figure with handle HF with NPANELS new panels, 
+%  
+%  
+%  H = IRF_NEW_FIGURE(HF,NPANELS,'reset')
+%  Initialize figure HF with NPANELS new panels
+%  and reset it's size and other properties to default
+
+narginchk(1, 3)
+
+args = varargin;
+nargs = nargin;
+hcf=[]; flag = '';
+switch nargs
+  case 1, nSubplots = args{1};
+  case 2, hcf = args{1}; nSubplots = args{2};
+  case 3, hcf = args{1}; nSubplots = args{2}; flag = args{3};
+  otherwise, error('invalid number of input arguments')
+end
+    
+if ~isnumeric(nSubplots), error('nSubplots must be a positive number'), end
+if length(nSubplots)>1 || nSubplots<1 || nSubplots>20
+  error('nSubplots must be >=1 && <=20')
+end
+
+flagReset = false;
+if ~ischar(flag), error('FLAG must be a string'), end
+switch lower(flag)
+  case ''
+  case 'reset', flagReset = true;
+  otherwise, error('unrecognized FLAG')
+end
+
+if isempty(hcf) 
+  if isempty(get(gcf,'children')) 
+    irf.log('notice','Using current empty figure (no reset)')
+    hcf = gcf;
+  else hcf = figure; flagReset = true;
+  end
+else
+  if numel(hcf)>1 || ~isnumeric(hcf)
+    error('HF must be a valid figure handle or figure number')
+  end
+  if ~ishghandle(hcf,'figure')
+    if flagReset
+      error('Expecting a valid figure handle HG for FLAG=reset')
+    end
+    hcf = figure(hcf); flagReset = true;
+  end
+end
+
+if nSubplots>=1 && nSubplots<=20,
+  nSubplots = floor(nSubplots);
+  c = gobjects(1,nSubplots);
+  
+  if flagReset
+		xSize = 11;
+		ySize = 5+5*sqrt(nSubplots);
+		xLeft = (21-xSize)/2; yTop = (30-ySize)/2;
+		set(hcf,'PaperPosition',[xLeft yTop xSize ySize])
+		un=get(0,'units');
+		set(0,'units','pixels');
+		sz=get(0,'screensize');
+		xx=min(min(700,sz(3))/xSize,min(900,sz(4))/ySize); % figure at least 600 wide or 900 height but not outside screen
+		set(hcf,'Position',[10 10 xSize*xx ySize*xx])
+		set(0,'units',un);
+		clear xSize sLeft ySize yTop
+    % XXX not sure these need to go here
+    set(hcf,'color','white'); % white background for figures (default is grey)
+    %set(hcf,'renderer','zbuffer'); % opengl has problems on Mac (no log scale in spectrograms)
+    %set(hcf,'PaperUnits','centimeters');
+    %set(hcf,'defaultlinelinewidth',1.0);
+    %set(hcf,'defaultAxesFontSize',14);
+    %set(hcf,'defaultTextFontSize',14);
+    %set(hcf,'defaultAxesFontUnits','pixels');
+    %set(hcf,'defaultTextFontUnits','pixels');
+    set(hcf,'defaultAxesColorOrder',[0 0 0;0 0 1;1 0 0;0.3 0.3 0.3;0 1 1 ;1 0 1; 1 1 0])
+	end
+  clf;
+  all_axis_position=[0.17 0.1 0.9 0.95]; % xmin ymin xmax ymax
+  subplot_width=all_axis_position(3)-all_axis_position(1);
+  subplot_height=(all_axis_position(4)-all_axis_position(2))/nSubplots;
+  for j=1:nSubplots,
+    c(j)=axes('position',[all_axis_position(1) ...
+      all_axis_position(4)-j*subplot_height ...
+      subplot_width subplot_height]); % [x y dx dy]
+    cla(c(j));
+    set(c(j),'box','on','tag','');
+  end
+  user_data = get(gcf,'userdata');
+  user_data.subplot_handles = c;
+  user_data.current_panel=0;
+  set(hcf,'userdata',user_data);
+  figure(hcf); % bring figure to front
+end
+end
