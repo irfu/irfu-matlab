52b1a447faa00659776f51e5ca239ddbeeded2ec
Yuri Khotyaintsev <yuri@irfu.se>
2015-09-28 12:37:55 +0200

    reorder

--- plots/mms/Example_MMS_B1.m
+++ plots/mms/Example_MMS_B1.m
@@ -4,12 +4,16 @@ gsmB1 = get_ts(dfgFile,'mms1_dfg_srvy_l2pre_gsm');
 gsmR1 = get_ts(dfgFile,'mms1_pos_gsm');
 
 %% Plot
-tintStr = '2015-09-11T07:30:00Z/2015-09-11T08:30:00Z';
 h = irf_plot(gsmB1,'comp');
+
+tintStr = '2015-09-11T07:30:00Z/2015-09-11T08:30:00Z';
+title(h(1),tintStr)
 irf_zoom(h,'x',irf.tint(tintStr));
 irf_zoom(h,'y')
+
 add_position(h(end),gsmR1), xlabel(h(end),'')
-title(h(1),tintStr)
+
 ylabel(h(1),'Bx GSM [nT]')
 ylabel(h(2),'By GSM [nT]')
 ylabel(h(3),'Bz GSM [nT]')
+irf_plot_ylabels_align(h)
