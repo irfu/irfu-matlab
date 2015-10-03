d3ecb1156f9dc94dbebbe85838608ad794c30e40
Daniel Graham <dgraham@irfu.se>
2015-10-03 23:53:16 +0200

    TSeries fix

--- irf/irf_add.m
+++ irf/irf_add.m
@@ -9,6 +9,11 @@ function [y]=irf_add(c1,x1,c2,x2)
 %
 % $Id$
 
+if isa(x1,'TSeries') % assumes x2 is TSeries and no. columns are the same
+    x2 = x2.resample(x1);
+    y = x1;
+    y.data = c1*x1.data+c2*x2.data;   
+else
 if size(x1,2)>size(x2,2),
   irf_log('proc','WARNING: x1 has more columns than x2');
   if size(x2,2)==2, for j=3:size(x1,2), x2(:,j)=x2(:,2); end % assume x2 is time series of scalar
@@ -36,4 +41,4 @@ end
 y=x1;
 
 y(:,2:end)=c1*x1(:,2:end)+c2*x2(:,2:end);
-
+end
