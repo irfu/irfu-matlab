fa5576941b3da7af5a7a7c9ec2cd86ea57b02523
thomas-nilsson-irfu <thomas-nilsson-irfu@users.noreply.github.com>
2015-10-12 11:58:34 +0200

    toepoch - Allow for year < 1970 in old Matlab. Aslo use built in function leapyear() - Req. Matlab 2006b or later, ie 9 years old Matlab by now..

--- irf/toepoch.m
+++ irf/toepoch.m
@@ -34,26 +34,29 @@ end
 if(verLessThan('matlab','8.4'))
   for year=unique(x(:,1))'
     daym=[0 31 28 31 30 31 30 31 31 30 31 30 31];
-    if (rem(year,4)==0)
-      daym(3) = 29; % Leap years: every four years, except;
-      if rem(year,100)==0 && rem(year,400)~=0
-        % Not leap years: every even century not evenly divisable with 400.
-        % ie. 1700, 1800, 1900, 2100, 2200
-        daym(3) = 28;
-      end
-    end
-    days=cumsum(daym(1:12))';
-    ind=find(years==year);
-    hours(ind,1)=(days(x(ind,2))+(x(ind,3)-1))*24+x(ind,4);
-    secs(ind,1)=(hours(ind)*60+x(ind,5))*60+x(ind,6);
+    if leapyear(year), daym(3) = 29; end
+    days = cumsum(daym(1:12))';
+    ind = find(years==year);
+    hours(ind,1) = (days(x(ind,2))+(x(ind,3)-1))*24+x(ind,4);
+    secs(ind,1) = (hours(ind)*60+x(ind,5))*60+x(ind,6);
     yr_sec = 0;
     diff_yr = year - 1970;
-    for i = 1:diff_yr
-        if rem(1969+i,4)==0
+    if(diff_yr>0)
+      for i = 1:diff_yr
+        if leapyear(1969+i)
             yr_sec = yr_sec + 31622400;
         else
             yr_sec = yr_sec + 31536000;
         end
+      end
+    elseif(diff_yr<0)
+      for i = -1:-1:diff_yr
+        if leapyear(1970+i)
+          yr_sec = yr_sec - 31622400;
+        else
+          yr_sec = yr_sec - 31536000;
+        end
+      end
     end
     secs(ind,1) = secs(ind,1) + yr_sec;
   end
