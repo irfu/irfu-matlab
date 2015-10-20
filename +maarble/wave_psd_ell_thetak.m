40ddc948c1c14e646ef12889c200afebda45c955
Yuri Khotyaintsev <yuri@irfu.se>
2015-10-20 14:49:42 +0200

    do not load pc12/pc35 when not neded minimize code duplication

--- +maarble/wave_psd_ell_thetak.m
+++ +maarble/wave_psd_ell_thetak.m
@@ -44,48 +44,54 @@ for ievent=1:numel(TT),
           'mat','tint',tintDl);
       end
       if size(gseR,1)<=3, continue, end
-      indexDir = [ULF_PATH 'Cluster/C' cl_s '/PC12/CDF'];
     else % THEMIS
       gseR = irf_tlim(Rth.(['Rth' lower(th_s)]),tintDl);
-      indexDir = [ULF_PATH 'THEMIS/TH' th_s '/PC12/CDF'];
     end
-    cd(indexDir);
-    d = dir(['C' cl_s '*.cdf']);
-    m=0; i=1;
-    while i <= length(d) && m == 0,
-      if isempty(th_s), tend = d(i).name(45:59);
-      else tend = d(i).name(49:63);
-      end
-      tenddate = datenum(tend,'yyyymmdd_HHMMSS');
-      if tenddate > epoch2date(tintDl(1)), m=i; end
-      i=i+1;
+    
+    if flagSkipPC35, ebspPC35 = []; else ebspPC35 = get_ebsp('PC35'); end
+    if flagSkipPC12, ebspPC12 = []; else ebspPC12 = get_ebsp('PC12'); end
+    
+    if isempty(ebspPC35) && isempty(ebspPC12)
+      irf.log('warning','No data')
+      continue
     end
-    get_ebsp(d(m).name)
     
     %% Combine PC12  &  PC35
     % Frequency limits
-    freq = [tocolumn(ebspPC35.f.data); tocolumn(ebspPC12.f.data)];
+    if flagSkipPC35, freq = tocolumn(ebspPC12.f.data);
+    elseif flagSkipPC12, freq = tocolumn(ebspPC35.f.data);
+    else
+      freq = [tocolumn(ebspPC35.f.data); tocolumn(ebspPC12.f.data)];
+    end
     loweridx = find(abs(freq-lowerFreqBound)<.001);
     upperidx = find(abs(freq-upperFreqBound)<.001);
     idxFreq = loweridx:upperidx;
     
-    [outTime, idxPC35] = irf_tlim(ebspPC35.t.data,tint);
-    BBssPC35 = double(ebspPC35.bb_xxyyzzss.data(idxPC35,:,4));
-    ellPC35  = double(ebspPC35.ellipticity.data(idxPC35,:));
-    ktPC35   = double(ebspPC35.k_tp.data(idxPC35,:,1));
-    if flagSkipPC12, BBss = BBssPC35; ell = ellPC35; thetaK = ktPC35;
+    if flagSkipPC35
+      tint1min = [floor(tint(1)/60) ceil(tint(2)/60)]*60;
+      outTime = ((tint1min(1)):60:(tint1min(end)))';
+    else 
+      [outTime, idxPC35] = irf_tlim(ebspPC35.t.data,tint);
+      BBssPC35 = double(ebspPC35.bb_xxyyzzss.data(idxPC35,:,4));
+      ellPC35  = double(ebspPC35.ellipticity.data(idxPC35,:));
+      ktPC35   = double(ebspPC35.k_tp.data(idxPC35,:,1));
+    end
+    if flagSkipPC12, BBss = BBssPC35; ell = ellPC35; thK = ktPC35;
     else
       [inTime, idxPC12] = irf_tlim(ebspPC12.t.data, tint + 30*[-1 1]); % 30 sec on each side for PC12 for propoper averaging
       if isempty(idxPC12), error('No PC12 data'), end
       BBssPC12 = double(ebspPC12.bb_xxyyzzss.data(idxPC12,:,4));
       BBssPC12av = AverageData(BBssPC12,inTime,outTime);
-      BBss = [BBssPC35, BBssPC12av]; % combined BBss for PC12 & 35
       ellPC12 = double(ebspPC12.ellipticity.data(idxPC12,:));
-      ellPC12 = AverageData(ellPC12,inTime,outTime);
-      ell = [ellPC35, ellPC12]; % combined ellipticity for PC12 & 35
-      ktPC12=double(ebspPC12.k_tp.data(idxPC12,:,1));
-      ktPC12 = AverageData(ktPC12,inTime,outTime);
-      thetaK = [ktPC35, ktPC12]; % combined thetaK for PC12 & 35
+      ellPC12av = AverageData(ellPC12,inTime,outTime);
+      ktPC12 = double(ebspPC12.k_tp.data(idxPC12,:,1));
+      ktPC12av = AverageData(ktPC12,inTime,outTime);
+      if flagSkipPC35, BBss = BBssPC12av; ell = ellPC12av; thK = ktPC12av;
+      else % combined PC12 & PC35
+        BBss = [BBssPC35, BBssPC12av];
+        ell = [ellPC35, ellPC12av]; 
+        thK = [ktPC35, ktPC12av];
+      end
     end
     BBss=BBss(:,idxFreq);
     [psdBpeak,idxPeakTmp]=max(BBss,[],2);
@@ -93,8 +99,8 @@ for ievent=1:numel(TT),
     idxPeak = ((1:size(BBss,1))'-1)*size(BBss,2)+idxPeakTmp;
     ell = ell(:,idxFreq)'; % transpose to get the idx right
     ellPeak = ell(idxPeak);
-    thetaK=thetaK(:,idxFreq)'; % transpose to get the idx right
-    thetaKpeak=thetaK(idxPeak);
+    thK=thK(:,idxFreq)'; % transpose to get the idx right
+    thKpeak=thK(idxPeak);
     
     % Mag coordinates
     gseR = irf_resamp(gseR,outTime);
@@ -108,7 +114,7 @@ for ievent=1:numel(TT),
     Out.fPeak = [Out.fPeak; fPeak];
     Out.psdBpeak = [Out.psdBpeak; psdBpeak];
     Out.ellipticity = [Out.ellipticity; ellPeak];
-    Out.thetaK = [Out.thetaK; thetaKpeak];
+    Out.thetaK = [Out.thetaK; thKpeak];
     Out.gseR = [Out.gseR; gseR];
     Out.mlt = [Out.mlt; mlt];
     Out.mLat = [Out.mLat; mLat];
@@ -131,20 +137,13 @@ cd (oldPwd)
     elseif ~any(cl_s=='1234'), error('TT Cluster header must start with C1..4')
     end
   end
-  function get_ebsp(fname)
+  function res = get_ebsp(range)
+    res = [];
+    fname = find_file();
+    if isempty(fname), return, end
     dobj=dataobj(fname);
-    if isempty(th_s), indexDir2 = [ULF_PATH 'Cluster/C' cl_s '/PC35/CDF'];
-    else indexDir2 = [ULF_PATH 'THEMIS/TH' th_s '/PC35/CDF'];
-    end
-    cd(indexDir2);
-    iSep=strfind(fname,'__');
-    productName = fname(1:iSep-1);
-    PCsep=strfind(fname,'PC12');
-    fnamePC35=[fname(1:PCsep-1) 'PC35' fname(PCsep+4:end)];
-    dobjPC35=dataobj(fnamePC35);
-    productNamePC35 = fnamePC35(1:iSep-1);
     
-    ebspPC12 = struct('t',[],'f',[],'flagFac',1,...
+    res = struct('t',[],'f',[],'flagFac',1,...
       'bb_xxyyzzss',[],'ee_xxyyzzss',[],'ee_ss',[],...
       'pf_xyz',[],'pf_rtp',[],...
       'dop',[],'dop2d',[],'planarity',[],'ellipticity',[],'k_tp',[],...
@@ -163,26 +162,18 @@ cd (oldPwd)
       {'B0','BMAG'}
       };
     
+    iSep=strfind(fname,'__');
+    productName = fname(1:iSep-1);
+
     for iField = 1:length(fields)
       fieldName = fields{iField}{1}; varName = fields{iField}{2};
       tmpVar = get_variable(dobj,[varName '__' productName]);
-      tmpVarPC35 = get_variable(dobjPC35,[varName '__' productNamePC35]);
       if isempty(tmpVar)
         % Specially treat GB data which are not in FAC system
         if strcmp(fieldName,'bb_xxyyzzss')
           tmpVar = get_variable(dobj,['BB_xxyyzz__' productName]);
           if isempty(tmpVar), continue
-          else ebspPC12.flagFac = 0;
-          end
-        else continue
-        end
-      end
-      if isempty(tmpVarPC35)
-        % Specially treat GB data which are not in FAC system
-        if strcmp(fieldName,'bb_xxyyzzss')
-          tmpVarPC35 = get_variable(dobjPC35,['BB_xxyyzz__' productName]);
-          if isempty(tmpVarPC35), continue
-          else ebspPC35.flagFac = 0;
+          else res.flagFac = 0;
           end
         else continue
         end
@@ -190,31 +181,46 @@ cd (oldPwd)
       if isnumeric(tmpVar.FILLVAL)
         tmpVar.data(tmpVar.data == tmpVar.FILLVAL) = NaN;
       end
-      if isnumeric(tmpVarPC35.FILLVAL)
-        tmpVarPC35.data(tmpVarPC35.data == tmpVarPC35.FILLVAL) = NaN;
-      end
-      ebspPC12.(fieldName) = struct('data',tmpVar.data,'units',tmpVar.UNITS);
-      ebspPC35.(fieldName) = struct('data',tmpVarPC35.data,'units',tmpVarPC35.UNITS);
+      res.(fieldName) = struct('data',tmpVar.data,'units',tmpVar.UNITS);
       if strcmp(fieldName,'bb_xxyyzzss')
-        if numel(size(ebspPC12.(fieldName).data))==2 && all(all(isnan(ebspPC12.(fieldName).data)))
+        if numel(size(res.(fieldName).data))==2 && all(all(isnan(res.(fieldName).data)))
           % For no data Qtran produces one record with fill values
           irf.log('warning',sprintf('No B data for %s',fname))
           return
         else
-          ebspPC12.(fieldName).data(:,:,4) = sum(ebspPC12.(fieldName).data(:,:,1:3),3);
-        end
-      end
-      if strcmp(fieldName,'bb_xxyyzzss')
-        if numel(size(ebspPC35.(fieldName).data))==2 && all(all(isnan(ebspPC35.(fieldName).data)))
-          % For no data Qtran produces one record with fill values
-          irf.log('warning',sprintf('No B data for %s',fname))
-          return
-        else
-          ebspPC35.(fieldName).data(:,:,4) = sum(ebspPC35.(fieldName).data(:,:,1:3),3);
+          res.(fieldName).data(:,:,4) = sum(res.(fieldName).data(:,:,1:3),3);
         end
       end
     end
-  end
+    function fn = find_file()
+      fn = '';
+      if isempty(th_s) % Cluster
+        indexDir = [ULF_PATH 'Cluster/C' cl_s '/' range '/CDF'];
+      else % THEMIS
+        indexDir = [ULF_PATH 'THEMIS/TH' th_s '/' range '/CDF'];
+      end
+      tStartStr = irf_fname(tintDl(1),3);
+      tStopStr = irf_fname(tintDl(end),3);
+      cd(indexDir);
+      d = dir(['C' cl_s '*_' tStartStr '_*.cdf']);
+      if ~strcmpi(tStopStr,tStartStr)
+        d1 = dir(['C' cl_s '*_' tStopStr '_*.cdf']);
+        d = [d; d1];
+      end
+      m=0; i=1;
+      while i <= length(d) && m == 0,
+        if isempty(th_s), tend = d(i).name(45:59);
+        else tend = d(i).name(49:63);
+        end
+        tenddate = datenum(tend,'yyyymmdd_HHMMSS');
+        if tenddate > epoch2date(tintDl(1)), m=i; end
+        i=i+1;
+      end
+      if m, fn = d(m).name; 
+      else irf.log('warning', ['no files found for ' range]);
+      end
+    end
+  end % GET_EBSP
 end
 
 function out = AverageData(data,x,y,avWindow,flagSerial)
