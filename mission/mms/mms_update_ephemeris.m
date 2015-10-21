ed98e35439aaef31b34f0cac2e7c24735ecd99a3
thomas-nilsson-irfu <thomas-nilsson-irfu@users.noreply.github.com>
2015-10-21 16:40:32 +0200

    MMS ephemiris corrections use new USNO-AA method for angle, do NOT use resample function as this introduces discrepencies (much larger than the difference from old Hapgood ref. vs new USNO-AA). Also keep TT2000 times (send input as TSeries object to coord.transform). Perhaps we should simply return TSeries object instead, but for now return same output as before.

--- mission/mms/mms_update_ephemeris.m
+++ mission/mms/mms_update_ephemeris.m
@@ -1,10 +1,10 @@
 function [R, V, RGSM, VGSM] = mms_update_ephemeris
-%MMS_UPDATE_EPHEMERIS  read DEFEPH files and compute R/V in GSE & GSM
+% MMS_UPDATE_EPHEMERIS  read DEFEPH files and compute R/V in GSE & GSM
 %
 % [R, V, RGSM, VGSM] = mms_update_ephemeris
 %
 % Read DEFEPH files from the start of the mission and up to current time
-% and compute spacecrfat position (R) and velocity (V) in GSE and GSM
+% and compute spacecraft position (R) and velocity (V) in GSE and GSM
 %
 % To save the data use:
 % save /data/mms/irfu/mmsR.mat R
@@ -12,41 +12,36 @@ function [R, V, RGSM, VGSM] = mms_update_ephemeris
 % save /data/mms/irfu/mmsRGSM.mat RGSM
 % save /data/mms/irfu/mmsVGSM.mat VGSM
 
-[~,nowUtc] = unix('TZ=UTC date "+%Y-%m-%dT%H:%M:%SZ"');
-nowUtc(end)=[]; % clip the cr in the end
-
+nowUtc = datestr(now,'yyyy-mm-ddTHH:MM:SSZ'); 
 tint = irf.tint(['2015-03-13T00:00:00Z/' nowUtc]);
-epoch0 = tint.start.epoch;
-newTime = (epoch0:int64(60*1e9):tint.stop.epoch)';
-R.time = newTime; V.time = newTime;
-RGSM.time = newTime; VGSM.time = newTime;
 
 for mmsId = 1:4
   mmsIdS = num2str(mmsId);
-  vPref = sprintf('mms%d_ancillary_defeph',mmsId);
-  
+  vPref = ['mms',mmsIdS,'_ancillary_defeph'];
   vars = 'rv';
   for i=1:length(vars)
     v = vars(i);
-    Gei=mms.db_get_variable(vPref,v,tint);
-    GeiR = irf_resamp([double(Gei.time-epoch0)*1e-9 Gei.(v)],...
-      double(newTime-epoch0)*1e-9);
-    
-    tTmp = GeiR(:,1)+EpochTT(epoch0).epochUnix;
-    gse = irf.geocentric_coordinate_transformation(...
-      [tTmp GeiR(:,2:4)],'gei>gse');
-    
-    gsm = irf.geocentric_coordinate_transformation(...
-      [tTmp GeiR(:,2:4)],'gei>gsm');
-    
+    Gei = mms.db_get_variable(vPref, v, tint);    
+    Gei_ts = TSeries(EpochTT(Gei.time), Gei.data, 'vec_xyz');
+    gse_ts = irf.geocentric_coordinate_transformation(Gei_ts,'gei>gse',false);
+    gsm_ts = irf.geocentric_coordinate_transformation(Gei_ts,'gei>gsm',false);
     vC = upper(v);
    switch v
      case 'r'
-       R.(['gse' vC mmsIdS]) = gse(:,2:4); 
-       RGSM.(['gsm' vC mmsIdS]) = gsm(:,2:4);
+       R.(['gse' vC mmsIdS]) = gse_ts.data;
+       RGSM.(['gsm' vC mmsIdS]) = gsm_ts.data;
+       if(mmsId==1)
+         R.time = gse_ts.time.ttns;
+         RGSM.time = gsm_ts.time.ttns;
+       end
      case 'v'
-       V.(['gse' vC mmsIdS]) = gse(:,2:4); 
-       VGSM.(['gsm' vC mmsIdS]) = gsm(:,2:4);
+       V.(['gse' vC mmsIdS]) = gse_ts.data; 
+       VGSM.(['gsm' vC mmsIdS]) = gsm_ts.data;
+       if(mmsId==1)
+         V.time = gse_ts.time.ttns;
+         VGSM.time = gsm_ts.time.ttns;
+       end
    end
   end
+end
 end
