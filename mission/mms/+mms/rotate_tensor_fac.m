69c0c199a4a630ac178a7cd04dbea333f5a77f3b
Daniel Graham <dgraham@irfu.se>
2015-12-12 20:12:28 +0100

    function now accepts a single input for tensor as input. No change to calculations

--- mission/mms/+mms/rotate_tensor_fac.m
+++ mission/mms/+mms/rotate_tensor_fac.m
@@ -1,6 +1,7 @@
-function [PeXXp,PeXYp,PeXZp,PeYYp,PeYZp,PeZZp] = rotate_tensor_fac(PeXX,PeXY,PeXZ,PeYY,PeYZ,PeZZ,Bback)
+function [PeXXp,PeXYp,PeXZp,PeYYp,PeYZp,PeZZp] = rotate_tensor_fac(varargin)
 %
 % [PeXXp,PeXYp,PeXZp,PeYYp,PeYZp,PeZZp] = mms.rotate_tensor_fac(PeXX,PeXY,PeXZ,PeYY,PeYZ,PeZZ,Bback)
+% [PeXXp,PeXYp,PeXZp,PeYYp,PeYZp,PeZZp] = mms.rotate_tensor_fac(Peall,Bback)
 %
 % Function to coordinate transform the pressure tensor term into
 % field-aligned coordinates. PeZZp is aligned with background magnetic
@@ -11,24 +12,46 @@ function [PeXXp,PeXYp,PeXZp,PeYYp,PeYZp,PeZZp] = rotate_tensor_fac(PeXX,PeXY,PeX
 % Input: (All data must be in TSeries format)
 %       PeXX,PeXY,PeXZ,PeYY,PeYZ,PeZZ - Pressure terms or temperature   
 %       Bback - Background magnetic field
+%       Peall - TSERIES of all tensor terms with column order PeXX,PeXY,PeXZ,PeYY,PeYZ,PeZZ
 % Output: 
 %       PeXXp,PeXYp,PeXZp,PeYYp,PeYZp,PeZZp - Pressure or temperature terms in
 %       field-aligned coordinates
 % 
 
-Ptensor = zeros(length(PeXX.time),3,3);
-Ptensor(:,1,1) = PeXX.data;
-Ptensor(:,2,1) = PeXY.data;
-Ptensor(:,3,1) = PeXZ.data;
-Ptensor(:,1,2) = PeXY.data;
-Ptensor(:,2,2) = PeYY.data;
-Ptensor(:,3,2) = PeYZ.data;
-Ptensor(:,1,3) = PeXZ.data;
-Ptensor(:,2,3) = PeYZ.data;
-Ptensor(:,3,3) = PeZZ.data;
+if nargin == 2;
+    Peall = varargin{1};
+    times = Peall.time;
+    Bback = varargin{2};
+    Ptensor = zeros(length(Peall.time),3,3);
+    Ptensor(:,1,1) = Peall.data(:,1);
+    Ptensor(:,2,1) = Peall.data(:,2);
+    Ptensor(:,3,1) = Peall.data(:,3);
+    Ptensor(:,1,2) = Peall.data(:,2);
+    Ptensor(:,2,2) = Peall.data(:,4);
+    Ptensor(:,3,2) = Peall.data(:,5);
+    Ptensor(:,1,3) = Peall.data(:,3);
+    Ptensor(:,2,3) = Peall.data(:,5);
+    Ptensor(:,3,3) = Peall.data(:,6);
+elseif nargin == 7;
+    times = varargin{1}.time;
+    Ptensor = zeros(length(varargin{1}.time),3,3);
+    Ptensor(:,1,1) = varargin{1}.data;
+    Ptensor(:,2,1) = varargin{2}.data;
+    Ptensor(:,3,1) = varargin{3}.data;
+    Ptensor(:,1,2) = varargin{2}.data;
+    Ptensor(:,2,2) = varargin{4}.data;
+    Ptensor(:,3,2) = varargin{5}.data;
+    Ptensor(:,1,3) = varargin{3}.data;
+    Ptensor(:,2,3) = varargin{5}.data;
+    Ptensor(:,3,3) = varargin{6}.data;
+    Bback = varargin{7};
+else
+    help mms.rotate_tensor_fac;
+    return;
+end
 
 % Make transformation matrix
-Bback = Bback.resample(PeXX);
+Bback = Bback.resample(times);
 Bmag = Bback.abs.data;
 Rz = Bback.data./([Bmag Bmag Bmag]);
 Rx = [1 0 0];
@@ -38,23 +61,25 @@ Ry = Ry./[Rmag Rmag Rmag];
 Rx = irf_cross(Ry, Rz);
 Rmag = irf_abs(Rx,1);
 Rx = Rx./[Rmag Rmag Rmag];
-Rotmat = zeros(length(PeXX.time),3,3);
+Rotmat = zeros(length(times),3,3);
 Rotmat(:,1,:) = Rx;
 Rotmat(:,2,:) = Ry;
 Rotmat(:,3,:) = Rz;
 
 
-Ptensorp = zeros(length(PeXX.time),3,3);
-for ii = 1:length(PeXX.time);
+Ptensorp = zeros(length(times),3,3);
+for ii = 1:length(times);
     rottemp = squeeze(Rotmat(ii,:,:));
     Ptensorp(ii,:,:) = rottemp*(squeeze(Ptensor(ii,:,:))*transpose(rottemp));
 end
 
-PeXXp = TSeries(PeXX.time,Ptensorp(:,1,1));
-PeXYp = TSeries(PeXX.time,Ptensorp(:,1,2));
-PeXZp = TSeries(PeXX.time,Ptensorp(:,1,3));
-PeYYp = TSeries(PeXX.time,Ptensorp(:,2,2));
-PeYZp = TSeries(PeXX.time,Ptensorp(:,2,3));
-PeZZp = TSeries(PeXX.time,Ptensorp(:,3,3));
+PeXXp = irf.ts_scalar(times,Ptensorp(:,1,1));
+PeXYp = irf.ts_scalar(times,Ptensorp(:,1,2));
+PeXZp = irf.ts_scalar(times,Ptensorp(:,1,3));
+PeYYp = irf.ts_scalar(times,Ptensorp(:,2,2));
+PeYZp = irf.ts_scalar(times,Ptensorp(:,2,3));
+PeZZp = irf.ts_scalar(times,Ptensorp(:,3,3));
+
+end
 
 
