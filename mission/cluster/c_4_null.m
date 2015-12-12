1b091d98cf38035c555f2dc360dc852ead7a624d
Elin Eriksson <eline@elins-mac.lan>
2015-12-12 14:04:29 +0100

    Changed input variables
    Changed the way variables are called and added a third option of
    looking for nulls only corresponding with strong current in comparison
    to a set value.

--- mission/cluster/c_4_null.m
+++ mission/cluster/c_4_null.m
@@ -6,34 +6,43 @@ function Nulls=c_4_null(R1,R2,R3,R4,B1,B2,B3,B4,varargin)
 %   magnetic null type, eigenvalues, eigenvectors .., are calculated.
 %		The routine assumes that B-field is varying linearly.
 %
-%   Nulls=C_4_NULL(R1,R2,R3,R4,B1,B2,B3,B4,[threshold],[boxLim])
+%   Nulls=C_4_NULL(R1,R2,R3,R4,B1,B2,B3,B4,arguments)
 %
 %
 %   INPUT
 %     B1..B4    - are B field times series measured by satellites C1..C4.
 %                 columns are [t Bx By Bz]
 %     R1..R4    - are position of C1..C4, columns are [t Rx Ry Rz]
-%     threshold - returned are only nulls satisfying
-%                 |divB/max(all eigenvalues of gradB)| < threshold
-%                 Default threshold value is 40%, threshold is given in percent units.
-%                 If threshold is 100% all points are accepted.
-%     boxLim    - only nulls from box size less than boxLim are returned.
+%     all arguments allowed by C_4_NULL command that can be mixed freely as
+%     long as it follows this format for each argument. See Examples below.
+%
+%     ['strong',currentLim] - returns nulls corresponding with a current
+%     larger than currentLim. Default option is all located nulls are returned.
+%                 Default threshold value is 40%, threshold is given in
+%                 units of percentage.
+%     ['threshold',thresholdValue] - returned are only nulls satisfying
+%                 |divB/max(all eigenvalues of gradB)| < thresholdValue.
+%                 thresholdValue is given in units of percentage.If
+%                 threshold is 100% all points are accepted. Default value
+%                 is 40%.
+%     ['boxLim',boxLimValue]    - only nulls from box size less than boxLim are returned.
 %                 Default is to have no limits on box size.
 %
+%
 %   OUTPUT
-%   Nulls.(fields) - Structure having different fields characterizing the
-%                      found magnetic nulls.
 %
 %   Nulls.t
 %   Nulls.R    [xn yn zn] - the position of the null.
 %   Nulls.dR   [dxn dyn dzn] - the distance from the
 %      spacecraft tetrahedron centrum to the magnetic null.
-%   Nulls.j =  [jx jy jz] - the total current for each null
-%   Nulls.PoincareIndex = [t PI] - the Poincar? index for each time tag.
+%   Nulls.j =  [jx jy jz] - the total current for each null.
+%         j = curlB/mu0*1E-6 [A/m^2 if B in nT and R in km]
+%
+%   Nulls.PoincareIndex = [t PI] - the Poincare index for each time tag.
 %   Nulls.error = [err] - for each found null returns
 %       err = |divB/max(all eigenvalues of gradB)|
 %
-%   Nulls.gradB  gradB for each time tag in the format given by c_4_grad function.
+%   Nulls.gradB  gradB for each time tag in the format given by c_4_grad function
 %
 %   Nulls.eigenVectors = [eigVect1 eigVect2 eigVect3] contains the
 %   eigenvectors for each null, where eigVector1 = [x y z]
@@ -43,63 +52,153 @@ function Nulls=c_4_null(R1,R2,R3,R4,B1,B2,B3,B4,varargin)
 %   Nulls.eigenvectorCorrespondingToSpine   the number of eigenvector,
 %               values can 1..3
 %
-%   Nulls.Is.A  
-%   Nulls.Is.As 
-%   Nulls.Is.B  
+%   Nulls.Is.A
+%   Nulls.Is.As
+%   Nulls.Is.B
 %   Nulls.Is.Bs
-%   Nulls.Is.O 
-%   Nulls.Is.X 
+%   Nulls.Is.O
+%   Nulls.Is.X
 %   Nulls.Is.unknown
 %       logical indexes for each type of null. 1(true) is when a null of
-%       that type has been found at that time tag. 
+%       that type has been found at that time tag.
 %
 %   Nulls.dB_AB the minimum magnetic field fluctuation that changes the
 %   kind of the null from A/As to B/Bs or vice versa.
-%   of a null.
+%   of a null. [nT]
 %
-
+% Examples:
+%   c_4_null(R1,R2,R3,R4,B1,B2,B3,B4) - Tries to locate nulls and returns
+%   the output for all nulls found. thresholdValue is set to 40%,
+%   boxLimValue is no box.
+%
+%   c_4_null(R1,R2,R3,R4,B1,B2,B3,B4,'threshold',thresholdValue) - Tries to
+%   locate nulls and returns the output for all nulls found where the
+%   threshold is set to thresholdValue. boxLimValue is no box.
+%
+%   c_4_null(R1,R2,R3,R4,B1,B2,B3,B4,'boxLim',boxLimValue) - Tries to
+%   locate nulls and returns the output for all nulls found where the
+%   threshold is set to default. boxLim is set to boxLimValue.
+%
+%   c_4_null(R1,R2,R3,R4,B1,B2,B3,B4,'strong',currentLim) - Tries to
+%   locate nulls with default thresholdValue, no box limit and returns the
+%   output for all null corresponding with currents larger than currentLim.
+%
+%   c_4_null(R1,R2,R3,R4,B1,B2,B3,B4,'strong',currentLim,'threshold',thresholdValue) - Tries to
+%   locate nulls with a set thresholdValue, no box limit and returns the
+%   output for all null corresponding with currents larger than currentLim.
+%
+%   c_4_null(R1,R2,R3,R4,B1,B2,B3,B4,'threshold',thresholdValue,'boxLim',boxLimValue) - Tries to
+%   locate nulls with a set thresholdValue and a set box limit and returns the
+%   output for all null found.
+%
+%   c_4_null(R1,R2,R3,R4,B1,B2,B3,B4,'boxLim',boxLimValue,'strong',currentLim) - Tries to
+%   locate nulls with a set thresholdValue and a set box limit and returns the
+%   output for all null found.
 %% Check TSeries
 idR = {R1,R2,R3,R4};
 idB = {B1,B2,B3,B4};
 
 if isa(B1,'TSeries') || isa(R1,'TSeries')
-for i=1:4
-if isa(idR{i},'TSeries')
-    idR{i} = [idR{i}.time.epochUnix double(idR{i}.data)];
-   
-end
-if isa(idB{i},'TSeries')
-    idB{i} =  [idB{i}.time.epochUnix double(idB{i}.data)];
-end
-end
+    for i=1:4
+        if isa(idR{i},'TSeries')
+            idR{i} = [idR{i}.time.epochUnix double(idR{i}.data)];
+            
+        end
+        if isa(idB{i},'TSeries')
+            idB{i} =  [idB{i}.time.epochUnix double(idB{i}.data)];
+        end
+    end
 end
 
 %% Check variables
 
 nCol=size(idB{1,1},2);
 if nCol<4
-	error('You need the time vector for each input [t x y z]')
+    error('You need the time vector for each input [t x y z] in either Tseries or in vector format')
 end
 if nargin == 0
-	help c_4_null;
-	return;
+    help c_4_null;
+    return;
 elseif nargin < 8
-	error('Too few input values. See usage: help c_4_null')
-elseif nargin > 10
-	error('Too many input values. See usage: help c_4_null')
+    error('Too few input values. See usage: help c_4_null')
+elseif nargin > 14
+    error('Too many input values. See usage: help c_4_null')
+end
+if isempty(varargin)
+    % Gives Default values
+    toSaveSpecial=false;
+    threshold = 40;
+    noBox=true;
+    %Check if first variable is one of the argument strings otherwise give
+    %error.
+elseif ~strcmp(varargin{1},'strong') && ~strcmp(varargin{1},'threshold') && ~strcmp(varargin{1},'boxLim')
+    error('Unapproved arguments given. See usage: help c_4_null')
+else
+    % Gives Default values that can be changed
+    toSaveSpecial=false;
+    threshold = 40;
+    noBox=true;
+ % Check values given in varargin 
+ % All arguments need to include a string+value so if the amount of vargin
+ % isn't even then give error
+    if length(varargin)==1
+        error('Unapproved combination of arguments. See usage: help c_4_null')
+        
+    elseif length(varargin)==3
+        error('Unapproved combination of arguments. See usage: help c_4_null')
+    elseif length(varargin)==5
+        error('Unapproved combination of arguments. See usage: help c_4_null')
+    else
+ % Time to check the combination of elements and change the default values for the given ones.       
+        for i=1:length(varargin)
+            if i+1>length(varargin)
+                break;
+            else
+                %Case 'strong'
+                if strcmp(varargin(i),'strong')
+                    if isnumeric(cell2mat(varargin(i+1)))
+                        currentLim=cell2mat(varargin(i+1));
+                        toSaveSpecial=true;
+                    else
+                        error('Unapproved combination of arguments. See usage: help c_4_null')
+                    end
+                    %Case 'threshold'
+                elseif strcmp(varargin(i),'threshold')
+                    
+                    if isnumeric(cell2mat(varargin(i+1)))
+                        threshold=cell2mat(varargin(i+1));
+                    else
+                        error('Unapproved combination of arguments. See usage: help c_4_null')
+                    end
+                    %Case 'boxLim'
+                elseif strcmp(varargin(i),'boxLim')
+                    
+                    if isnumeric(cell2mat(varargin(i+1)))
+                        noBox=false;
+                        boxLim=cell2mat(varargin(i+1));
+                    else
+                        error('Unapproved combination of arguments. See usage: help c_4_null')
+                    end
+                    % If two strings are after each other give error
+                elseif iscellstr(varargin(i)) && iscellstr(varargin(i+1))
+                    error('Unapproved combination of arguments. See usage: help c_4_null')
+                    % If to numeric values are after each other give error
+                elseif isnumeric(cell2mat(varargin(i)))
+                    if isnumeric(cell2mat(varargin(i+1)))
+                        error('Unapproved combination of arguments. See usage: help c_4_null')
+                    else
+                        continue;
+                    end
+                    % If strings are other than 'threshold', boxLim', and
+                    % 'strong' give error.
+                else
+                    error('Unapproved arguments. See usage: help c_4_null')
+                end
+            end
+        end
+    end
 end
 
-if isempty(varargin) == true
-	threshold = 40;
-    noBox=true;
-elseif length(varargin)<2
-	threshold = varargin{1};
-    noBox=true;
-else
-    threshold = varargin{1};
-    boxLim=varargin{2};
-    noBox=false;
-end
 
 %% Resample all input vectors to B1 timeline and remove time column
 t = idB{1,1}(:,1);
@@ -119,21 +218,21 @@ gradB = c_4_grad(idR{1,1},idR{1,2},idR{1,3},idR{1,4},idB{1,1},idB{1,2},idB{1,3},
 % remove points with NaN magnetic field data
 badPoints = any(isnan(gradB(:,2:end)),2);
 if any(badPoints)
-	t(badPoints)       = [];
-	nPoints            = numel(t);
-	gradB(badPoints,:) = [];
-	idR{1,1}(badPoints,:)=[];
-	idR{1,2}(badPoints,:)=[];
-	idR{1,3}(badPoints,:)=[];
-	idR{1,4}(badPoints,:)=[];
-	idB{1,1}(badPoints,:)=[];
-	idB{1,2}(badPoints,:)=[];
-	idB{1,3}(badPoints,:)=[];
-	idB{1,4}(badPoints,:)=[];
+    t(badPoints)       = [];
+    nPoints            = numel(t);
+    gradB(badPoints,:) = [];
+    idR{1,1}(badPoints,:)=[];
+    idR{1,2}(badPoints,:)=[];
+    idR{1,3}(badPoints,:)=[];
+    idR{1,4}(badPoints,:)=[];
+    idB{1,1}(badPoints,:)=[];
+    idB{1,2}(badPoints,:)=[];
+    idB{1,3}(badPoints,:)=[];
+    idB{1,4}(badPoints,:)=[];
 end
 divB  = c_4_grad(idR{1,1},idR{1,2},idR{1,3},idR{1,4},idB{1,1},idB{1,2},idB{1,3},idB{1,4},'div');
 curlB = c_4_grad(idR{1,1},idR{1,2},idR{1,3},idR{1,4},idB{1,1},idB{1,2},idB{1,3},idB{1,4},'curl');
-j     = curlB./1.0e3.*1e-9./(4*pi*1e-7);
+j     = curlB./1.0e3.*1e-9./(4*pi*1e-7); %A/m^2 if B in nT and R in km
 
 %% Calculate the null position
 dR  = t*[0 0 0];
@@ -150,20 +249,20 @@ Bmean=0.25.*(idB{1,1}+idB{1,2}+idB{1,3}+idB{1,4});
 % 1) calculate the distance on null from the center of the four satellites
 
 for i=1:nPoints
-	deltaBnull = reshape(gradB(i,:),3,3)';
-	dR(i,:) = Bmean(i,:)/deltaBnull;
+    deltaBnull = reshape(gradB(i,:),3,3)';
+    dR(i,:) = Bmean(i,:)/deltaBnull;
 end
 
 R = Rmean - dR;
 
 
-% 2) Determinant of gradB 
+% 2) Determinant of gradB
 detB = abs(B41v(:,1).*B42v(:,2).*B43v(:,3)...
-	   + B41v(:,2).*B42v(:,3).*B43v(:,1)...
-	   + B41v(:,3).*B42v(:,1).*B43v(:,2)...
-	   - B41v(:,3).*B42v(:,2).*B43v(:,1)...
-	   - B41v(:,2).*B42v(:,1).*B43v(:,3)...
-	   - B41v(:,1).*B42v(:,3).*B43v(:,2));
+    + B41v(:,2).*B42v(:,3).*B43v(:,1)...
+    + B41v(:,3).*B42v(:,1).*B43v(:,2)...
+    - B41v(:,3).*B42v(:,2).*B43v(:,1)...
+    - B41v(:,2).*B42v(:,1).*B43v(:,3)...
+    - B41v(:,1).*B42v(:,3).*B43v(:,2));
 
 % 3) estimate deltaB_AB
 l1vec=cross(B42v,B43v,2);
@@ -189,22 +288,22 @@ minZ = min(([idR{1,1}(:,3) idR{1,2}(:,3) idR{1,3}(:,3) idR{1,4}(:,3)]),[],2);
 maxZ = max(([idR{1,1}(:,3) idR{1,2}(:,3) idR{1,3}(:,3) idR{1,4}(:,3)]),[],2);
 
 %For each eigenvalue corresponding to the tolerance level (the two errors
-%less or equal to 40%) break out their corresponding time and dR value 
+%less or equal to 40%) break out their corresponding time and dR value
 %(the minimum distance from all s/c to the null)
 disp('Sorting based on the null located within the s/c box made up of the maximum and minimum values for each direction of all satellites');
 if noBox
-sortNullDx = R(:,1) >= minX & R(:,1) <= maxX;
-sortNullDy = R(:,2) >= minY & R(:,2) <= maxY;
-sortNullDz = R(:,3) >= minZ & R(:,3) <= maxZ;
-okNulls    = sortNullDx & sortNullDy & sortNullDz;   
+    sortNullDx = R(:,1) >= minX & R(:,1) <= maxX;
+    sortNullDy = R(:,2) >= minY & R(:,2) <= maxY;
+    sortNullDz = R(:,3) >= minZ & R(:,3) <= maxZ;
+    okNulls    = sortNullDx & sortNullDy & sortNullDz;
 else
-sortsizedX = (maxX-minX) <= boxLim;
-sortsizedY = (maxY-minY) <= boxLim;
-sortsizedZ = (maxZ-minZ) <= boxLim;
-sortNullDx = R(:,1) >= minX & R(:,1) <= maxX;
-sortNullDy = R(:,2) >= minY & R(:,2) <= maxY;
-sortNullDz = R(:,3) >= minZ & R(:,3) <= maxZ;
-okNulls    = sortsizedX & sortsizedY & sortsizedZ & sortNullDx & sortNullDy & sortNullDz;
+    sortsizedX = (maxX-minX) <= boxLim;
+    sortsizedY = (maxY-minY) <= boxLim;
+    sortsizedZ = (maxZ-minZ) <= boxLim;
+    sortNullDx = R(:,1) >= minX & R(:,1) <= maxX;
+    sortNullDy = R(:,2) >= minY & R(:,2) <= maxY;
+    sortNullDz = R(:,3) >= minZ & R(:,3) <= maxZ;
+    okNulls    = sortsizedX & sortsizedY & sortsizedZ & sortNullDx & sortNullDy & sortNullDz;
 end
 
 %Removes nulls not within spacecraft box
@@ -227,10 +326,10 @@ eigVectors  = zeros(nPoints,9);
 eigValErr   = t*NaN;
 
 for i=1:nPoints
-	deltaB_null      = reshape(gradB(i,:),3,3);
-	[V,D]            = eig(deltaB_null,'vector');
-	eigVectors(i,:)  = V(:)';
-	eigValues(i,:)   = D';
+    deltaB_null      = reshape(gradB(i,:),3,3);
+    [V,D]            = eig(deltaB_null,'vector');
+    eigVectors(i,:)  = V(:)';
+    eigValues(i,:)   = D';
 end
 
 %% Identify points where error larger than threshold
@@ -238,7 +337,7 @@ eigValErr(:,2)          = abs(divB./max(abs(eigValues),[],2)) * 100;
 badPoints=false(length(eigValErr(:,1)),1);
 if threshold == 100
 else
-badPoints               = (eigValErr(:,2) > threshold);
+    badPoints               = (eigValErr(:,2) > threshold);
 end
 t(badPoints)       = [];
 gradB(badPoints,:) = [];
@@ -317,3 +416,49 @@ Nulls.j=j;
 Nulls.R  = R;
 Nulls.dR = dR;
 Nulls.t  = t;
+if sum(Nulls.t)==0
+    disp('No Nulls found')
+else
+    disp('Nulls found')
+    if toSaveSpecial
+        strongcurrent=(irf_abs(Nulls.j,1)>currentLim);
+        if sum(strongcurrent)==0
+            disp('No Nulls with strong currents found')
+        else
+            Nulls.eigenValues=Nulls.eigenValues(strongcurrent,:);
+            Nulls.eigenVectors=Nulls.eigenVectors(strongcurrent,:);
+            Nulls.eigenvectorCorrespondingToSpine=Nulls.eigenvectorCorrespondingToSpine(strongcurrent,:);
+            Nulls.error=Nulls.error(strongcurrent,:);
+            Nulls.poincareindex=Nulls.poincareindex(strongcurrent,:);
+            Nulls.dB_AB=Nulls.dB_AB(strongcurrent,:);
+            Nulls.Is.A=Nulls.Is.A(strongcurrent,:);
+            Nulls.Is.As=Nulls.Is.As(strongcurrent,:);
+            Nulls.Is.B=Nulls.Is.B(strongcurrent,:);
+            Nulls.Is.Bs=Nulls.Is.Bs(strongcurrent,:);
+            Nulls.Is.X=Nulls.Is.X(strongcurrent,:);
+            Nulls.Is.O=Nulls.Is.O(strongcurrent,:);
+            Nulls.Is.unknown=Nulls.Is.unknown(strongcurrent,:);
+            Nulls.gradB=Nulls.gradB(strongcurrent,:);
+            Nulls.j=Nulls.j(strongcurrent,:);
+            Nulls.R=Nulls.R(strongcurrent,:);
+            Nulls.dR=Nulls.dR(strongcurrent,:);
+            Nulls.t=Nulls.t(strongcurrent,:);
+        end
+    end
+end
+end
+
+    
+                   
+        
+            
+              
+              
+              
+          
+              
+              
+              
+              
+              
+          
