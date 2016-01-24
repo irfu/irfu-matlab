10c1f506cb8600379d61612e40622e056d045552
Daniel Graham <dgraham@irfu.se>
2016-01-24 01:28:56 +0100

    more general tensor rotation function. Can replace mms.rotate_tensor_fac when finished.

---
+++ mission/mms/+mms/rotate_tensor.m
@@ -0,0 +1,153 @@
+function [PeXXp,PeXYp,PeXZp,PeYYp,PeYZp,PeZZp] = rotate_tensor(varargin)
+% MMS.ROTATE_TENSOR compute moments from the FPI particle phase-space densities 
+%
+% Examples:
+% Rotate tensor into field-aligned coordinates
+% [PeXXp,PeXYp,PeXZp,PeYYp,PeYZp,PeZZp] = mms.rotate_tensor(PeXX,PeXY,PeXZ,PeYY,PeYZ,PeZZ,'fac',Bback)
+% [PeXXp,PeXYp,PeXZp,PeYYp,PeYZp,PeZZp] = mms.rotate_tensor(Peall,'fac',Bback)
+% 
+% Rotate tensor into user-defined coordinate system
+% [PeXXp,PeXYp,PeXZp,PeYYp,PeYZp,PeZZp] = mms.rotate_tensor(Peall,'rot',xnew,[ynew,znew])
+%
+% Rotate tensor into
+% [PeXXp,PeXYp,PeXZp,PeYYp,PeYZp,PeZZp] = mms.rotate_tensor(Peall,'gse')
+%
+%
+% Function to rotate the pressure tensor term into field-aligned coordinates or 
+% another coordinate system. 
+% For rotate into field-aligned coordinates PeZZp is aligned with background magnetic
+% field, PeXXp is closest to the x-direction and PeYYp completes the system.
+% 
+% Written by D. B. Graham.
+%
+% Input: (All data must be in TSeries format)
+%       PeXX,PeXY,PeXZ,PeYY,PeYZ,PeZZ - Pressure terms or temperature in TSeries 
+%       Peall - TSERIES of all tensor terms with column order PeXX,PeXY,PeXZ,PeYY,PeYZ,PeZZ
+%       'fac' - Transform tensor into field-aligned coordinates. PeZZp is
+%           field-aligned. PeXXp and PeYYp are closest orthogonal components to
+%           the x and y directions
+%           * Bback - Background magnetic field (TSERIES format)
+%       'rot' - Transform tensor into an arbitrary coordinate system
+%           * xnew - new x-direction (required after 'rot')
+%           * ynew, znew - new y and z directions (if not included y and 
+%               z directions are orthogonal to xnew and closest to the orginal 
+%               y and z directions).
+%       'gse' - Transform tensor into GSE coordinates (not yet
+%           implimented).
+% Output: 
+%       PeXXp,PeXYp,PeXZp,PeYYp,PeYZp,PeZZp - Pressure or temperature terms in
+%       field-aligned, user-defined, or GSE coordinates
+% 
+
+% Check input and load pressure/temperature terms
+if isa(varargin{2},'char'),
+    rotflag = varargin{2};
+    rotflagpos = 2;
+    Peall = varargin{1};
+    Petimes = Peall.time;
+    Ptensor = zeros(length(Petimes),3,3);
+    Ptensor(:,1,1) = Peall.data(:,1);
+    Ptensor(:,2,1) = Peall.data(:,2);
+    Ptensor(:,3,1) = Peall.data(:,3);
+    Ptensor(:,1,2) = Peall.data(:,2);
+    Ptensor(:,2,2) = Peall.data(:,4);
+    Ptensor(:,3,2) = Peall.data(:,5);
+    Ptensor(:,1,3) = Peall.data(:,3);
+    Ptensor(:,2,3) = Peall.data(:,5);
+    Ptensor(:,3,3) = Peall.data(:,6);
+elseif isa(varargin{7},'char'),
+    rotflag = varargin{7};
+    rotflagpos = 7;
+    Petimes = varargin{1}.time;
+    Ptensor = zeros(length(varargin{1}.time),3,3);
+    Ptensor(:,1,1) = varargin{1}.data;
+    Ptensor(:,2,1) = varargin{2}.data;
+    Ptensor(:,3,1) = varargin{3}.data;
+    Ptensor(:,1,2) = varargin{2}.data;
+    Ptensor(:,2,2) = varargin{4}.data;
+    Ptensor(:,3,2) = varargin{5}.data;
+    Ptensor(:,1,3) = varargin{3}.data;
+    Ptensor(:,2,3) = varargin{5}.data;
+    Ptensor(:,3,3) = varargin{6}.data;
+else
+    irf_log('proc','Something is wrong with the input.')
+    help mms.rotate_tensor;
+    return;
+end    
+
+Rotmat = zeros(length(Petimes),3,3);
+
+if (rotflag(1) == 'f'),
+    irf_log('proc','Transforming tensor into field-aligned coordinates.')
+    if (nargin == rotflagpos)
+        irf_log('proc','B TSeries is missing.')
+        return;
+    end
+    Bback = varargin{rotflagpos+1};
+    Bback = Bback.resample(Petimes);
+    Bmag = Bback.abs.data;
+    Rz = Bback.data./([Bmag Bmag Bmag]);
+    Rx = [1 0 0];
+    Ry = irf_cross(Rz,Rx);
+    Rmag = irf_abs(Ry,1);
+    Ry = Ry./[Rmag Rmag Rmag];
+    Rx = irf_cross(Ry, Rz);
+    Rmag = irf_abs(Rx,1);
+    Rx = Rx./[Rmag Rmag Rmag];
+    Rotmat(:,1,:) = Rx;
+    Rotmat(:,2,:) = Ry;
+    Rotmat(:,3,:) = Rz;
+elseif (rotflag(1) == 'r'),
+    irf_log('proc','Transforming tensor into user defined coordinate system.')
+    if (nargin == rotflagpos),
+        irf_log('proc','Vector(s) is(are) missing.')
+        return;
+    end
+    vectors = varargin(rotflagpos+1:end);
+    if (numel(vectors) == 1),
+        Rx = vectors{1};
+        if(length(Rx) ~= 3);
+            irf_log('proc','Vector format not recognized.')
+            return;
+        end
+        Rx = Rx/norm(Rx);
+        Ry = [0 1 0];
+        Rz = cross(Rx,Ry);
+        Rz = Rz/norm(Rz);
+        Ry = cross(Rz,Rx);
+        Ry = Ry/norm(Ry);
+    elseif (numel(vectors) == 3)
+        Rx = vectors{1};
+        Ry = vectors{2};
+        Rz = vectors{3};
+        Rx = Rx/norm(Rx);
+        Ry = Ry/norm(Ry);
+        Rz = Rz/norm(Rz);
+        % TO DO: add check that vectors are orthogonal
+    else
+        irf_log('proc','Vector format not recognized.')
+        return; 
+    end
+    Rotmat(:,1,:) = ones(length(Petimes),1)*Rx;
+    Rotmat(:,2,:) = ones(length(Petimes),1)*Ry;
+    Rotmat(:,3,:) = ones(length(Petimes),1)*Rz;    
+elseif (rotflag(1) == 'g'),
+    irf_log('proc','Transforming tensor into GSE coordinates.')
+    irf_log('proc','Not yet implemented.')
+    return;
+end
+
+Ptensorp = zeros(length(Petimes),3,3);
+for ii = 1:length(Petimes);
+    rottemp = squeeze(Rotmat(ii,:,:));
+    Ptensorp(ii,:,:) = rottemp*(squeeze(Ptensor(ii,:,:))*transpose(rottemp));
+end
+
+PeXXp = irf.ts_scalar(Petimes,Ptensorp(:,1,1));
+PeXYp = irf.ts_scalar(Petimes,Ptensorp(:,1,2));
+PeXZp = irf.ts_scalar(Petimes,Ptensorp(:,1,3));
+PeYYp = irf.ts_scalar(Petimes,Ptensorp(:,2,2));
+PeYZp = irf.ts_scalar(Petimes,Ptensorp(:,2,3));
+PeZZp = irf.ts_scalar(Petimes,Ptensorp(:,3,3));
+
+end
