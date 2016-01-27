ad4b6812d40dad66bbcbdf5a7d04bd08cd19777c
thomas-nilsson-irfu <thomas-nilsson-irfu@users.noreply.github.com>
2016-01-27 18:38:44 +0100

    MMS SQL _DO NOT_ automatically create an sql index file, expecially when running at SDC. (Then we use MMS_DB, in RAM only, to locate apropriate ASPOC and DEFATT). At SDC we use the eq. of "/data/mms/" but for the entire mission and all teams, we SHALL NOT put files in this path! When running locally at IRFU, we can create files in /data/mms.

--- mission/mms/mms_file_db.m
+++ mission/mms/mms_file_db.m
@@ -14,7 +14,14 @@ classdef (Abstract) mms_file_db
     function obj = mms_file_db(id)
       obj.id = id;
       obj.cache = mms_db_cache();
-      obj.index = mms_db_sql([id filesep 'index_sql']);
+      if(false)
+        % DO NOT CREATE a file "index_sql" when running on SDC in the
+        % data path for the entire mission (SDC eqivalent of "/data/mms" on
+        % Spis/Brain at IRFU).
+        obj.index = mms_db_sql([id filesep 'index_sql']); %#ok<UNRCH>
+      else
+        obj.index = [];
+      end
     end
     fileList = list_files(obj,filePrefix,tint)
     dataObj = load_file(obj,fileName)
