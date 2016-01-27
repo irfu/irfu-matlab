445f1da2798d16e50d47338e6dd6a178e24412fb
thomas-nilsson-irfu <thomas-nilsson-irfu@users.noreply.github.com>
2016-01-27 15:18:56 +0100

    Check arbitrary elements in version numbers, as long as both strings have the same number of elements. Or should we pad the end with zeros? (To compare 1.2.3.1 with 1.2.3)

--- irf/is_version_larger.m
+++ irf/is_version_larger.m
@@ -1,23 +1,30 @@
 function res = is_version_larger(newVersionString,oldVersionString)
 % res = is_version_larger(newVersionString,oldVersionString)
-% compares version in format X.Y.Z
+% compares version in format X.Y.Z (or an arbitrary number of elements as
+% long as both strings contain the same number of elements separated by a
+% single ".").
 %
-% res is true if newVersionString is larger than oldVersionString
+% res is true if newVersionString is larger than oldVersionString. With
+% highest priority given to first element X, if X is the same in both then
+% it compares values of Y, etc.
 %
 verNew = get_ver(newVersionString);
 verOld = get_ver(oldVersionString);
-if(verNew.maj>verOld.maj) || ... % Newer major version
-		(verNew.maj==verOld.maj && verNew.min>verOld.min) || ... % Same major version, newer calibration
-		(verNew.maj==verOld.maj && verNew.min==verOld.min && verNew.rev>verOld.rev) % Same major and calib. but newer revision, replace file
-	res = true;
-else
-	res = false;
+if(~all(size(verOld) == size(verNew)))
+  errStr='Unexpected format of version strings';
+  irf.log('critical',errStr); error(errStr);
 end
+res = false; % Assume it is not newer.
+for ii=1:length(verOld) % Loop through each version segment.
+  if(verNew{ii} > verOld{ii})
+    res = true; % It is newer.
+    return % Exit function with result.
+  end
+end
+
 	function ver = get_ver(verS)
 		vT = strsplit(verS,'.');
 		for iTmp=1:length(vT), vT{iTmp} = str2double(vT{iTmp}); end
-		ver = struct('maj',vT{1},'min',0,'rev',0);
-		if isempty(vT{2}), return, end, ver.min = vT{2};
-		if isempty(vT{3}), return, end, ver.rev = vT{3};
+        ver = vT;
 	end
 end
