b1ae5f8156430ad6ff05128914dd0cd1785d75de
thomas-nilsson-irfu <thomas-nilsson-irfu@users.noreply.github.com>
2016-02-25 10:39:15 +0100

    Add some more License (in order to comply with license) and source links.

--- contrib/matlab_central/arrow.m
+++ contrib/matlab_central/arrow.m
@@ -120,6 +120,36 @@ function [h,yy,zz] = arrow(varargin)
 % Permission is granted to Dr. Josef Bigun to distribute ARROW with his
 % software to reproduce the figures in his image analysis text.
 
+% Source: Matlab Central File Exchange
+% https://www.mathworks.com/matlabcentral/fileexchange/278-arrow-m/
+
+% LICENSE
+% Copyright (c) 2009, Erik A Johnson
+% All rights reserved.
+% 
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions are
+% met:
+% 
+% * Redistributions of source code must retain the above copyright
+%   notice, this list of conditions and the following disclaimer.
+% * Redistributions in binary form must reproduce the above copyright
+%   notice, this list of conditions and the following disclaimer in
+%   the documentation and/or other materials provided with the distribution
+% 
+%   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+%   AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+%   IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+%   ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+%   LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+%   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+%   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+%   INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+%   CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+%   ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+%   POSSIBILITY OF SUCH DAMAGE.
+% END OF LICENSE
+
 % global variable initialization
 global ARROW_PERSP_WARN ARROW_STRETCH_WARN ARROW_AXLIMITS
 if isempty(ARROW_PERSP_WARN  ), ARROW_PERSP_WARN  =1; end;
