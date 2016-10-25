6792adfc5eca995d68bd4a56b5b952dbb29750f5
Cecilia Norgren <cecilia.norgren@irfu.se>
2016-10-25 21:30:16 +0200

    small change

--- mission/thor/orbit_coverage/thor_netdata.m
+++ mission/thor/orbit_coverage/thor_netdata.m
@@ -1,4 +1,4 @@
-function tsNetData = thor_netdata(data,downlink)
+function [tsNetData,~] = thor_netdata(data,downlink,memory_limit,downlink_delay)
 % THOR_NETDATA - calculates how much data accumulates onboard THOR
 %   netdata = thor_netdata(data,downlink)
 
@@ -18,8 +18,9 @@ netData = NaN(data_stacked.length,n_dim_data);
 netDataT = NaN(data_stacked.length,n_dim_data);
 
 netData(1,:) = data_stacked.data(1,:);
-
+delay = 0;
 for iOrbit = 2:nOrbits  
+  delay = delay + 1;
   new_data = data.data(iOrbit,:);
   if sum(new_data)>0
     1;
@@ -28,36 +29,23 @@ for iOrbit = 2:nOrbits
   tot_data = netData(iOrbit-1,:) + new_data;
   tot_data_stacked = cumsum(tot_data,2);
   
-  % remove the downlinked data
-  to_downlink = downlink_stacked.data(iOrbit,:);
-  new_net_data_stacked = tot_data_stacked - to_downlink;  
-  % there can be no negative data, so remove all negative values
-  new_net_data_stacked(new_net_data_stacked<0) = 0;
+  if delay == downlink_delay
+    delay = 0;
+    % remove the downlinked data
+    to_downlink = downlink_stacked.data(iOrbit,:);
+    new_net_data_stacked = tot_data_stacked - to_downlink;  
+    % there can be no negative data, so remove all negative values
+    new_net_data_stacked(new_net_data_stacked<0) = 0;
+    new_net_data_stacked(new_net_data_stacked>memory_limit) = memory_limit;
+    %downloaded = tot_data_stacked-new_net_data_stacked;
+    %tot_data = diff([0 new_net_data_stacked]);
+  end
   
-  new_data = diff([0 new_net_data_stacked]);
-  
-  
-  netData(iOrbit,:) = new_data;
+  netData(iOrbit,:) = tot_data;
+  if any(new_data>memory_limit)
+    1;
+  end
 
-%   tmpdata = data_stacked.data(iOrbit,:);
-%   tmpdownlink = downlink_stacked.data(iOrbit,:);
-%   tmpdiffQ = tmpdata-tmpdownlink;
-%   tmpdiffQ(tmpdiffQ<0) = 0; % there's no negative data
-%   netData(iOrbit,:) = tmpdiffQ;
-%   
-%   tmpdata = dataBSstackedT.data(iOrbit,:);
-%   tmpdownlink = downlinkBSstacked.data(iOrbit,:);
-%   tmpdiffQ = tmpdata-tmpdownlink;
-%   tmpdiffQ(tmpdiffQ<0) = 0; % there's no negative data
-%   netDataT(iOrbit,:) = tmpdiffQ;
-%   if sum(tmpdata)>0
-%     %iOrbit
-%     %tmpdata
-%    % pause;
-%   end
 end
 
 tsNetData = irf.ts_scalar(data.time,netData);
-%netDataBS = irf.ts_scalar(dataBSstacked.time,netData);
-%netDataBSstackedT = irf.ts_scalar(netDataBS.time,cumsum(netDataBS.data,1));
-%netDataBST = irf.ts_scalar(netDataBS.time,netDataT);
