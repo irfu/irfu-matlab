fdb2a29cd88059c18fe7c6bca471c4239b6c5a48
Yuri Khotyaintsev <yuri@irfu.se>
2016-10-26 10:50:10 +0200

    add new code for cleanup of spin residual in e12, e34

--- mission/mms/mms_sdp_dmgr.m
+++ mission/mms/mms_sdp_dmgr.m
@@ -174,6 +174,7 @@ classdef mms_sdp_dmgr < handle
           chk_sweep_on()
 %          chk_maneuvers()
           chk_sdp_v_vals()
+          %e_corr_cmd()
           e_from_asym()
           sensors = {'e12','e34','e56'};
           apply_nom_amp_corr() % AFTER all V values was calculated but before most processing.
@@ -987,6 +988,38 @@ classdef mms_sdp_dmgr < handle
         function l = sensor_dist(len)
           l = 1.67 + len + .07 + 1.75  + .04; % meters, sc+boom+preAmp+wire+probe
         end
+      end
+      
+      function e_corr_cmd()
+        % Correct E for CMD
+        Phase = DATAC.phase;
+        if isempty(Phase)
+          errStr='Bad PHASE input, cannot proceed.';
+          irf.log('critical',errStr); error(errStr);
+        end
+        if(DATAC.tmMode == DATAC.CONST.TmMode.brst)
+          % XXX implement something
+        else
+          irf.log('notice','Correcting E for CMD');
+          NOM_BOOM_L = .12; % 120 m
+          if 1 % using spin resudual
+            SpinModel = mms_sdp_model_spin_residual(DATAC.dce,DATAC.dcv,Phase,...
+            {'v1','v2','v3','v4'},DATAC.samplerate);
+            DATAC.dce.e12.data = single( double(DATAC.dce.e12.data) - ...
+              (SpinModel.v1 - SpinModel.v2)/NOM_BOOM_L );
+            DATAC.dce.e34.data = single( double(DATAC.dce.e34.data) - ...
+              (SpinModel.v3 - SpinModel.v4)/NOM_BOOM_L );
+          else % using CMD
+            CmdModel = mms_sdp_model_spin_residual_cmd312(DATAC.dcv,...
+              Phase, DATAC.samplerate,'e12');
+            DATAC.dce.e12.data = single( ...
+              double(DATAC.dce.e12.data) - CmdModel/NOM_BOOM_L );
+            CmdModel = mms_sdp_model_spin_residual_cmd312(DATAC.dcv,...
+              Phase, DATAC.samplerate,'e34');
+            DATAC.dce.e34.data = single( ...
+              double(DATAC.dce.e34.data) - CmdModel/NOM_BOOM_L );
+          end
+        end   
       end
       
       function e_from_asym()
