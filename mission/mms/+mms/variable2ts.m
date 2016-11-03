65f756efeb9bb08d6bb570e081c181b0e015e200
wyli <wyli.space@gmail.com>
2016-11-03 17:57:49 +0100

    minor change

--- mission/mms/+mms/variable2ts.m
+++ mission/mms/+mms/variable2ts.m
@@ -60,15 +60,15 @@ if ~isempty(regexp(v.name,'^mms[1-4]_d[ei]s_','once'))
                 if strcmp(v.DEPEND_0.DELTA_MINUS_VAR.UNITS, 's')
                     flag_MINUS = 1e3;           % s --> ms
                 elseif strcmp(v.DEPEND_0.DELTA_MINUS_VAR.UNITS, 'ms')
-                    flag_MINUS = 1;             % s --> ms
+                    flag_MINUS = 1;
                 else
                     irf.log('warning','Epoch_minus_var units are not clear, assume s');
                     flag_MINUS = 1e3;       
                 end
                 if strcmp(v.DEPEND_0.DELTA_PLUS_VAR.UNITS, 's')
-                    flag_PLUS = 1e3;           % s --> ns
+                    flag_PLUS = 1e3;           % s --> ms
                 elseif strcmp(v.DEPEND_0.DELTA_PLUS_VAR.UNITS, 'ms')
-                    flag_PLUS = 1;           % s --> ns
+                    flag_PLUS = 1;
                 else
                     irf.log('warning','Epoch_plus_var units are not clear, assume s');
                     flag_PLUS = 1e3;
