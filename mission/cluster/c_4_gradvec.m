2d59d79d56c1512e49435209c9efa15e431c38b2
chasapis <chasapis@udel.edu>
2016-12-01 00:55:05 -0500

    make c_4_gradvec for timeseries
    c_4_gradvec fix to return 3x3 tensor for each timestamp and not just the first element

--- mission/cluster/c_4_gradvec.m
+++ mission/cluster/c_4_gradvec.m
@@ -19,10 +19,13 @@ function [varargout]=c_4_gradvec(r1,r2,r3,r4)
 
 if nargin<4;    disp('Too few parameters. See usage:');help c_4_gradVec;     return;end
 
-R1=r1(1,2:4);R2=r2(1,2:4);R3=r3(1,2:4);R4=r4(1,2:4);
+n=size(r1,1);
+gradVec=zeros(n,3,3);
+for i=1:n % to be vectorized at some point
+R1=r1(i,2:4);R2=r2(i,2:4);R3=r3(i,2:4);R4=r4(i,2:4);
 R_Center=(R1+R2+R3+R4)/4;
 dR1=R1-R_Center;dR2=R2-R_Center;dR3=R3-R_Center;dR4=R4-R_Center;
-x1=r1(1,5:7);x2=r2(1,5:7);x3=r3(1,5:7);x4=r4(1,5:7);
+x1=r1(i,5:7);x2=r2(i,5:7);x3=r3(i,5:7);x4=r4(i,5:7);
 
 S=0;
 for a=1:4, for b=1:a,     S=S+eval(irf_ssub('(x?-x!)''*(dR?-dR!)',a,b));      end,end
@@ -30,7 +33,8 @@ S=S/16;
 
 Rinv=c_4_r(dR1,dR2,dR3,dR4,-1);                                     % inverse of volumetric tensor
 
-gradVec=S*Rinv;
+gradVec(i,:,:)=S*Rinv;
+end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  OUTPUT %%%%%%%%%%%%%%%%%%%%%%%%%
 
