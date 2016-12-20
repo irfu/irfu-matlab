41996057091ed1eaecdb3e81d36f5cc4a4ec4f67
Yuri Khotyaintsev <yuri@irfu.se>
2016-12-20 16:36:38 +0100

    try lower level data if L2 is not available

--- plots/mms/Example_MMS_EDP_vs_FPI_vs_HPCA_brst.m
+++ plots/mms/Example_MMS_EDP_vs_FPI_vs_HPCA_brst.m
@@ -2,10 +2,25 @@ mmsId = 4;
 Tint = irf.tint('2015-09-19T09:09:00Z/2015-09-19T09:11:00Z');
 
 %% FGM & EDP
-B_bcs_fgm_srvy_l2 = mms.get_data('B_dmpa_fgm_srvy_l2',Tint,mmsId);
-E_dsl_edp_l2 = mms.get_data('E_dsl_edp_brst_l2',Tint,mmsId);
-E2d_dsl_edp_l2pre = mms.get_data('E2d_dsl_edp_brst_l2pre',Tint,mmsId);
-
+B_dmpa_fgm_srvy = mms.get_data('B_dmpa_fgm_srvy_l2',Tint,mmsId);
+if isempty(B_dmpa_fgm_srvy)
+  irf.log('warning','loading L2pre DFG')
+  B_dmpa_fgm_srvy = mms.get_data('B_dmpa_dfg_srvy_l2pre',Tint,mmsId);
+  if isempty(B_dmpa_fgm_srvy)
+    irf.log('warning','loading QL DFG')
+    B_dmpa_fgm_srvy = mms.get_data('B_dmpa_dfg_srvy_ql',Tint,mmsId);
+  end
+end
+E_dsl_edp = mms.get_data('E_dsl_edp_brst_l2',Tint,mmsId);
+if isempty(E_dsl_edp)
+  irf.log('warning','loading QL DCE')
+  E_dsl_edp = mms.get_data('E_dsl_edp_brst_ql',Tint,mmsId);
+end
+E2d_dsl_edp = mms.get_data('E2d_dsl_edp_brst_l2pre',Tint,mmsId);
+if isempty(E2d_dsl_edp)
+  irf.log('warning','loading QL DCE2d')
+  E2d_dsl_edp = mms.get_data('E2d_dsl_edp_brst_ql',Tint,mmsId);
+end
 %% FPI
 Vi_dbcs_fpi = mms.get_data('Vi_dbcs_fpi_brst_l2',Tint,mmsId);
 Ve_dbcs_fpi = mms.get_data('Ve_dbcs_fpi_brst_l2',Tint,mmsId);
@@ -15,27 +30,27 @@ if isempty(Vhplus_dbcs_hpca)
 end
 
 %%
-[~, Vi_perp] = irf_dec_parperp(B_bcs_fgm_srvy_l2,Vi_dbcs_fpi);
-[~, Ve_perp] = irf_dec_parperp(B_bcs_fgm_srvy_l2,Ve_dbcs_fpi);
-[~, E_perp] = irf_dec_parperp(B_bcs_fgm_srvy_l2,E_dsl_edp_l2);
-VExB = irf_e_vxb(E_dsl_edp_l2,B_bcs_fgm_srvy_l2,-1);
-VExB_l2pre = irf_e_vxb(E2d_dsl_edp_l2pre,B_bcs_fgm_srvy_l2,-1);
-EVixB = irf_e_vxb(Vi_dbcs_fpi,B_bcs_fgm_srvy_l2.resample(Vi_dbcs_fpi));
-EVexB = irf_e_vxb(Ve_dbcs_fpi,B_bcs_fgm_srvy_l2.resample(Ve_dbcs_fpi));
+[~, Vi_perp] = irf_dec_parperp(B_dmpa_fgm_srvy,Vi_dbcs_fpi);
+[~, Ve_perp] = irf_dec_parperp(B_dmpa_fgm_srvy,Ve_dbcs_fpi);
+[~, E_perp] = irf_dec_parperp(B_dmpa_fgm_srvy,E_dsl_edp);
+VExB = irf_e_vxb(E_dsl_edp,B_dmpa_fgm_srvy,-1);
+VE2dxB = irf_e_vxb(E2d_dsl_edp,B_dmpa_fgm_srvy,-1);
+EVixB = irf_e_vxb(Vi_dbcs_fpi,B_dmpa_fgm_srvy.resample(Vi_dbcs_fpi));
+EVexB = irf_e_vxb(Ve_dbcs_fpi,B_dmpa_fgm_srvy.resample(Ve_dbcs_fpi));
 if isempty(Vhplus_dbcs_hpca), Vhplus_perp = []; EVphlusxB = [];
 else
-  [~, Vhplus_perp] = irf_dec_parperp(B_bcs_fgm_srvy_l2,Vhplus_dbcs_hpca);
-  EVphlusxB = irf_e_vxb(Vhplus_dbcs_hpca,B_bcs_fgm_srvy_l2.resample(Vhplus_dbcs_hpca));
+  [~, Vhplus_perp] = irf_dec_parperp(B_dmpa_fgm_srvy,Vhplus_dbcs_hpca);
+  EVphlusxB = irf_e_vxb(Vhplus_dbcs_hpca,B_dmpa_fgm_srvy.resample(Vhplus_dbcs_hpca));
 end
 
 %%
 f = irf_figure(2387456,3);
 set(gcf,'defaultAxesColorOrder',[0 0 0;0 0 1;1 0 0;0 0.9 0;0 1 1 ;1 0 1; 1 1 0])
-h = irf_plot({VExB,VExB_l2pre,Ve_perp,Vi_perp,Vhplus_perp},'comp');
+h = irf_plot({VExB,VE2dxB,Ve_perp,Vi_perp,Vhplus_perp},'comp');
 title(h(1),sprintf('MMS%d',mmsId))
-legend(h(1),'VExB','VExB l2pre','V_{e\perp}','V_{i\perp}','V_{H+\perp}')
-legend(h(2),'VExB','VExB l2pre','V_{e\perp}','V_{i\perp}','V_{H+\perp}')
-legend(h(3),'VExB','VExB l2pre','V_{e\perp}','V_{i\perp}','V_{H+\perp}')
+legend(h(1),'VExB','VE2dxB','V_{e\perp}','V_{i\perp}','V_{H+\perp}')
+legend(h(2),'VExB','VE2dxB','V_{e\perp}','V_{i\perp}','V_{H+\perp}')
+legend(h(3),'VExB','VE2dxB','V_{e\perp}','V_{i\perp}','V_{H+\perp}')
 ylabel(h(1),'V_x DSL [km/s]')
 ylabel(h(2),'V_y DSL [km/s]')
 ylabel(h(3),'V_z DSL [km/s]')
@@ -46,11 +61,11 @@ irf_print_fig(['mms' num2str(mmsId) '_VExB_EDP_vs_FPI_vs_HPCA_brst_' irf_fname(T
 %%
 f = irf_figure(2387457,3);
 set(gcf,'defaultAxesColorOrder',[0 0 0;0 0 1;1 0 0;0 0.9 0;0 1 1 ;1 0 1; 1 1 0])
-h = irf_plot({E2d_dsl_edp_l2pre,E_dsl_edp_l2,E_perp,EVexB,EVixB,EVphlusxB},'comp');
+h = irf_plot({E2d_dsl_edp,E_dsl_edp,E_perp,EVexB,EVixB,EVphlusxB},'comp');
 title(h(1),sprintf('MMS%d',mmsId))
-legend(h(1),'E L2pre','E L2','E_perp','V_{e}xB','V_{i}xB','V_{H+}xB')
-legend(h(2),'E L2pre','E L2','E_perp','V_{e}xB','V_{i}xB','V_{H+}xB')
-legend(h(3),'E L2pre','E L2','E_perp','V_{e}xB','V_{i}xB','V_{H+}xB')
+legend(h(1),'E2d','E','E_perp','V_{e}xB','V_{i}xB','V_{H+}xB')
+legend(h(2),'E2d','E','E_perp','V_{e}xB','V_{i}xB','V_{H+}xB')
+legend(h(3),'E2d','E','E_perp','V_{e}xB','V_{i}xB','V_{H+}xB')
 ylabel(h(1),'E_x DSL [mV/m]')
 ylabel(h(2),'E_y DSL [mV/m]')
 ylabel(h(3),'E_z DSL [mV/m]')
