ec7dc6dbe65590822c07516654f6479ee03fff99
Yuri Khotyaintsev <yuri@irfu.se>
2017-02-14 18:41:18 +0100

    bug fix

--- mission/mms/mms_sdp_1sec_db_batch.m
+++ mission/mms/mms_sdp_1sec_db_batch.m
@@ -7,9 +7,9 @@
 % this stuff is worth it, you can buy me a beer in return.   Yuri Khotyaintsev
 % ----------------------------------------------------------------------------
 
-yymm = '2015/12'; mmsId = 'mms1'; dataPath = '/Users/yuri/Data/mms';
+yymm = '2015/12'; mmsId = 'mms1'; dataPath = '/data/mms';
 %%
-db.files = {}; db.tint = {};db.data = {};
+db.files = {}; db.tint = {}; db.data = [];
 for dd=1:31
   day = sprintf('%02d',dd)
   dataDir = [dataPath '/' mmsId '/edp/fast/l2a/dce2d/' yymm '/'];
@@ -20,11 +20,14 @@ for dd=1:31
   disp(['Processing: ' fName.name])
   [out,Tint] = mms_sdp_1sec_db(fName.name,dataDir);
   
-  % Append data
-  fi = fields(out);
-  for idx = 1:length(fi)
-    db.data.(fi{idx}) = [db.data.(fi{idx}) out.(fi{idx})];
+  if isempty(db.data), db.data = out;
+  else
+    % Append data
+    fi = fields(out);
+    for idx = 1:length(fi)
+      db.data.(fi{idx}) = [db.data.(fi{idx}); out.(fi{idx})];
+    end
   end
   % Save file names for Bookkeeping and possible reprocessing
-  db.files = [db.files fName]; db.tint = [db.tint fName];
+  db.files = [db.files fName.name]; db.tint = [db.tint Tint];
 end
