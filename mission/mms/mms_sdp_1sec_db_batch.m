cf45a2fe7351726de1987f7b0649e8df2fecdcee
Yuri Khotyaintsev <yuri@irfu.se>
2017-02-15 17:45:02 +0100

    fixes, add missing params

--- mission/mms/mms_sdp_1sec_db_batch.m
+++ mission/mms/mms_sdp_1sec_db_batch.m
@@ -7,14 +7,17 @@
 % this stuff is worth it, you can buy me a beer in return.   Yuri Khotyaintsev
 % ----------------------------------------------------------------------------
 
-yymm = '2015/12'; mmsId = 'mms1'; dataPath = '/data/mms';
+yymm = '201511'; 
+%yymmList = {'201511','201512','201601','201602'}
+mmsId = 'mms1'; dataPath = '/data/mms';
+%for yy=2015
 %%
-db.files = {}; db.tint = {}; db.data = [];
+db.files = {}; db.tint = {}; db.data = []; db.mmsId = mmsId;
 for dd=1:31
   day = sprintf('%02d',dd)
-  dataDir = [dataPath '/' mmsId '/edp/fast/l2a/dce2d/' yymm '/'];
+  dataDir = [dataPath '/' mmsId '/edp/fast/l2a/dce2d/' yymm(1:4) '/' yymm(5:6) '/'];
   fName = mms_find_latest_version_cdf(...
-    [dataDir '*' yymm(1:4) yymm(6:7) day '*.cdf'])
+    [dataDir '*' yymm day '*.cdf'])
   if isempty(fName), continue, end
 
   disp(['Processing: ' fName.name])
@@ -31,3 +34,5 @@ for dd=1:31
   % Save file names for Bookkeeping and possible reprocessing
   db.files = [db.files fName.name]; db.tint = [db.tint Tint];
 end
+save([mmsId '__' yymm], 'db');
+
