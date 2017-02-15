dc55e0fd070e4848531b0c748f01f0aaa6f849c8
Yuri Khotyaintsev <yuri@irfu.se>
2017-02-15 17:52:05 +0100

    make it loop over month

--- mission/mms/mms_sdp_1sec_db_batch.m
+++ mission/mms/mms_sdp_1sec_db_batch.m
@@ -1,4 +1,4 @@
-% Script for processing one moth of data
+% Script for processing one month of data
 
 % ----------------------------------------------------------------------------
 % "THE BEER-WARE LICENSE" (Revision 42):
@@ -7,32 +7,35 @@
 % this stuff is worth it, you can buy me a beer in return.   Yuri Khotyaintsev
 % ----------------------------------------------------------------------------
 
-yymm = '201511'; 
-%yymmList = {'201511','201512','201601','201602'}
+yymmList = {'201511','201512','201601','201602','201609','201610','201611','201612'};
 mmsId = 'mms1'; dataPath = '/data/mms';
-%for yy=2015
-%%
-db.files = {}; db.tint = {}; db.data = []; db.mmsId = mmsId;
-for dd=1:31
-  day = sprintf('%02d',dd)
-  dataDir = [dataPath '/' mmsId '/edp/fast/l2a/dce2d/' yymm(1:4) '/' yymm(5:6) '/'];
-  fName = mms_find_latest_version_cdf(...
-    [dataDir '*' yymm day '*.cdf'])
-  if isempty(fName), continue, end
-
-  disp(['Processing: ' fName.name])
-  [out,Tint] = mms_sdp_1sec_db(fName.name,dataDir);
-  
-  if isempty(db.data), db.data = out;
-  else
-    % Append data
-    fi = fields(out);
-    for idx = 1:length(fi)
-      db.data.(fi{idx}) = [db.data.(fi{idx}); out.(fi{idx})];
+for idxMo=yymmList
+  yymm = idxMo{:};
+  %%
+  %yymm = '201511';
+  db.files = {}; db.tint = {}; db.data = []; db.mmsId = mmsId;
+  for dd=1:31
+    day = sprintf('%02d',dd)
+    dataDir = [dataPath '/' mmsId '/edp/fast/l2a/dce2d/' yymm(1:4) '/' yymm(5:6) '/'];
+    fName = mms_find_latest_version_cdf(...
+      [dataDir '*' yymm day '*.cdf'])
+    if isempty(fName), continue, end
+    
+    disp(['Processing: ' fName.name])
+    [out,Tint] = mms_sdp_1sec_db(fName.name,dataDir);
+    
+    if isempty(db.data), db.data = out;
+    else
+      % Append data
+      fi = fields(out);
+      for idx = 1:length(fi)
+        db.data.(fi{idx}) = [db.data.(fi{idx}); out.(fi{idx})];
+      end
     end
+    % Save file names for Bookkeeping and possible reprocessing
+    db.files = [db.files fName.name]; db.tint = [db.tint Tint];
   end
-  % Save file names for Bookkeeping and possible reprocessing
-  db.files = [db.files fName.name]; db.tint = [db.tint Tint];
+  save([mmsId '__' yymm], 'db');
+  %%
 end
-save([mmsId '__' yymm], 'db');
 
