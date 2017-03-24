0df01dc48d36563cd5856e6d3492172c56a860ab
Yuri Khotyaintsev <yuri@irfu.se>
2017-03-24 17:48:25 +0100

    add 201701

--- mission/mms/mms_sdp_1sec_db_batch.m
+++ mission/mms/mms_sdp_1sec_db_batch.m
@@ -7,8 +7,8 @@
 % this stuff is worth it, you can buy me a beer in return.   Yuri Khotyaintsev
 % ----------------------------------------------------------------------------
 
-yymmList = {'201511','201512','201601','201602','201610','201611','201612'};
-mmsId = 'mms1'; dataPath = '/data/mms';
+yymmList = {'201511','201512','201601','201602','201610','201611','201612','201701'};
+mmsId = 'mms4'; dataPath = '/data/mms';
 for idxMo=yymmList
   yymm = idxMo{:};
   %%
