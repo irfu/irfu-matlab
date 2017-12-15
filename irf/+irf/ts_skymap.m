bb18a938f4b4b65329e8cc2ce8fd4ca455c8c400
thomas-nilsson-irfu <thomas-nilsson-irfu@users.noreply.github.com>
2017-12-15 11:46:15 +0100

    Mlint

--- irf/+irf/ts_skymap.m
+++ irf/+irf/ts_skymap.m
@@ -6,7 +6,7 @@ function TS = ts_skymap(time,data,energy,phi,theta,varargin)
 % Create TSeries object - skymap
 
 if ~isa(time,'GenericTimeArray'), epoch = EpochTT(time);
-else epoch = time;
+else, epoch = time;
 end
 args = varargin;
 if isempty(energy) % must check that energy0, energy1 and esteptable is given
@@ -15,12 +15,17 @@ if isempty(energy) % must check that energy0, energy1 and esteptable is given
   esteptable_ok = 0;
   while ~isempty(args)
     x = args{1}; args(1) = [];
-    if ischar(x);
+    if ischar(x)
       switch lower(x)
-        case {'energy0'}; energy0_ok = 1; energy0 = args{1}; args(1) = [];
-        case {'energy1'}; energy1_ok = 1; energy1 = args{1}; args(1) = [];
-        case {'esteptable'}; esteptable_ok = 1; esteptable = args{1}; args(1) = [];
-      end     
+        case {'energy0'}
+          energy0_ok = 1; energy0 = args{1}; args(1) = [];
+        case {'energy1'}
+          energy1_ok = 1; energy1 = args{1}; args(1) = [];
+        case {'esteptable'}
+          esteptable_ok = 1; esteptable = args{1}; args(1) = [];
+        otherwise
+          irf.log('debug', ['Unexpected input: ', x]);
+      end
     end
   end
   if ~(energy0_ok && energy1_ok && esteptable_ok); error('Energy input required'); end
@@ -36,3 +41,4 @@ if ~isnumeric(phi); phi = phi.data; end
 if ~isnumeric(theta); theta = theta.data; end
 
 TS = PDist(epoch,data,'skymap',energy,phi,theta,varargin{:});
+end
