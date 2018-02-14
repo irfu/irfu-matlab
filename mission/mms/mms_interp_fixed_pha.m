3f4479a3c47f4aa6fdf4e859f997f0544c640790
Yuri Khotyaintsev <yuri@irfu.se>
2018-02-14 18:21:45 +0100

    make it work

--- mission/mms/mms_interp_fixed_pha.m
+++ mission/mms/mms_interp_fixed_pha.m
@@ -1,28 +1,32 @@
-function [dataFixedPha,fixedPha,epochFixedPha] = mms_interp_fixed_pha(data, tEpoch2000, phaseDeg)
+function [dataFixedPha,fixedPha,epoch2000FixedPha] = mms_interp_fixed_pha(data, tEpoch2000, phaseDeg)
 
-STEPS_PER_DEG = 1; phaShift=STEPS_PER_DEG/2;
+STEPS_PER_DEG = 0.5; phaShift = 0; %phaShift=STEPS_PER_DEG/2;
+PHASE_OFF = 30; % offset the phase so that the boom is 45 deg to the sun
 
 epoch0 = tEpoch2000(1); epochTmp = double(tEpoch2000-epoch0);
 
-if 1 % no gaps
+if max(diff(epochTmp))>1e9*60/3.1/2 % Gap > 1/ spin period
+  irf.log('critical','long data gap')
+  error('long data gap')
+else % no gaps
   phaDegSeg = phaseDeg;
   epochSeg = epochTmp;
   dataSeg = data;
   
   [dataFixedPha,fixedPha,epochFixedPha] = interp_cont_segment();
+  epoch2000FixedPha = epoch0 + int64(epochFixedPha);
   return
 end
 
   function [dataFixedPhaSeg,phaFixedWrpSeg,epochFixedPhaSeg] = interp_cont_segment()
-    phaRad = unwrap(phaDegSeg*pi/180);
-    
-    phaDegUnw = phaRad*180/pi;
-    phaFixed = (fix(phaDegUnw(1)):STEPS_PER_DEG:fix(phaDegUnw(end)))' ...
-      + phaShift;
-    pha360 = 0:STEPS_PER_DEG:360; pha360 = pha360' + phaShift; pha360(end) = [];
+    phaDegUnw = unwrap(phaDegSeg*pi/180)*180/pi + PHASE_OFF;
+    %phaFixed = (fix(phaDegUnw(1)):STEPS_PER_DEG:fix(phaDegUnw(end)))' ...
+    %  + phaShift;
+    phaFixed = ((phaDegUnw(1)-rem(phaDegUnw(1),360)):STEPS_PER_DEG:...
+      (phaDegUnw(end)-rem(phaDegUnw(end),360)+360))' + phaShift;
     epochFixedPhaSeg = interp1(phaDegUnw,epochSeg,phaFixed,'linear');
-    phaFixed(isnan(epochFixedPhaSeg)) = []; 
-    epochFixedPhaSeg(isnan(epochFixedPhaSeg)) = [];
+    %phaFixed(isnan(epochFixedPhaSeg)) = []; 
+    %epochFixedPhaSeg(isnan(epochFixedPhaSeg)) = [];
     phaFixedWrpSeg = mod(phaFixed,360);
     dataFixedPhaSeg = interp1(epochSeg,double(dataSeg),epochFixedPhaSeg,'spline');
   end
