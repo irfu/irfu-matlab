48ee125e191531e078e1fb7e77af65827bebc70e
wyli <wyli.space@gmail.com>
2018-03-20 15:55:43 +0800

    allow irf.nansum in pitch angle calculation.

--- mission/mms/+mms/get_pitchangledist.m
+++ mission/mms/+mms/get_pitchangledist.m
@@ -32,6 +32,7 @@ function [paddist,theta,energy,tint] = get_pitchangledist(varargin)
 %       'angles' - flag to set number of pitch angles used (single value Default is
 %       12) or pitch angle edges (array). When counts are high (plasma is dense) 24 pitch angles for
 %       electrons may be used. 
+%       'meanorsum' - 'mean' (Default) or 'sum'; + @ 2018-03-20;
 %
 % Support for v0.2.0 should be removed when it is not needed
 % 
@@ -46,25 +47,38 @@ lengththeta = 16;
 anglevec = [15:15:180]; % Default pitch angles. 15 degree angle widths
 dangle = median(diff(anglevec))*ones(1,length(anglevec));
 tmpnargin = nargin;
-if isa(varargin{tmpnargin-1},'char')
-    if strcmp(varargin{tmpnargin-1},'angles')
-        if (length(varargin{tmpnargin}) == 1)
-            nangles = varargin{tmpnargin};
-            nangles = floor(nangles); % Make sure input is integer
-            dangle = 180/nangles;
-            anglevec = [dangle:dangle:180];
-            dangle = dangle*ones(1,length(anglevec));
-            irf.log('notice','User defined number of pitch angles.')
-        elseif (length(varargin{tmpnargin}) > 1)
-            anglevec = varargin{tmpnargin};
-            dangle = diff(anglevec);
-            anglevec = anglevec(2:end);
-            irf.log('notice','User defined pitch angle limits.')
+meanorsum = 'mean';
+have_char = 0;
+
+if isa(varargin{tmpnargin-1},'char'), have_char = 1; end
+
+while have_char
+    if isa(varargin{tmpnargin-1},'char')
+        if strcmp(varargin{tmpnargin-1},'angles')
+            if (length(varargin{tmpnargin}) == 1)
+                nangles = varargin{tmpnargin};
+                nangles = floor(nangles); % Make sure input is integer
+                dangle = 180/nangles;
+                anglevec = [dangle:dangle:180];
+                dangle = dangle*ones(1,length(anglevec));
+                irf.log('notice','User defined number of pitch angles.')
+            elseif (length(varargin{tmpnargin}) > 1)
+                anglevec = varargin{tmpnargin};
+                dangle = diff(anglevec);
+                anglevec = anglevec(2:end);
+                irf.log('notice','User defined pitch angle limits.')
+            else
+                irf.log('warning','angles parameter not understood. ')
+            end
+        elseif strcmp(varargin{tmpnargin-1},'meanorsum')
+            meanorsum = varargin{tmpnargin};
         else
-            irf.log('warning','angles parameter not understood. ')
-        end
+            irf.log('warning', 'parameters not understood. ')
+        end 
     end
     tmpnargin = tmpnargin-2;
+    have_char = 0;
+    if isa(varargin{tmpnargin-1},'char'), have_char = 1; end
 end
 
 pitcha = anglevec-dangle/2;
@@ -255,8 +269,11 @@ for jj = 2:(length(anglevec)-1)
 	c_eval('dist?(thetab > anglevec(?)) = NaN;',jj);
 end 
 c_eval('dist?(thetab < (anglevec(end)-dangle(?))) = NaN;',length(anglevec));
-c_eval('dist? =  squeeze(irf.nanmean(irf.nanmean(dist?,4),3));',1:length(anglevec));
-
+if strcmp(meanorsum, 'mean')
+    c_eval('dist? =  squeeze(irf.nanmean(irf.nanmean(dist?,4),3));',1:length(anglevec));
+elseif strcmp(meanorsum, 'sum')
+    c_eval('dist? =  squeeze(irf.nansum(irf.nansum(dist?,4),3));',1:length(anglevec));
+end    
 %paddistarr = cat(3,dist15,dist30,dist45,dist60,dist75,dist90,dist105,dist120,dist135,dist150,dist165,dist180);
 paddistarr = dist1;
 for ii = 2:length(anglevec)
