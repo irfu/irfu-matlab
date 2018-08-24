d40f7aeb5934ec5caf73176f1905ef868bfaba9d
Andreas Johlander <andreas.johlander@gmail.com>
2018-08-24 14:18:16 +0200

    add cartesian binning to 2D reduced particle plots

--- irf/irf_int_sph_dist.m
+++ irf/irf_int_sph_dist.m
@@ -16,7 +16,7 @@ function [pst] = irf_int_sph_dist(F,v,phi,th,vg,varargin)
 %   'x'     -   axis that is not integrated along in 1D and x-axis
 %               (phig = 0) in 2D. x = [1,0,0] if omitted.
 %   'z'     -   Axis that is integrated along in 2D. z = [0,0,1] if omitted.
-%   'nMC'   -   average number of Monte Carlo iterations used per bin for 
+%   'nMC'   -   average number of Monte Carlo iterations used per bin for
 %               integration, default is 10. Number of iterations can be
 %               weighted data value in each bin.
 %   'weight'-   how the number of MC iterations per bin is weighted, can be
@@ -65,6 +65,7 @@ vzint = [-inf,inf]; % limit on out-of-plane velocity
 aint = [-180,180]; % limit on out-of-plane velocity
 projDim = 1; % number of dimensions of the projection
 weight = 'none'; % how number of MC points is weighted to data
+base = 'pol'; % If 1D then this does not matter
 
 args = varargin;
 nargs = length(varargin);
@@ -77,10 +78,10 @@ while have_options
             xphat = args{2};
         case 'z'
             zphat = args{2};
-        case 'phig' % azimuthal angle of projection plane
             % If this flag is given, it assumes 2D projection
-            phig = args{2};
             projDim = 2;
+        case 'phig' % azimuthal angle of projection plane
+            phig = args{2};
         case 'nmc'
             nMC = args{2};
         case 'vzint'
@@ -89,6 +90,8 @@ while have_options
             aint = args{2};
         case 'weight'
             weight = args{2};
+        case 'base'
+            base = args{2};
     end
     args = args(3:end);
     if isempty(args), break, end
@@ -120,34 +123,47 @@ else
     nAzg = 0; % for 1D
 end
 
-% bin edges
-% vg_edges = [vg(1)-dVg(1)/2,vg+dVg/2]; % quick and dirty
+% Number of instrument bins
+nAz = length(phi);
+nEle = length(th);
+nV = length(v);
+
+
+
+%% bin edges
+
 vg_edges = zeros(1,length(vg)+1);
 vg_edges(1) = vg(1)-diff(vg(1:2))/2;
 vg_edges(2:end-1) = vg(1:end-1)+diff(vg)/2;
 vg_edges(end) = vg(end)+diff(vg(end-1:end))/2;
 
-if projDim == 2
-    phig_edges = [phig-dPhig/2,phig(end)+dPhig/2];
+switch lower(base)
+    case 'pol'
+        if projDim == 2
+            phig_edges = [phig-dPhig/2,phig(end)+dPhig/2];
+        end
+        
+        % primed (grid) diffs
+        dVg = diff(vg_edges);
+        
+        % convert to cartesian mesh, only for output
+        if projDim == 2
+            [phiMesh,vMesh] = meshgrid(phig_edges+dPhig/2,vg); % Creates the mesh, center of bins, phi has one extra bin at the end
+            [vxMesh,vyMesh] = pol2cart(phiMesh-pi/nAzg,vMesh);    % Converts to cartesian
+            
+            [phiMesh_edges,vMesh_edges] = meshgrid(phig_edges,vg_edges); % Creates the mesh, edges of bins
+            [vxMesh_edges,vyMesh_edges] = pol2cart(phiMesh_edges,vMesh_edges); % Converts to cartesian, edges
+        end
+        
+
+        
+    case 'cart'
+        if std(diff(vg)) > 1 % limit good?
+            error('vg must be monotonically increasing for cartesian case'); 
+        end
+        
+        dVg = vg(2)-vg(1);
 end
-
-% primed (grid) diffs
-dVg = diff(vg_edges);
-
-% convert to cartesian mesh, only for output
-if projDim == 2
-    [phiMesh,vMesh] = meshgrid(phig_edges+dPhig/2,vg); % Creates the mesh, center of bins, phi has one extra bin at the end
-    [vxMesh,vyMesh] = pol2cart(phiMesh-pi/nAzg,vMesh);    % Converts to cartesian
-    
-    [phiMesh_edges,vMesh_edges] = meshgrid(phig_edges,vg_edges); % Creates the mesh, edges of bins
-    [vxMesh_edges,vyMesh_edges] = pol2cart(phiMesh_edges,vMesh_edges); % Converts to cartesian, edges
-end
-
-% Number of instrument bins
-nV = length(v);
-nAz = length(phi);
-nEle = length(th);
-
 
 % 3D matrices for instrumental bin centers
 TH = repmat(th,nV,1,nAz);       % [phi,th,v]
@@ -171,14 +187,23 @@ switch weight
         NMC = ceil(Nsum/(sum(sum(sum(log10(F+1)))))*log10(F+1));
 end
 
-% init Fp
-Fg = zeros(nAzg+1,nVg);
-Fg_ = zeros(nAzg,nVg); % use this one with 'edges bins'
-% Volume element
-dtau = ( VEL.^2.*cos(TH).*DV*dPhi*dTh );
-% Area or line element (primed)
-dAg = vg.^(projDim-1).*dVg*dPhig;
 
+
+% "Volume" element in velocity space of an instrument bin
+dtau = ( VEL.^2.*cos(TH).*DV*dPhi*dTh );
+% set grid data matrix and grid "area" element
+switch lower(base)
+    case 'pol'
+        % init Fp
+        Fg = zeros(nAzg+1,nVg);
+        Fg_ = zeros(nAzg,nVg); % use this one with 'edges bins'
+        % Area or line element (primed)
+        dAg = vg.^(projDim-1).*dVg*dPhig;
+        
+    case 'cart'
+        Fg = zeros(nVg,nVg);
+        dAg = dVg^2;
+end
 
 %% Perform projection
 % Loop through all instrument bins
@@ -191,6 +216,7 @@ for i = 1:nV % velocity (energy)
             if F(i,j,k) == 0
                 continue;
             end
+            
             % first is not random
             dV_MC = [0;(rand(nMCt-1,1)-.5)*dV(i)];
             dPHI_MC = [0;(rand(nMCt-1,1)-.5)*dPhi];
@@ -214,40 +240,61 @@ for i = 1:nV % velocity (energy)
             
             if projDim == 1
                 vp = vxp;
-            else
+            elseif strcmp(base,'pol')
                 % convert to polar coordinates (phip could become negative)
                 [phip,vp] = cart2pol(vxp,vyp);
                 % fix if negative
                 phip(phip<0) = 2*pi+phip(phip<0);
             end
             
-            % get indicies for all MC points
-            iVg = discretize(vp,vg_edges);
             
-            if projDim == 2
-                iAzg = discretize(phip,phig_edges);
-            else
-                iAzg = ones(1,nMCt);
-            end
-            
-            % Loop through MC points and add value of instrument bin to the
-            % appropriate projection bin
-            for l = 1:nMCt
-                % add value to appropriate projection bin
-                if usePoint(l) && ~isempty(iAzg(l)) && ~isempty(iVg(l)) && (iAzg(l)<nAzg+1 || iAzg(l)==1) && iVg(l)<nVg+1
-                    Fg(iAzg(l),iVg(l)) = Fg(iAzg(l),iVg(l))+F(i,j,k)*dtau(i,j,k)/dAg(iVg(l))/nMCt;
-                    if projDim == 2
-                      Fg_(iAzg(l),iVg(l)) = Fg(iAzg(l),iVg(l))+F(i,j,k)*dtau(i,j,k)/dAg(iVg(l))/nMCt;
+            % different procedure for 1D or polar OR cartesian
+            if strcmpi(base,'pol') || projDim == 1
+                % ------ 1D AND POLAR CASE ------
+                % get indicies for all MC points
+                iVg = discretize(vp,vg_edges);
+                
+                if projDim == 2
+                    iAzg = discretize(phip,phig_edges);
+                else
+                    iAzg = ones(1,nMCt);
+                end
+                
+                % Loop through MC points and add value of instrument bin to the
+                % appropriate projection bin
+                for l = 1:nMCt
+                    % add value to appropriate projection bin
+                    if usePoint(l) && ~isempty(iAzg(l)) && ~isempty(iVg(l)) && (iAzg(l)<nAzg+1 || iAzg(l)==1) && iVg(l)<nVg+1
+                        Fg(iAzg(l),iVg(l)) = Fg(iAzg(l),iVg(l))+F(i,j,k)*dtau(i,j,k)/dAg(iVg(l))/nMCt;
+                        if projDim == 2
+                            Fg_(iAzg(l),iVg(l)) = Fg(iAzg(l),iVg(l))+F(i,j,k)*dtau(i,j,k)/dAg(iVg(l))/nMCt;
+                        end
+                    end
+                end
+                
+            elseif strcmpi(base,'cart')
+                % ------ CARTESIAN CASE ------
+                
+                % get indicies for all MC points
+                iVxg = discretize(vxp,vg_edges);
+                iVyg = discretize(vyp,vg_edges);
+                
+                % Loop through MC points and add value of instrument bin to the
+                % appropriate projection bin
+                for l = 1:nMCt
+                    if usePoint(l) && vxp(l)>min(vg_edges) && vxp(l)<max(vg_edges) && vyp(l)>min(vg_edges) && vyp(l)<max(vg_edges)
+                        Fg(iVxg(l),iVyg(l)) = Fg(iVxg(l),iVyg(l))+F(i,j,k)*dtau(i,j,k)/dAg/nMCt;
                     end
                 end
             end
+            
         end
     end
 end
 
 
 %% Output
-if projDim == 2
+if projDim == 2 && strcmpi(base,'pol')
     % fix for interp shading, otherwise the last row can be whatever
     Fg(end,:) = mean([Fg(end-1,:);Fg(1,:)]);
 end
@@ -255,16 +302,18 @@ end
 % Calculate density
 if projDim == 1
     dens = sum(Fg.*dAg);
-else
+elseif strcmpi(base,'pol')
     dAG = repmat(dAg,nAzg,1);
     dens = sum(sum(Fg(1:end-1,:).*dAG));
+elseif strcmpi(base,'cart')
+    dens = sum(sum(Fg*dAg));
 end
 
 % Calculate velocity moment
 
 if projDim == 1
     vel = nansum(Fg.*dAg.*vg);
-else
+elseif strcmpi(base,'pol')
     VG = repmat(vg',1,nAzg);
     PHIG = repmat(phig,nVg,1);
     [VXG,VYG] = pol2cart(PHIG,VG);
@@ -275,6 +324,8 @@ else
             vel = vel+[VXG(l,m),VYG(l,m)]*Fg(m,l)*dAg(l);
         end
     end
+elseif strcmpi(base,'cart')
+    vel = [0,0,0]; % whatever
 end
 vel = vel/dens;
 
@@ -284,12 +335,18 @@ pst = [];
 pst.F = Fg;
 if projDim == 1
     pst.v = vg;
-else
+elseif strcmpi(base,'pol')
     pst.vx = vxMesh;
     pst.vy = vyMesh;
     pst.F_using_edges = Fg_;
     pst.vx_edges = vxMesh_edges;
     pst.vy_edges = vyMesh_edges;
+elseif strcmpi(base,'cart')
+    pst.vx = vg;
+    pst.vy = vg;
+    pst.vx_edges = vg_edges;
+    pst.vy_edges = vg_edges;
+    
 end
 pst.dens = dens;
 pst.vel = vel;
