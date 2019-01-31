96bcd9232e1f3b7e1aab0933fa435750129c4deb
Yuri Khotyaintsev <yuri@irfu.se>
2019-02-01 00:16:57 +0100

    small fixes

--- mission/cluster/c_efw_dsi_off_plot.m
+++ mission/cluster/c_efw_dsi_off_plot.m
@@ -1,6 +1,6 @@
 %c_efw_dsi_off_plot
 
-t=toepoch([2001 02 01 00 00 0]):86400:toepoch([2010 01 01 00 00 0]); 
+t=toepoch([2001 02 01 00 00 0]):86400:toepoch([2017 01 01 00 00 0]); 
 t = t' + 600;
 
 
@@ -10,17 +10,19 @@ for cl_id = 1:4
   off=zeros(length(t),2);
   off(:,1) = t;
   for idx=1:length(t)
-    off(idx,2) = c_efw_dsi_off(t(idx),cl_id,[t(idx) -30]);
+    off(idx,2) = c_efw_dsi_off(t(idx),cl_id,'magnetosphere');
   end
   c_eval('msoff?=off;',cl_id);
   clear off
 end
 
 figure
+h = irf_plot(1,'reset');
 c_pl_tx msoff?
 ylabel('Ex offset [mV/m]')
 xlabel('Time [year]')
 legend('C1','C2','C3','C4')
+title('Tail offsets')
 print -dpdf ms_offsets
 
 %% sh offsets
@@ -34,10 +36,12 @@ for cl_id = 1:4
   clear off
 end
 figure
+h = irf_plot(1,'reset');
 c_pl_tx shoff?
 ylabel('Ex offset [mV/m]')
 xlabel('Time [year]')
 legend('C1','C2','C3','C4')
+title('SH/SW offsets')
 print -dpdf sh_offsets
   
   
