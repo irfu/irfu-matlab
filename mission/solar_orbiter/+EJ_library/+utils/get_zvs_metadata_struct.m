98adb07603c7b59fa99ca806796e62c23eb465a2
Erik P G Johansson <erik.johansson@irfu.se>
2020-03-25 22:53:02 +0100

    SO: Updated validate_BIAS_master_CDFs.m
    new file:   ../../../../+EJ_library/+utils/get_zvs_metadata_struct.m
    new file:   ../../../../+EJ_library/+utils/glob_files_dirs.m
    new file:   ../../../../+EJ_library/+utils/recurse_directory_tree.m
    modified:   validate_BIAS_master_CDFs.m

---
+++ mission/solar_orbiter/+EJ_library/+utils/get_zvs_metadata_struct.m
@@ -0,0 +1,181 @@
+%
+% Convert "Variables" and "VariableAttributes" structs for CDF file into a single more easy-to-use struct suitable for
+% looking up metadata for a specific zVariable.
+%
+%
+% NOTES
+% =====
+% NOTE: Some Epoch zVariable attributes are represented in two different ways:
+% * "VariableAttributes": VALIDMIN/-MAX, SCALEMIN/-MAX, FILLVAL on UTC format, i.e. not on the same format as
+%                         the zVariable itself.
+% * "Variables"         : VALIDMIN/-MAX, FILLVAL using the same data type as the zVariable iteself.
+%   NOTE: The above might possibly(?) be a Solar Orbiter RPW dataset bug. This function returns both to be on the safe
+%   side.
+% --
+% NOTE: spdfcdfinfo can return Variables in a struct format using
+%   S = spdfcdfinfo(filePath, 'VARSTRUCT', true)
+% It consists of a 1x1 struct where each field contains the content of one column. That is thus not a replacement for
+% the struct returned by this function.
+% --
+% NOTE: zVariable names and zVariable attribute names do not have to be legal MATLAB struct fieldnames and may
+% contain e.g. blanks. See "CDF USer's Guide".
+% From the spdfcdfinfo help page:
+% """"NOTE: Attribute names which spdfcdfinfo uses for field names in
+%     "GlobalAttributes" and "VariableAttributes" may not match the names
+%     of the attributes in the CDF file exactly.  Because attribute names
+%     can contain characters which are illegal in MATLAB field names, they
+%     may be translated into legal field names.  Illegal characters which
+%     appear at the beginning of attributes are removed; other illegal
+%     characters are replaced with underscores ('_').  If an attribute's
+%     name is modified, the attribute's internal number is appended to the
+%     end of the field name.  For example, '  Variable%Attribute ' might
+%     become 'Variable_Attribute_013'.""""
+% * If a zVariable           has a name that can not be used as a struct fieldname, then this function will not work (assertion).
+% * If a zVariable attribute has a name that can not be used as a struct fieldname, then a modified fieldname will be
+%   used, since zVariable attribute names in "VariableAttributes" are always modified that way.
+%
+%
+% ARGUMENTS
+% =========
+% Variables          : Data structure on the format used by
+%                      D.Variable, where D = spdfcdfinfo(....) (with 'VARSTRUCT' false i.e. the default), and
+%                      D.Variable, where D = dataobj(...) .
+% VariableAttributes : Data structure on the format used by
+%                      D.VariableAttributes, where D = spdfcdfinfo(....), and
+%                      D.VariableAttributes, where D = dataobj(...) .
+%
+%
+% RETURN VALUE
+% ============
+% S : Recursive struct in three levels. Has fields
+%       .(zvName)
+%           .Attributes.(zvAttributeName)   : The content of "VariableAttributes".
+%           .Other.(metadataField)          : The content of "Variables".
+%
+%
+% Author: Erik P G Johansson
+% First created 2020-03-25
+%
+function [S] = get_zvs_metadata_struct(Variables, VariableAttributes)
+    %
+    % NOTE: Returned struct can be naturally generalized to incorporate content if entire CDF file (add global
+    % attributes, zVariable data, and some other metadata from spdfcdfinfo(?)).
+    % 
+    % TODO: Make execute_sw_mode (fill & pad values), validate_BIAS_master_CDFs use function.
+    %
+    % PROPOSAL: Make capable of handling all zVariable names.
+    
+    % ASSERTIONS: Guard against confusing the two arguments.
+    assert(iscell(Variables), 'Argument "Variables" is not a cell array.')
+    assert(isstruct(VariableAttributes))
+
+
+
+    % Correct default value for the special case of having no zVars.
+    S = struct();        
+    
+    
+    
+    %==================
+    % Read "Variables"
+    %==================
+    vTable = Variables;    % Change variable name.
+    for iZv = 1:size(vTable, 1)
+        % NOTE: The meaning of "Variables" columns can be found on the help page for "spdfcdfinfo".
+        zvName         = vTable{iZv,  1};
+        sizeOfRecord   = vTable{iZv,  2};
+        nRecords       = vTable{iZv,  3};
+        dataType       = vTable{iZv,  4};   % NOTE: Not same as MATLAB class.
+        recordVariance = vTable{iZv,  5};
+        sparsity       = vTable{iZv,  6};
+        compression    = vTable{iZv,  7};
+        blockingFactor = vTable{iZv,  8};
+        padValue       = vTable{iZv,  9};
+        FILLVAL        = vTable{iZv, 10};
+        VALIDMIN       = vTable{iZv, 11};
+        VALIDMAX       = vTable{iZv, 12};
+
+        % ASSERTIONS: Check values against assumptions on what they mean.
+        EJ_library.assert.castring(zvName)
+        assert(isnumeric(sizeOfRecord))
+        EJ_library.assert.vector(sizeOfRecord)
+        assert(all(sizeOfRecord >= 1))
+        assert(all(round(sizeOfRecord) == sizeOfRecord))
+        EJ_library.assert.castring(dataType)
+        EJ_library.assert.castring(recordVariance)
+        EJ_library.assert.castring(sparsity)
+        EJ_library.assert.castring(compression)
+        assert(isscalar(blockingFactor) && isnumeric(blockingFactor))
+        assert(isscalar(padValue) || ischar(padValue))
+        assert(isempty(FILLVAL)   || isscalar(FILLVAL)  || ischar(FILLVAL))
+        assert(isempty(VALIDMIN)  || isscalar(VALIDMIN) || ischar(VALIDMIN))
+        assert(isempty(VALIDMAX)  || isscalar(VALIDMAX) || ischar(VALIDMAX))
+        
+        % ~ASSERTION:
+        % IMPLEMENTATION NOTE: Using try-catch to:
+        %   (1) Give proper error message, instead of hard-to-understand error.
+        %   (2) Make sure error happens here, rather than risking any kind of zVar mismatch when reading VariableAttributes.
+        try
+            % IMPLEMENTATION NOTE: Always create empty sub-struct "Attributes", just in case it is not filled with any
+            % zVariable attributes later.
+            S.(zvName) = struct('Other', struct(), 'Attributes', struct());
+        catch Exception
+            error('Argument "Variables" contains a zVariable name "%s" that can not be used as a struct fieldname.', zvName)
+        end
+        
+        % IMPLEMENTATION NOTE: Field "zvName" added because it could be useful if sub-struct is passed around/copied
+        % outside its parent struct.
+        % NOTE: Not including all source variables yet. Add as the are needed.
+        Other = struct();
+        Other.name         = zvName;
+        Other.nRecords     = nRecords;
+        Other.sizeOfRecord = sizeOfRecord;
+        Other.dataType     = dataType;
+        Other.padValue     = padValue;
+        Other.VALIDMIN     = VALIDMIN;
+        Other.VALIDMAX     = VALIDMAX;
+        Other.FILLVAL      = FILLVAL;
+        S.(zvName).Other = Other;
+    end
+
+    
+    
+    % CASE: Sub-structs for all zVars have already been created.
+    
+    
+    
+    %===========================
+    % Read "VariableAttributes"
+    %===========================
+    zvAttrNameList = fieldnames(VariableAttributes);
+    for i = 1:numel(zvAttrNameList)
+        zvAttrName = zvAttrNameList{i};
+        
+        zvAttrTable = VariableAttributes.(zvAttrName);
+        
+        for jZv = 1:size(zvAttrTable, 1)
+            zvName      = zvAttrTable{jZv, 1};
+            zvAttrValue = zvAttrTable{jZv, 2};
+            
+            % ASSERTION: Sub-struct for zVar already exists.
+            % "VariableAttributes" shoud contain a subset of the zVars in "Variables". Do not want to just assume this,
+            % but assert it so as to not mistakenly create more sub-structs.
+            if ~isfield(S, zvName)
+                error(...
+                    'Argument "VariableAttributes" contains reference to a zVariable "%s" not present in argument "Variables".', ...
+                    zvName);
+            end
+            S.(zvName).Attributes.(zvAttrName) = zvAttrValue;
+        end
+    end
+    
+    
+    
+    % Overkill?
+    % ASSERTIONS
+    for iZv = 1:size(vTable, 1)
+        zvName = vTable{iZv,  1};
+        EJ_library.assert.struct(S.(zvName), {'Attributes', 'Other'}, {})
+    end
+    
+end
---
+++ mission/solar_orbiter/+EJ_library/+utils/recurse_directory_tree.m
@@ -0,0 +1,366 @@
+%
+% Generic function for recursing over the files & directories under an arbitrary directory.
+% 
+% Recurses over all, or a subset of, files and directories under a directory subtree ("the root path/directory"). Calls
+% one arbitrary function for every directory and another arbitrary function for every file. A third arbitrary function
+% determines whether the children of any directory should be recursed over (useful for improving speed by avoiding
+% processing).
+%
+%
+%
+% ARGUMENTS
+% =========
+% rootDirPath
+% FileFunc          : Function pointer that is called for every file that is recursed over.
+%                           result = file_func(argStruct).
+%                     result    : An arbitrary value that is passed on to the call for "DirFunc" for the parent
+%                                 directory, via the "childrenResultsList" argument.
+%                     argStruct : Struct with fields
+%                          .rootDirPath
+%                          .relativePath
+%                          .fullPath
+%                          .dirCmdResult
+%                          .recursionDepth
+%
+% DirFunc           : Function pointer, that is called for every directory recursed over, including the root directory
+%                     ("rootDirPath").
+%                           result = dir_func(argStruct)
+%                     result    : An arbitrary value passed on to the call for "DirFunc" for the parent directory. For
+%                                 the root directory, the value is returned by "recurse_directory_tree" itself.
+%                     argStruct : Struct with fields
+%                          .rootDirPath
+%                          .relativePath
+%                          .fullPath
+%                          .dirCmdResult
+%                          .recursionDepth
+%                          .childrenResultsList     : Cell array of "result" values returned from FileFunc and DirFunc
+%                                                     when called for the directory's children.
+%                          .hasRecursedOverChildren : The result of ShouldRecurseFunc for the same directory.
+%                                                     Is used to distinguish the absence of children from the absence of
+%                                                     recursion over children.
+%
+% ShouldRecurseFunc : Function pointer:
+%                           shouldRecurse = should_recurse(argStruct)
+%                     shouldRecurse : True/false. True iff "recurse_directory_tree" should recurse over the children of
+%                                     the specified directory.
+%                                     NOTE: DirFunc will always be called for the directory specified here.
+%                     argStruct     : Struct with fields
+%                          .rootDirPath
+%                          .relativePath
+%                          .fullPath
+%                          .dirCmdResult
+%                          .recursionDepth
+%                     
+% varargin          : Optional settings on format determined by EJ_library.utils.interpret_settings_args .
+%                     useRelativeDirectorySlash : True/false. Whether "relativePath" for directories should have a trailing
+%                                                 slash/backslash or not.
+%                     useRootRelativePathPeriod : True/false. Whether ".relativePath" for rootDirPath (the "root path")
+%                                                 itself should be represented by a period (instead of an empty string).
+%
+%
+%
+% RETURN VALUE
+% ============
+% result            : What DirFunc returns for the root path. Its format and content are determined by FileFunc, DirFunc
+%                     and ShouldRecurseFunc.
+%
+%
+%
+% VARIABLE NAMING CONVENTION
+% ==========================
+% rootDirPath    : Path to "root path/directory", i.e. the root of the subtree to be recursed over.
+% relativePath   : Path to a file/directory relative to "rootDirPath".
+%                  NOTE: Code uses an empty string instead of "." (or "./") to represent the root path.
+% fullPath       : Path to a file/directory beginning with "rootDirPath". Is thus still relative if "rootDirPath" is.
+% dirCmdResult   : The struct with information for this object that is returned by MATLAB's "dir", 
+%                  i.e. NOT information on all the children, only the object itself.
+% recursionDepth : Number of levels into the rootDirPath subdirectory tree. rootDirPath is level zero.
+%                  Files and directories have the same level.
+%
+%
+%
+% NOTE: Function might be used with Lapdog. Should therefore be compatible with MATLAB R2009a.   % How? Why?
+%
+% 
+% DESIGN RATIONALE / IMPLEMENTATION NOTE
+% ======================================
+% The purpose of this function is partly to make it easy to implement many other generic functions.
+% Examples:
+% (1) Constructing a list of files and/or directories under a directory path, incl. selectively.
+% (2) Summing up the file sizes of files in a directory tree.
+% (3) Constructing a recursive data structure analogous to, or based upon, the subtree.
+% (4) Executing code for every file and/or directory in a subtree.
+%     Alt 1) If needes shared state, collect list of paths to file and/or directories. Then iterate over them.
+%     Alt 2) Execute code in FileFunc and/or DirFunc.
+%
+% IMPLEMENTATION NOTE: Function pointers take structs as arguments instead of conventional lists of arguments to,
+% (1) make it easier(?) to write one-line anonymous functions,
+% (2) make it easier to add new (effective) arguments to the function pointers while maintaining backward compatibility
+%     with code that uses recurse_directory_tree,
+% (3) avoid having to remember the order of the arguments when specifying function pointers (both for internal
+%     implementation and for external callers),
+% (4) make it possible to use the same function pointer for both FileFunc and DirFunc (can use "dirCmdResult.isdir" to
+%     distinguish calls to FileFunc and DirFunc). Therefore, argStruct has as many identical field names as possible.
+%
+%
+% HANDLING OF SPECIAL CASES -- NEEDS TO BE CONFIRMED
+% =========================
+% Should work for:
+%    (1) rootDirPath = "/" (Linux).
+%    (2) rootDirPath = Path without the name for the final subdirectory, e.g. ".", "/qwe/asd/.", "/qwe/asd/.."
+%    (3) Empty directory (or subdirectory)
+% Not tested for: 
+%    (1) Symbolic links, hard links, devices etc but it should be possible to derive the behaviour for symbolic links 
+%        using the notes below.
+%
+%
+%
+% NOTE: "dir" COMMAND RESULTS (EXAMPLES) FOR SYMBOLIC LINKS
+% =========================================================
+% Symbolic link to existing file:
+%       name: 'filelink'
+%       date: '28-feb-2017 17:38:21'
+%      bytes: 5
+%      isdir: 0
+%    datenum: 7.3675e+05
+% Symbolic link to existing directory:
+%       name: 'dirlink'
+%       date: '28-feb-2017 17:38:21'
+%      bytes: 0
+%      isdir: 1
+%    datenum: 7.3675e+05
+% Symbolic link to nothing:
+%       name: 'badlink'
+%       date: ''
+%      bytes: []
+%      isdir: 0
+%    datenum: []
+%
+%
+%
+% Initially created by Erik P G Johansson, 2016-11-29.
+%
+function result = recurse_directory_tree(rootDirPath, FileFunc, DirFunc, ShouldRecurseFunc, varargin)
+% GUIDING NEEDS / RATIONALE / MOTIVATION
+% ======================================
+% NEED: Få namn för varje objekt.
+% NEED: Relativa sökvägar för varje objekt.
+% NEED: Få tillgång till (1D) lista med objekt i katalogträd.
+% NEED: "Snabb" kod för stora träd, iaf om bara behöver mindre delar av det.
+%     NEED: Utesluta delar av katalogträd för att snabba upp iteration.
+%         NEED: Utesluta rekursion under vissa kataloger.
+%         NEED: Utesluta rekursion bort visst rekursionsdjup.
+% NEED: Ska kunna användas för att lätt implementera
+%     (1) Ta fram objektlista mha globbing (fixt rekursionsdjup; ~regex).
+%     (2) Producera lista med alla filer, eller filtrerad delmängd därav, under katalog.
+%     (3) Sökning efter fil(er) (baserat på filnamn/plats, innehåll).
+% NEED: Lätt kunna ta fram aggregerande information rekursivt för helt träd.
+%     Ex: Total storlek (bytes).
+%     Ex: Antal filer/kataloger.
+%     Ex: Aggregerade data från flera filer.
+%     (Ex: Lista med objekt (eller urval därav).)
+%     ==> Exekvera kod för varje objekt: fil-->data, katalog+barndata-->data
+
+% PROPOSAL: Returnera full sökväg (~path=rootDirPath+relativePath; utöver relativePath). -- DONE
+%   ~CON: FileFunc, DirFunc, ShouldRecurseFunc can easily put this together themselves.
+%       CON: Triples the code.
+%       CON: Very common that call wants to do something with the full path, not just the relative path.
+% PROPOSAL: Argument för maximalt rekursionsdjup.
+%   CON: Behövs inte. ShouldRecurseFunc kan lätt implementera det om det behövs.
+% PROPOSAL: Tillåt kombination  Settings.useRelativeDirectorySlash && ~Settings.useRootRelativePathPeriod.
+%           Lägg då INTE till snedstreck för rootkatalogen.
+% PROPOSAL: Avskaffa useRelativeDirectorySlash. Aldrig sluta med slash.
+% Se MATLAB-anteckningsfil.
+    
+
+    % ASSERTION
+    if ~exist(rootDirPath, 'dir')
+        error('Can not find directory "%s".', rootDirPath)
+    end
+
+
+    
+    % Set default settings.
+    DEFAULT_SETTINGS.useRelativeDirectorySlash = false;
+    DEFAULT_SETTINGS.useRootRelativePathPeriod = false;
+    Settings = EJ_library.utils.interpret_settings_args(DEFAULT_SETTINGS, varargin);
+
+    % ASSERTION
+    if Settings.useRelativeDirectorySlash && ~Settings.useRootRelativePathPeriod
+        error('Illegal combination of settings.')
+    end
+    
+    % Convert settings into values to actually use (set once here for speed; to avoid repetition).
+    if Settings.useRelativeDirectorySlash ; relativeDirectoryPathSuffix = '/';
+    else                                    relativeDirectoryPathSuffix = '';
+    end
+    if Settings.useRootRelativePathPeriod ; relativePathRoot = '.';    % Should NOT include trailing slash. Is added automatically if needed.
+    else                                    relativePathRoot = '';     % Should NOT include trailing slash. Is added automatically if needed.
+    end
+
+
+
+    dirCmdResultRootPath = get_dir_cmd_result_for_single_object(rootDirPath);
+
+    % NOTE: Using empty string to represent relative path to rootDirPath instead of period. The string is used for is
+    % used for building other relative paths (with "fullfile"). If one uses '.', or './' then one gets "ugly" relative
+    % paths beginning with "./" which is unnecessary for the actual children of the directory.
+    result = recurse_directory_tree_INTERNAL(...
+        rootDirPath, '', ...
+        dirCmdResultRootPath, 0, ...
+        FileFunc, DirFunc, ShouldRecurseFunc, ...
+        relativeDirectoryPathSuffix, relativePathRoot);
+end
+
+
+
+% Function that should be called for every subdirectory, and never for files.
+%
+% ARGUMENTS
+% =========
+% rootDirPath         : The path to the root of the ENTIRE recursion. This value stays constant for all calls.
+% relativePath        : Path to directory relative to "rootDirPath".
+%                       NOTE: Uses empty string to represent "rootDirPath".
+%                       NOTE: Never has trailing slash/backslash.
+% dirCmdResultCurrent : The "dir" command result (data, record) that pertains to this specific directory, i.e. NOT
+%                       the data/records for all of its immediate children (files and directories) contained in it.
+% recursionDepth      : Zero for the first call, i.e. for rootDirPath (relativePath empty). Incremented by one for every
+%                       step into the directory structure.
+% FileFunc, DirFunc, ShouldRecurseFunc : Same as arguments to main function.
+% 
+% result              : [] (not cell) if ShouldRecurseFunc returns false for this directory.
+% 
+function result = recurse_directory_tree_INTERNAL(...
+        rootDirPath, relativePath, ...
+        dirCmdResultCurrent, recursionDepth, ...
+        FileFunc, DirFunc, ShouldRecurseFunc, ...
+        relativeDirectoryPathSuffix, relativePathRoot)
+    
+    % "dir" command results to remove/ignore, as identified by their .name fields.
+    NON_CHILDREN_NAMES = {'.', '..'};
+    
+    % Value to return when shouldRecurse == false
+    NO_RECURSE_DIRECTORY_RESULT = {};
+
+    %fprintf(1, 'BEGIN: recurse_directory_tree_INTERNAL("%s")\n', relativePath)
+    
+    
+    % Set "relativeDirPath". Only used for calling ShouldRecurseFunc and DirFunc.
+    if recursionDepth == 0
+        relativeDirPath = [relativePathRoot, relativeDirectoryPathSuffix];
+    else
+        relativeDirPath = [relativePath,     relativeDirectoryPathSuffix];
+    end
+    fullDirPath = fullfile(rootDirPath, relativeDirPath);
+    
+    
+    
+    %========================
+    % Call ShouldRecurseFunc
+    %========================
+    willRecurseOverChildren = ShouldRecurseFunc(struct(...
+        'rootDirPath',    rootDirPath, ...
+        'relativePath',   relativeDirPath, ...
+        'fullPath',       fullDirPath, ...
+        'dirCmdResult',   dirCmdResultCurrent, ...
+        'recursionDepth', recursionDepth));
+
+    %=============================================
+    % Derive "childrenResultsList" is supposed to.
+    %=============================================
+    if willRecurseOverChildren
+
+        %==========================================================
+        % Get information on the children of the current directory
+        %==========================================================
+        currentFullPath   = fullfile(rootDirPath, relativePath);
+        dirCmdResultsList = dir(currentFullPath);                 % Returns column vector (of structs).
+        % Remove non-children from dir command results.
+        iDelete = ismember({dirCmdResultsList.name}, NON_CHILDREN_NAMES);
+        dirCmdResultsList(iDelete) = [];
+
+        %=======================================
+        % Iterate over the directory's children
+        %=======================================
+        childrenResultsList  = cell(length(dirCmdResultsList), 1);
+        for iChild = 1:length(dirCmdResultsList)
+            dirCmdResultChild = dirCmdResultsList(iChild);
+            childRelativePath = fullfile(relativePath, dirCmdResultChild.name);
+
+            if dirCmdResultChild.isdir
+                % CASE: Child is a directory.
+
+                %================
+                % RECURSIVE CALL
+                %================
+                childrenResultsList{iChild} = recurse_directory_tree_INTERNAL(...
+                    rootDirPath, childRelativePath, ...
+                    dirCmdResultChild, recursionDepth+1, ...
+                    FileFunc, DirFunc, ShouldRecurseFunc, ...
+                    relativeDirectoryPathSuffix, relativePathRoot);
+            else
+                % CASE: Child is a file.
+
+                %===============
+                % Call FileFunc
+                %===============
+                %fprintf(1, 'Calling FileFunc("%s")\n', childRelativePath)
+                %childrenResultsList{iChild} = FileFunc(rootDirPath, childRelativePath, dirCmdResultChild);
+                childrenResultsList{iChild} = FileFunc(struct(...
+                    'rootDirPath',    rootDirPath, ...
+                    'relativePath',   childRelativePath, ...
+                    'fullPath',       fullfile(rootDirPath, childRelativePath), ...
+                    'dirCmdResult',   dirCmdResultChild, ...
+                    'recursionDepth', recursionDepth+1));
+            end
+        end
+
+    else
+        childrenResultsList = NO_RECURSE_DIRECTORY_RESULT;
+    end
+
+    %fprintf(1, 'Calling DirFunc("%s")\n', relativePath)
+    
+    %==============
+    % Call DirFunc
+    %==============
+    % NOTE: Requires cell braces around "childrenResultsList" to prevent "struct" from creating an analogous array of
+    % structs. (Other fields are identical for all array components.)
+    result = DirFunc(struct(...
+        'rootDirPath',              rootDirPath, ...
+        'relativePath',             relativeDirPath, ...
+        'fullPath',                 fullDirPath, ...
+        'dirCmdResult',             dirCmdResultCurrent, ...
+        'recursionDepth',           recursionDepth, ...
+        'childrenResultsList',      {childrenResultsList}, ...
+        'hasRecursedOverChildren',  willRecurseOverChildren));
+
+    %fprintf(1, 'END:   recurse_directory_tree_INTERNAL("%s")\n', relativePath)
+end
+
+
+
+% Return information for specific directory using "dir" (i.e. NOT information for all the children).
+% Utility function for clarifying the code.
+%
+% Should be called only once, for the root directory.
+function dirCmdResult = get_dir_cmd_result_for_single_object(path)
+    % TODO-DECISION: How handle "/"?
+    
+    % IMPLEMENTATION NOTE: Need correct path to later determine the name of the directory (e.g. for relative paths, "/", "..").
+    % EJ_library.utils.get_abs_path might not be enough.
+    absPath = EJ_library.utils.get_abs_path(path);   
+    
+    dirCmdResultsList = dir(absPath);
+
+    % Replace "." (current directory) with the actual name of the current directory.
+    [junk, baseName, ext] = fileparts(absPath);   % IMPLEMENTATION NOTE: Keep compatible with MATLAB R2009a ==> Do NOT use "~" notation.
+    iPath = find(strcmp({dirCmdResultsList.name}, '.'));
+    dirCmdResult = dirCmdResultsList(iPath);
+    
+    % IMPLEMENTATION NOTE: Empirically, "dir" returns zero entries for non-readable directories WITHOUT THROWING ANY
+    % EXCEPTION. This can give very non-intuitive errors.
+    assert(length(dirCmdResult) == 1, '"dir" returned zero entries for "%s". Non-existent? No read permissions?', absPath)
+    dirCmdResult.name = [baseName, ext];            
+end
+
