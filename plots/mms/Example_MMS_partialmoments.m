160387d9a5e1c7d93d7b56c2568976523cc6457a
Daniel Graham <dgraham@irfu.se>
2020-06-05 15:46:47 +0200

    Update Example_MMS_partialmoments.m

--- plots/mms/Example_MMS_partialmoments.m
+++ plots/mms/Example_MMS_partialmoments.m
@@ -1,39 +1,46 @@
 % Examples of Partial moments calculations using mms.psd_moments.
 
 %% Example 1. Compute moments of ions with positive and negative V_x individually
+% Load ion data
+ic = 1; % Spacecraft number
+Tint = irf.tint('2015-11-04T04:57:50.00Z/2015-11-04T04:58:30.00Z');
+c_eval('iPDist = mms.get_data(''PDi_fpi_brst_l2'',Tint,?);',ic);
+c_eval('SCpot = mms.get_data(''V_edp_brst_l2'',Tint,?);',ic);
 
-% Load ion skymap data...
-
-x = zeros(length(dist.time),32,16);
-for ii = 1:length(dist.time)
-  x(ii,:,:) = -cosd(phi.data(ii,:)')*sind(theta.data);
+x = zeros(length(iPDist.time),32,16);
+for ii = 1:length(iPDist.time)
+  x(ii,:,:) = -cosd(iPDist.depend{1,2}(ii,:)')*sind(iPDist.depend{1,3});
 end
 x = repmat(x,1,1,1,32);
 x = squeeze(permute(x,[1 4 2 3]));
 xpositive = zeros(size(x)); xnegative = zeros(size(x));
 xpositive(x > 0) = 1; xnegative(x < 0) = 1;
 
-imoms = mms.psd_moments(dist,phi,theta,stepTable,energy0,energy1,SCpot,'ion');
-imomspos = mms.psd_moments(dist,phi,theta,stepTable,energy0,energy1,SCpot,'ion','partialmoms',xpositive);
-imomsneg = mms.psd_moments(dist,phi,theta,stepTable,energy0,energy1,SCpot,'ion','partialmoms',xnegative);
+imoms = mms.psd_moments(iPDist,SCpot);
+imomspos = mms.psd_moments(iPDist,SCpot,'partialmoms',xpositive);
+imomsneg = mms.psd_moments(iPDist,SCpot,'partialmoms',xnegative);
 
 %% Example 2. Compute electron moments for pitch-angles < 90 deg and > 90 deg.
 
-% Load skymap data and B field
+ic = 1; % Spacecraft number
+Tint = irf.tint('2015-12-06T23:38:27.00Z/2015-12-06T23:38:37.00Z');
+c_eval('ePDist = mms.get_data(''PDe_fpi_brst_l2'',Tint,?);',ic);
+c_eval('SCpot = mms.get_data(''V_edp_brst_l2'',Tint,?);',ic);
+c_eval('Bxyz = mms.get_data(''B_dmpa_brst_l2'',Tint,?);',ic);
 
-Bxyz = Bxyz.resample(dist);
+Bxyz = Bxyz.resample(ePDist);
 Bvec = Bxyz/Bxyz.abs;
 Bvecx = repmat(Bvec.data(:,1),1,32,32,16);
 Bvecy = repmat(Bvec.data(:,2),1,32,32,16);
 Bvecz = repmat(Bvec.data(:,3),1,32,32,16);
-x = zeros(length(dist.time),32,16);
-y = zeros(length(dist.time),32,16);
-z = zeros(length(dist.time),32,16);
+x = zeros(length(ePDist.time),32,16);
+y = zeros(length(ePDist.time),32,16);
+z = zeros(length(ePDist.time),32,16);
 
-for ii = 1:length(dist.time)
-  x(ii,:,:) = -cosd(phi.data(ii,:)')*sind(theta.data);
-  y(ii,:,:) = -sind(phi.data(ii,:)')*sind(theta.data);
-  z(ii,:,:) = -ones(32,1)*cosd(theta.data);
+for ii = 1:length(ePDist.time)
+  x(ii,:,:) = -cosd(ePDist.depend{1,2}(ii,:)')*sind(ePDist.depend{1,3});
+  y(ii,:,:) = -sind(ePDist.depend{1,2}(ii,:)')*sind(ePDist.depend{1,3});
+  z(ii,:,:) = -ones(32,1)*cosd(ePDist.depend{1,3});
 end
 x = repmat(x,1,1,1,32);
 x = squeeze(permute(x,[1 4 2 3]));
@@ -47,12 +54,12 @@ thetab = acosd(x.*Bvecx+y.*Bvecy+z.*Bvecz);
 pitchl90 = zeros(size(x)); pitchg90 = zeros(size(x));
 pitchl90(thetab < 90) = 1; pitchg90(thetab > 90) = 1;
 
-emoms = mms.psd_moments(dist,phi,theta,stepTable,energy0,energy1,SCpot,'electron');
-emomsl90 = mms.psd_moments(dist,phi,theta,stepTable,energy0,energy1,SCpot,'electron','partialmoms',pitchl90);
-emomsg90 = mms.psd_moments(dist,phi,theta,stepTable,energy0,energy1,SCpot,'electron','partialmoms',pitchg90);
+emoms = mms.psd_moments(ePDist,SCpot);
+emomsl90 = mms.psd_moments(ePDist,SCpot,'partialmoms',pitchl90);
+emomsg90 = mms.psd_moments(ePDist,SCpot,'partialmoms',pitchg90);
 
-ne = TSeries(emoms.n_psd.time,[emoms.n_psd.data emomsl90.n_psd.data emomsg90.n_psd.data]);
-Te = irf.ts_scalar(emoms.T_psd.time,(emoms.T_psd.data(:,1)+emoms.T_psd.data(:,4)+emoms.T_psd.data(:,3))/3);
-Tel90 = irf.ts_scalar(emomsl90.T_psd.time,(emomsl90.T_psd.data(:,1)+emomsl90.T_psd.data(:,4)+emomsl90.T_psd.data(:,3))/3);
-Teg90 = irf.ts_scalar(emomsg90.T_psd.time,(emomsg90.T_psd.data(:,1)+emomsg90.T_psd.data(:,4)+emomsg90.T_psd.data(:,3))/3);
+neall = TSeries(emoms.n_psd.time,[emoms.n_psd.data emomsl90.n_psd.data emomsg90.n_psd.data]);
+Te = emoms.T_psd.trace/3;
+Tel90 = emomsl90.T_psd.trace/3;
+Teg90 = emomsg90.T_psd.trace/3;
 Te = TSeries(Te.time,[Te.data Tel90.data Teg90.data]);
