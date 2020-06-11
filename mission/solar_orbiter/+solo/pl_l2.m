ade2a6397a075187f9eb0af78e41ab93cdbd6d64
Yuri Khotyaintsev <yuri@irfu.se>
2020-06-11 15:12:38 +0200

    update to new var names

--- mission/solar_orbiter/+solo/pl_l2.m
+++ mission/solar_orbiter/+solo/pl_l2.m
@@ -4,20 +4,23 @@ function hout = pl_l2(fName)
 % H = PL_L2(fName)
 
 %fName = 'solo_L2_rpw-lfr-surv-cwf-e-cdag_20200312_V01.cdf';
+%% load data
 d =dataobj(fName);
 
-El2 = get_ts(d,'E'); El2.units = 'V';
-Vl2 = get_ts(d,'V');
+El2 = get_ts(d,'EDC');
+Vl2 = get_ts(d,'VDC');
 
-%%
+%% plot data
 
 h = irf_plot(2,'reset');
 
 irf_plot(h(1),El2);
 legend(h(1),'V12','V13','V23')
-title(h(1),fName,'Interpreter','none')
+[~,fName_s,~] = fileparts(fName);
+title(h(1),fName_s,'Interpreter','none')
 
 irf_plot(h(2),Vl2);
 legend(h(2),'V1','V2','V3')
 
+%%
 if nargout, hout = h; end 
