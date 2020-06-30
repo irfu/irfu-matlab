35d66681413121827fa8c46aefae7b23adcd90ab
Yuri Khotyaintsev <yuri@irfu.se>
2020-06-30 10:39:38 +0200

    catchup with var renaming

--- mission/solar_orbiter/+solo/pl_l2.m
+++ mission/solar_orbiter/+solo/pl_l2.m
@@ -6,18 +6,18 @@ function hout = pl_l2(fName)
 %fName = 'solo_L2_rpw-lfr-surv-cwf-e-cdag_20200312_V01.cdf';
 d =dataobj(fName);
 
-El2 = get_ts(d,'E'); El2.units = 'V';
-Vl2 = get_ts(d,'V');
+EDC = get_ts(d,'EDC'); 
+VDC = get_ts(d,'VDC');
 
 %%
 
 h = irf_plot(2,'reset');
 
-irf_plot(h(1),El2);
+irf_plot(h(1),EDC);
 legend(h(1),'V12','V13','V23')
 title(h(1),fName,'Interpreter','none')
 
-irf_plot(h(2),Vl2);
+irf_plot(h(2),VDC);
 legend(h(2),'V1','V2','V3')
 
 if nargout, hout = h; end 
