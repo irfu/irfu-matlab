f4b66e08951a6292cc3b06a44658ae4799b6078f
Niklas Edberg <ne@irfu.se>
2020-10-19 10:48:15 +0200

    Add the possibility to change coordinate frame.

--- mission/solar_orbiter/+solo/get_position.m
+++ mission/solar_orbiter/+solo/get_position.m
@@ -1,12 +1,18 @@
-function soloPosition = get_position(tint);
+function soloPosition = get_position(tint, frame_in);
 % A function to get the position of Solar Orbiter for a given time interval
 % Output is a TSeries object with position in the data variable "pos",
 % if any positons found for the time interval "tint".
 %
 % Example
 % tint = irf.tint('2020-07-31T00:00:00Z/2020-08-01T15:00:00Z'); % time interval in TT2000 UTC
-% soloPosition = solo.get_position(tint);
+% soloPosition = solo.get_position(tint, [frame]); The default frame is 'ECLIPJ2000'
 % irf_plot(soloPosition, '.');
+
+if nargin == 2, 
+    frame=frame_in;
+else
+   frame='ECLIPJ2000';
+end 
 
 t_step = 60*60; %3600 sec
 
@@ -24,8 +30,8 @@ cspice_furnsh(kernelFile);
 et = tint.start.tts:t_step:tint.stop.tts;
 
 % The position of Solar Orbiter in units of km
-pos = cspice_spkpos('solo', et, 'ECLIPJ2000', 'LT+S', 'Sun');
-% More frames ('ECLIPJ2000') described in/share/SPICE/Solar-Orbiter/kernels/fk/solo_ANC_soc-sci-fk_V06.tf
+pos = cspice_spkpos('solo', et, frame, 'LT+s', 'Sun');
+% More frames (instead of 'ECLIPJ2000') described in/share/SPICE/Solar-Orbiter/kernels/fk/solo_ANC_soc-sci-fk_V06.tf
 
 % Convert to utc and then to TT2000 in TSeries object
 utc_tmp = cspice_et2utc(et, 'ISOC', 0);
@@ -33,5 +39,5 @@ utc_tmp = cspice_et2utc(et, 'ISOC', 0);
 % Note pos' since it is returned as 3xN but TSeries expexcts Nx3 (where N is number of records).
 soloPosition = irf.ts_vec_xyz(EpochTT(utc_tmp), pos');
 soloPosition.units = 'km'; % Add some metadata information (read when plotting in irf_plot)
-
+soloPosition.coordinateSystem=frame;
 end
