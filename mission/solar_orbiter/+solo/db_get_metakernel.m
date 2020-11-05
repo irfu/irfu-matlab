99f082c30914806163cb21aa1eb54fa3862a3528
thomas-nilsson-irfu <thomas-nilsson-irfu@users.noreply.github.com>
2020-11-05 10:55:27 +0100

    Add function "solo.db_get_metakernel"
    to get a local copy of the latest* metakernel adapted with locally
    mounted paths to the SPICE files.
    Usage: solo.db_init('local_file_db','/data/solo/mounted');
    solo_metakernel = solo.db_get_metakernel('flown');
    cspice_furnsh(solo_metakernel);
    % And then do some SPICE computations...
    or
    solo_metakernel = solo.db_get_metakernel('predicted');
    
    * Note is syncing done elsewhere by my cronjob for IRFU, by cloning
    https://repos.cosmos.esa.int/socci/projects/SPICE_KERNELS/repos/solar-orbiter/browse
    into a folder "SPICE" in the root of a SOLO database.
    
    * Note 2: This script is not yet adapted for Windows systems.

---
+++ mission/solar_orbiter/+solo/db_get_metakernel.m
@@ -0,0 +1,13 @@
+function solo_metakernel = db_get_metakernel(flown_or_predicted)
+% Function to get a locally adapted SPICE metakernel for SolO (assuming
+% folder "SPICE" is a subfolder of the database root).
+% flown_or_predicted = 'flown' return metakernel of actually
+%                       flown orbit only
+% flown_or_predicted = 'predicted' return metakernel of latest orbit
+%                      prediction (while still using reconstructed flown 
+%                      orbit information as much as possible).
+narginchk(1,2)
+global SOLO_DB; if isempty(SOLO_DB), solo.db_init(), end
+
+solo_metakernel = SOLO_DB.get_metakernel(flown_or_predicted);
+end
