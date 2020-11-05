a8478aacfaef6f69876a5622d11301d21e602bbf
Yuri Khotyaintsev <yuri@irfu.se>
2020-11-05 17:05:25 +0100

    add help

--- mission/solar_orbiter/+solo/db_get_metakernel.m
+++ mission/solar_orbiter/+solo/db_get_metakernel.m
@@ -1,6 +1,11 @@
 function solo_metakernel = db_get_metakernel(flown_or_predicted)
+% SOLO.DB_GET_METAKERNEL  Get the local SolO SPICE metakernel to load
+%
+% solo_metakernel = solo.db_get_metakernel(flown_or_predicted)
+%
 % Function to get a locally adapted SPICE metakernel for SolO (assuming
 % folder "SPICE" is a subfolder of the database root).
+%
 % flown_or_predicted = 'flown' return metakernel of actually
 %                       flown orbit only
 % flown_or_predicted = 'predicted' return metakernel of latest orbit
