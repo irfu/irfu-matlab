7fafee18780f6314b39df7159dd2eef03c98c65a
Yuri Khotyaintsev <yuri@irfu.se>
2020-11-05 17:34:53 +0100

    add help, options and use solo.db_get_metakernel()

--- mission/solar_orbiter/+solo/get_velocity.m
+++ mission/solar_orbiter/+solo/get_velocity.m
@@ -1,34 +1,93 @@
-function soloVelocity = get_velocity(tint, frame_in);
+function soloVelocity = get_velocity(Tint, varargin)
+%SOLO.GET_VELOCITY  Get the velocity of Solar Orbiter 
+%
+% soloVelocity = solo.get_velocity(tint, PARAMS)
+%
+%
 % A function to get the Velocity of Solar Orbiter for a given time interval
 % Output is a TSeries object with velocity in the data variable "soloVelocity",
 % if any positons found for the time interval "tint".
 %
-% Example
+% Options:
+%    'predicted' - get predicted position instead of flown
+%    'frame' - frame of the data (see below, default: ECLIPJ2000)
+% 
+% The following generic frames are defined:
+%
+%      SPICE Frame Name            Long-name
+%      -------------------------   --------------------------------------------
+%
+%   SOLO mission specific generic frames:
+%
+%      SOLO_SUN_RTN                Sun Solar Orbiter Radial-Tangential-Normal
+%      SOLO_SOLAR_MHP              S/C-centred mirror helioprojective
+%      SOLO_IAU_SUN_2009           Sun Body-Fixed based on IAU 2009 report
+%      SOLO_IAU_SUN_2003           Sun Body-Fixed based on IAU 2003 report
+%      SOLO_GAE                    Geocentric Aries Ecliptic at J2000 (GAE)
+%      SOLO_GSE                    Geocentric Solar Ecliptic at J2000 (GSE)
+%      SOLO_HEE                    Heliocentric Earth Ecliptic at J2000 (HEE)
+%
+%   Heliospheric Coordinate Frames developed for the NASA STEREO mission:
+%
+%      SOLO_ECLIPDATE              Mean Ecliptic of Date Frame
+%      SOLO_HCI                    Heliocentric Inertial Frame
+%      SOLO_HEE_NASA               Heliocentric Earth Ecliptic Frame
+%      SOLO_HEEQ                   Heliocentric Earth Equatorial Frame
+%      SOLO_HEEQ                   Heliocentric Earth Equatorial Frame
+%      SOLO_GEORTN                 Geocentric Radial Tangential Normal Frame
+%
+%   Heliocentric Generic Frames(*):
+%
+%      SUN_ARIES_ECL               Heliocentric Aries Ecliptic   (HAE)
+%      SUN_EARTH_CEQU              Heliocentric Earth Equatorial (HEEQ)
+%      SUN_EARTH_ECL               Heliocentric Earth Ecliptic   (HEE)
+%      SUN_INERTIAL                Heliocentric Inertial         (HCI)
+%
+%   Geocentric Generic Frames:
+%
+%      EARTH_SUN_ECL   (*)         Geocentric Solar Ecliptic     (GSE)
+%      EARTH_MECL_MEQX (*)         Earth Mean Ecliptic and Equinox of date
+%                                  frame (Auxiliary frame for EARTH_SUN_ECL)
+%      EARTH_MECL_MEQX_J2000       Earth Mean Ecliptic and Equinox at J2000
+%                                  frame (Auxiliary frame for SOLO_GSE and
+%                                  SOLO_HEE)
+% 
+% Example:
 % tint = irf.tint('2020-02-11T00:00:00Z/2020-08-01T15:00:00Z'); % time interval in TT2000 UTC
 % soloVelocity = solo.get_velocity(tint, [frame]); The default frame is 'ECLIPJ2000'
 % irf_plot(soloVelocity, '.');
 
 
-if nargin == 2 
-    frame=frame_in;
-else
-   frame='ECLIPJ2000';
-end 
+frame = 'ECLIPJ2000';
+flagFlownPredicted = 'flown';
+
+if nargin > 1, have_options = 1; args = varargin;
+else, have_options = 0;
+end
+while have_options
+  l = 1;
+  switch lower(args{1})
+    case 'frame'
+      frame = args{2}; l=2;
+    case 'flown'
+      flagFlownPredicted = 'flown';
+    case 'predicted'
+      flagFlownPredicted = 'predicted';
+    otherwise
+      irf_log('fcal,',['Option ''' args{1} ''' not recognized'])
+  end
+  if length(args) > l, args = args(l+1:end);
+  else, break
+  end
+end
 
 t_step = 60*60; %3600 sec
 
-% Find the most recent metakernel file and load it
-sharedPath = '/share/SPICE/'; % SPICE kernels for different missions are found in this folder on IRFU servers
-dirs = dir([sharedPath,'Solar-Orbiter/kernels/mk/*pred-mk_v*.tm']);
-if size(dirs, 1) > 1
-  % Multiple kernels could be found if executing this script at the same time as syncing new kernel files
-  error('Found multiple metakernels, please check your folder.');
-end
-kernelFile = [dirs.folder, filesep, dirs.name];
-cspice_furnsh(kernelFile);
+solo_metakernel = solo.db_get_metakernel(flagFlownPredicted);
+cspice_furnsh(solo_metakernel);
 
 % Compute et (SPICE ephemeries time, make use of input in TT2000)
-et = tint.start.tts:t_step:tint.stop.tts;
+et = Tint.start.tts:t_step:Tint.stop.tts;
 
 % The position of Solar Orbiter in units of km
 posvel = cspice_spkezr('solo', et, frame, 'LT+s', 'Sun'); %This gives both position and velocity
