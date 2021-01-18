810769356bcbf407389eb3740233fa653ac814eb
Erik P G Johansson <erik.johansson@irfu.se>
2021-01-18 15:03:24 +0100

    BICAS: vdccal.m: Derive density when having only one probe
    Making vdccal.m able to derive density (not E field)
    for case of having only V1_DC by using
    the same value for the other probes in calculations.
    
    modified:   +solo/vdccal.m

--- mission/solar_orbiter/+solo/vdccal.m
+++ mission/solar_orbiter/+solo/vdccal.m
@@ -20,6 +20,7 @@ function [DCE_SRF,PSP,ScPot,codeVerStr,matVerStr] = vdccal(VDC,EDC)
 
 a = load('d23K123.mat');
 
+%===========================================================================
 % Date strings that represent the version of calibration. These strings are
 % used by BICAS to set a CDF global attribute in official datasets for
 % traceability.
@@ -27,11 +28,34 @@ a = load('d23K123.mat');
 % Version of the function (not .mat file).
 % NOTE: This value is meant to be be updated by hand, not by an automatic
 % timestamp, so that a constant value represents the same function.
+%===========================================================================
 codeVerStr = '2020-11-11T19:10:17';
 % Version of the .mat file. Not yet used. Meant to be read from .mat file.
 matVerStr  = [];
 
-d23R = a.d23.resample(VDC);
+%=============================================================================
+% "Normalize" input data, i.e. data on any one of two legitimate formats is
+% converted to one common format so that algorithms for deriving spacecraft
+% potential and E field afterwards do not need to handle two different cases.
+% -
+% vdccal() only uses DC data (not AC). For mux=0, there are two cases:
+% (1) all single probes (DC) available
+%     ==> Keep data as it is.
+% (2) only probe 1 (DC) available.
+%     ==> Use V1_DC as value also for V2_DC & V3_DC.
+%     NOTE: Explicitly sets DCE_SRF=NaN for this case since deriving E-field
+%           makes no sense (not even setting E=0).
+% NOTE: This code only works for mux=0 and fails safely for other (produces no
+%       data).
+% NOTE: Does not modify EDC, since it is only used for calculation of DCE_SRF.
+%==============================================================================
+bReconstr = isnan(VDC.y.data) & isnan(VDC.z.data);
+VDC.data(bReconstr, 2) = VDC.x.data(bReconstr);
+VDC.data(bReconstr, 3) = VDC.x.data(bReconstr);
+
+
+
+d23R  = a.d23.resample(VDC);
 K123R = a.K123.resample(VDC);
 
 V2corr = double(VDC.y.data) -double(d23R.data);
@@ -42,7 +66,9 @@ V23corr = (V23.*K123R.data(:,1) + K123R.data(:,2));
 PSP = irf.ts_scalar(VDC.time,(V23corr + double(VDC.x.data))/2);
 PSP.units = 'V';
 
-PLASMA_POT = 1.5; SHORT_FACTOR = 2.5; % XXX: these are just adhoc numbers
+
+
+PLASMA_POT = 1.5; SHORT_FACTOR = 2.5; % XXX: these are just ad hoc numbers.
 
 ScPot = irf.ts_scalar(VDC.time, -(PSP.data-PLASMA_POT)*SHORT_FACTOR);
 ScPot.units = PSP.units;
@@ -60,4 +86,7 @@ Ez_SRF = Ez_SRF*1e3/6.97;
 DCE_SRF = irf.ts_vec_xyz(VDC.time,[Ey_SRF*0 Ey_SRF Ez_SRF]);
 DCE_SRF.units = 'mV/m';
 DCE_SRF.coordinateSystem = 'SRF';
+
+DCE_SRF.data(bReconstr,:) = NaN;
+
 
