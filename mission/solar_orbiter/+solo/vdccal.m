6db0efd5b921f823002788561cadd812bbb85caa
Erik P G Johansson <erik.johansson@irfu.se>
2021-01-26 12:09:35 +0100

    BICAS: vdccal.m: PSP for single-probe data (other impl./algo.)
    New implementation: Uses PSP=V1_DC for single-probe.
    (Not V3_DC := V2_DC := V1_DC)
    
    Likely implicit bugfix for bug introduced at merge devel-to-SOdevel: E field for single-probe

--- mission/solar_orbiter/+solo/vdccal.m
+++ mission/solar_orbiter/+solo/vdccal.m
@@ -32,7 +32,7 @@ a = load('d23K123_20210124');
 % NOTE: This value is meant to be be updated by hand, not by an automatic
 % timestamp, so that a constant value represents the same function.
 %===========================================================================
-codeVerStr = '2020-12-17T08:58:00';
+codeVerStr = '2021-01-26T12:00:00';
 % Version of the .mat file. Not yet used. Meant to be read from .mat file.
 matVerStr  = [];
 
@@ -45,24 +45,16 @@ cc = a.CC;
 
 
 %=============================================================================
-% "Normalize" input data, i.e. data on any one of two legitimate formats is
-% converted to one common format so that algorithms for deriving spacecraft
-% potential and E field afterwards do not need to handle two different cases.
-% -
-% vdccal() only uses DC data (not AC). For mux=0, there are two cases:
+% Find data points/CDF records for which only V1_DC is available
+%
+% NOTE: solo.vdccal() only uses DC data (not AC). For mux=0, there are two
+% cases:
 % (1) all single probes (DC) available
-%     ==> Keep data as it is.
 % (2) only probe 1 (DC) available.
-%     ==> Use V1_DC as value also for V2_DC & V3_DC.
-%     NOTE: Explicitly sets DCE_SRF=NaN for this case since deriving E-field
-%           makes no sense (not even setting E=0).
-% NOTE: This code only works for mux=0 and fails safely for other (produces no
-%       data).
-% NOTE: Does not modify EDC, since it is only used for calculation of DCE_SRF.
+% NOTE: Only works for mux=0,2,3,4 (not mux=1).
+% NOTE: Ignores EDC argument.
 %==============================================================================
-bReconstr = isnan(VDC.y.data) & isnan(VDC.z.data);
-VDC.data(bReconstr, 2) = VDC.x.data(bReconstr);
-VDC.data(bReconstr, 3) = VDC.x.data(bReconstr);
+bSingleProbe = isnan(VDC.y.data) & isnan(VDC.z.data);
 
 
 
@@ -81,7 +73,12 @@ V23 = (V2cmr + V3cmr)/2; % (V2cmr + V3) /2
 
 V23corr = (V23.*K123R.data(:,1) + K123R.data(:,2)); %Correcting V23 to V1
 
+
 PSP = irf.ts_scalar(VDC.time,(V23corr + double(VDC.x.data))/2); %Compute PSP from corrected quantities.
+
+% Use alternate, simpler "calculation" for single-probe data.
+PSP.data(bSingleProbe) = VDC.x.data(bSingleProbe);
+
 PSP.units = 'V';
 
 
@@ -104,7 +101,3 @@ Ez_SRF = Ez_SRF*1e3/6.97;
 DCE_SRF = irf.ts_vec_xyz(VDC.time,[Ey_SRF*0 Ey_SRF Ez_SRF]);
 DCE_SRF.units = 'mV/m';
 DCE_SRF.coordinateSystem = 'SRF';
-
-DCE_SRF.data(bReconstr,:) = NaN;
-
-
