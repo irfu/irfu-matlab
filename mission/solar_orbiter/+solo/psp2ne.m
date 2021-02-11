6512550358c1eee9a1b610a532c360a3a61b3a1e
Yuri Khotyaintsev <yuri@irfu.se>
2021-02-11 15:52:29 +0100

    change to exp()

--- mission/solar_orbiter/+solo/psp2ne.m
+++ mission/solar_orbiter/+solo/psp2ne.m
@@ -25,66 +25,61 @@ function [NeScp, codeVerStr] = psp2ne(PSP)
 % NOTE: This value is meant to be be updated by hand, not by an automatic
 % timestamp, so that a constant value represents the same algorithm.
 %===========================================================================
-codeVerStr = '2021-02-09T17:13:00';
-
+codeVerStr = '2021-02-11T15:35:00';
 
 
 % Based on data from 2020-04-07
 CalEntry = irf.ts_vec_xy(...
   irf.tint('2020-03-08T00:00:00Z/2020-05-18T04:05:54Z'),...
-  repmat([0.3835   1.4908],2,1));
-
+  repmat([0.8889   3.4389],2,1));
 Cal = CalEntry;
 
 CalEntry = irf.ts_vec_xy(...
   irf.tint('2020-05-18T04:05:55Z/2020-05-29T23:59:59Z'),...
-  repmat([0.3539   1.9785],2,1));
-
+  repmat([0.8154   4.5562],2,1));
 Cal = Cal.combine(CalEntry);
 
 % cal based up to 2020-07-05T23:59:59Z
 CalEntry = irf.ts_vec_xy(...
   irf.tint('2020-05-30T00:00:00Z/2020-08-11T21:27:02Z'),...
-  repmat([0.2260   1.9106],2,1));
-
+  repmat([0.5310   4.4010],2,1));
 Cal = Cal.combine(CalEntry);
 
 % data until Sept 4
 CalEntry = irf.ts_vec_xy(...
   irf.tint('2020-08-11T21:27:03Z/2020-09-04T23:59:59Z'),...
-  repmat([0.2863  1.6844],2,1));
-
+  repmat([0.6593  3.8785],2,1));
 Cal = Cal.combine(CalEntry);
 
 % data for Sept 5
 CalEntry = irf.ts_vec_xy(...
   irf.tint('2020-09-05T00:00:00Z/2020-09-13T23:59:59Z'),...
-  repmat([0.3104  1.5638],2,1));
+  repmat([0.7148  3.6008],2,1));
 Cal = Cal.combine(CalEntry);
 
 %
 CalEntry = irf.ts_vec_xy(...
   irf.tint('2020-09-14T00:00:00Z/2020-11-02T23:59:59Z'),...
-  repmat([0.2921  1.4582],2,1));
+  repmat([0.6726  3.3577],2,1));
 Cal = Cal.combine(CalEntry);
 
 %
 CalEntry = irf.ts_vec_xy(...
   irf.tint('2020-11-03T00:00:00Z/2020-11-30T23:59:59Z'),...
-  repmat([0.3199  1.5274],2,1));
+  repmat([0.7367  3.5170],2,1));
 Cal = Cal.combine(CalEntry);
 
 %
 CalEntry = irf.ts_vec_xy(...
   irf.tint('2020-12-01T00:00:00Z/2020-12-14T23:59:59Z'),...
-  repmat([0.2956  1.6881],2,1));
+  repmat([0.6834  3.8871],2,1));
 Cal = Cal.combine(CalEntry);
 
 %% calibrate
 CalR = Cal.resample(PSP);
 NeScp = PSP; 
 
-NeScp.data = 10.^(CalR.x.data.*NeScp.data +CalR.y.data);
+NeScp.data = exp(CalR.x.data.*NeScp.data +CalR.y.data);
 
 NeScp.name = 'NeScp';
 NeScp.units = 'cm^-3';
