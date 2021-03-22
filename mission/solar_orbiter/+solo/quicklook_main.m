87c0d6f7b72a803622dfbe5f91491239751db8f1
Erik P G Johansson <erik.johansson@irfu.se>
2021-03-22 19:37:54 +0100

    SolO QLI: quicklook_main.m: Cleanup; add logging speed

--- mission/solar_orbiter/+solo/quicklook_main.m
+++ mission/solar_orbiter/+solo/quicklook_main.m
@@ -2,7 +2,17 @@
 % Generate multiple IRFU-local types of quicklooks (files) for SolO data (not
 % just RPW) for a specified time interval.
 %
-% Intended for batch processing.
+% Intended for batch processing, e.g. being called from bash script.
+%
+% NOTE: Uses SPICE implicitly, and therefore relies on some path convention. Not
+% sure which, but presumably it does at least find /data/solo/SPICE/.
+% NOTE: Still uses some hard-coded paths for locating datasets.
+% NOTE: Creates subdirectories to the output directory if not pre-existing.
+% NOTE: The time coverage of weekly plots uses 7-day periods that always start
+% with utcBegin.
+% NOTE: There will only be plots that cover full 7-day periods. If the global
+% time interval is not a multiple of 7 days, then end of that global time
+% interval will not be covered by a 7-day plot.
 %
 %
 % ARGUMENTS
@@ -13,32 +23,50 @@
 %       NOTE: Will create subdirectories if not pre-existing.
 %
 %
-% NOTE: Uses SPICE implicitly, and therefore relies on some path convention.
-% NOTE: Still uses some hard-coded paths for locating datasets.
-%
-%
 % Initially created ~<2021-03-11, based on code by Konrad Steinvall, IRF,
 % Uppsala, Sweden. Modified by Erik P G Johansson.
 %
 function quicklook_main(utcBegin, utcEnd, speedDataDir, outputDir)
+% PROPOSAL: Log wall time used.
+%   NOTE: ~Can not time per plot.
+%   PROPOSAL: Log wall time per day of data.
+% TODO-NI: Overwrites pre-existing plots?
+% PROPOSAL: Reorg to separate functions for non-weekly and weekly plots
+% respectively.
+%   NOTE: Would need to have arguments for debugging constants like DISABLE_B etc.
+% PROPOSAL: Arguments for processing only weekly or nonweekly plots.
+%   PRO: Useful for rerunning only relevant subsets.
 
-% IMPLEMENTATION NOTE: Needed to make "DB" work.
+
+
+% IMPLEMENTATION NOTE: Needed to make "DB" work. Necessary when calling from
+% bash.
 irf
 
 
 
-% Specify Solar Orbiter database.
+%================================
+% Specify Solar Orbiter database
+%================================
 % For data on the server the following two are sufficient. You also need
 % V_RPW.mat and V_RPW_1h.mat, found on solo/data_yuri.
 solo.db_init('local_file_db', '/data/solo/');
 solo.db_init('local_file_db', '/data/solo/data_irfu');
 
 % Setup cache
-solo.db_init('db_cache_size_max',4096)
-solo.db_cache('on','save')
+solo.db_init('db_cache_size_max', 4096)
+solo.db_cache('on', 'save')
 
-% Speeds up solo.quicklooks_24_6_2_h(). Useful for testing.
-DISABLE_B = 0;
+
+
+%===========
+% Constants
+%===========
+% "Disabling B" speeds up solo.quicklooks_24_6_2_h(). Useful for testing.
+ENABLE_B               = 1;
+% Useful for testing/debugging.
+ENABLE_NONWEEKLY_PLOTS = 1;
+ENABLE_WEEKLY_PLOTS    = 1;
 
 % Specify subdirectories for saving the respective types of plots.
 PATHS.path_2h  = fullfile(outputDir, '2h' );
@@ -48,14 +76,21 @@ PATHS.path_1w  = fullfile(outputDir, '1w' );
 
 
 
+tSec = tic();
+
 % Specify time interval for plots
 TimeInterval = irf.tint(utcBegin, utcEnd);
 
+
+
+%=======================
 % Create subdirectories
-% NOTE: Works if subdirectories pre-exist.
+%=======================
 for fnCa = fieldnames(PATHS)'
     dirPath = PATHS.(fnCa{1});
     [parentDir, dirBasename, dirSuffix] = fileparts(dirPath);
+    % NOTE: Works (without warnings) also if subdirectories pre-exist ("msg"
+    % contains warning which is enver printed.)
     [success, msg] = mkdir(parentDir, [dirBasename, dirSuffix]);
     assert(success, 'Failed to create directory "%s". %s', dirPath, msg)
 end
@@ -65,49 +100,53 @@ end
 %=============================================
 % Run the code for 2-, 6-, 24-hour quicklooks
 %=============================================
-times_1d = make_tints(TimeInterval,1);% Daily time-intervals
+if ENABLE_NONWEEKLY_PLOTS
+    
+    times_1d = make_tints(TimeInterval,1); % Daily time-intervals
 
-% Load data
-% This is the .mat file containing RPW speeds at 1h resolution.
-% The file should be in the current path. This file can be found in
-% /solo/data/data_yuri/.
-S = load(fullfile(speedDataDir, 'V_RPW_1h'));
+    % Load data
+    % This is the .mat file containing RPW speeds at 1h resolution.
+    % The file should be in the current path. This file can be found in
+    % /solo/data/data_yuri/.
+    S = load(fullfile(speedDataDir, 'V_RPW_1h'));
 
-for iTint=1:length(times_1d)-1
-    % Time interval
-    Tint=irf.tint(times_1d(iTint),times_1d(iTint+1));
+    for iTint=1:length(times_1d)-1
+        % Time interval
+        Tint=irf.tint(times_1d(iTint), times_1d(iTint+1));
+        
+        Data.Vrpw = S.V_RPW_1h.tlim(Tint);
+        
+        % E-field:
+        Data.E = db_get_ts('solo_L3_rpw-bia-efield-10-seconds', 'EDC_SRF', Tint);
+        
+        % RPW density:
+        Data.Ne = db_get_ts('solo_L3_rpw-bia-density-10-seconds', 'DENSITY', Tint);
+        
+        % B-field:
+        Data.B = db_get_ts('solo_L2_mag-srf-normal','B_SRF', Tint);
+        
+        % Proton & alpha temperature:
+        Data.Tpas = db_get_ts('solo_L2_swa-pas-grnd-mom','T', Tint);
+        
+        % Proton & alpha velocity:
+        Data.Vpas = db_get_ts('solo_L2_swa-pas-grnd-mom','V_SRF', Tint);
+        
+        % Proton & alpha density:
+        Data.Npas = db_get_ts('solo_L2_swa-pas-grnd-mom','N', Tint);
+        
+        % Solar Orbiter position
+        % Note: solopos uses SPICE, but should be taken care of by
+        % "solo.get_position".
+        Data.solopos = solo.get_position(Tint,'frame','SOLO_SUN_RTN');
+        
+        if ~ENABLE_B
+            Data.B = [];
+        end
 
-    Data.Vrpw = S.V_RPW_1h.tlim(Tint);
-
-    % E-field:
-    Data.E = db_get_ts('solo_L3_rpw-bia-efield-10-seconds', 'EDC_SRF', Tint);
-
-    % RPW density:
-    Data.Ne = db_get_ts('solo_L3_rpw-bia-density-10-seconds', 'DENSITY', Tint);
-
-    % B-field:
-    Data.B = db_get_ts('solo_L2_mag-srf-normal','B_SRF', Tint);
-
-    % Proton & alpha temperature:
-    Data.Tpas = db_get_ts('solo_L2_swa-pas-grnd-mom','T', Tint);
-
-    % Proton & alpha velocity:
-    Data.Vpas = db_get_ts('solo_L2_swa-pas-grnd-mom','V_SRF', Tint);
-
-    % Proton & alpha density:
-    Data.Npas = db_get_ts('solo_L2_swa-pas-grnd-mom','N', Tint);
-
-    % Solar Orbiter position
-    % Note: solopos uses SPICE, but should be taken care of by
-    % "solo.get_position".
-    Data.solopos = solo.get_position(Tint,'frame','SOLO_SUN_RTN');
-
-    if DISABLE_B
-        Data.B = [];
-    end
-    %Plot data and save figure
-    solo.quicklooks_24_6_2_h(Data,PATHS,Tint)
-
+        %Plot data and save figure
+        solo.quicklooks_24_6_2_h(Data,PATHS,Tint)
+        
+    end    % for
 end
 
 
@@ -115,65 +154,77 @@ end
 %===================================
 % Run the code for weekly overviews
 %===================================
-times_7d = make_tints(TimeInterval,7);% weekly time-intervals
-
-
-% Load data
-% This is the .mat file containing RPW speeds at 6h resolution.
-% The file should be in the same folder as this script (quicklook_main).
-S = load(fullfile(speedDataDir, 'V_RPW'));
-
-for iTint=1:length(times_7d)-1
-    % Time interval
-    Tint = irf.tint(times_7d(iTint),times_7d(iTint+1));
-    data2.Vrpw = S.V_RPW.tlim(Tint);
-
-    % E-field:
-    data2.E = db_get_ts('solo_L3_rpw-bia-efield-10-seconds', 'EDC_SRF', Tint);
-
-    % RPW density:
-    data2.Ne = db_get_ts('solo_L3_rpw-bia-density-10-seconds', 'DENSITY', Tint);
-
-    % B-field:
-    data2.B = db_get_ts('solo_L2_mag-rtn-normal-1-minute','B_RTN', Tint);
-
-    % Proton & alpha temperature:
-    data2.Tpas = db_get_ts('solo_L2_swa-pas-grnd-mom','T', Tint);
-
-    % Proton & alpha velocity:
-    data2.Vpas = db_get_ts('solo_L2_swa-pas-grnd-mom','V_SRF', Tint);
-
-    % Proton & alpha density:
-    data2.Npas = db_get_ts('solo_L2_swa-pas-grnd-mom','N', Tint);
-
-    % Solar Orbiter position
-    % Note: solopos uses SPICE, but should be taken care of by
-    % "solo.get_position".
-    data2.solopos = solo.get_position(Tint,'frame','SOLO_SUN_RTN');
-
-    % Plot data and save figure
-    solo.quicklooks_7days(data2,PATHS,Tint)
-
+if ENABLE_WEEKLY_PLOTS
+    
+    times_7d = make_tints(TimeInterval,7);% weekly time-intervals
+    
+    % Load data
+    % This is the .mat file containing RPW speeds at 6h resolution.
+    % The file should be in the same folder as this script (quicklook_main).
+    S = load(fullfile(speedDataDir, 'V_RPW'));
+    
+    for iTint=1:length(times_7d)-1
+        
+        % Time interval
+        Tint = irf.tint(times_7d(iTint), times_7d(iTint+1));
+        Data2.Vrpw = S.V_RPW.tlim(Tint);
+        
+        % E-field:
+        Data2.E = db_get_ts('solo_L3_rpw-bia-efield-10-seconds', 'EDC_SRF', Tint);
+        
+        % RPW density:
+        Data2.Ne = db_get_ts('solo_L3_rpw-bia-density-10-seconds', 'DENSITY', Tint);
+        
+        % B-field:
+        Data2.B = db_get_ts('solo_L2_mag-rtn-normal-1-minute','B_RTN', Tint);
+        
+        % Proton & alpha temperature:
+        Data2.Tpas = db_get_ts('solo_L2_swa-pas-grnd-mom','T', Tint);
+        
+        % Proton & alpha velocity:
+        Data2.Vpas = db_get_ts('solo_L2_swa-pas-grnd-mom','V_SRF', Tint);
+        
+        % Proton & alpha density:
+        Data2.Npas = db_get_ts('solo_L2_swa-pas-grnd-mom','N', Tint);
+        
+        % Solar Orbiter position
+        % Note: solopos uses SPICE, but should be taken care of by
+        % "solo.get_position".
+        Data2.solopos = solo.get_position(Tint,'frame','SOLO_SUN_RTN');
+        
+        % Plot data and save figure
+        solo.quicklooks_7days(Data2,PATHS,Tint)
+        
+    end    % for
 end
+
+
+
+wallTimeSec   = toc(tSec);
+wallTimeHours = wallTimeSec/3600;
+plotsTimeDays = (TimeInterval.tts(2) - TimeInterval.tts(1)) / 86400;
+
+fprintf('Wall time used:                  %g [h]\n',     wallTimeHours);
+fprintf('Wall time used per day of plots: %g [h/day]\n', wallTimeHours / plotsTimeDays);
+
+end    % function
 
 
 
 % Auxilliary function
 %
-function out_times=make_tints(inputTint,days)
+function out_times = make_tints(Tint, days)
 
-    t0       = inputTint(1);
-    tlength  = inputTint(2)-inputTint(1);
-    stepsize = days*24*60*60; %seconds
+    t0       = Tint(1);
+    tlength  = Tint(2) - Tint(1);
+    stepsize = days*24*60*60;    % seconds
 
-    dt = 0:stepsize:tlength;
+    dt        = 0:stepsize:tlength;
     out_times = t0+dt;
 
 end
 
 
-
-end    % function
 
 % Wrapper around solo.db_get_ts() that normalizes the output to a TSeries.
 %
@@ -184,12 +235,14 @@ end    % function
 % /Erik P G Johansson 2021-03-22
 %
 function Ts = db_get_ts(varargin)
-    Ts = solo.db_get_ts(varargin{:});
+    temp = solo.db_get_ts(varargin{:});
     
     % Normalize (TSeries or cell array) --> TSeries.
-    if iscell(Ts)
-        Ts = cell_array_TS_to_TS(Ts);
+    if iscell(temp)
+        temp = cell_array_TS_to_TS(temp);
     end
+    
+    Ts = temp;
 end
 
 
