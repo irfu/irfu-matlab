5704cb7fc2c481688b344fa2f6e4b92ae5e1ad2e
Erik P G Johansson <erik.johansson@irfu.se>
2021-03-30 08:21:58 +0200

    SolO QLI: quicklook_main: Arg.order change, minor cleanup

--- mission/solar_orbiter/+solo/quicklook_main.m
+++ mission/solar_orbiter/+solo/quicklook_main.m
@@ -6,7 +6,7 @@
 %
 % NOTE: Uses SPICE implicitly, and therefore relies on some path convention. Not
 % sure which, but presumably it does at least find /data/solo/SPICE/.
-% NOTE: Still uses some hard-coded paths for locating datasets.
+% NOTE: Still uses some hard-coded paths for locating datasets (solo.db_init()).
 % NOTE: Creates subdirectories to the output directory if not pre-existing.
 % NOTE: The time coverage of weekly plots uses 7-day periods that always start
 % with utcBegin.
@@ -17,29 +17,49 @@
 %
 % ARGUMENTS
 % =========
+% vhtDataDir
+%       Path to directory containing VHT (velocity) .mat files.
+% outputDir
+%       Plots will be placed in subdirectories under this directory.
+%       NOTE: Will create subdirectories if not pre-existing.
 % runNonweeklyPlots, runWeeklyPlots
 %       Whether to run the resp. groups of plots.
 %       NOTE: Permits chars "0" and "1" for when calling from bash.
+%       Useful for testing and not re-running unnecessary time-consuming plots.
 % utcBegin, utcEnd : Strings.
 %       Defines time interval for which quicklooks should be generated.
-% outputDir : Path to directory.
-%       NOTE: Will create subdirectories if not pre-existing.
 %
 %
 % Initially created ~<2021-03-11, based on code by Konrad Steinvall, IRF,
 % Uppsala, Sweden. Modified by Erik P G Johansson.
 %
-function quicklook_main(runNonweeklyPlots, runWeeklyPlots, utcBegin, utcEnd, speedDataDir, outputDir)
+function quicklook_main(vhtDataDir, outputDir, runNonweeklyPlots, runWeeklyPlots, utcBegin, utcEnd)
+%
 % PROPOSAL: Log wall time used.
 %   NOTE: ~Can not time per plot.
 %   PROPOSAL: Log wall time per day of data.
+%
 % TODO-NI: Overwrites pre-existing plots?
-% PROPOSAL: Reorg to separate functions for non-weekly and weekly plots
+%
+% PROPOSAL: Reorg to separate internal functions for non-weekly and weekly plots
 % respectively.
 %   NOTE: Would need to have arguments for debugging constants like DISABLE_B etc.
-% PROPOSAL: Arguments for processing only weekly or nonweekly plots.
-%   PRO: Useful for rerunning only relevant subsets.
-
+%
+% PROPOSAL: Always start weeks with same weekday.
+%   NOTE: Currently only plots weeks which are entirely covered by input time
+%   interval. Weeks always start with start date.
+%   PROBLEM: If rounding to nearest previous/successive Monday, then one will
+%   always plot too much, or too little.
+%       PROPOSAL: Argument/setting for policy.
+%           ~all     : Always plot time interval. Round to larger time interval.
+%                      Incomplete plots if global time interval does not fall on Monday 00:00.
+%           ~smaller : Round to smaller time interval.
+%       PROPOSAL: Always round to smaller time interval. The caller has to be
+%       aware of time intervals if want to cover specifik weeks.
+%   PROBLEM: Mission begins on 2020-02-12=Wednesday.
+%   ==> There is no SPICE data on Monday-Tuesday before this date.
+%   ==> Code fails for week Monday-to-Sunday.
+%       PROPOSAL: Additionally round start time up to start date.
 
 
 runNonweeklyPlots = interpret_argument_flag(runNonweeklyPlots);
@@ -79,13 +99,22 @@ PATHS.path_6h  = fullfile(outputDir, '6h' );
 PATHS.path_24h = fullfile(outputDir, '24h');
 PATHS.path_1w  = fullfile(outputDir, '1w' );
 
+VHT_1H_DATA_FILENAME = 'V_RPW_1h.mat';
+VHT_6H_DATA_FILENAME = 'V_RPW.mat';
+
+% Beginning of SPICE kernels.
+% MISSION_BEGIN = irf.time_array('2020-02-12T00:00:00');
+
 
 
 tSec = tic();
 
 % Specify time interval for plots
-TimeInterval = irf.tint(utcBegin, utcEnd);
-
+TimeInterval      = irf.tint(utcBegin, utcEnd);
+% TimeIntervalWeeks = irf.tint(...
+%     round_to_week(TimeInterval(1),  1), ...
+%     round_to_week(TimeInterval(2), -1));
+TimeIntervalWeeks      = TimeInterval;
 
 
 %=======================
@@ -107,13 +136,13 @@ end
 %=============================================
 if runNonweeklyPlots
     
-    times_1d = make_tints(TimeInterval,1); % Daily time-intervals
+    times_1d = make_tints(TimeInterval, 1); % Daily time-intervals
 
     % Load data
     % This is the .mat file containing RPW speeds at 1h resolution.
     % The file should be in the current path. This file can be found in
     % /solo/data/data_yuri/.
-    S = load(fullfile(speedDataDir, 'V_RPW_1h'));
+    S = load(fullfile(vhtDataDir, VHT_1H_DATA_FILENAME));
 
     for iTint=1:length(times_1d)-1
         % Time interval
@@ -166,7 +195,7 @@ if runNonweeklyPlots
             Data.B = [];
         end
 
-        %Plot data and save figure
+        % Plot data and save figure
         solo.quicklooks_24_6_2_h(Data,PATHS,Tint)
         
     end    % for
@@ -178,13 +207,13 @@ end
 % Run the code for weekly overviews
 %===================================
 if runWeeklyPlots
-    
-    times_7d = make_tints(TimeInterval,7);% weekly time-intervals
+        
+    times_7d = make_tints(TimeIntervalWeeks, 7);% weekly time-intervals
     
     % Load data
     % This is the .mat file containing RPW speeds at 6h resolution.
     % The file should be in the same folder as this script (quicklook_main).
-    S = load(fullfile(speedDataDir, 'V_RPW'));
+    S = load(fullfile(vhtDataDir, VHT_6H_DATA_FILENAME));
     
     for iTint=1:length(times_7d)-1
   
@@ -250,7 +279,7 @@ plotsTimeDays = (TimeInterval.tts(2) - TimeInterval.tts(1)) / 86400;
 
 % NOTE: Execution speed may vary by orders of magnitude depending on settings
 % (nonweekly vs weekly plots). May therefore want scientific notation.
-fprintf('Wall time used:                  %g [h]\n',     wallTimeHours);
+fprintf('Wall time used:                  %g [h] = %g [s]\n',     wallTimeHours, wallTimeSec);
 fprintf('Wall time used per day of plots: %g [h/day]\n', wallTimeHours / plotsTimeDays);
 
 end    % function
@@ -259,14 +288,15 @@ end    % function
 
 % Auxilliary function
 %
-function out_times = make_tints(Tint, days)
+function out_times = make_tints(Tint, nDays)
 
-    t0       = Tint(1);
-    tlength  = Tint(2) - Tint(1);
-    stepsize = days*24*60*60;    % seconds
+    t0          = Tint(1);
+    tlength     = Tint(2) - Tint(1);
+    % NOTE: Does not take leap seconds into account.
+    stepSizeSec = nDays*24*60*60;    % seconds.
 
-    dt        = 0:stepsize:tlength;
-    out_times = t0+dt;
+    dt          = 0:stepSizeSec:tlength;
+    out_times   = t0+dt;
 
 end
 
