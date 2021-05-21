d54d22d0b18ce74c1724e2c23811d0285001306d
Konrad Steinvall <konrad.steinvall@irfu.se>
2021-05-21 11:52:09 +0200

    Updated SolO calibration

--- mission/solar_orbiter/+solo/vdccal.m
+++ mission/solar_orbiter/+solo/vdccal.m
@@ -1,9 +1,12 @@
-function [DCE_SRF,PSP,ScPot,codeVerStr,matVerStr] = vdccal(VDC,EDC)
+function [DCE_SRF_out,PSP_out,ScPot_out,codeVerStr,matVerStr] = vdccal(VDC_inp,EDC_inp,calfile_name)
 %SOLO.VDCCAL  Calibrate VDC to get DC E and PSP
 %
-%    [DCE_SRF,PSP,ScPot,codeVerStr,matVerStr] = solo.vdccal(VDC,EDC)
+%    [DCE_SRF,PSP,ScPot,codeVerStr,matVerStr] = solo.vdccal(VDC,EDC,calfilename)
 %
-% Inputs: VDC,EDC from L2 CWF files
+% Inputs: VDC,EDC from L2 CWF files, 
+%         calfile_name is the name of the calibration file (.mat) one wishes to
+%         use. If calfile_name is empty, the code used the calibration file
+%         used by BICAS for producing official datasets
 %
 % Outputs:
 %   DCE_SRF    - DC electric field in SRF (Ex=0)
@@ -13,13 +16,22 @@ function [DCE_SRF,PSP,ScPot,codeVerStr,matVerStr] = vdccal(VDC,EDC)
 %   matVerStr  - Date format version string for .mat file. Used by BICAS.
 %                (Not yet used.)
 %
-% Loads .mat file produced by solo.correlate_probes_batch (script)
+% Loads .mat file produced by solo.correlate_probes_batch (script) if
+% calfile_name is empty.
 % NOTE: .mat needs to be updated before processing the new data.
 %
 % NOTE: This function is used by BICAS for producing official datasets.
 
-a = load('d23K123_20210129');
-
+% Check if a calibration file is specified, otherwise choose the
+% calibration filed used by BICAS for producing official datasets.
+if isempty(calfile_name)
+    % calibration file used by BICAS for producing official datasets.
+    a = load('d23K123_20210521.mat');
+else
+    % specified calibration file. Useful for debugging/testing new
+    % calibrations.
+    a = load(calfile_name);
+end
 
 
 %===========================================================================
@@ -35,14 +47,6 @@ codeVerStr = '2021-04-08T14:00:00';
 % Version of the .mat file. Not yet used. Meant to be read from .mat file.
 matVerStr  = [];
 
-
-
-Gamma0 = a.Gamma0;
-Gamma1 = a.Gamma1;
-cc = a.CC;
-
-
-
 %=============================================================================
 % Find data points/CDF records for which only V1_DC is available
 %
@@ -51,53 +55,81 @@ cc = a.CC;
 % (1) all single probes (DC) available
 % (2) only probe 1 (DC) available.
 % NOTE: Only works for mux=0,2,3,4 (not mux=1).
-% NOTE: Ignores EDC argument.
+% NOTE: Ignores EDC_inp argument.
 %==============================================================================
-bSingleProbe = isnan(VDC.y.data) & isnan(VDC.z.data);
 
+%load Probe-potential discontinuities
+discontTimes=solo.ProbePotDiscontinuities;
+mainTint = irf.tint(VDC_inp.time(1),VDC_inp.time(end));
+sub_int_times = EpochTT(split_tint(mainTint,discontTimes));
 
+%Predefine output variables:
+DCE_SRF_out=irf.ts_vec_xyz(EpochTT([]),double.empty(0,3));
+PSP_out=irf.ts_scalar(EpochTT([]),[]);
+ScPot_out=irf.ts_scalar(EpochTT([]),[]);
 
-d23R  = a.d23.resample(VDC);
-K123R = a.K123.resample(VDC);
-Gamma1R = Gamma1.resample(VDC);
-Gamma0R = Gamma0.resample(VDC);
-ccR = cc.resample(VDC);
+% Perform calibration on each subinterval (if any probe-to-spacecraft
+% potential discontinuities are present)
 
-V2corr = double(VDC.y.data) -double(d23R.data); %Remove potential offset between 2,3
-V23_corr = (V2corr+double(VDC.z.data))/2; %(V2corr+V3)/2
-V2cmr = double(V2corr)-(Gamma0R.data+V23_corr.*Gamma1R.data)/2; %Remove common mode from V2.
-V3cmr = double(VDC.z.data)+(Gamma0R.data+V23_corr.*Gamma1R.data)/2;
-
-V23 = (V2cmr + V3cmr)/2; % (V2cmr + V3) /2
-
-V23corr = (V23.*K123R.data(:,1) + K123R.data(:,2)); %Correcting V23 to V1
-
-
-PSP = irf.ts_scalar(VDC.time,(V23corr + double(VDC.x.data))/2); %Compute PSP from corrected quantities.
-
-% Use alternate, simpler "calculation" for single-probe data.
-PSP.data(bSingleProbe) = VDC.x.data(bSingleProbe);
-
-PSP.units = 'V';
-
-
-
-PLASMA_POT = 1.5; SHORT_FACTOR = 2.5; % XXX: these are just ad hoc numbers.
-
-ScPot = irf.ts_scalar(VDC.time, -(PSP.data-PLASMA_POT)*SHORT_FACTOR);
-ScPot.units = PSP.units;
-
-% Ey_SRF = V3 - V2, 6.99 - 1/2 of distance between the antennas
-V_delta23_corr = V2cmr-V3cmr; %Fixed V2-V3.
-Ey_SRF = -V_delta23_corr*1e3/6.99;
-
-% Ez_SRF = V23corr - V1
-% Here we use the antenna length of 11.2 m, which correponds to 
-% the distance between the center of ANT1 and a symmetric antenna on the 
-% other side having voltage V23 corr.
-Ez_SRF = V23corr - double(VDC.x.data);	
-Ez_SRF = Ez_SRF*1e3/11.2;
-
-DCE_SRF = irf.ts_vec_xyz(VDC.time,[Ey_SRF*0 Ey_SRF Ez_SRF]);
-DCE_SRF.units = 'mV/m';
-DCE_SRF.coordinateSystem = 'SRF';
+for isub=1:length(sub_int_times)-1
+    
+    subTint=sub_int_times(isub:isub+1);
+    VDC = VDC_inp.tlim(subTint);
+    
+    bSingleProbe = isnan(VDC.y.data) & isnan(VDC.z.data);
+    
+    % Resample calibration parameters
+    d23R  = a.d23.tlim(subTint).resample(VDC);
+    K123R = a.K123.tlim(subTint).resample(VDC);
+    Gamma0R = a.Gamma0.tlim(subTint).resample(VDC);
+    Gamma1R = a.Gamma1.tlim(subTint).resample(VDC);
+    ccR = a.CC.tlim(subTint).resample(VDC);
+    
+    % Start calibration
+    V2corr = double(VDC.y.data) -double(d23R.data); %Remove potential offset between 2,3
+    V23_corr = (V2corr+double(VDC.z.data))/2; %(V2corr+V3)/2
+    V2cmr = double(V2corr)-(Gamma0R.data+V23_corr.*Gamma1R.data)/2; %Remove common mode from V2.
+    V3cmr = double(VDC.z.data)+(Gamma0R.data+V23_corr.*Gamma1R.data)/2;
+    
+    V23 = (V2cmr + V3cmr)/2; % (V2cmr + V3) /2
+    
+    V23corr = (V23.*K123R.data(:,1) + K123R.data(:,2)); %Correcting V23 to V1
+    
+    PSP = irf.ts_scalar(VDC.time,(V23corr + double(VDC.x.data))/2); %Compute PSP from corrected quantities.
+    
+    % Use alternate, simpler "calculation" for single-probe data.
+    PSP.data(bSingleProbe) = VDC.x.data(bSingleProbe);
+    PSP.units = 'V';
+    PSP_out=PSP_out.combine(PSP);
+    
+    
+    PLASMA_POT = 1.5; SHORT_FACTOR = 2.5; % XXX: these are just ad hoc numbers.
+    
+    ScPot = irf.ts_scalar(VDC.time, -(PSP.data-PLASMA_POT)*SHORT_FACTOR);
+    ScPot.units = PSP.units;
+    ScPot_out=ScPot_out.combine(ScPot);
+    
+    % Ey_SRF = V3 - V2, 6.99 - 1/2 of distance between the antennas
+    V_delta23_corr = V2cmr-V3cmr; %Fixed V2-V3.
+    Ey_SRF = -V_delta23_corr*1e3/6.99;
+    
+    % Ez_SRF = V23corr - V1
+    % Here we use the antenna length of 11.2 m, which correponds to
+    % the distance between the center of ANT1 and a symmetric antenna on the
+    % other side having voltage V23 corr.
+    Ez_SRF = V23corr - double(VDC.x.data);
+    Ez_SRF = Ez_SRF*1e3/11.2;
+    
+    DCE_SRF = irf.ts_vec_xyz(VDC.time,[Ey_SRF*0 Ey_SRF Ez_SRF]);
+    DCE_SRF.units = 'mV/m';
+    DCE_SRF.coordinateSystem = 'SRF';
+    
+    DCE_SRF_out=DCE_SRF_out.combine(DCE_SRF);
+    
+end %for
+    % Specify units and coordinate system
+    DCE_SRF_out.units = 'mV/m';
+    DCE_SRF_out.coordinateSystem = 'SRF';
+    PSP_out.units = 'V';
+    ScPot_out.units=PSP_out.units;
+end
