7e3494e73a52f352c32ced743366b4dcaa10af1a
Erik P G Johansson <erik.johansson@irfu.se>
2021-08-16 17:01:35 +0200

    BICAS: Add cal_RCT___UTEST.m
    Updating regexpf.m to permit empty cell arrays necessary for consistency
    reasons revealed in tests.
    
    modified:   ../../+EJ_library/+str/regexpf.m
    new file:   +bicas/+proc/+L1L2/cal_RCT___UTEST.m

---
+++ mission/solar_orbiter/bicas/src/+bicas/+proc/+L1L2/cal_RCT___UTEST.m
@@ -0,0 +1,171 @@
+%
+% matlab.unittest automatic test code for bicas.proc.L1L2.cal_RCT.
+%
+% NOTE: Only tests one function.
+% NOTE: Tests for "bicas.proc.L1L2.cal_RCT.find_RCT_regexp()" has partly been
+% written in order to try out functionality for testing code with file
+% operations.
+% NOTE: Creates temporary directory and files for every test, separately.
+%
+%
+% Author: Erik P G Johansson, IRF Uppsala, Sweden
+% First created 2021-08-16
+%
+classdef cal_RCT___UTEST < matlab.unittest.TestCase
+    % PROPOSAL: Tests for bicas.proc.L1L2.cal_RCT.read_RCT_modify_log() for BIAS
+    %           RCT. Creates BIAS RCT using bicas.tools.create_RCT() as part of
+    %           the test.
+    %
+    % PROPOSAL: Use TestMethodSetup and TestMethodTeardown.
+    %   CON: Directory and files can only be shared among tests for find_RCT_regexp().
+    %       CON-PROPOSAL: Move find_RCT_regexp() test code to separate file.
+    % PROPOSAL: Use TestClassSetup and TestClassTeardown for logger object.
+    %   PRO: Can share logger object.
+    %   
+
+
+
+    properties(Constant)
+    end
+    
+    
+    
+    properties(TestParameter)
+    end
+    
+    
+    
+    %##############
+    %##############
+    % TEST METHODS
+    %##############
+    %##############
+    methods(Test)
+
+        
+        
+        function test_find_RCT_regexp_empty(testCase)
+%             function test(inputsCa, expOutputsCa)
+%                 actOutputs = cell(size(expOutputsCa));
+%                 [actOutputs{:}] = bicas.proc.L1L2.cal_RCT.find_RCT_regexp(inputsCa{:});
+%                 testCase.verifyEqual(actOutputs, expOutputsCa)
+%             end
+%             function test_exc(varargin)
+%                 testCase.verifyError(...
+%                     @() bicas.proc.L1L2.cal_RCT.find_RCT_regexp(varargin{:}), ...
+%                     ?MException)
+%             end
+            %===================================================================
+            
+            [tempDir, L] = bicas.proc.L1L2.cal_RCT___UTEST.setup_files(testCase, {});
+            
+            testCase.verifyError(...
+                @() bicas.proc.L1L2.cal_RCT.find_RCT_regexp(...
+                tempDir, '20[0-9][0-9]\.cdf', L), ...
+                'BICAS:CannotFindRegexMatchingRCT')            
+            
+        end
+        
+        
+        
+        function test_find_RCT_regexp_no_match(testCase)
+            [tempDir, L] = bicas.proc.L1L2.cal_RCT___UTEST.setup_files(...
+                testCase, {'20201.cdf', '2020.CDF'});
+            
+            testCase.verifyError(...
+                @() bicas.proc.L1L2.cal_RCT.find_RCT_regexp(...
+                tempDir, '20[0-9][0-9]\.cdf', L), ...
+                'BICAS:CannotFindRegexMatchingRCT')
+        end
+        
+        
+        
+        function test_find_RCT_regexp_1_match(testCase)
+            [tempDir, L] = bicas.proc.L1L2.cal_RCT___UTEST.setup_files(...
+                testCase, {'2020.cdf', 'asdsf'});
+            
+            path = bicas.proc.L1L2.cal_RCT.find_RCT_regexp(...
+                tempDir, '20[0-9][0-9]\.cdf', L);
+            
+            testCase.verifyEqual(...
+                path, ...
+                fullfile(tempDir, '2020.cdf'))            
+        end
+        
+        
+        
+        function test_find_RCT_regexp_2_match(testCase)
+            [tempDir, L] = bicas.proc.L1L2.cal_RCT___UTEST.setup_files(...
+                testCase, {'2020.cdf', '2021.cdf'});
+            
+            path = bicas.proc.L1L2.cal_RCT.find_RCT_regexp(...
+                tempDir, '20[0-9][0-9]\.cdf', L);
+            
+            testCase.verifyEqual(...
+                path, ...
+                fullfile(tempDir, '2021.cdf'))            
+        end
+        
+        
+        
+        function test_find_RCT_regexp_realistic(testCase)
+            FN_1 = 'SOLO_CAL_RPW-BIAS_V202111191204.cdf';
+            FN_2 = 'SOLO_CAL_RPW-BIAS_V202011191204.cdf';
+            
+            [tempDir, L] = bicas.proc.L1L2.cal_RCT___UTEST.setup_files(...
+                testCase, {FN_1, FN_2});
+            
+            path = bicas.proc.L1L2.cal_RCT.find_RCT_regexp(...
+                tempDir, 'SOLO_CAL_RPW-BIAS_V20[0-9]{10,10}.cdf', L);
+            
+            testCase.verifyEqual(...
+                path, ...
+                fullfile(tempDir, FN_1))
+        end
+        
+        
+        
+    end    % methods(Test)
+        
+        
+    
+    %########################
+    %########################
+    % PRIVATE STATIC METHODS
+    %########################
+    %########################
+    methods(Static, Access=private)
+        
+        
+        
+%         function test(filenamesCa, inputsCa, expOutputsCa)
+%             L = bicas.proc.L1L2.cal_RCT___UTEST.setup_files(testCase, filenamesCa);
+% 
+%             actOutputs = cell(size(expOutputsCa));
+%             [actOutputs{:}] = bicas.proc.L1L2.cal_RCT.find_RCT_regexp(inputsCa{:}, L);
+%             testCase.verifyEqual(actOutputs, expOutputsCa)
+%         end
+        
+        
+            
+        function [tempDir, L] = setup_files(testCase, filenamesCa)
+            L = bicas.logger('none', false);
+            
+            DirFixture = testCase.applyFixture(...
+                matlab.unittest.fixtures.TemporaryFolderFixture(...
+                    'WithSuffix', ['.', mfilename()]));
+            
+            tempDir = DirFixture.Folder;
+            %fprintf('tempDir = %s\n', tempDir)
+
+            for fileCa = filenamesCa(:)'
+                EJ_library.fs.write_file(fullfile(tempDir, fileCa{1}), zeros(1,0,'uint8'))
+            end
+
+        end
+        
+    end    % methods(Static, Access=private)
+
+    
+    
+end
