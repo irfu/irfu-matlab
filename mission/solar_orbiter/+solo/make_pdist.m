6a16a1f0c3b6a7f34ebfa2c2980f7bcbbe356b0c
Daniel Graham <dgraham@irfu.se>
2021-10-17 16:17:59 +0200

    add solo.make_pdist to make a PDist format variable for Solar Orbiter PAS Ion distributions

---
+++ mission/solar_orbiter/+solo/make_pdist.m
@@ -0,0 +1,91 @@
+function PDout = make_pdist(varargin)
+% SOLO.MAKE_PDIST Make a PDist skymap variable from Solar Orbiter Particle data
+%
+% PDist = mms.make_pdist(filename)
+%
+% Input: 
+%   file - filename and directory of cdf file, or loaded cdf file
+%
+% Output: 
+%   PDout - PDist skymap variable
+%
+% Examples: 
+%   PDout = make_pdist('solo_L2_swa-pas-vdf_20200729_V01.cdf')
+%
+%   tmpD = dataobj('solo_L2_swa-pas-vdf_20200729_V01.cdf');
+%   PDout = solo.make_pdist(tmpD)
+% 
+% Written by D. B. Graham
+%
+% TO DO: Add electron distributions
+
+if (nargin > 1)
+  error('Too many inputs. Need to add electron data.')
+end
+
+file = varargin{1};
+
+if isa(file,'dataobj')
+  tmpDataObj = file;
+elseif isa(file,'char')
+  tmpDataObj = dataobj(file);
+else
+  error('Input not recognized.')
+end
+
+if isempty(tmpDataObj); return; end
+
+% Identify variable type 
+fname = tmpDataObj.GlobalAttributes.Logical_file_id{1};
+if ~isempty(regexp(fname,'swa-pas-vdf','once'))
+  % Load ion data
+  Energyarr = get_variable(tmpDataObj,'Energy');
+  dEnergyplus = get_variable(tmpDataObj,'delta_p_Energy');
+  dEnergyminus = get_variable(tmpDataObj,'delta_m_Energy');
+  Azimuthangle = get_variable(tmpDataObj,'Azimuth');
+  deltaAzimuth = get_variable(tmpDataObj,'delta_Azimuth');
+  vdf = get_variable(tmpDataObj,'vdf');
+  vdfp  = permute(vdf.data,[1 4 2 3]);
+  Polarangle = get_variable(tmpDataObj,'Elevation');
+  deltaPolar = get_variable(tmpDataObj,'delta_Elevation');
+  time = vdf.DEPEND_0.data;
+  time = EpochTT(time);
+  dt = median(diff(time.epochUnix));
+  energymat = Energyarr.data*ones(size(time))';
+  
+  PDout = PDist(time,vdfp,'skymap',energymat,Azimuthangle.data,90-Polarangle.data);
+  PDout.species = 'ions';
+  PDout.units = 's^3/m^6';
+  PDout.siConversion = '1e12';
+  PDout.ancillary.energy0 = Energyarr.data;
+  PDout.ancillary.energy1 = Energyarr.data;
+  PDout.ancillary.delta_energy_plus = Energyarr.data-dEnergyplus.data; % Need to check this
+  PDout.ancillary.delta_energy_minus = Energyarr.data-dEnergyminus.data;
+  PDout.ancillary.esteptable = zeros(size(time));
+  PDout.ancillary.delta_theta_plus = deltaPolar.data;
+  PDout.ancillary.delta_theta_minus = deltaPolar.data;
+  PDout.ancillary.delta_phi_plus = deltaAzimuth.data;
+  PDout.ancillary.delta_phi_minus = deltaAzimuth.data;
+  
+  PDout.ancillary.dt_minus = dt/2; % Need to check if time tags are the beginning or middle of distributions
+  PDout.ancillary.dt_plus = dt/2;
+  
+  
+  
+  PDout.name = 'solo_L2_swa-pas-vdf'; % Temp fix
+  
+  ud = [];
+  ud.GlobalAttributes = tmpDataObj.GlobalAttributes;
+  ud.VALIDMIN = tmpDataObj.VariableAttributes.VALIDMIN{13,2};
+  ud.VALIDMAX = tmpDataObj.VariableAttributes.VALIDMAX{13,2};
+  PDout.userData = ud;
+  
+  
+else
+  PDout = NaN;
+  error('Data Object not recognized.')
+end
+
+
+end
+
