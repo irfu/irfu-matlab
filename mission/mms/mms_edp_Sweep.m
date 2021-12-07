irfu/irfu-matlab

commit 2eb76580e9c8519b8b3d58a143b4450fabc66fb8

MMS sweep analysis code update, compute "iPh knee"
Details discussed in e-mails and teleconfs back in 2021-09 between
Per-Arne, Andris, Yuri, Daniel G. and me.

Long story short:
1) code now tries to split "-+" and "-+" type sweeps and compute two values
each per sweep for these (interpreting "-" and "+" parts separately).
Previously these sweeps were simply not computed.

2) try to compute a "knee" intersection of the almost vertical and
almost horizontal part of the sweep based on the spread in the resulting
voltage measured (vertical part have very little spread in measured
voltage per bias current setting while the horizontal part of the sweep
have a very large spread). ("Vertical/Horizontal" when plotting voltage
responses at X-axis and bias current settings at Y-axis). If no "knee"
is found this value resort back to being simply the old "Iph" value,
that is the bias current value at "(2*Vmin+V0)/3".

3) Phase output is now the z-phase of this "knee" intersection.

The reason for trying to find this "knee" is that it is a (current)
region in which the probe I-V response is no longer simply linear and we
want to avoid this region when commanding the bias currents of the
probes. Similar to "iPh", which is typically computed as a value
slightly higher (20-30nA) than this "knee" intersection, especially when
running sweeps of type "--".

mission/mms/mms_testFunctions/mms_sweep_plot_cron.m
@@ -99,6 +99,7 @@ for iSc = 1:4
   c_eval('p?_time=[sObj.p?.time];p?_time=p?_time(1:2:end);', 1:6); % Create time array keeping only the start time
   c_eval('S.p?_impedance_ts=irf.ts_scalar(p?_time,[sObj.p?.impedance]);', 1:6); % TSeries of impedance
   c_eval('S.p?_iPh_ts=irf.ts_scalar(p?_time,[sObj.p?.iPh]);', 1:6); % TSeries of iPh
+  c_eval('S.p?_iPh_knee_ts=irf.ts_scalar(p?_time,[sObj.p?.iPh_knee]);', 1:6); % TSeries of iPh_knee
   c_eval('S.p?_phase_ts=irf.ts_scalar(p?_time,mod([sObj.p?.phase],360));', 1:6); % TSeries of phase
   c_eval('S.p?_type_ts=irf.ts_vec_xy(p?_time, double(reshape([sObj.p?.type],2,length(sObj.p?))''));',1:6); % TSeries of type ('--' or '++' etc converted from ASCII char to double)
   %save(sprintf('%sobj/mms%i_%s_sweepTsObj.mat', sweepFolder, iSc, dayToRun), 'S');
@@ -129,6 +130,18 @@ for iSc = 1:4
   c_eval('print(h?.Parent, ''-dpng'', [sweepFolder,''summary_plots/ADP/iPhVsTime_mms'',scStr,''_p?.png'']);', 5:6);
   close all % close plots
   
+  % Iph_knee vs time
+  c_eval('iPh_knee_movm?=movmedian(Sw.p?_iPh_knee_ts.data, days(15), ''omitnan'', ''SamplePoints'', t?);', 1:6);
+  c_eval('figure(''units'', ''normalized'', ''outerposition'', [0 0 1 1]); h?=irf_plot({Sw.p?_iPh_knee_ts, irf.ts_scalar(Sw.p?_iPh_knee_ts.time, iPh_movm?)},''comp'',''linestyle'',{lineStyle{?},''-black''});', 1:6);
+  c_eval('title(h?,[''Plot created: '',nowStr,''. MMS'',scStr,'' I_{ph}_{knee} vs time from sweep on probe ?.'']);', 1:6);
+  c_eval('ylabel(h?,{''I_{ph}_{knee}'',''[nA]''});', 1:6);
+  c_eval('ylim(h?,[-55 555]);', 1:6);
+  c_eval('legend(h?,''I_{ph}_{knee}, sweep p?'',''15 days moving median'');', 1:6);
+  c_eval('set(h?.Children(1),''LineWidth'',2);', 1:6);
+  c_eval('print(h?.Parent, ''-dpng'', [sweepFolder,''summary_plots/SDP/iPhKneeVsTime_mms'',scStr,''_p?.png'']);', 1:4);
+  c_eval('print(h?.Parent, ''-dpng'', [sweepFolder,''summary_plots/ADP/iPhKneeVsTime_mms'',scStr,''_p?.png'']);', 5:6);
+  close all % close plots
+
   % impedance vs time
   c_eval('imp_movm?=movmedian(Sw.p?_impedance_ts.data, days(15),''omitnan'',''SamplePoints'',t?);', 1:6);
   c_eval('figure(''units'', ''normalized'', ''outerposition'', [0 0 1 1]); h?=irf_plot({Sw.p?_impedance_ts, irf.ts_scalar(Sw.p?_iPh_ts.time, imp_movm?)},''comp'',''linestyle'',{lineStyle{?},''-black''});', 1:6);
@@ -182,6 +195,19 @@ for iSc = 1:4
   %c_eval('fig?=figure(''units'',''normalized'',''outerposition'',[0 0 1 1]);subplot(1,2,1);polarplot(deg2rad(Sw.p?_phase_ts.data(ind?)),Sw.p?_iPh_ts.data(ind?),lineStyle{?});rlim([0 600]);title(''All times, where 0<I_{ph}.'');subplot(1,2,2);polarplot(deg2rad(2.5+0:360/nPhaseSegm:(360-1)),iPhvsPhaseSegm?,''-black'',''LineWidth'',2);rlim([0 600]);title(''Median I_{ph}, where 0<I_{ph}, over 5 degrees of phase.'');suptitle([''MMS'',scStr,'' I_{ph} vs phase, p?.'']);', 5:6);
   c_eval('print(fig?, ''-dpng'', [sweepFolder,''summary_plots/SDP/iPhVsPhasePolar_mms'',scStr,''_p?.png'']);', 1:4);
   c_eval('print(fig?, ''-dpng'', [sweepFolder,''summary_plots/ADP/iPhVsPhasePolar_mms'',scStr,''_p?.png'']);', 5:6);
+  close all % close plots
+
+  % iPh_knee vs phase
+  %c_eval('ind?=Sw.p?_iPh_ts.data>50&Sw.p?_iPh_ts.data<500;', 1:4);
+  c_eval('ind?=Sw.p?_iPh_knee_ts.data>0;', 1:6);
+  nPhaseSegm = 360 / 5; % Number of phase segments
+  c_eval('phaseInd?=discretize(Sw.p?_phase_ts.data,nPhaseSegm);', 1:6); % Index of which phase segment data was measured in
+  c_eval('iPhvsPhaseSegm?=zeros(nPhaseSegm,1);',1:6);
+  c_eval('iPhvsPhaseSegm!(?)=median(Sw.p!_iPh_knee_ts.data(phaseInd!==? & ind!));', 1:nPhaseSegm, 1:6);
+  c_eval('fig?=figure(''units'',''normalized'',''outerposition'',[0 0 1 1]);subplot(1,2,1);polarplot(deg2rad(Sw.p?_phase_ts.data(ind?)),Sw.p?_iPh_knee_ts.data(ind?),lineStyle{?});rlim([0 600]);title(''All times, where I_{ph}_{knee}>0.'');subplot(1,2,2);polarplot(deg2rad(360/(2*nPhaseSegm)+(0:360/nPhaseSegm:360)),[iPhKneevsPhaseSegm?; iPhKneevsPhaseSegm?(1)],''-black'',''LineWidth'',2);rlim([0 600]);title(''Median I_{ph}_{knee}, where I_{ph}_{knee}>0, over 5 degrees of phase.'');rticks([0 100 200 300 400 500 600]);suptitle([''Plot created: '',nowStr,''. MMS'',scStr,'' I_{ph}_{knee} vs phase, p?.'']);', 1:6);
+  %c_eval('fig?=figure(''units'',''normalized'',''outerposition'',[0 0 1 1]);subplot(1,2,1);polarplot(deg2rad(Sw.p?_phase_ts.data(ind?)),Sw.p?_iPh_ts.data(ind?),lineStyle{?});rlim([0 600]);title(''All times, where 0<I_{ph}.'');subplot(1,2,2);polarplot(deg2rad(2.5+0:360/nPhaseSegm:(360-1)),iPhvsPhaseSegm?,''-black'',''LineWidth'',2);rlim([0 600]);title(''Median I_{ph}, where 0<I_{ph}, over 5 degrees of phase.'');suptitle([''MMS'',scStr,'' I_{ph} vs phase, p?.'']);', 5:6);
+  c_eval('print(fig?, ''-dpng'', [sweepFolder,''summary_plots/SDP/iPhKneeVsPhasePolar_mms'',scStr,''_p?.png'']);', 1:4);
+  c_eval('print(fig?, ''-dpng'', [sweepFolder,''summary_plots/ADP/iPhKneeVsPhasePolar_mms'',scStr,''_p?.png'']);', 5:6);
   close all % close plots
   
   % impedance vs phase

commit a3de502a5a5b637f6b2e873f53c4adf00cb22719

MMS sweep "phase" is reverted back to phase of iPh
and a new "phase_knee" is added, computed as spin phase of instance when
iPh_knee was measured. For times where some attitude information is
available (be it sunpulse or defatt), and potentially split for
"+-" & "+-" types of sweeps.

Fixes potential backwards compatibility issue introduced in 2eb76580e9.

mission/mms/mms_edp_Sweep.m
@@ -121,6 +121,7 @@ classdef mms_edp_Sweep < handle
         tmp.iPh_knee = NaN; % New knee current value, new "2021 iPh"
         tmp.impedance = NaN;
         tmp.phase = NaN;
+        tmp.phase_knee = NaN;
         tmp.type = type;
         tmp.impedance_src = ''; % keep track source used for ".impedance" (is it from "plot_time()" or "analyse()")
         switch prb1
@@ -192,9 +193,13 @@ classdef mms_edp_Sweep < handle
       tmp2 = compute_IPh_Impedance(voltage2, v02, biasRes2, type, sweepTime);
       % Default NaN-filled phase
       if ismember(type, {'+-', '-+'})
+        ph_knee_tmp1 = {NaN, NaN};
+        ph_knee_tmp2 = {NaN, NaN};
         ph_tmp1 = {NaN, NaN};
         ph_tmp2 = {NaN, NaN};
       else
+        ph_knee_tmp1 = NaN;
+        ph_knee_tmp2 = NaN;
         ph_tmp1 = NaN;
         ph_tmp2 = NaN;
       end
@@ -206,57 +211,103 @@ classdef mms_edp_Sweep < handle
           case {'+-', '-+'}
             % sweep has been split
             if isfield(sps, 'zphase')
-              % phase from defatt
+              % phase from defatt, knee times
               if ~isnan(tmp1.iPh_knee_time{1})
                 phase = mms_defatt_phase(sps, tmp1.iPh_knee_time{1});
-                ph_tmp1{1} = phase.data;
+                ph_knee_tmp1{1} = phase.data;
               end
               if ~isnan(tmp1.iPh_knee_time{2})
                 phase = mms_defatt_phase(sps, tmp1.iPh_knee_time{2});
-                ph_tmp1{2} = phase.data;
+                ph_knee_tmp1{2} = phase.data;
               end
               if ~isnan(tmp2.iPh_knee_time{1})
                 phase = mms_defatt_phase(sps, tmp2.iPh_knee_time{1});
-                ph_tmp2{1} = phase.data;
+                ph_knee_tmp2{1} = phase.data;
               end
               if ~isnan(tmp2.iPh_knee_time{2})
                 phase = mms_defatt_phase(sps, tmp2.iPh_knee_time{2});
+                ph_knee_tmp2{2} = phase.data;
+              end
+              % phase from defatt, iPh times
+              if ~isnan(tmp1.iPh_time{1})
+                phase = mms_defatt_phase(sps, tmp1.iPh_time{1});
+                ph_tmp1{1} = phase.data;
+              end
+              if ~isnan(tmp1.iPh_time{2})
+                phase = mms_defatt_phase(sps, tmp1.iPh_time{2});
+                ph_tmp1{2} = phase.data;
+              end
+              if ~isnan(tmp2.iPh_time{1})
+                phase = mms_defatt_phase(sps, tmp2.iPh_time{1});
+                ph_tmp2{1} = phase.data;
+              end
+              if ~isnan(tmp2.iPh_time{2})
+                phase = mms_defatt_phase(sps, tmp2.iPh_time{2});
                 ph_tmp2{2} = phase.data;
               end
             else
-              % phase from sunpulse
+              % phase from sunpulse, knee times
               if ~isnan(tmp1.iPh_knee_time{1})
-                [ph_tmp1{1}, ~] = mms_sdp_phase_2(sps, tmp1.iPh_knee_time{1});
+                [ph_knee_tmp1{1}, ~] = mms_sdp_phase_2(sps, tmp1.iPh_knee_time{1});
               end
               if ~isnan(tmp1.iPh_knee_time{2})
-                [ph_tmp1{2}, ~] = mms_sdp_phase_2(sps, tmp1.iPh_knee_time{2});
+                [ph_knee_tmp1{2}, ~] = mms_sdp_phase_2(sps, tmp1.iPh_knee_time{2});
               end
               if ~isnan(tmp2.iPh_knee_time{1})
-                [ph_tmp2{1}, ~] = mms_sdp_phase_2(sps, tmp2.iPh_knee_time{1});
+                [ph_knee_tmp2{1}, ~] = mms_sdp_phase_2(sps, tmp2.iPh_knee_time{1});
               end
               if ~isnan(tmp2.iPh_knee_time{2})
-                [ph_tmp2{2}, ~] = mms_sdp_phase_2(sps, tmp2.iPh_knee_time{2});
+                [ph_knee_tmp2{2}, ~] = mms_sdp_phase_2(sps, tmp2.iPh_knee_time{2});
+              end
+              % phase from sunpulse, iPh times
+              if ~isnan(tmp1.iPh_time{1})
+                [ph_tmp1{1}, ~] = mms_sdp_phase_2(sps, tmp1.iPh_time{1});
+              end
+              if ~isnan(tmp1.iPh_time{2})
+                [ph_tmp1{2}, ~] = mms_sdp_phase_2(sps, tmp1.iPh_time{2});
+              end
+              if ~isnan(tmp2.iPh_time{1})
+                [ph_tmp2{1}, ~] = mms_sdp_phase_2(sps, tmp2.iPh_time{1});
+              end
+              if ~isnan(tmp2.iPh_time{2})
+                [ph_tmp2{2}, ~] = mms_sdp_phase_2(sps, tmp2.iPh_time{2});
               end
             end
           case {'++', '--'}
             % Plain "++" or "--" sweeps
             if isfield(sps, 'zphase')
-              % phase from Defatt
+              % phase from Defatt, knee times
               if ~isnan(tmp1.iPh_knee_time)
                 phase = mms_defatt_phase(sps, tmp1.iPh_knee_time);
-                ph_tmp1 = phase.data;
+                ph_knee_tmp1 = phase.data;
               end
               if ~isnan(tmp2.iPh_knee_time)
                 phase = mms_defatt_phase(sps, tmp2.iPh_knee_time);
+                ph_knee_tmp2 = phase.data;
+              end
+              % phase from Defatt, iPh times
+              if ~isnan(tmp1.iPh_time)
+                phase = mms_defatt_phase(sps, tmp1.iPh_time);
+                ph_tmp1 = phase.data;
+              end
+              if ~isnan(tmp2.iPh_time)
+                phase = mms_defatt_phase(sps, tmp2.iPh_time);
                 ph_tmp2 = phase.data;
               end
             else
-              % phase from sunpulse
+              % phase from sunpulse, knee times
               if ~isnan(tmp1.iPh_knee_time)
-                [ph_tmp1, ~] = mms_sdp_phase_2(sps, tmp1.iPh_knee_time);
+                [ph_knee_tmp1, ~] = mms_sdp_phase_2(sps, tmp1.iPh_knee_time);
               end
               if ~isnan(tmp2.iPh_knee_time)
-                [ph_tmp2, ~] = mms_sdp_phase_2(sps, tmp2.iPh_knee_time);
+                [ph_knee_tmp2, ~] = mms_sdp_phase_2(sps, tmp2.iPh_knee_time);
+              end
+              % phase from sunpulse, iPh times
+              if ~isnan(tmp1.iPh_time)
+                [ph_tmp1, ~] = mms_sdp_phase_2(sps, tmp1.iPh_time);
+              end
+              if ~isnan(tmp2.iPh_time)
+                [ph_tmp2, ~] = mms_sdp_phase_2(sps, tmp2.iPh_time);
               end
             end
           otherwise
@@ -284,9 +335,13 @@ classdef mms_edp_Sweep < handle
       obj.(p_1)(obj.pTable(2,iSweep)).impedance = tmp1.impedance;
       obj.(p_2)(obj.pTable(2,iSweep)).impedance = tmp2.impedance;
       if ismember(type, {'+-', '-+'})
+        obj.(p_1)(obj.pTable(2,iSweep)).phase_knee = {mod(ph_knee_tmp1{1}+angle1+90,360)-90, mod(ph_knee_tmp1{2}+angle1+90,360)-90};
+        obj.(p_2)(obj.pTable(2,iSweep)).phase_knee = {mod(ph_knee_tmp2{1}+angle2+90,360)-90, mod(ph_knee_tmp2{2}+angle2+90,360)-90};
         obj.(p_1)(obj.pTable(2,iSweep)).phase = {mod(ph_tmp1{1}+angle1+90,360)-90, mod(ph_tmp1{2}+angle1+90,360)-90};
         obj.(p_2)(obj.pTable(2,iSweep)).phase = {mod(ph_tmp2{1}+angle2+90,360)-90, mod(ph_tmp2{2}+angle2+90,360)-90};
       else
+        obj.(p_1)(obj.pTable(2,iSweep)).phase_knee = mod(ph_knee_tmp1+angle1+90,360)-90;
+        obj.(p_2)(obj.pTable(2,iSweep)).phase_knee = mod(ph_knee_tmp2+angle2+90,360)-90;
         obj.(p_1)(obj.pTable(2,iSweep)).phase = mod(ph_tmp1+angle1+90,360)-90;
         obj.(p_2)(obj.pTable(2,iSweep)).phase = mod(ph_tmp2+angle2+90,360)-90;
       end
@@ -307,10 +362,12 @@ classdef mms_edp_Sweep < handle
           tmp.impedance = {NaN, NaN};
           tmp.iPh_knee_time = {NaN, NaN}; % Time of derived iPh_knee (first and second part)
           tmp.iPh = {NaN, NaN};
+          tmp.iPh_time = {NaN, NaN};  % Time of derived iPh (first & second part)
         else
           tmp.iPh_knee = NaN;
           tmp.impedance = NaN;
           tmp.iPh_knee_time = NaN;
+          tmp.iPh_time = NaN;
         end
         
         %% Find settling time (with fallback max 10% of sweep) based on
@@ -400,17 +457,24 @@ classdef mms_edp_Sweep < handle
           vmin = min(voltFirst);
           [~, old_indIph] = min(abs(voltFirst-(2*v0+vmin)/3));
           tmp.iPh{1} = -biasFirst(old_indIph);
+          % Compute time of iPh
+          timeArray = int64(linspace(0, double(sweepTime.stop.ttns-sweepTime.start.ttns), length(biasRes))) + sweepTime.start.ttns;
+          tmp.iPh_time{1} = timeArray(8+old_indIph);
           % second half
           biasSecond = biasResSegm(splitInd+1:end);
           voltSecond = voltageSegm(splitInd+1:end);
           [~, old_indIph] = min(abs(voltSecond-(2*v0+vmin)/3));         
           tmp.iPh{2} = -biasSecond(old_indIph);
+          tmp.iPh_time{2} = timeArray(splitInd + old_indIph);
         else
           % basic one way no need to split
           % Find maximum negative voltage
           vmin = min(voltage(9:end));
           [~, old_indIph] = min(abs(voltage(9:end)-(2*v0+vmin)/3));
           tmp.iPh = -biasRes(8+old_indIph);
+          % Compute time of iPh
+          timeArray = int64(linspace(0, double(sweepTime.stop.ttns-sweepTime.start.ttns), length(biasRes))) + sweepTime.start.ttns;
+          tmp.iPh_time = timeArray(8+old_indIph);
         end
         if oldFallback
           if ismember(type, {'+-', '-+'})
@@ -722,11 +786,14 @@ classdef mms_edp_Sweep < handle
         pha2 = obj.(p_2)(obj.pTable(2,iSweep)).phase;
         Iph1_knee = obj.(p_1)(obj.pTable(2,iSweep)).iPh_knee;
         Iph2_knee = obj.(p_2)(obj.pTable(2,iSweep)).iPh_knee;
+        pha1_knee = obj.(p_1)(obj.pTable(2,iSweep)).phase_knee;
+        pha2_knee = obj.(p_2)(obj.pTable(2,iSweep)).phase_knee;
       else
         Iph1 = NaN; Iph2 = NaN;
         dVdI1 = NaN; dVdI2 = NaN;
         pha1 = NaN; pha2 = NaN;
         Iph1_knee = NaN; Iph2_knee = NaN;
+        pha1_knee = NaN; pha2_knee = NaN;
       end
       % Output for debugging
       % [double(prb1) double(prb2) Iph1 Iph2 dVdI1 dVdI2]
@@ -734,14 +801,14 @@ classdef mms_edp_Sweep < handle
         legend(h, ...
           sprintf('V%d  Iph %0.1f & %0.1f (knee: %0.1f & %0.1f) nA,  Imp: %0.2f (& %0.2f) MOhm @ %0.1f (& %0.1f) nA', prb1, Iph1{1}, Iph1{2}, Iph1_knee{1}, Iph1_knee{2}, dVdI1{1}, dVdI1{2}, 0.75*Iph1{1}, 0.75*Iph1{2}),...
           sprintf('V%d  Iph %0.1f & %0.1f (knee: %0.1f & %0.1f) nA,  Imp: %0.2f (& %0.2f) MOhm @ %0.1f (& %0.1f) nA', prb2, Iph2{1}, Iph2{2}, Iph2_knee{1}, Iph2_knee{2}, dVdI2{1}, dVdI2{2}, 0.75*Iph2{1}, 0.75*Iph2{2}),...
-          sprintf('Phase%d  %0.1f & %0.1f,  Phase%d  %0.1f & %0.1f degrees Type %s', prb1, pha1{1}, pha1{2}, prb2, pha2{1}, pha2{2}, type),...
+          sprintf('Phase%d  %0.1f & %0.1f (knee: %0.1f & %0.1f),  Phase%d  %0.1f & %0.1f (knee: %0.1f & %0.1f) degrees Type %s', prb1, pha1{1}, pha1{2}, pha1_knee{1}, pha1_knee{2}, prb2, pha2{1}, pha2{2}, pha2_knee{1}, pha2_knee{2}, type),...
           'Location', 'NorthWest', ...
           'AutoUpdate', 'off');
       else
         legend(h, ...
           sprintf('V%d  Iph %0.1f (knee: %0.1f) nA,  %0.2f MOhm @ %0.1f nA', prb1, Iph1, Iph1_knee, dVdI1, 0.75*Iph1),...
           sprintf('V%d  Iph %0.1f (knee: %0.1f) nA,  %0.2f MOhm @ %0.1f nA', prb2, Iph2, Iph2_knee, dVdI2, 0.75*Iph2),...
-          sprintf('Phase%d  %0.1f,  Phase%d  %0.1f degrees Type %s', prb1, pha1, prb2, pha2, type),...
+          sprintf('Phase%d  %0.1f (knee: %0.1f),  Phase%d  %0.1f (knee: %0.1f) degrees Type %s', prb1, pha1, pha1_knee, prb2, pha2, pha2_knee, type),...
           'Location', 'NorthWest', ...
           'AutoUpdate', 'off');
       end

mission/mms/mms_testFunctions/mms_sweep_plot_cron.m
@@ -101,6 +101,7 @@ for iSc = 1:4
   c_eval('S.p?_iPh_ts=irf.ts_scalar(p?_time,[sObj.p?.iPh]);', 1:6); % TSeries of iPh
   c_eval('S.p?_iPh_knee_ts=irf.ts_scalar(p?_time,[sObj.p?.iPh_knee]);', 1:6); % TSeries of iPh_knee
   c_eval('S.p?_phase_ts=irf.ts_scalar(p?_time,mod([sObj.p?.phase],360));', 1:6); % TSeries of phase
+  c_eval('S.p?_phase_knee_ts=irf.ts_scalar(p?_time,mod([sObj.p?.phase_knee],360));', 1:6); % TSeries of phase
   c_eval('S.p?_type_ts=irf.ts_vec_xy(p?_time, double(reshape([sObj.p?.type],2,length(sObj.p?))''));',1:6); % TSeries of type ('--' or '++' etc converted from ASCII char to double)
   %save(sprintf('%sobj/mms%i_%s_sweepTsObj.mat', sweepFolder, iSc, dayToRun), 'S');
   
@@ -197,14 +198,14 @@ for iSc = 1:4
   c_eval('print(fig?, ''-dpng'', [sweepFolder,''summary_plots/ADP/iPhVsPhasePolar_mms'',scStr,''_p?.png'']);', 5:6);
   close all % close plots
 
-  % iPh_knee vs phase
+  % iPh_knee vs phase_knee
   %c_eval('ind?=Sw.p?_iPh_ts.data>50&Sw.p?_iPh_ts.data<500;', 1:4);
   c_eval('ind?=Sw.p?_iPh_knee_ts.data>0;', 1:6);
   nPhaseSegm = 360 / 5; % Number of phase segments
-  c_eval('phaseInd?=discretize(Sw.p?_phase_ts.data,nPhaseSegm);', 1:6); % Index of which phase segment data was measured in
+  c_eval('phaseInd?=discretize(Sw.p?_phase_knee_ts.data,nPhaseSegm);', 1:6); % Index of which phase segment data was measured in
   c_eval('iPhvsPhaseSegm?=zeros(nPhaseSegm,1);',1:6);
   c_eval('iPhvsPhaseSegm!(?)=median(Sw.p!_iPh_knee_ts.data(phaseInd!==? & ind!));', 1:nPhaseSegm, 1:6);
-  c_eval('fig?=figure(''units'',''normalized'',''outerposition'',[0 0 1 1]);subplot(1,2,1);polarplot(deg2rad(Sw.p?_phase_ts.data(ind?)),Sw.p?_iPh_knee_ts.data(ind?),lineStyle{?});rlim([0 600]);title(''All times, where I_{ph}_{knee}>0.'');subplot(1,2,2);polarplot(deg2rad(360/(2*nPhaseSegm)+(0:360/nPhaseSegm:360)),[iPhKneevsPhaseSegm?; iPhKneevsPhaseSegm?(1)],''-black'',''LineWidth'',2);rlim([0 600]);title(''Median I_{ph}_{knee}, where I_{ph}_{knee}>0, over 5 degrees of phase.'');rticks([0 100 200 300 400 500 600]);suptitle([''Plot created: '',nowStr,''. MMS'',scStr,'' I_{ph}_{knee} vs phase, p?.'']);', 1:6);
+  c_eval('fig?=figure(''units'',''normalized'',''outerposition'',[0 0 1 1]);subplot(1,2,1);polarplot(deg2rad(Sw.p?_phase_knee_ts.data(ind?)),Sw.p?_iPh_knee_ts.data(ind?),lineStyle{?});rlim([0 600]);title(''All times, where I_{ph}_{knee}>0.'');subplot(1,2,2);polarplot(deg2rad(360/(2*nPhaseSegm)+(0:360/nPhaseSegm:360)),[iPhKneevsPhaseSegm?; iPhKneevsPhaseSegm?(1)],''-black'',''LineWidth'',2);rlim([0 600]);title(''Median I_{ph}_{knee}, where I_{ph}_{knee}>0, over 5 degrees of phase.'');rticks([0 100 200 300 400 500 600]);suptitle([''Plot created: '',nowStr,''. MMS'',scStr,'' I_{ph}_{knee} vs phase, p?.'']);', 1:6);
   %c_eval('fig?=figure(''units'',''normalized'',''outerposition'',[0 0 1 1]);subplot(1,2,1);polarplot(deg2rad(Sw.p?_phase_ts.data(ind?)),Sw.p?_iPh_ts.data(ind?),lineStyle{?});rlim([0 600]);title(''All times, where 0<I_{ph}.'');subplot(1,2,2);polarplot(deg2rad(2.5+0:360/nPhaseSegm:(360-1)),iPhvsPhaseSegm?,''-black'',''LineWidth'',2);rlim([0 600]);title(''Median I_{ph}, where 0<I_{ph}, over 5 degrees of phase.'');suptitle([''MMS'',scStr,'' I_{ph} vs phase, p?.'']);', 5:6);
   c_eval('print(fig?, ''-dpng'', [sweepFolder,''summary_plots/SDP/iPhKneeVsPhasePolar_mms'',scStr,''_p?.png'']);', 1:4);
   c_eval('print(fig?, ''-dpng'', [sweepFolder,''summary_plots/ADP/iPhKneeVsPhasePolar_mms'',scStr,''_p?.png'']);', 5:6);