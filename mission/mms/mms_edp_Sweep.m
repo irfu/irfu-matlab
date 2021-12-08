irfu/irfu-matlab

commit b77c01f76367c074bdacf7aa0cf706dec8fb3121

updates to plot_time

mission/mms/mms_edp_Sweep.m
@@ -194,7 +194,7 @@ classdef mms_edp_Sweep < handle
       % do the same for the other probe
       tmp2 = compute_IPh_Impedance(voltage2, v02, biasRes2);
       if doPhase
-        if strcmp(type,'+-') || strcmp(type,'-+')
+        if strcmp(type,'+-') || strcmp(type,'-+') || strcmp(type,'00')
           disp(['*** Warning, phase not computed for type ',type,' sweep ',num2str(iSweep),...
             ' of ',num2str(obj.nSweeps)]);
           ph_tmp1 = NaN;
@@ -508,6 +508,11 @@ classdef mms_edp_Sweep < handle
       [sweepTime, prb1, prb2, voltage1, biasRes1, voltage2, biasRes2,...
         eVolt, eBias, v01, v02]...
         = getSweep(obj,iSweep);
+      if isempty(voltage1)
+        disp(['*** Warning, not plotted empty sweep ',num2str(iSweep),...
+          ' of ',num2str(obj.nSweeps)]);
+        return
+      end
       dwl = obj.sweep.data.([obj.scId '_sweep_dwell']).data(iSweep);
       stp = obj.sweep.data.([obj.scId '_sweep_steps']).data(iSweep);
       len = length(voltage1);
@@ -555,16 +560,26 @@ classdef mms_edp_Sweep < handle
       if doPhase
         if plotev
           evobj=dataobj(evfile);
-          %efield = getmat(evobj,[obj.scId,'_edp_dce_sensor']);
+          efield = getmat(evobj,[obj.scId,'_edp_dce_sensor']);
           vfield = getmat(evobj,[obj.scId,'_edp_dcv_sensor']);
           ind = find(vfield(:,1)>=2*time(1)-time(2) & ...
             vfield(:,1)<= 2*time(end)-time(length(time)-1));
-          %v12 = [efield(ind,1) vfield(ind,2) vfield(ind,2)-efield(ind,2)*0.12];
-          v12 = [vfield(ind,1) vfield(ind,2) vfield(ind,3)];
-          %irf_plot(h1(2),[v12(:,1) v12(:,2) v12(:,3)],'-')
+        switch prb1
+            case 1
+                v12 = [efield(ind,1) efield(ind,2)];
+                v1 = [efield(ind,1) vfield(ind,2) vfield(ind,2)-efield(ind,2)*0.12];
+            case 3
+                v12 = [efield(ind,1) efield(ind,3)];
+                v1 = [efield(ind,1) vfield(ind,3) vfield(ind,3)-efield(ind,3)*0.12];
+            case 5
+                v12 = [efield(ind,1) efield(ind,4)];
+                v1 = [efield(ind,1) vfield(ind,4) vfield(ind,4)-efield(ind,4)*0.12];
+            otherwise, error('Bad probe number')
+        end
           h1=irf_plot({[[2*time(1)-time(2); time] [NaN; biasRes1]],...
             [[2*time(1)-time(2); time] double([v01; voltage1]) double([v02; voltage2])], ...
-            [v12(:,1) v12(:,2) v12(:,3)],...
+            [v1(:,1) v1(:,2) v1(:,3)],...
+            [v12(:,1) v12(:,2)],...
             [time(ind_impedance)...
             impedance1(ind_impedance)...
             impedance2(ind_impedance)],...
@@ -592,23 +607,30 @@ classdef mms_edp_Sweep < handle
       ylabel(h1(2),{'Voltage',['[' getunits(obj.sweep,[obj.scId '_edp_sweeps']) ']']});
       legend(h1(1),sprintf('p%d',prb1))
       legend(h1(2),['p',num2str(prb1)],['p',num2str(prb2)])
+      imp1 = impedance1(ind_impedance); medi1 = median(imp1(isfinite(imp1)));
+      imp2 = impedance2(ind_impedance); medi2 = median(imp2(isfinite(imp2)));
       if doPhase
         if plotev
-          ylabel(h1(3),{'Voltage',['[' getunits(obj.sweep,[obj.scId '_edp_sweeps']) ']']});
-          ylabel(h1(4),{'dVdI','[MOhm]'})
-          ylabel(h1(5),{'Phase','[deg]'})
+          ylabel(h1(3),{'V',['[' getunits(obj.sweep,[obj.scId '_edp_sweeps']) ']']});
+          ylabel(h1(4),{'E',['[' 'mV/m' ']']});
+          ylabel(h1(5),{'dVdI','[MOhm]'})
+          ylabel(h1(6),{'Phase','[deg]'})
           legend(h1(3),['p',num2str(prb1)],['p',num2str(prb2)])
-          legend(h1(4),['p',num2str(prb1)],['p',num2str(prb2)])
-          legend(h1(5),sprintf('p%d',prb1))
+          legend(h1(4),['p',num2str(prb1),num2str(prb2)])
+          legend(h1(5),['p',num2str(prb1),' median=',num2str(medi1,'%4.1f')],...
+              ['p',num2str(prb2),' median=',num2str(medi2,'%4.1f')])
+          legend(h1(6),sprintf('p%d',prb1))
         else
           ylabel(h1(3),{'dVdI','[MOhm]'})
           ylabel(h1(4),{'Phase','[deg]'})
-          legend(h1(3),['p',num2str(prb1)],['p',num2str(prb2)])
+          legend(h1(3),['p',num2str(prb1),' median=',num2str(medi1,'%4.1f')],...
+              ['p',num2str(prb2),' median=',num2str(medi2,'%4.1f')])
           legend(h1(4),sprintf('p%d',prb1))
         end
       else
         ylabel(h1(3),{'dVdI','[MOhm]'})
-        legend(h1(3),['p',num2str(prb1)],['p',num2str(prb2)])
+        legend(h1(3),['p',num2str(prb1),' median=',num2str(medi1,'%4.1f')],...
+            ['p',num2str(prb2),' median=',num2str(medi2,'%4.1f')])
       end
       if nargout, hout = h1; end
     end % PLOT_TIME

commit e2e146c8a07ae6c7027d6427b2a843d5891cdba2

Correct scaling ADP probe 0.0292 (0.12 is for SDP)
ADP booms are 29.2m apart and e-field measured in mV/m, the 0.120
factor should be valid for SDP probes only (120m apart).

This bug was first introduced back in commit b77c01f763 and appears to
have gone unnoticed since then (perhaps the "plot_time()" with "evfile"
is not used that often)..

mission/mms/mms_edp_Sweep.m
@@ -844,13 +844,13 @@ classdef mms_edp_Sweep < handle
           switch prb1
             case 1
               v12 = irf.ts_scalar(efield.time(ind), efield.data(ind, 1));
-              v1 = irf.ts_vec_xy(efield.time(ind), [vfield.data(ind, 1), vfield.data(ind, 1)-efield.data(ind, 1)*0.12]);
+              v1 = irf.ts_vec_xy(efield.time(ind), [vfield.data(ind, 1), vfield.data(ind, 1)-efield.data(ind, 1)*0.12]); % SDP distance 120 m (*10^-3 since mV->V)
             case 3
               v12 = irf.ts_scalar(efield.time(ind), efield.data(ind, 2));
               v1 = irf.ts_vec_xy(efield.time(ind), [vfield.data(ind, 2), vfield.data(ind, 2)-efield.data(ind, 2)*0.12]);
             case 5
               v12 = irf.ts_scalar(efield.time(ind), efield.data(ind, 3));
-              v1 = irf.ts_vec_xy(efield.time(ind), [vfield.data(ind, 3), vfield.data(ind, 3)-efield.data(ind, 3)*0.12]);
+              v1 = irf.ts_vec_xy(efield.time(ind), [vfield.data(ind, 3), vfield.data(ind, 3)-efield.data(ind, 3)*0.0292]); % ADP distance 29.2 m
             otherwise, error('Bad probe number')
           end
           h1=irf_plot({...