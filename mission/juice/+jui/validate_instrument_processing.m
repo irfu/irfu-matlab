a262eea10e0037533917b6f8092ddce4c99dba15
thomas-nilsson-irfu <thomas-nilsson-irfu@users.noreply.github.com>
2023-11-27 09:57:13 +0100

    Correct indentation as per irfu-matlab guidelines
    As specified in the .github/CONTRIBUTING.md,
    new code should use only two (2) spaces for indentation (only exceptions
    is contrib/ and old SolO code).

--- mission/juice/+jui/validate_instrument_processing.m
+++ mission/juice/+jui/validate_instrument_processing.m
@@ -32,136 +32,136 @@
 % Author: Erik P G Johansson, IRF, Uppsala, Sweden
 %
 function validate_instrument_processing(dirPath, reportErrorCounterNonzero)
-    % PROPOSAL: Better name.
-    %   TM, L1a, CDFs, datasets, data
-    %   validate, check
-    %   instrument
-    %   processing
-    %       CON: Does not have to do with TM-to-L1a processing or GS.
-    %   validate_instrument_processing
-    %
-    % PROPOSAL: Implement in Python.
-    %   PRO: Could load MIB with pre-existing code.
-    %   CON: Harder to plot.
-    %       CON-PROPOSAL: Once a problem has been identified, can then just as
-    %                     well plot outside the script, i.e. in MATLAB.
-    %       CON-PROPOSAL: (Should) use RPWIQuicklooks.
-    %
-    % PROPOSAL: Print timestamps for when error counters increment.
-    %   CON: Makes code too complicated. Detracts from core purpose.
-    % PROPOSAL: Add plots somehow for determining when error counters increment.
-    %   How plot time axis?!
-    %   CON: Makes code too complicated. Detracts from core purpose.
-    %
-    % PROPOSAL: Log to file.
-    %
-    % NOTE: Has no access to MIB data here, as opposed to in the GS pipeline.
-    %       Can therefore not look up e.g. packet type descriptions.
+% PROPOSAL: Better name.
+%   TM, L1a, CDFs, datasets, data
+%   validate, check
+%   instrument
+%   processing
+%       CON: Does not have to do with TM-to-L1a processing or GS.
+%   validate_instrument_processing
+%
+% PROPOSAL: Implement in Python.
+%   PRO: Could load MIB with pre-existing code.
+%   CON: Harder to plot.
+%       CON-PROPOSAL: Once a problem has been identified, can then just as
+%                     well plot outside the script, i.e. in MATLAB.
+%       CON-PROPOSAL: (Should) use RPWIQuicklooks.
+%
+% PROPOSAL: Print timestamps for when error counters increment.
+%   CON: Makes code too complicated. Detracts from core purpose.
+% PROPOSAL: Add plots somehow for determining when error counters increment.
+%   How plot time axis?!
+%   CON: Makes code too complicated. Detracts from core purpose.
+%
+% PROPOSAL: Log to file.
+%
+% NOTE: Has no access to MIB data here, as opposed to in the GS pipeline.
+%       Can therefore not look up e.g. packet type descriptions.
 
-    assert(ischar(dirPath))
+assert(ischar(dirPath))
 
-    validate_error_packets(dirPath)
-    validate_error_counters(dirPath, reportErrorCounterNonzero)
+validate_error_packets(dirPath)
+validate_error_counters(dirPath, reportErrorCounterNonzero)
 end
 
 
 
 % Search for 5.2, 5.3, and 5.4 packets.
 function validate_error_packets(dirPath)
-    PPD_FILENAME_RE       = 'JUICE_LU_RPWI-PPD_.*.bin.cdf';
-    SERVICE_TYPE          = 5;
-    SERVICE_SUBTYPE_ARRAY = [2,3,4];
+PPD_FILENAME_RE       = 'JUICE_LU_RPWI-PPD_.*.bin.cdf';
+SERVICE_TYPE          = 5;
+SERVICE_SUBTYPE_ARRAY = [2,3,4];
 
-    DirInfoArray = find_files(dirPath, PPD_FILENAME_RE);
+DirInfoArray = find_files(dirPath, PPD_FILENAME_RE);
 
-    for i = 1:numel(DirInfoArray)
-        cdfPath = fullfile(DirInfoArray(i).folder, DirInfoArray(i).name);
-        do      = dataobj(cdfPath);
+for i = 1:numel(DirInfoArray)
+  cdfPath = fullfile(DirInfoArray(i).folder, DirInfoArray(i).name);
+  do      = dataobj(cdfPath);
 
-        spidCa              = cellstr(read_DO_field(do, {'SPID'}));
-        serviceTypeArray    =         read_DO_field(do, {'DFH_SERVICE_TYPE'});
-        serviceSubtypeArray =         read_DO_field(do, {'DFH_SERVICE_SUBTYPE'});
+  spidCa              = cellstr(read_DO_field(do, {'SPID'}));
+  serviceTypeArray    =         read_DO_field(do, {'DFH_SERVICE_TYPE'});
+  serviceSubtypeArray =         read_DO_field(do, {'DFH_SERVICE_SUBTYPE'});
 
-        fprintf('cdfFile = "%s"\n', cdfPath)
+  fprintf('cdfFile = "%s"\n', cdfPath)
 
-        b = (serviceTypeArray == SERVICE_TYPE) & ismember(serviceSubtypeArray, SERVICE_SUBTYPE_ARRAY);
-        iEPackets = find(b);
+  b = (serviceTypeArray == SERVICE_TYPE) & ismember(serviceSubtypeArray, SERVICE_SUBTYPE_ARRAY);
+  iEPackets = find(b);
 
-        if ~isempty(iEPackets)
-            nErrorPackets = numel(iEPackets);
+  if ~isempty(iEPackets)
+    nErrorPackets = numel(iEPackets);
 
-            [~, jUniqueEPacketsArray] = unique(spidCa(iEPackets));
-            iUniqueEPacketsArray = iEPackets(jUniqueEPacketsArray);
+    [~, jUniqueEPacketsArray] = unique(spidCa(iEPackets));
+    iUniqueEPacketsArray = iEPackets(jUniqueEPacketsArray);
 
-            log_warning(0, 'Found %i error packet(s) (5.2, 5.3, 5.4). Unique packet types:', ...
-                nErrorPackets)
-            for iUniqueEPacket = iUniqueEPacketsArray(:)'
-                st = serviceTypeArray(   iUniqueEPacket);
-                ss = serviceSubtypeArray(iUniqueEPacket);
-                spid = spidCa{           iUniqueEPacket};
-                log_warning(1, '%i.%i: SPID=%s', st, ss, spid)
-            end
-        end
+    log_warning(0, 'Found %i error packet(s) (5.2, 5.3, 5.4). Unique packet types:', ...
+      nErrorPackets)
+    for iUniqueEPacket = iUniqueEPacketsArray(:)'
+      st = serviceTypeArray(   iUniqueEPacket);
+      ss = serviceSubtypeArray(iUniqueEPacket);
+      spid = spidCa{           iUniqueEPacket};
+      log_warning(1, '%i.%i: SPID=%s', st, ss, spid)
     end
+  end
+end
 
 end
 
 
 
 function validate_error_counters(dirPath, reportErrorCounterNonzero)
-    PPTD_HK64_FILENAME_RE = 'JUICE_LU_RPWI-PPTD-LWYHK[01]0064_.*\.cdf';
-    ERROR_CORE0_MPN_CA    = {'LWT0343D', 'LWT0456B'};
-    ERROR_CORE1_MPN_CA    = {'LWT0343E', 'LWT0456C'};
+PPTD_HK64_FILENAME_RE = 'JUICE_LU_RPWI-PPTD-LWYHK[01]0064_.*\.cdf';
+ERROR_CORE0_MPN_CA    = {'LWT0343D', 'LWT0456B'};
+ERROR_CORE1_MPN_CA    = {'LWT0343E', 'LWT0456C'};
 
-    DirInfoArray = find_files(dirPath, PPTD_HK64_FILENAME_RE);
+DirInfoArray = find_files(dirPath, PPTD_HK64_FILENAME_RE);
 
-    for i = 1:numel(DirInfoArray)
-        cdfPath = fullfile(DirInfoArray(i).folder, DirInfoArray(i).name);
-        do = dataobj(cdfPath);
+for i = 1:numel(DirInfoArray)
+  cdfPath = fullfile(DirInfoArray(i).folder, DirInfoArray(i).name);
+  do = dataobj(cdfPath);
 
-        fprintf('cdfFile = "%s"\n', cdfPath)
+  fprintf('cdfFile = "%s"\n', cdfPath)
 
-        errorCounter0Array = read_DO_field(do, ERROR_CORE0_MPN_CA);
-        errorCounter1Array = read_DO_field(do, ERROR_CORE1_MPN_CA);
+  errorCounter0Array = read_DO_field(do, ERROR_CORE0_MPN_CA);
+  errorCounter1Array = read_DO_field(do, ERROR_CORE1_MPN_CA);
 
-        validate_error_counter_array(0, errorCounter0Array, reportErrorCounterNonzero)
-        validate_error_counter_array(1, errorCounter1Array, reportErrorCounterNonzero)
-    end
+  validate_error_counter_array(0, errorCounter0Array, reportErrorCounterNonzero)
+  validate_error_counter_array(1, errorCounter1Array, reportErrorCounterNonzero)
+end
 end
 
 
 
 % Validate one array of error counter values.
 function validate_error_counter_array(iCounter, counterArray, reportErrorCounterNonzero)
-    assert(isscalar(reportErrorCounterNonzero) & islogical(reportErrorCounterNonzero))
+assert(isscalar(reportErrorCounterNonzero) & islogical(reportErrorCounterNonzero))
 
-    strUniqueValues = sprintf('Unique values: %s', num2str(unique(counterArray(:)')));
+strUniqueValues = sprintf('Unique values: %s', num2str(unique(counterArray(:)')));
 
-    n = numel(unique(counterArray));
-    if ~ismember(n, [0, 1])
-        log_warning(0, 'Core%i error counter values are NOT CONSTANT. %s', ...
-            iCounter, strUniqueValues)
-    end
+n = numel(unique(counterArray));
+if ~ismember(n, [0, 1])
+  log_warning(0, 'Core%i error counter values are NOT CONSTANT. %s', ...
+    iCounter, strUniqueValues)
+end
 
-    if reportErrorCounterNonzero
-        if ~all(counterArray == 0)
-            log_warning(0, 'Core%i error counter values are not equal to zero. %s', ...
-                iCounter, strUniqueValues)
-        end
-    end
+if reportErrorCounterNonzero
+  if ~all(counterArray == 0)
+    log_warning(0, 'Core%i error counter values are not equal to zero. %s', ...
+      iCounter, strUniqueValues)
+  end
+end
 end
 
 
 
 function DirInfoArray = find_files(rootDirPath, rePattern)
-    DirInfoArray = dir(fullfile(rootDirPath, '**/*.cdf'));
+DirInfoArray = dir(fullfile(rootDirPath, '**/*.cdf'));
 
-    bIsFile = ~[DirInfoArray.isdir];
+bIsFile = ~[DirInfoArray.isdir];
 
-    ca = regexp({DirInfoArray.name}, rePattern);
-    bFilenameMatch = ~cellfun(@isempty, ca);
+ca = regexp({DirInfoArray.name}, rePattern);
+bFilenameMatch = ~cellfun(@isempty, ca);
 
-    DirInfoArray = DirInfoArray(bIsFile & bFilenameMatch);
+DirInfoArray = DirInfoArray(bIsFile & bFilenameMatch);
 end
 
 
@@ -171,25 +171,25 @@ end
 % zvNameCandidatesCa
 %       1D CA of ZV names. Exactly one should be valid.
 function value = read_DO_field(do, zvNameCandidatesCa)
-    assert(isa(do, 'dataobj'))
+assert(isa(do, 'dataobj'))
 
-    bArray = ismember(zvNameCandidatesCa, fieldnames(do.data));
-    assert(sum(bArray) == 1, 'Can not find exactly one matching field.')
+bArray = ismember(zvNameCandidatesCa, fieldnames(do.data));
+assert(sum(bArray) == 1, 'Can not find exactly one matching field.')
 
-    fieldName = zvNameCandidatesCa{bArray};
-    value = do.data.(fieldName).data;
+fieldName = zvNameCandidatesCa{bArray};
+value = do.data.(fieldName).data;
 end
 
 
 
 % Print a one-row warning to stdout.
 function log_warning(iLevel, s, varargin)
-    assert(iLevel >= 0)
+assert(iLevel >= 0)
 
-    if iLevel == 0
-        s = ['WARNING: ', s];
-    end
+if iLevel == 0
+  s = ['WARNING: ', s];
+end
 
-    s = [repmat('    ', 1, iLevel+1), s, '\n'];
-    fprintf(s, varargin{:})
+s = [repmat('    ', 1, iLevel+1), s, '\n'];
+fprintf(s, varargin{:})
 end
