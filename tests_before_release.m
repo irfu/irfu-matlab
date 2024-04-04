a7763f581c9c562969f756bb2b7449af7ea32412
Erik P G Johansson <erik.johansson@irfu.se>
2024-04-04 11:34:54 +0200

    solo.db_list_files: Add tests

--- tests_before_release.m
+++ tests_before_release.m
@@ -37,7 +37,8 @@ suite = testsuite(testsToRun);
 % Add tests for MATLAB packages in which automated test files can be
 % automatically found via MATLAB's filenaming convention.
 % NOTE: Excludes BICAS which requires MATLAB R2019b.
-for pkgPathCa = {'irf.str', 'irf.utils', 'solo.adm', 'solo.hwzv', 'solo.shk', 'solo.qli'}
+% NOTE: Searches for tests recursively.
+for pkgPathCa = {'irf.str', 'irf.utils', 'solo'}
   suite = [ ...
     suite, matlab.unittest.TestSuite.fromPackage(...
     pkgPathCa{1}, 'IncludingSubpackages', true) ...
