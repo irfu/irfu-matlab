352c4d8ff8cbdd80b786b33c18c5d2f252e57d1c
Erik P G Johansson <erik.johansson@irfu.se>
2024-05-16 15:58:59 +0200

    Cleanup

--- irf/+irf/+fs/read_text_file.m
+++ irf/+irf/+fs/read_text_file.m
@@ -1,29 +1,26 @@
 %
-% Reads file as text file and stores lines in cell array.
+% Reads file as text file and returns the rows in a 1D cell array.
 %
 %
 % RETURN VALUE
 % ============
-% rowsList : Column cell array of strings (one per row). All characters in the line breaks have been removed so
-%            that it works for both Windows-style and Unix-style text files (assuming linebreakRegexp has been set
-%            correctly).
-%            NOTE: There will be a row after the last line break. The caller has to decide whether this is a legitimate
-%            row.
-% linebreakRegexp : Regular expression used as line break.
+% rowsCa
+%       Column cell array of strings (one per row). All characters in the line
+%       breaks have been removed so that it works for both Windows-style and
+%       Unix-style text files (assuming linebreakRegexp has been set correctly).
+%       NOTE: There will be a row after the last line break. The caller has to
+%       decide whether this is a legitimate row.
+% linebreakRegexp
+%       Regular expression used as line break.
 %
-%
-% IMPLEMENTATION NOTE
-% ===================
-% Old implementation using textscan merged consequtive linebreaks (CR+LF) into one (i.e. bug). Therefore not using.
-%
-function [rowsList] = read_text_file(filePath, linebreakRegexp)
+function [rowsCa] = read_text_file(filePath, linebreakRegexp)
 % PROPOSAL: Use s=textscan(fileId, '%s', 'delimiter', '\n').
 % PROPOSAL: Read entire file as a (byte) string, then split it using a specified line-break string.
 %   PROPOSAL: Return both string and row string list.
 %       CON: Unnecessary to return row string list. Trivial to produce with str_split.
 %           CON: Only if what is line-break is unambiguous.
 %       CON: Row string list is line break-dependent.
-%           CON: If want automatic detection of type of line break, then rowsList is the proper format, not the
+%           CON: If want automatic detection of type of line break, then rowsCa is the proper format, not the
 %                multi-row string.
 %       CON: Row string list is dependent on how to interpret chars after the last line break.
 
@@ -31,13 +28,19 @@ function [rowsList] = read_text_file(filePath, linebreakRegexp)
 
 bytesArray = irf.fs.read_file(filePath);
 
-%     % IMPLEMENTATION NOTE: "delimiter" does not refer to end-of-line (there is a separate property for that).
-%     % Default textscan behaviour is to detect which of LF, CR, or CR+LF to use for end-of-line.
-%     % delimiter='' probably means to ignore "
-%     temp = textscan(fileId, '%s', 'delimiter', '', 'whitespace', '');
-%     assert(numel(temp) == 1, 'textscan unexpectedly returned a non-scalar cell array.')
+
+% IMPLEMENTATION NOTE: The old implementation using textscan merged consequtive
+% linebreaks (CR+LF) into one (i.e. bug). Therefore not using it.
+% % IMPLEMENTATION NOTE: "delimiter" does not refer to end-of-line (there is a separate property for that).
+% % Default textscan behaviour is to detect which of LF, CR, or CR+LF to use for end-of-line.
+% % delimiter='' probably means to ignore "
+% temp = textscan(fileId, '%s', 'delimiter', '', 'whitespace', '');
+% assert(numel(temp) == 1, 'textscan unexpectedly returned a non-scalar cell array.')
 %
-%     rowsList = temp{1};
-rowsList = strsplit(char(bytesArray(:)'), linebreakRegexp, 'DelimiterType', 'RegularExpression', 'CollapseDelimiters', false)';
+% rowsCa = temp{1};
+
+rowsCa = strsplit(...
+  char(bytesArray(:)'), linebreakRegexp, ...
+  'DelimiterType', 'RegularExpression', 'CollapseDelimiters', false)';
 
 end
