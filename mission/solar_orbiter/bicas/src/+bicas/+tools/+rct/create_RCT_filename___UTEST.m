736ce864f1500354c70ea86a7eb709b832288580
Erik P G Johansson <erik.johansson@irf.se>
2024-07-11 16:04:12 +0200

    BICAS: CI-fix: create_RCT_filename___UTEST

--- mission/solar_orbiter/bicas/src/+bicas/+tools/+rct/create_RCT_filename___UTEST.m
+++ mission/solar_orbiter/bicas/src/+bicas/+tools/+rct/create_RCT_filename___UTEST.m
@@ -21,7 +21,10 @@ classdef create_RCT_filename___UTEST < matlab.unittest.TestCase
       beginDt = datetime('2020-01-01T00:00:00');
       endDt   = datetime('2099-12-31T00:00:00');
 
-      [actDestFilename, actGaCALIBRATION_VERSION] = bicas.tools.rct.create_RCT_filename(beginDt, endDt, 3);
+      actDestFilename = bicas.tools.rct.create_RCT_filename(beginDt, endDt, 3);
+
+      irf.assert.castring(actDestFilename)
+      testCase.assertTrue(numel(actDestFilename) > 4)
     end
 
 
