50d12d219b0d16d41e5b4b33c8801707e7b8b88a
Erik P G Johansson <erik.johansson@irf.se>
2024-09-19 19:47:27 +0200

    BICAS: ASID/SSID/SDID,Routing constants: reorg., add k values (unused)
    modified:   const.m

--- mission/solar_orbiter/bicas/src/+bicas/const.m
+++ mission/solar_orbiter/bicas/src/+bicas/const.m
@@ -868,48 +868,89 @@ classdef const
       C.SSID = struct();
       C.SDID = struct();
 
-      function add_ASR(fn, asidCategory, asidAntennas)
+      C.SSID_K = struct();
+      C.SDID_K = struct();
+      %C.K_SSID_DICT = configureDictionary('uint8', 'bicas.proc.L1L2.SignalSourceId');
+      %C.K_SDID_DICT = configureDictionary('uint8', 'bicas.proc.L1L2.SignalDestinationId');
+      C.SSID_K_DICT = configureDictionary('bicas.proc.L1L2.SignalSourceId',      'uint8');
+      C.SDID_K_DICT = configureDictionary('bicas.proc.L1L2.SignalDestinationId', 'uint8');
+      kGlobalArray = [];   % Global list of k values for all classes.
+
+      function add_ASR(fn, k, asidCategory, asidAntennas)
         Asid = bicas.proc.L1L2.AntennaSignalId(asidCategory, asidAntennas);
         Ssid = bicas.proc.L1L2.SignalSourceId(Asid);
 
-        C.ASID.(fn)    = Asid;
-        C.SSID.(fn)    = Ssid;
-        C.SDID.(fn)    = bicas.proc.L1L2.SignalDestinationId(Asid);
+        C.ASID.(fn) = Asid;
+        add_SSID(fn, k, Ssid)
+        add_SDID(fn, k, bicas.proc.L1L2.SignalDestinationId(Asid))
         C.ROUTING.(fn) = bicas.proc.L1L2.Routing(Ssid);
       end
 
-      % =====================================
-      % Add every possible unique ASID object
-      % =====================================
-      add_ASR("DC_V1",  'DC_SINGLE', [1   ]);
-      add_ASR("DC_V2",  'DC_SINGLE', [2   ]);
-      add_ASR("DC_V3",  'DC_SINGLE', [3   ]);
+      function assert_new_k(k)
+        assert(isnumeric(k))
+        assert(~ismember(k, kGlobalArray))
+        kGlobalArray(end+1) = k;
+      end
 
-      add_ASR("DC_V12", 'DC_DIFF',   [1, 2]);
-      add_ASR("DC_V13", 'DC_DIFF',   [1, 3]);
-      add_ASR("DC_V23", 'DC_DIFF',   [2, 3]);
+      function add_SSID(fn, k, Ssid)
+        k2 = k+100;
+        assert_new_k(k2)
 
-      add_ASR("AC_V12", 'AC_DIFF',   [1, 2]);
-      add_ASR("AC_V13", 'AC_DIFF',   [1, 3]);
-      add_ASR("AC_V23", 'AC_DIFF',   [2, 3]);
+        C.SSID.(fn)   = Ssid;
+        C.SSID_K.(fn) = k2;
+        %C.K_SSID_DICT = add_key_value(C.K_SSID_DICT, k+100, Ssid);
+        C.SSID_K_DICT(Ssid) = uint8(k2);
+      end
 
-      C.SSID.REF25V  = bicas.proc.L1L2.SignalSourceId('2.5V_REF');
-      C.SSID.GND     = bicas.proc.L1L2.SignalSourceId('GND');
-      C.SSID.UNKNOWN = bicas.proc.L1L2.SignalSourceId('UNKNOWN');
+      function add_SDID(fn, k, Sdid)
+        k2 = k+200;
+        assert_new_k(k2)
 
-      C.SDID.NOWHERE = bicas.proc.L1L2.SignalDestinationId('NOWHERE');
+        C.SDID.(fn)   = Sdid;
+        C.SDID_K.(fn) = k2;
+        %C.K_SDID_DICT = add_key_value(C.K_SDID_DICT, k+200, Sdid);
+        C.SDID_K_DICT(Sdid) = uint8(k2);
+      end
 
-      C.ROUTING.REF25V_TO_DC_V1    = bicas.proc.L1L2.Routing(C.SSID.REF25V,  C.SDID.DC_V1);
-      C.ROUTING.REF25V_TO_DC_V2    = bicas.proc.L1L2.Routing(C.SSID.REF25V,  C.SDID.DC_V2);
-      C.ROUTING.REF25V_TO_DC_V3    = bicas.proc.L1L2.Routing(C.SSID.REF25V,  C.SDID.DC_V3);
-      C.ROUTING.GND_TO_DC_V1       = bicas.proc.L1L2.Routing(C.SSID.GND,     C.SDID.DC_V1);
-      C.ROUTING.GND_TO_DC_V2       = bicas.proc.L1L2.Routing(C.SSID.GND,     C.SDID.DC_V2);
-      C.ROUTING.GND_TO_DC_V3       = bicas.proc.L1L2.Routing(C.SSID.GND,     C.SDID.DC_V3);
-      C.ROUTING.UNKNOWN_TO_NOWHERE = bicas.proc.L1L2.Routing(C.SSID.UNKNOWN, C.SDID.NOWHERE);
+      function main()
+        % =====================================
+        % Add every possible unique ASID object
+        % =====================================
+        add_ASR("DC_V1",  1, 'DC_SINGLE', [1   ]);
+        add_ASR("DC_V2",  2, 'DC_SINGLE', [2   ]);
+        add_ASR("DC_V3",  3, 'DC_SINGLE', [3   ]);
 
-      ca = struct2cell(C.ASID);   C.ASID_ARRAY = [ca{:}]';
-      %ca = struct2cell(C.SSID);   C.SSID_ARRAY = [ca{:}]';
-      %ca = struct2cell(C.SDID);   C.SDID_ARRAY = [ca{:}]';
+        add_ASR("DC_V12", 4, 'DC_DIFF',   [1, 2]);
+        add_ASR("DC_V13", 5, 'DC_DIFF',   [1, 3]);
+        add_ASR("DC_V23", 6, 'DC_DIFF',   [2, 3]);
+
+        add_ASR("AC_V12", 7, 'AC_DIFF',   [1, 2]);
+        add_ASR("AC_V13", 8, 'AC_DIFF',   [1, 3]);
+        add_ASR("AC_V23", 9, 'AC_DIFF',   [2, 3]);
+
+        add_SSID("REF25V",  10, bicas.proc.L1L2.SignalSourceId('2.5V_REF'));
+        add_SSID("GND",     11, bicas.proc.L1L2.SignalSourceId('GND'));
+        add_SSID("UNKNOWN", 12, bicas.proc.L1L2.SignalSourceId('UNKNOWN'));
+
+        add_SDID("NOWHERE", 13, bicas.proc.L1L2.SignalDestinationId('NOWHERE'));
+
+        C.ROUTING.REF25V_TO_DC_V1    = bicas.proc.L1L2.Routing(C.SSID.REF25V,  C.SDID.DC_V1);
+        C.ROUTING.REF25V_TO_DC_V2    = bicas.proc.L1L2.Routing(C.SSID.REF25V,  C.SDID.DC_V2);
+        C.ROUTING.REF25V_TO_DC_V3    = bicas.proc.L1L2.Routing(C.SSID.REF25V,  C.SDID.DC_V3);
+        C.ROUTING.GND_TO_DC_V1       = bicas.proc.L1L2.Routing(C.SSID.GND,     C.SDID.DC_V1);
+        C.ROUTING.GND_TO_DC_V2       = bicas.proc.L1L2.Routing(C.SSID.GND,     C.SDID.DC_V2);
+        C.ROUTING.GND_TO_DC_V3       = bicas.proc.L1L2.Routing(C.SSID.GND,     C.SDID.DC_V3);
+        C.ROUTING.UNKNOWN_TO_NOWHERE = bicas.proc.L1L2.Routing(C.SSID.UNKNOWN, C.SDID.NOWHERE);
+
+        ca = struct2cell(C.ASID);   C.ASID_ARRAY = [ca{:}]';
+        %ca = struct2cell(C.SSID);   C.SSID_ARRAY = [ca{:}]';
+        %ca = struct2cell(C.SDID);   C.SDID_ARRAY = [ca{:}]';
+
+        assert(~ismember(255, kGlobalArray))
+        assert(~ismember(0,   kGlobalArray))
+      end
+
+      main()
     end
 
 
