5a74100eb41de6b35097375143cc9709376d9364
Erik P G Johansson <erik.johansson@irf.se>
2025-07-10 19:59:36 +0200

    Cleanup

--- irf/+irf/+utils/split_by_change___UTEST.m
+++ irf/+irf/+utils/split_by_change___UTEST.m
@@ -19,42 +19,25 @@ classdef split_by_change___UTEST < matlab.unittest.TestCase
 
     function test_1(testCase)
 
-      function test(arraysCa, expI1Array, expI2Array)
-        expN = numel(expI1Array);
-
-        [actI1Array, actI2Array, actN] = ...
-          irf.utils.split_by_change(arraysCa{:});
-
-        testCase.assertEqual(actI1Array, expI1Array)
-        testCase.assertEqual(actI2Array, expI2Array)
-        testCase.assertEqual(actN,       expN)
-      end
-
-      function test_exc(arraysCa)
-        testCase.assertError(...
-          @() irf.utils.split_by_change(arraysCa{:}), ...
-          ?MException)
-      end
-
-      test_exc({})
+      testCase.test_exc({})
 
       % Unequal number of rows.
-      test_exc({zeros(0, 1), [1]})
-      test_exc({[2;3], [1]})
-      test_exc({[2,3], [1;2]})
+      testCase.test_exc({zeros(0, 1), [1]})
+      testCase.test_exc({[2;3], [1]})
+      testCase.test_exc({[2,3], [1;2]})
 
       % Empty arrays
-      test({zeros(0, 0)}, zeros(0, 1), zeros(0, 1))
-      test({zeros(1, 0)}, [1], [1])
-      test({zeros(3, 0)}, [1], [3])
+      testCase.test({zeros(0, 0)}, zeros(0, 1), zeros(0, 1))
+      testCase.test({zeros(1, 0)}, [1], [1])
+      testCase.test({zeros(3, 0)}, [1], [3])
 
       % Non-empty numeric arrays
-      test({[4]},           [1],       [1]);
-      test({[3;3]},         [1],       [2]);
-      test({[7;7;1;1]},     [1;3],     [2;4]);
-      test({[11;12;13;14]}, [1;2;3;4], [1;2;3;4]);
-      test({[NaN;NaN;3]},   [1;3],     [2;3]);
-      test({[ ...
+      testCase.test({[4]},           [1],       [1]);
+      testCase.test({[3;3]},         [1],       [2]);
+      testCase.test({[7;7;1;1]},     [1;3],     [2;4]);
+      testCase.test({[11;12;13;14]}, [1;2;3;4], [1;2;3;4]);
+      testCase.test({[NaN;NaN;3]},   [1;3],     [2;3]);
+      testCase.test({[ ...
         1 1; ...
         1 1; ...
         1 3; ...
@@ -66,12 +49,12 @@ classdef split_by_change___UTEST < matlab.unittest.TestCase
       % 3D arrays
       A(:,:,1) = [1 2; 1 2; 1 3];
       A(:,:,2) = [3 4; 3 4; 3 4];
-      test({A}, [1;3], [2;3])
+      testCase.test({A}, [1;3], [2;3])
 
       % Non-empty char string cell arrays.
-      test({{'abc'; 'DEFG'; 'HIJKL'}}, [1;2;3], [1;2;3]);
-      test({{'abc'; 'abc';  'abc'  }}, [1],     [3]);
-      test({
+      testCase.test({{'abc'; 'DEFG'; 'HIJKL'}}, [1;2;3], [1;2;3]);
+      testCase.test({{'abc'; 'abc';  'abc'  }}, [1],     [3]);
+      testCase.test({
         {'abc'; 'abc'; 'abc'; 'DEFG'; 'DEFG'; 'DEFG'}, ...
         logical([0;0;1;1;0;0])}, ...
         [1;3;4;5], [2;3;4;6]);
@@ -85,12 +68,12 @@ classdef split_by_change___UTEST < matlab.unittest.TestCase
         1, 'cde'; ...
         1, 'cde'};
       % Numeric + char arrays
-      test( ...
+      testCase.test( ...
         { cell2mat(DATA_AR(:, 1)), DATA_AR(:, 2) }, ...
         [1; 3; 4; 5], ...
         [2; 3; 4; 6])
       % Numeric + string
-      test( ...
+      testCase.test( ...
         { cell2mat(DATA_AR(:, 1)), string(DATA_AR(:, 2)) }, ...
         [1; 3; 4; 5], ...
         [2; 3; 4; 6])
@@ -99,6 +82,39 @@ classdef split_by_change___UTEST < matlab.unittest.TestCase
 
 
   end    % methods(Test)
+
+
+
+  %##########################
+  %##########################
+  % PRIVATE INSTANCE METHODS
+  %##########################
+  %##########################
+  methods(Access=private)
+
+
+
+    function test(testCase, arraysCa, expI1Array, expI2Array)
+      expN = numel(expI1Array);
+
+      [actI1Array, actI2Array, actN] = ...
+        irf.utils.split_by_change(arraysCa{:});
+
+      testCase.assertEqual(actI1Array, expI1Array)
+      testCase.assertEqual(actI2Array, expI2Array)
+      testCase.assertEqual(actN,       expN)
+    end
+
+    function test_exc(testCase, arraysCa)
+      testCase.assertError(...
+        @() irf.utils.split_by_change(arraysCa{:}), ...
+        ?MException)
+    end
+
+
+
+
+  end    % methods(Access=private)
 
 
 
