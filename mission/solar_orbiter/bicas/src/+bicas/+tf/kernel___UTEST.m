adb7dd2f15efcfdc2e08c40f9a3c9cc976ca3291
Erik P G Johansson <erik.johansson@irf.se>
2025-08-12 20:18:57 +0200

    BICAS: Cleanup/reorg tf.{kernel,time}___UTEST
    modified:   src/+bicas/+tf/kernel___UTEST.m
    modified:   src/+bicas/+tf/time___UTEST.m

--- mission/solar_orbiter/bicas/src/+bicas/+tf/kernel___UTEST.m
+++ mission/solar_orbiter/bicas/src/+bicas/+tf/kernel___UTEST.m
@@ -25,13 +25,13 @@ classdef kernel___UTEST < matlab.unittest.TestCase
 
 
 
-    % Tests which are independent of edgePolicy.
-    function test_0(T, EDGE_POLICY)
-      T.test( ...
+    % Tests which are independent of edgePolicy by their nature.
+    function test_apply_TF___0(T, EDGE_POLICY)
+      T.test_apply_TF( ...
         [2], [3], 1, EDGE_POLICY, ...
         [3]*2)
 
-      T.test( ...
+      T.test_apply_TF( ...
         [0,0,2,0,0,0], [3], 1, EDGE_POLICY, ...
         [0,0,3,0,0,0]*2)
 
@@ -39,23 +39,23 @@ classdef kernel___UTEST < matlab.unittest.TestCase
       % Even & odd length kernels.
       % Kernel origin at beginning and end of kernel.
       %===============================================
-      T.test( ...
+      T.test_apply_TF( ...
         [0,0,1,0,0,0], [1,2], 1, EDGE_POLICY, ...
         [0,0,1,2,0,0]*1)
-      T.test( ...
+      T.test_apply_TF( ...
         [0,0,1,0,0,0], [1,2], 2, EDGE_POLICY, ...
         [0,1,2,0,0,0]*1)
-      T.test( ...
+      T.test_apply_TF( ...
         [0,0,1,0,0,0], [1,2,3], 1, EDGE_POLICY, ...
         [0,0,1,2,3,0]*1)
-      T.test( ...
+      T.test_apply_TF( ...
         [0,0,1,0,0,0], [1,2,3], 3, EDGE_POLICY, ...
         [1,2,3,0,0,0]*1)
 
       %=====================================
       % Multiple non-unit, non-zero samples
       %=====================================
-      T.test( ...
+      T.test_apply_TF( ...
         [0,2,3,0,0,0], [1,2,3], 2, EDGE_POLICY, ...
         [1,2,3,0,0,0]*2 + ...
         [0,1,2,3,0,0]*3)
@@ -63,7 +63,7 @@ classdef kernel___UTEST < matlab.unittest.TestCase
       % Empty kernel ==> Error
       % NOTE: Check error ID to differentiate error from any other empty
       % kernel-caused error.
-      T.verifyError(...
+      T.assertError(...
         @() bicas.tf.kernel.apply_kernel([2]', zeros(0,1), 1, EDGE_POLICY), ...
         'BICAS:Assertion:IllegalArgument')
     end
@@ -72,19 +72,19 @@ classdef kernel___UTEST < matlab.unittest.TestCase
 
     % Special tests for edgeCase == 'ZEROS'
     %
-    function test_ZEROS(T)
+    function test_apply_TF___ZEROS(T)
       y1      = [1,0,0,0,0,2];
       yKernel = [1,2,3];
       y2p     = conv(y1, yKernel);
 
       for iKc = 1:3
-        test(T, ...
+        T.test_apply_TF( ...
           y1, yKernel, iKc, 'ZEROS', ...
           y2p(iKc + [0:5]))
       end
 
       % Kernel longer than signal.
-      test(T, ...
+      T.test_apply_TF( ...
         [1,0,0], [1,2,3,4], 2, 'ZEROS', ...
         [2,3,4])
     end
@@ -93,17 +93,17 @@ classdef kernel___UTEST < matlab.unittest.TestCase
 
     % Special tests for edgeCase == 'CYCLIC'
     %
-    function test_CYCLIC(T)
+    function test_apply_TF___CYCLIC(T)
       for iKo = 1:3
-        T.test_pad( ...
+        T.test_apply_TF_pad( ...
           [0,0,2], [1,0,0,0,0,2], [1,0,0], [1,2,3], iKo, 'CYCLIC')
       end
 
-      T.test_pad( ...
+      T.test_apply_TF_pad( ...
         [3,4], [2,1,0,0,3,4], [2,1], [1,2,3,4,5], 3, 'CYCLIC')
 
       % Kernel longer than signal (but still legal).
-      T.test_pad( ...
+      T.test_apply_TF_pad( ...
         [1,0,0], [1,0,0], [1,0,0], [1,2,3,4], 3, 'CYCLIC')
     end
 
@@ -111,42 +111,42 @@ classdef kernel___UTEST < matlab.unittest.TestCase
 
     % Special tests for edgeCase == 'MIRROR'
     %
-    function test_MIRROR(T)
+    function test_apply_TF___MIRROR(T)
       for iKo = 1:3
-        T.test_pad( ...
+        T.test_apply_TF_pad( ...
           [0,0,1], [1,0,0,0,0,2], [2,0,0], [1,2,3], iKo, 'MIRROR')
       end
 
-      T.test_pad( ...
+      T.test_apply_TF_pad( ...
         [1,2], [2,1,0,0,3,4], [4,3], [1,2,3,4,5], 3, 'MIRROR')
 
       % Kernel longer than signal (but still legal).
-      T.test_pad( ...
+      T.test_apply_TF_pad( ...
         [0,0,1], [1,0,0], [0,0,1], [1,2,3,4], 2, 'MIRROR')
     end
 
 
 
-    function test_NaN(T, EDGE_POLICY)
+    function test_apply_TF___NaN(T, EDGE_POLICY)
       N = NaN;   % Shorthand.
 
       %===============
       % NaN in signal
       %===============
-      T.test( ...
+      T.test_apply_TF( ...
         [0,0,0,N,0,0,0], [1,2,3], 3, EDGE_POLICY, ...
         [0,N,N,N,0,0,0])
-      T.test( ...
+      T.test_apply_TF( ...
         [0,0,0,N,0,0,0], [1,2,3], 1, EDGE_POLICY, ...
         [0,0,0,N,N,N,0])
 
       %===============
       % NaN in kernel
       %===============
-      T.test( ...
+      T.test_apply_TF( ...
         [0,0,0,0,0,0,0], [N,0,0], 3, EDGE_POLICY, ...
         [N,N,N,N,N,N,N])
-      T.test( ...
+      T.test_apply_TF( ...
         [0,0,0,0,0,0,0], [N,0,0], 1, EDGE_POLICY, ...
         [N,N,N,N,N,N,N])
     end
@@ -166,43 +166,59 @@ classdef kernel___UTEST < matlab.unittest.TestCase
 
 
 
-    function test(T, y1, yKernel, iKernelOrigin, edgePolicy, expY2)
+    function test_apply_TF(T, y1, yKernel, iKernelOrigin, edgePolicy, expY2)
 
       % NOTE: Transposes vectors.
       actY2 = bicas.tf.kernel.apply_kernel(...
         y1', yKernel', iKernelOrigin, edgePolicy);
 
-      T.verifyEqual(actY2, expY2')
+      T.assertEqual(actY2, expY2')
     end
 
 
 
-    % Manually pad y1.
-    % NOTE: Function derives the expected output itself, using conv().
+    % Helper function for testing edgePolicy<>ZEROS.
+    % (1) Manually pad y1 via arguments, corresponding to some edgePolicy value.
+    % (2) derive expected y2 using bicas.tf.kernel.apply_kernel() with
+    %     edgePolicy=ZEROS.
+    % (3) Check consistency of result (2) with bicas.tf.kernel.apply_kernel()
+    %     WITHOUT manual padding, but specified edgePolicy<>ZEROS.
     %
-    function test_pad(T, y1a, y1, y1b, yKernel, iKernelOrigin, edgePolicy)
+    function test_apply_TF_pad(T, ...
+        y1PadA, y1, y1PadB, yKernel, iKernelOrigin, edgePolicy)
+      % PROBLEM: Overlaps too much with the tested function itself?!
+      %   PROPOSAL: Tests should hardcode explicit outputs?!
+      %     CON: Not only, due to there being too many cases (iKernelorigin).
+      %          Consistency checks between different types of calls are still
+      %          useful.
+      %       CON-PROPOSAL: There should be some non-ZEROS test with explicitly
+      %                     hardcoded output.
 
-      assert(isrow(y1a))
+      assert(isrow(y1PadA))
       assert(isrow(y1))
-      assert(isrow(y1b))
+      assert(isrow(y1PadB))
       assert(isrow(yKernel))
 
-      y1a     = y1a';
+      y1PadA  = y1PadA';
       y1      = y1';
-      y1b     = y1b';
+      y1PadB  = y1PadB';
       yKernel = yKernel';
 
-      y1p   = [y1a; y1; y1b];
-      % NOTE: Using conv() when applying the kernel.
-      y2p   = conv(y1p, yKernel);
+      y1padded = [y1PadA; y1; y1PadB];
 
-      iBegin = length(y1a) + iKernelOrigin;
+      iBegin = length(y1PadA) + 1;
       iEnd   = iBegin + length(y1) - 1;
-      y2exp = y2p(iBegin : iEnd);
 
-      T.test(...
+      % Derive expected y2 using bicas.tf.kernel.apply_kernel() using MANUALLY
+      % PADDED y1.
+      expY2padded = bicas.tf.kernel.apply_kernel(...
+        y1padded, yKernel, iKernelOrigin, 'ZEROS');
+      expY2 = expY2padded(iBegin:iEnd);
+
+      % Call bicas.tf.kernel.apply_kernel() WITHOUT manual padding.
+      T.test_apply_TF(...
         y1', yKernel', iKernelOrigin, edgePolicy, ...
-        y2exp')
+        expY2')
     end
 
 
--- mission/solar_orbiter/bicas/src/+bicas/+tf/time___UTEST.m
+++ mission/solar_orbiter/bicas/src/+bicas/+tf/time___UTEST.m
@@ -42,8 +42,8 @@ classdef time___UTEST < matlab.unittest.TestCase
 
     % No HW, no detrending.
     %
-    function test0(T)
-      % PROPOSITION: Almost unnecessary since bicas.tf.kernel.apply_kernel() tests for same.
+    function test_apply_TF___0(T)
+      % PROPOSITION: Almost unnecessary since bicas.tf.kernel.apply_kernel() tests for the same.
       %   CON: Tests conversion TF-->kernel.
 
       dt          = 0.1;
@@ -52,40 +52,41 @@ classdef time___UTEST < matlab.unittest.TestCase
       tf = bicas.tf.utest_utils.get_TF_delay(nDelaySmpls*dt);
 
       t  = [0:N-1]' * dt;
-      y1 = 3 + 2*t - exp(-t);    % Arbitrary input signal.
+      % NOTE: Arbitrary NON-TRIVIAL input signal with magnitude of order ~1.
+      y1 = 3 + 2*t - exp(-t);
 
       %=====================
       % edgePolicy == ZEROS
       %=====================
       % Manually shift/delay signal.
-      y2_exp = [zeros(nDelaySmpls, 1); y1(1:end-nDelaySmpls)];
+      expY2 = [zeros(nDelaySmpls, 1); y1(1:end-nDelaySmpls)];
 
-      y2 = bicas.tf.time.apply_TF(dt, y1, tf, N, 'ZEROS');
+      actY2 = bicas.tf.time.apply_TF(dt, y1, tf, N, 'ZEROS');
 
-      T.verifyEqual(y2, y2_exp, 'AbsTol', 1e-13)
+      T.verifyEqual(actY2, expY2, 'AbsTol', 1e-13)
 
       %======================
       % edgePolicy == MIRROR
       %======================
       % Manually pad with mirrored samples and shift.
-      y2_exp = [y1(nDelaySmpls:-1:1); y1(1:end-nDelaySmpls)];
+      expY2 = [y1(nDelaySmpls:-1:1); y1(1:end-nDelaySmpls)];
 
-      y2 = bicas.tf.time.apply_TF(dt, y1, tf, N, 'MIRROR');
+      actY2 = bicas.tf.time.apply_TF(dt, y1, tf, N, 'MIRROR');
 
-      T.verifyEqual(y2, y2_exp, 'AbsTol', 1e-13)
+      T.verifyEqual(actY2, expY2, 'AbsTol', 1e-13)
     end
 
 
 
     % Constant delay+HW. ==> Constant scaling due to HW.
-    % Even-length kernel/HW.
     %
-    function test_HW0(T)
+    function test_apply_TF___HW_delay_0(T)
       dt     = 0.1;
       nSmpls = 100;
 
       t      = [0:nSmpls-1]' * dt;
-      y1     = 2 + 3*t - exp(-t);    % Arbitrary input signal.
+      % NOTE: Arbitrary NON-TRIVIAL input signal with magnitude of order ~1.
+      y1     = 2 + 3*t - exp(-t);
 
       % NOTE: Use both even and odd numbers.
       for lenKernel = [7, 17, 32]
@@ -96,60 +97,123 @@ classdef time___UTEST < matlab.unittest.TestCase
         nds2 = floor(lenKernel / 2);
 
         for nDelaySmpls = [nds0, nds1, nds2]
-
-          %============================================================
-          % Factor by which the signal will be weakened/multiplied due
-          % to the Hann window.
-          %============================================================
-          % IMPLEMENTATION NOTE: The effect of the Hann window can be
-          % boiled down to multiplication with a scalar number since
-          % the kernel only has one non-zero value.
-          % --
-          if mod(lenKernel, 2) == 0
-            % CASE: EVEN-numbered length kernel
-            % Normalized delay: -1 <= x <= 1
-            xHw      = nDelaySmpls/lenKernel;
-            hwFactor = 0.5 * (1 + cos(xHw*2*pi));
-          else
-            % CASE: ODD-numbered length kernel
-            % ==> Has to compensate for rounding to samples.
-            xHw      = (nDelaySmpls-0.5)/lenKernel;
-            hwFactor = 0.5 * (1 + cos(xHw*2*pi));
-          end
-
-          tf = bicas.tf.utest_utils.get_TF_delay(nDelaySmpls*dt);
-
-          %=====================
-          % edgePolicy == ZEROS
-          %=====================
-          % Manually delay, and multiply Hann window factor.
-          y2exp = [zeros(nDelaySmpls, 1); y1(1:end-nDelaySmpls)] * hwFactor;
-          y2act = bicas.tf.time.apply_TF(...
-            dt, y1, tf, lenKernel, ...
-            'ZEROS', 'hannWindow', true);
-
-          T.verifyEqual(y2act, y2exp, 'AbsTol', 1e-13)
-
-          %======================
-          % edgePolicy == MIRROR
-          %======================
-          % Manually pad with mirrored samples, delay, and multiply
-          % Hann window factor.
-          y2exp = [y1(nDelaySmpls:-1:1); y1(1:end-nDelaySmpls)] * hwFactor;
-          y2act = bicas.tf.time.apply_TF(...
-            dt, y1, tf, lenKernel, ...
-            'MIRROR', 'hannWindow', true);
-
-          T.verifyEqual(y2act, y2exp, 'AbsTol', 1e-13)
-
-        end    % for
-      end    % for
+          T.test_apply_TF___HW_delay( ...
+            y1=y1, dt=dt, ...
+            nDelaySmpls=nDelaySmpls, ...
+            lenKernel  =lenKernel)
+        end
+      end
 
     end    % function
 
 
 
+    % NOTE: Expected values have not been verified, but only seem plausible.
+    %
+    function test_get_shifted_Hann_window(T)
+      function test(iMax, n, expShiftedHw)
+        actShiftedHw = bicas.tf.time.get_shifted_Hann_window(iMax, n);
+        T.assertEqual(actShiftedHw, expShiftedHw, "AbsTol", 1e-15)
+      end
+
+      % iMax=1
+      test(1, 1, [1])
+      test(1, 2, [1    0]')
+      test(1, 3, [0.75 0.75 0.00]')
+      test(1, 4, [1.00 0.50 0.00 0.50]')
+
+      % iMax=end
+      test(1, 1, [1])
+      test(2, 2, [0    1]')
+      test(3, 3, [0.75 0.00 0.75]')
+      test(4, 4, [0.50 0.00 0.50 1.00]')
+
+      % iMax = middle
+      test(2, 4, [0.50 1.00 0.50 0.00]')
+      test(3, 5, [...
+        0; ...
+        0.345491502812526; ...
+        0.904508497187474; ...
+        0.904508497187474; ...
+        0.345491502812526])
+    end
+
+
+
   end    % methods(Test)
+
+
+
+  %##########################
+  %##########################
+  % PRIVATE INSTANCE METHODS
+  %##########################
+  %##########################
+  methods(Access=private)
+
+
+
+    % Test delaying arbitrary signal by an arbitrary amount. Assume slight
+    % numerical errors.
+    %
+    function test_apply_TF___HW_delay(T, A)
+      arguments
+        T
+        A.y1
+        A.lenKernel
+        A.nDelaySmpls
+        A.dt
+      end
+
+      %===========================================================
+      % Derive factor by which the signal will be (very slightly)
+      % weakened/multiplied due to the Hann window.
+      %===========================================================
+      % IMPLEMENTATION NOTE: The effect of the Hann window can be boiled down
+      % to multiplication with a scalar number since the kernel (but not the
+      % frequency-domain TF) has only one non-zero value.
+      if mod(A.lenKernel, 2) == 0
+        % CASE: EVEN-numbered length kernel
+        % Normalized delay: -1 <= x <= 1
+        xHw = A.nDelaySmpls/A.lenKernel;
+      else
+        % CASE: ODD-numbered length kernel
+        % ==> Has to compensate for rounding to samples.
+        xHw = (A.nDelaySmpls-0.5)/A.lenKernel;
+      end
+      hwFactor = 0.5 * (1 + cos(xHw*2*pi));
+
+      tf = bicas.tf.utest_utils.get_TF_delay(A.nDelaySmpls*A.dt);
+
+
+
+      %=====================
+      % edgePolicy == ZEROS
+      %=====================
+      % Manually delay, and multiply with Hann window factor.
+      expY2 = [zeros(A.nDelaySmpls, 1); A.y1(1:end-A.nDelaySmpls)] * hwFactor;
+      actY2 = bicas.tf.time.apply_TF(...
+        A.dt, A.y1, tf, A.lenKernel, ...
+        'ZEROS', 'hannWindow', true);
+
+      T.verifyEqual(actY2, expY2, 'AbsTol', 1e-13)
+
+      %======================
+      % edgePolicy == MIRROR
+      %======================
+      % Manually pad with mirrored samples, delay, and multiply by Hann window
+      % factor.
+      expY2 = [A.y1(A.nDelaySmpls:-1:1); A.y1(1:end-A.nDelaySmpls)] * hwFactor;
+      actY2 = bicas.tf.time.apply_TF(...
+        A.dt, A.y1, tf, A.lenKernel, ...
+        'MIRROR', 'hannWindow', true);
+
+      T.verifyEqual(actY2, expY2, 'AbsTol', 1e-13)
+    end
+
+
+
+  end    % methods(Access=private)
 
 
 
