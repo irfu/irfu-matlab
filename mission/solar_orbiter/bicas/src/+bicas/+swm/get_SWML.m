c8347c4a7f4eed7b67b34003ed2798857587b970
Erik P G Johansson <erik.johansson@irf.se>
2025-08-13 18:41:47 +0200

    BICAS: Use L3 master CDFs V07/V08

--- mission/solar_orbiter/bicas/src/+bicas/+swm/get_SWML.m
+++ mission/solar_orbiter/bicas/src/+bicas/+swm/get_SWML.m
@@ -269,8 +269,8 @@ end
 
 function SwmArray = get_SWMs_L2_to_l3()
 
-L3_EFIELD_SCPOT_SKT_VERSION = '06';
-L3_DENSITY_SKT_VERSION      = '07';
+L3_EFIELD_SCPOT_SKT_VERSION = '07';
+L3_DENSITY_SKT_VERSION      = '08';
 
 
 
