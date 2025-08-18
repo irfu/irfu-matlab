2d853943469f3aeb11a62e9bab12d6ba2e614eee
Erik P G Johansson <erik.johansson@irf.se>
2025-08-18 18:19:35 +0200

    split_by_false: Only permit column array arg.

--- irf/+irf/+utils/split_by_false___UTEST.m
+++ irf/+irf/+utils/split_by_false___UTEST.m
@@ -1,6 +1,5 @@
 %
-% matlab.unittest automatic test code for
-% irf.utils.split_by_false().
+% matlab.unittest automatic test code for irf.utils.split_by_false().
 %
 %
 % Author: Erik P G Johansson, IRF, Uppsala, Sweden
@@ -19,53 +18,60 @@ classdef split_by_false___UTEST < matlab.unittest.TestCase
 
 
 
-    function test0(testCase)
+    function test_0(testCase)
 
       % Arbitrary number output variables.
-      function test(boolArray, expI1Array, expI2Array)
-        % Pre-allocate correct size for later assignment via function
-        actOutputs = cell(1, 2);
+      function test(bArray, expI1Array, expI2Array)
+        bArray     = logical(bArray);
 
-        [actI1Array, actI2Array] = irf.utils.split_by_false(boolArray);
+        [actI1Array, actI2Array] = irf.utils.split_by_false(bArray);
+        testCase.verifyTrue(iscolumn(actI1Array))
+        testCase.verifyTrue(iscolumn(actI2Array))
         testCase.verifyEqual(actI1Array, expI1Array)
         testCase.verifyEqual(actI2Array, expI2Array)
-
-        % Use transposed argument.
-        [actOutputs{:}] = irf.utils.split_by_false(boolArray');
-        testCase.verifyEqual(actOutputs, {expI1Array, expI2Array})
       end
 
+      ECA = ones(0, 1);   % Empty Column Array
+
+      test(ECA, ECA, ECA)
+
+      test(0, ECA, ECA)
+      test(1, 1,   1  )
+
+      test([0,0,0]', ECA, ECA)
+      test([1,1,1]', 1,   3)
+
+      test([1,0,0]', 1, 1);
+      test([0,1,0]', 2, 2);
+      test([0,0,1]', 3, 3);
+
+      test([0,1,1]', 2, 3);
+      test([1,0,1]', [1 3]', [1 3]');
+      test([1,1,0]', 1, 2);
+
+      test([1,1, 0,0,0, 1,1,1,1]', [1,6]', [2,9]');
+      test([0,0, 1,1,1, 0,0,0,0]', 3,      5     );
+      test([1,1,0,0]', 1, 2);
+      test([0,0,1,1]', 3, 4);
+
+      % Complex test
+      test([0,0,1,1,1,0,0,1]', [3,8]',    [5,8]');
+
+    end
+
+
+
+    function test_exc(testCase)
       function test_exc(varargin)
         testCase.verifyError(...
           @() irf.utils.split_by_false(varargin{:}), ...
           ?MException)
       end
 
-      ECA = ones(0,1);   % Empty Column Array
-
-      test(ECA, ECA, ECA)
-
-      test(0, ECA, ECA)
-      test(1, 1, 1)
-      test(0, ECA, ECA)
-
-      test([0,0,0], ECA, ECA)
-      test([1,1,1], 1, 3)
-
-      test([1,0,0], 1, 1);
-      test([0,1,0], 2, 2);
-      test([0,0,1], 3, 3);
-
-      test([1,1,0,0,1,1], [1,5]', [2,6]');
-      test([0,0,1,1,0,0], 3,    4);
-      test([1,1,0,0],     1,    2);
-      test([0,0,1,1],     3,    4);
-
-      test([0,0,1,1,1,0,0,1], [3,8]',    [5,8]');
-
-      test_exc(ones(2,2))
-      test_exc(ones(0,2))
-      test_exc(ones(2,0))
+      test_exc(true(0,0))
+      test_exc(true(2,2))
+      test_exc(true(0,2))   % Too many columns (2).
+      test_exc(true(2,0))
     end
 
 
