a8537140c616bdb718ed4e70cb95625019098f72
Cecilia <cecilia.ae.norgren@gmail.com>
2026-05-12 16:09:41 +0200

    small fixes

--- PDist.m
+++ PDist.m
@@ -701,6 +701,8 @@ classdef PDist < TSeries
       % Calculate phase space volume of FPI bins.
       %
       % Get partial density by doing: dn = pdist*pdist.d3v;
+      % Units are the same as the units of the underlying PDist. E.g.,. if
+      % PDist.units = 's^3/cm^6', then the units of d3v is cm^3/s^3.
       %
       %   Options:
       %     'scpot',scpot - Corrects for spacecraft potential. For better
@@ -4145,6 +4147,23 @@ classdef PDist < TSeries
           error('Units not supported.');
       end
     end
+    function TS = SI(obj)
+      % PDIST.SI Convert to SI units using property PDist.siConversion
+      % Implemented: 's^3/cm^6'
+      TS = obj;
+      switch TS.units
+        case 's^3/cm^6'
+          if isscalar(TS.siConversion)
+            TS.data = TS.data*TS.siConversion;
+          elseif ischar(TS.siConversion) || isstring(TS.siConversion)
+            TS.data = TS.data*str2num(TS.siConversion);
+          end
+          TS.units = 's^3/m^6';
+          TS.siConversion = '1';
+        otherwise
+          disp('SI conversion not implemented.')
+      end
+    end
     function PD = pitchangles(obj,obj1,obj2,varargin) %,method
       %PITCHANGLES Calculate pitchangle distribution
       % PitchangleDistribution = Distribution.pitchangles(B,[nangles])
@@ -4516,12 +4535,15 @@ classdef PDist < TSeries
       % Phase space density of each cell, can be different units depending
       % on original PDist
       f = obj.data;
+
       % Phase space volume of each cell, same base length and time units as f
       if doScpot
-        vol = obj.d3v('scpot',scpot).data;
+        ts_vol = obj.d3v('scpot',scpot); 
       else
-        vol = obj.d3v.data;
+        ts_vol = obj.d3v;
       end
+      vol = ts_vol.data;
+      dv_units = ts_vol.units;
 
       % Assign a method of how the particles should be
       % divided/partitioned amongst the cells.
@@ -4529,7 +4551,7 @@ classdef PDist < TSeries
         case {'dn','fdv','f*dv'} % Proportional to the partial density of each cell.
           % The sum of out.dn should give the particle density
           % rounds up
-          dn = f.*vol;
+          dn = f.*vol; % units of input * (km/s)^2
           f_round = @(x) ceil(x);
         case {'f'} % Proportional to f, rounds up
           dn = f;
@@ -4632,7 +4654,7 @@ classdef PDist < TSeries
                 case 'random'
                   tmp_energy = energy_minus(it,iEnergy) + denergy(it,iEnergy)*rand(N_bin,1); % eV
                   tmp_azim   = azim_minus(iAzim)        + dazim*rand(N_bin,1);   % deg
-                  tmp_polar  = polar_minus(iPolar)      + dpolar*rand(N_bin,1);  % deg
+                  tmp_polar  = polar_minus(iPolar)      + dpolar*rand(N_bin,1);  % deg, need to change to the proper initialization here, like in reduce
                   tmp_v = sqrt(tmp_energy*units.eV*2/mass)/1000; % km/s
 
 
@@ -4702,6 +4724,9 @@ classdef PDist < TSeries
             p(it).dv = dv_all(1:i_part_count-1);
         %end
       end % end time loop
+      p(it).df_units = obj.units;
+      p(it).dv_units = dv_units;
+      p(it).v_units = 'km/s';
       particles = p;
     end
     %     function e = energy(obj)
@@ -5034,6 +5059,16 @@ classdef PDist < TSeries
       pmat(:,3,2) = pmat(:,2,3);
 
       TS = irf.ts_tensor_xyz(obj.time,pmat);
+    end
+    function TS = T(obj,varargin)
+      units = irf_units;
+      P = obj.p;
+      n = obj.n;
+      T_si = P*1e-9/(n*1e6);
+      T_eV = T_si/units.eV;
+      T_eV.units = 'eV';
+      TS = T_eV;      
+
     end
     function varargout = remove_noise(obj,nMean,nThresh,PD_counts)
       % PDIST.REMOVE_NOISE
